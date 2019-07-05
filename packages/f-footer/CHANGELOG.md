4f4e39e85b6fc214d9c7078d9e11e37d1712dd15
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-07-05 12:59:33 +0100

    f-footer@2.0.0-beta.1 footerList child component with resource files (#10)
    * f-footer@2.0.0-beta.1 footerList child component with resource files
    
    * f-footer@2.0.0-beta.1 remove unused babel plugins and --no-cache flag for jest tests
    
    * f-footer@2.0.0-beta.1 remove ca resource files
    
    * f-footer@2.0.0-beta.1 remove cross-env plugin and npm script for copying recourse files into dist folder

--- packages/f-footer/CHANGELOG.md
+++ packages/f-footer/CHANGELOG.md
@@ -3,6 +3,19 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+
+v2.0.0-beta.1
+------------------------------
+*July 4, 2019*
+
+### Added
+- `FooterList` container component
+- `TestComponentForFooter` wrapper component for local testing
+- tenants resource files
+- logic to pick up tenants locales either from component's props or vue i18n plugin
+- `@justeat/f-footer` to mono repo dependencies, was missed from initial setup
+
+
 v2.0.0-beta.0
 ------------------------------
 *June 21, 2019*
---
+++ packages/f-footer/src/tenants/en-GB.js
@@ -0,0 +1,30 @@
+export default {
+    customerServiceTitle: 'Customer service',
+    customerServiceLinks: [
+        {
+            url: '/contact',
+            text: 'Contact us'
+        },
+        {
+            url: '/account/login',
+            text: 'Log in',
+            rel: 'nofollow'
+        },
+        {
+            url: '/account/register',
+            text: 'Sign up'
+        },
+        {
+            url: '/blog',
+            text: 'Blog'
+        },
+        {
+            url: '/apps',
+            text: 'Mobile apps'
+        },
+        {
+            url: '/member/updateuserinfo',
+            text: 'My account'
+        }
+    ]
+};
