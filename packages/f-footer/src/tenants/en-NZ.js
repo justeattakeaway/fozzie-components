2f8e4dbb25d6ab0ed0a0062c0d3a03a747cbbac0
Maryia Radchuk <maryia.radchuk@just-eat.com>
2019-07-05 15:55:45 +0100

    f-footer@2.0.0-beta.2 transfer all footer data from the original f-footer module

--- packages/f-footer/src/tenants/en-NZ.js
+++ packages/f-footer/src/tenants/en-NZ.js
@@ -25,5 +25,200 @@ export default {
             url: '/help/',
             text: 'Help centre'
         }
-    ]
+    ],
+    cuisines: 'Top cuisines',
+    cuisineLinks: [
+        {
+            url: '/browse/cuisines/chinese',
+            text: 'Chinese'
+        },
+        {
+            url: '/browse/cuisines/indian',
+            text: 'Indian'
+        },
+        {
+            url: '/browse/cuisines/thai',
+            text: 'Thai'
+        },
+        {
+            url: '/browse/cuisines/pizza',
+            text: 'Pizza'
+        },
+        {
+            url: '/browse',
+            text: 'View all cuisines'
+        }
+    ],
+    towns: 'Locations',
+    locationLinks: [
+        {
+            url: '/browse/locations/auckland/',
+            text: 'Auckland'
+        },
+        {
+            url: '/browse/locations/wellington/',
+            text: 'Wellington'
+        },
+        {
+            url: '/browse/locations/christchurch/',
+            text: 'Christchurch'
+        },
+        {
+            url: '/browse/locations/dunedin/',
+            text: 'Dunedin'
+        }
+    ],
+    brands: 'Brands',
+    brandLinks: [],
+    aboutUs: 'A bit more about us',
+    aboutUsLinks: [
+        {
+            url: '/about_menulog',
+            text: 'Get to know us'
+        },
+        {
+            url: '/privacy-policy#privacy_policy',
+            text: 'Privacy Policy / Terms & Conditions'
+        },
+        {
+            url: 'https://connect.menulog.co.nz/login?client_id=RappsOrigPartnerCentre&response_type=code&redirect_uri=https:%2F%2Fpartner.menulog.co.nz%2Fsignin-jeconnect&state=QaF4ae5nfj7FUpuoevTVCHvmMBxIA1hO_4mDtePNEJOasCly6wdTLaHjTuvS7_If4SwPYZelgwT0ZFYb7CCZvfIIr9X0OVLKiRpdCLYU9zJPuorMgnFjjRlyQNdU8L_5ZvkfGGBtXJx-jNOlh5lU_aqiQ-KgrKthGa83ZbYH8GjV81bwGb7iJ8PRFJPYbwTyys-zclUwNFxXhfdsOpqIWsIlfpQ',
+            text: 'Partner Centre'
+        },
+        {
+            url: '/join_takeaway_section',
+            text: 'Restaurant sign up'
+        }
+    ],
+    downloadOurApps: 'Download our apps',
+    appStoreIcons: [
+        {
+            url: 'https://itunes.apple.com/nz/app/menulog-order-takeaway-food/id485249021?mt=8&&referrer=click%3D69811f6e-2144-4706-bafc-3d9ae07efd62',
+            key: 'ios',
+            iconSrc: 'iosIconSrc',
+            altText: 'Download on the App Store'
+        },
+        {
+            url: 'https://play.google.com/store/apps/details?id=nz.co.menulog.m',
+            key: 'android',
+            iconSrc: 'androidIconSrc',
+            altText: 'Get it on Google Play'
+        }
+    ],
+    feedback: 'Feedback',
+    improveOurWebsite: 'Help us improve our website',
+    sendFeedback: 'Send feedback',
+    followUs: 'Follow us',
+    socialIcons: [
+        {
+            url: 'https://www.facebook.com/Menulog.co.nz',
+            key: 'facebook',
+            iconSrc: 'facebookIconUrl',
+            altText: 'Menu Log on Facebook'
+        },
+        {
+            url: 'https://twitter.com/Menulog',
+            key: 'twitter',
+            iconSrc: 'twitterIconUrl',
+            altText: 'Menu Log on Twitter'
+        },
+        {
+            url: 'https://www.instagram.com/menulog/',
+            key: 'instagram',
+            iconSrc: 'instagramIconUrl',
+            altText: 'Menu Log on Instagram'
+        }
+    ],
+    currentCountryLocalisedName: 'New Zealand',
+    currentCountryFlagKey: 'nz',
+    countries: [
+        {
+            key: 'au',
+            flagUrl: 'australiaFlagIconUrl',
+            localisedName: 'Australia',
+            siteUrl: 'https://www.menulog.com.au'
+        },
+        {
+            key: 'br',
+            flagUrl: 'brasilFlagIconUrl',
+            localisedName: 'Brazil',
+            siteUrl: 'https://www.ifood.com.br'
+        },
+        {
+            key: 'ca',
+            flagUrl: 'canadaFlagIconUrl',
+            localisedName: 'Canada',
+            siteUrl: 'https://www.just-eat.ca'
+        },
+        {
+            key: 'dk',
+            flagUrl: 'denmarkFlagIconUrl',
+            localisedName: 'Denmark',
+            siteUrl: 'https://www.just-eat.dk'
+        },
+        {
+            key: 'fr',
+            flagUrl: 'franceFlagIconUrl',
+            localisedName: 'France',
+            siteUrl: 'https://www.just-eat.fr/'
+        },
+        {
+            key: 'ie',
+            flagUrl: 'irelandFlagIconUrl',
+            localisedName: 'Ireland',
+            siteUrl: 'https://www.just-eat.ie'
+        },
+        {
+            key: 'it',
+            flagUrl: 'italyFlagIconUrl',
+            localisedName: 'Italy',
+            siteUrl: 'https://www.justeat.it'
+        },
+        {
+            key: 'no',
+            flagUrl: 'norwayFlagIconUrl',
+            localisedName: 'Norway',
+            siteUrl: 'https://www.just-eat.no'
+        },
+        {
+            key: 'es',
+            flagUrl: 'spainFlagIconUrl',
+            localisedName: 'Spain',
+            siteUrl: 'https://www.just-eat.es'
+        },
+        {
+            key: 'ch',
+            flagUrl: 'switzerlandFlagIconUrl',
+            localisedName: 'Switzerland',
+            siteUrl: 'https://www.eat.ch'
+        },
+        {
+            key: 'gb',
+            flagUrl: 'ukFlagIconUrl',
+            localisedName: 'United Kingdom',
+            siteUrl: 'https://www.just-eat.co.uk'
+        }
+    ],
+    changeCurrentCountry: 'You are on the NZ website. Click here to change.',
+    buttonClose: 'Close',
+    paymentIcons: [
+        {
+            type: 'card',
+            key: 'mastercard',
+            iconSrc: 'mastercardCardIconSrc',
+            altText: 'Mastercard'
+        },
+        {
+            type: 'card',
+            key: 'visa',
+            iconSrc: 'visaCardIconSrc',
+            altText: 'Visa'
+        },
+        {
+            type: 'card',
+            key: 'amex',
+            iconSrc: 'amexCardIconSrc',
+            altText: 'American Express'
+        }
+    ],
+    vatInfo: ''
 };
--- packages/f-footer/src/tenants/es-ES.js
+++ packages/f-footer/src/tenants/es-ES.js
@@ -17,5 +17,254 @@ export default {
             url: '/account/info/',
             text: 'Información De La Cuenta'
         }
-    ]
+    ],
+    cuisines: 'Tipos de cocina',
+    cuisineLinks: [
+        {
+            url: '/a-domicilio/cerca-de-mi/pizza/',
+            text: 'Pizza a domicilio'
+        },
+        {
+            url: '/a-domicilio/cerca-de-mi/kebab/',
+            text: 'Kebab a domicilio'
+        },
+        {
+            url: '/a-domicilio/cerca-de-mi/comida-china/',
+            text: 'Comida china a domicilio'
+        },
+        {
+            url: '/a-domicilio/cerca-de-mi/sushi/',
+            text: 'Sushi a domicilio'
+        },
+        {
+            url: '/a-domicilio/cerca-de-mi/hamburguesas/',
+            text: 'Hamburguesas a domicilio'
+        },
+        {
+            url: '/a-domicilio/cerca-de-mi/',
+            text: 'Mas tipos de cocina'
+        }
+    ],
+    towns: 'Ciudades',
+    locationLinks: [
+        {
+            url: '/a-domicilio/madrid/',
+            text: 'Madrid'
+        },
+        {
+            url: '/a-domicilio/barcelona/',
+            text: 'Barcelona'
+        },
+        {
+            url: '/a-domicilio/valencia/',
+            text: 'Valencia'
+        },
+        {
+            url: '/a-domicilio/zaragoza/',
+            text: 'Zaragoza'
+        },
+        {
+            url: '/a-domicilio/palmas-de-gran-canaria/',
+            text: 'Las Palmas'
+        },
+        {
+            url: '/a-domicilio/',
+            text: 'Más ciudades'
+        }
+    ],
+    brands: 'Las marcas',
+    brandLinks: [],
+    aboutUs: 'Sobre nosotros',
+    aboutUsLinks: [
+        {
+            url: 'https://restaurantes.just-eat.es/',
+            text: 'Registrarse en el restaurante'
+        },
+        {
+            url: '/info/acerca-de-just-eat/',
+            text: 'Quienes somos'
+        },
+        {
+            url: '/help/',
+            text: 'Ayuda'
+        },
+        {
+            url: '/info/garantia-de-precio/',
+            text: 'Garantía de precio'
+        },
+        {
+            url: '/info/politica-de-privacidad/',
+            text: 'Política de Privacidad'
+        },
+        {
+            url: '/info/terminos-y-condiciones/',
+            text: 'Términos y Condiciones'
+        },
+        {
+            url: '/info/politica-de-cookies/',
+            text: 'Política de Cookies'
+        },
+        {
+            url: '/blog/descuento-5e-just-eat/',
+            text: '5€ de descuento'
+        },
+        {
+            url: '/campana/afiliacion',
+            text: 'Programa de Afiliación'
+        },
+        {
+            url: 'https://partner.just-eat.es/',
+            text: 'Gestiona tu restaurante'
+        }
+    ],
+    downloadOurApps: 'Descárgate la app',
+    appStoreIcons: [
+        {
+            url: 'https://itunes.apple.com/es/app/just-eat/id615238455?&mt=8',
+            key: 'ios',
+            iconSrc: 'iosIconSrc',
+            altText: 'Consíguelo en el App Store'
+        },
+        {
+            url: 'https://play.google.com/store/apps/details?id=com.justeat.app.es',
+            key: 'android',
+            iconSrc: 'androidIconSrc',
+            altText: 'Disponible en Google play'
+        }
+    ],
+    feedback: 'Opinión',
+    improveOurWebsite: 'Ayúdanos a mejorar la web',
+    sendFeedback: 'Enviar opinión',
+    followUs: 'Síguenos',
+    socialIcons: [
+        {
+            url: 'https://www.facebook.com/JustEat.es',
+            key: 'facebook',
+            iconSrc: 'facebookIconUrl',
+            altText: ''
+        },
+        {
+            url: 'https://twitter.com/JustEat_es',
+            key: 'twitter',
+            iconSrc: 'twitterIconUrl',
+            altText: ''
+        },
+        {
+            url: 'https://plus.google.com/+Just-eatEs',
+            key: 'google-plus',
+            iconSrc: 'googlePlusIconUrl',
+            altText: ''
+        },
+        {
+            url: 'https://www.instagram.com/justeat_es/',
+            key: 'instagram',
+            iconSrc: 'instagramIconUrl',
+            altText: ''
+        }
+    ],
+    currentCountryLocalisedName: 'España',
+    currentCountryFlagKey: 'es',
+    countries: [
+        {
+            key: 'au',
+            flagUrl: 'australiaFlagIconUrl',
+            localisedName: 'Australia',
+            siteUrl: 'https://www.menulog.com.au'
+        },
+        {
+            key: 'br',
+            flagUrl: 'brasilFlagIconUrl',
+            localisedName: 'Brasil',
+            siteUrl: 'https://www.ifood.com.br'
+        },
+        {
+            key: 'ca',
+            flagUrl: 'canadaFlagIconUrl',
+            localisedName: 'Canadá',
+            siteUrl: 'https://www.just-eat.ca'
+        },
+        {
+            key: 'dk',
+            flagUrl: 'denmarkFlagIconUrl',
+            localisedName: 'Dinamarca',
+            siteUrl: 'https://www.just-eat.dk'
+        },
+        {
+            key: 'fr',
+            flagUrl: 'franceFlagIconUrl',
+            localisedName: 'Francia',
+            siteUrl: 'https://www.just-eat.fr/'
+        },
+        {
+            key: 'ie',
+            flagUrl: 'irelandFlagIconUrl',
+            localisedName: 'Irlanda',
+            siteUrl: 'https://www.just-eat.ie'
+        },
+        {
+            key: 'it',
+            flagUrl: 'italyFlagIconUrl',
+            localisedName: 'Italia',
+            siteUrl: 'https://www.justeat.it'
+        },
+        {
+            key: 'no',
+            flagUrl: 'norwayFlagIconUrl',
+            localisedName: 'Noruega',
+            siteUrl: 'https://www.just-eat.no'
+        },
+        {
+            key: 'nz',
+            flagUrl: 'newzealandFlagIconUrl',
+            localisedName: 'Nueva Zelanda',
+            siteUrl: 'https://www.menulog.co.nz'
+        },
+        {
+            key: 'gb',
+            flagUrl: 'ukFlagIconUrl',
+            localisedName: 'Reino Unido',
+            siteUrl: 'https://www.just-eat.co.uk'
+        },
+        {
+            key: 'ch',
+            flagUrl: 'switzerlandFlagIconUrl',
+            localisedName: 'Suiza',
+            siteUrl: 'https://www.eat.ch'
+        }
+    ],
+    changeCurrentCountry: 'Estás en Just Eat España. Haz clic aquí para cambiar país.',
+    buttonClose: 'Cerrar',
+    paymentIcons: [
+        {
+            type: 'card',
+            key: 'mastercard--securecode',
+            iconSrc: 'mastercardIconSrc',
+            altText: 'Mastercard'
+        },
+        {
+            type: 'card',
+            key: 'visa--verified',
+            iconSrc: 'visaIconSrc',
+            altText: 'Visa'
+        },
+        {
+            type: 'card',
+            key: 'amex--safekey',
+            iconSrc: 'amexIconSrc',
+            altText: 'American Express'
+        },
+        {
+            type: 'card',
+            key: 'paypal',
+            iconSrc: 'paypalIconSrc',
+            altText: 'PayPal'
+        },
+        {
+            type: 'certificate',
+            key: 'confianza',
+            iconSrc: 'confianzaIconSrc',
+            altText: ''
+        }
+    ],
+    vatInfo: ''
 };
