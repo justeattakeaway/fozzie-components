aaa5f7b220870f882d65d88dbb1873ab64766335
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-07-09 09:53:44 +0100

    f-footer@2.0.0-beta.2 transfer all footer data from the original f-footer module (#11)

--- packages/f-footer/src/tenants/nb-NO.js
+++ packages/f-footer/src/tenants/nb-NO.js
@@ -14,5 +14,216 @@ export default {
             url: '/account/register',
             text: 'Registrer deg'
         }
-    ]
+    ],
+    cuisines: 'Hva frister?',
+    cuisineLinks: [
+        {
+            url: '/take-away',
+            text: 'Take-away'
+        },
+        {
+            url: '/take-away/pizza',
+            text: 'Pizza'
+        },
+        {
+            url: '/take-away/sushi',
+            text: 'Sushi'
+        },
+        {
+            url: '/take-away/indisk',
+            text: 'Indisk'
+        },
+        {
+            url: '/take-away/thaimat',
+            text: 'Thaimat'
+        },
+        {
+            url: '/take-away/kebab',
+            text: 'Kebab'
+        },
+        {
+            url: '/take-away/cuisines',
+            text: 'Alle kjøkkentyper'
+        }
+    ],
+    towns: 'Finn din by',
+    locationLinks: [
+        {
+            url: '/take-away/Oslo',
+            text: 'Oslo'
+        },
+        {
+            url: '/take-away/Bergen',
+            text: 'Bergen'
+        },
+        {
+            url: '/take-away/Trondheim',
+            text: 'Trondheim'
+        },
+        {
+            url: '/take-away/Drammen',
+            text: 'Drammen'
+        },
+        {
+            url: '/take-away/Fredrikstad',
+            text: 'Fredrikstad'
+        },
+        {
+            url: '/take-away/Asker',
+            text: 'Asker & Bærum'
+        },
+        {
+            url: '/take-away/cities',
+            text: 'Alle byer'
+        }
+    ],
+    brands: 'Brands',
+    brandLinks: [],
+    aboutUs: 'Om oss',
+    aboutUsLinks: [
+        {
+            url: '/take-away/bedrift-overtidsmat',
+            text: 'Restaurant påmelding'
+        },
+        {
+            url: '/about',
+            text: 'Om oss'
+        },
+        {
+            url: 'https://partner.just-eat.no/',
+            text: 'Partner centre'
+        },
+        {
+            url: '/jobs',
+            text: 'Ledige stillinger'
+        },
+        {
+            url: '/cookiespolicy',
+            text: 'Cookies'
+        },
+        {
+            url: '/privacy-policy',
+            text: 'Handelsvilkår'
+        }
+    ],
+    downloadOurApps: 'Last ned appen vår',
+    appStoreIcons: [
+        {
+            url: 'https://itunes.apple.com/no/app/just-eat.no/id748246908?ls=1&mt=8',
+            key: 'ios',
+            iconSrc: 'iosIconSrc',
+            altText: 'Hent i App Store'
+        },
+        {
+            url: 'https://play.google.com/store/apps/details?id=com.justeat.app.no',
+            key: 'android',
+            iconSrc: 'androidIconSrc',
+            altText: 'Finn den på Google Play'
+        }
+    ],
+    feedback: 'Tilbakemelding',
+    improveOurWebsite: 'Hjelp oss med å gjøre vår hjemmeside bedre',
+    sendFeedback: 'Send tilbakemelding',
+    followUs: 'Følg oss',
+    socialIcons: [
+        {
+            url: 'https://www.facebook.com/JustEatNO/',
+            key: 'facebook',
+            iconSrc: 'facebookIconUrl',
+            altText: ''
+        },
+        {
+            url: '/blog',
+            key: 'rss',
+            iconSrc: 'rssIconUrl',
+            altText: ''
+        }
+    ],
+    currentCountryLocalisedName: 'Norge',
+    currentCountryFlagKey: 'no',
+    countries: [
+        {
+            key: 'au',
+            flagUrl: 'australiaFlagIconUrl',
+            localisedName: 'Australia',
+            siteUrl: 'https://www.menulog.com.au'
+        },
+        {
+            key: 'br',
+            flagUrl: 'brasilFlagIconUrl',
+            localisedName: 'Brasil',
+            siteUrl: 'https://www.ifood.com.br'
+        },
+        {
+            key: 'ca',
+            flagUrl: 'canadaFlagIconUrl',
+            localisedName: 'Canada',
+            siteUrl: 'https://www.just-eat.ca'
+        },
+        {
+            key: 'dk',
+            flagUrl: 'denmarkFlagIconUrl',
+            localisedName: 'Danmark',
+            siteUrl: 'https://www.just-eat.dk'
+        },
+        {
+            key: 'fr',
+            flagUrl: 'franceFlagIconUrl',
+            localisedName: 'Frankrike',
+            siteUrl: 'https://www.just-eat.fr/'
+        },
+        {
+            key: 'ie',
+            flagUrl: 'irelandFlagIconUrl',
+            localisedName: 'Irland',
+            siteUrl: 'https://www.just-eat.ie'
+        },
+        {
+            key: 'it',
+            flagUrl: 'italyFlagIconUrl',
+            localisedName: 'Italia',
+            siteUrl: 'https://www.justeat.it'
+        },
+        {
+            key: 'nz',
+            flagUrl: 'newzealandFlagIconUrl',
+            localisedName: 'New Zealand',
+            siteUrl: 'https://www.menulog.co.nz'
+        },
+        {
+            key: 'es',
+            flagUrl: 'spainFlagIconUrl',
+            localisedName: 'Spania',
+            siteUrl: 'https://www.just-eat.es'
+        },
+        {
+            key: 'gb',
+            flagUrl: 'ukFlagIconUrl',
+            localisedName: 'Storbritannia',
+            siteUrl: 'https://www.just-eat.co.uk'
+        },
+        {
+            key: 'ch',
+            flagUrl: 'switzerlandFlagIconUrl',
+            localisedName: 'Sveits',
+            siteUrl: 'https://www.eat.ch'
+        }
+    ],
+    changeCurrentCountry: 'Du er på den norske nettsiden. Klikk her for å endre.',
+    buttonClose: 'Lukk',
+    paymentIcons: [
+        {
+            type: 'card',
+            key: 'visa--verified',
+            iconSrc: 'visaIconSrc',
+            altText: 'Visa'
+        },
+        {
+            type: 'card',
+            key: 'mastercard--securecode',
+            iconSrc: 'mastercardIconSrc',
+            altText: 'Mastercard'
+        }
+    ],
+    vatInfo: ''
 };
