74f7ce3d4dfc38bea69ab8e2967486d2f4d3dff8
Maryia Radchuk <maryia.radchuk@just-eat.com>
2019-07-12 16:19:15 +0100

    f-footer@2.0.0-beta.4 fozzie base variables, mixins and colours + FooterList component

--- packages/f-footer/src/components/FooterDemo.vue
+++ packages/f-footer/src/components/FooterDemo.vue
@@ -1,6 +1,6 @@
 <template>
     <vue-footer
-        locale="en-AU" />
+        locale="en-GB" />
 </template>
 
 <script>
@@ -10,3 +10,8 @@ export default {
     components: { VueFooter }
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Ubuntu:300,500&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Hind+Vadodara:300,500&display=swap');
+</style>
