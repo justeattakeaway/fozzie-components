1075d535ce719a7f2ea70e56aca310a167f7a27b
Xander Marjoram <xander.marjoram@just-eat.com>
2019-07-17 15:02:13 +0100

    f-vue-icons@0.11.0 - Pass locale into AppStoreIcon And fix icon component names for Danish and Norwegian

--- packages/f-vue-icons/src/components/AppStoreIcon/AppStoreIcon.vue
+++ packages/f-vue-icons/src/components/AppStoreIcon/AppStoreIcon.vue
@@ -3,45 +3,49 @@
 </template>
 
 <script>
-import AndroidIconDk from '@justeat/f-icons/src/img/icons/appstore/android.dk.svg';
+import AndroidIconDa from '@justeat/f-icons/src/img/icons/appstore/android.dk.svg';
 import AndroidIconEn from '@justeat/f-icons/src/img/icons/appstore/android.svg';
 import AndroidIconEs from '@justeat/f-icons/src/img/icons/appstore/android.es.svg';
 import AndroidIconIt from '@justeat/f-icons/src/img/icons/appstore/android.it.svg';
-import AndroidIconNo from '@justeat/f-icons/src/img/icons/appstore/android.no.svg';
-import IosIconDk from '@justeat/f-icons/src/img/icons/appstore/ios.dk.svg';
+import AndroidIconNb from '@justeat/f-icons/src/img/icons/appstore/android.no.svg';
+import IosIconDa from '@justeat/f-icons/src/img/icons/appstore/ios.dk.svg';
 import IosIconEn from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
 import IosIconEs from '@justeat/f-icons/src/img/icons/appstore/ios.es.svg';
 import IosIconIt from '@justeat/f-icons/src/img/icons/appstore/ios.it.svg';
-import IosIconNo from '@justeat/f-icons/src/img/icons/appstore/ios.no.svg';
-import MicrosoftIconDk from '@justeat/f-icons/src/img/icons/appstore/microsoft.dk.svg';
+import IosIconNb from '@justeat/f-icons/src/img/icons/appstore/ios.no.svg';
+import MicrosoftIconDa from '@justeat/f-icons/src/img/icons/appstore/microsoft.dk.svg';
 
 export default {
     components: {
-        AndroidIconDk,
+        AndroidIconDa,
         AndroidIconEn,
         AndroidIconEs,
         AndroidIconIt,
-        AndroidIconNo,
-        IosIconDk,
+        AndroidIconNb,
+        IosIconDa,
         IosIconEn,
         IosIconEs,
         IosIconIt,
-        IosIconNo,
-        MicrosoftIconDk
+        IosIconNb,
+        MicrosoftIconDa
     },
     props: {
         name: {
             type: String,
             required: true
         },
-        language: {
+        locale: {
             type: String,
-            default: 'en'
+            required: false,
+            default: 'en-GB'
         }
     },
     computed: {
         iconComponent () {
             return `${this.name}-icon-${this.language}`;
+        },
+        language () {
+            return this.locale.split('-')[0];
         }
     }
 };
