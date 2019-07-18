0523589abfe8a1457e907a1f64b96964b79435db
Xander Marjoram <xander.marjoram@just-eat.com>
2019-07-18 09:21:34 +0100

    f-vue-icons@0.11.0 - Pass locale into AppStoreIcon (#19)
    * f-vue-icons@0.11.0 - Pass locale into AppStoreIcon
    And fix icon component names for Danish and Norwegian
    
    * f-vue-icons@0.11.0 - Changelog entry
    
    * f-vue-icons@0.11.0 - Convert AppStoreIcon to take locale instead of language

--- packages/f-vue-icons/src/components/AppStoreIcon/AppStoreIcon.vue
+++ packages/f-vue-icons/src/components/AppStoreIcon/AppStoreIcon.vue
@@ -3,45 +3,58 @@
 </template>
 
 <script>
-import AndroidIconDk from '@justeat/f-icons/src/img/icons/appstore/android.dk.svg';
-import AndroidIconEn from '@justeat/f-icons/src/img/icons/appstore/android.svg';
-import AndroidIconEs from '@justeat/f-icons/src/img/icons/appstore/android.es.svg';
-import AndroidIconIt from '@justeat/f-icons/src/img/icons/appstore/android.it.svg';
-import AndroidIconNo from '@justeat/f-icons/src/img/icons/appstore/android.no.svg';
-import IosIconDk from '@justeat/f-icons/src/img/icons/appstore/ios.dk.svg';
-import IosIconEn from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
-import IosIconEs from '@justeat/f-icons/src/img/icons/appstore/ios.es.svg';
-import IosIconIt from '@justeat/f-icons/src/img/icons/appstore/ios.it.svg';
-import IosIconNo from '@justeat/f-icons/src/img/icons/appstore/ios.no.svg';
-import MicrosoftIconDk from '@justeat/f-icons/src/img/icons/appstore/microsoft.dk.svg';
+import AndroidIconDaDk from '@justeat/f-icons/src/img/icons/appstore/android.dk.svg';
+import AndroidIconEnGb from '@justeat/f-icons/src/img/icons/appstore/android.svg';
+import AndroidIconEnIe from '@justeat/f-icons/src/img/icons/appstore/android.svg';
+import AndroidIconEnAu from '@justeat/f-icons/src/img/icons/appstore/android.svg';
+import AndroidIconEnNz from '@justeat/f-icons/src/img/icons/appstore/android.svg';
+import AndroidIconEsEs from '@justeat/f-icons/src/img/icons/appstore/android.es.svg';
+import AndroidIconItIt from '@justeat/f-icons/src/img/icons/appstore/android.it.svg';
+import AndroidIconNbNo from '@justeat/f-icons/src/img/icons/appstore/android.no.svg';
+import IosIconDaDk from '@justeat/f-icons/src/img/icons/appstore/ios.dk.svg';
+import IosIconEnGb from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
+import IosIconEnIe from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
+import IosIconEnAu from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
+import IosIconEnNz from '@justeat/f-icons/src/img/icons/appstore/ios.svg';
+import IosIconEsEs from '@justeat/f-icons/src/img/icons/appstore/ios.es.svg';
+import IosIconItIt from '@justeat/f-icons/src/img/icons/appstore/ios.it.svg';
+import IosIconNbNo from '@justeat/f-icons/src/img/icons/appstore/ios.no.svg';
+import MicrosoftIconDaDk from '@justeat/f-icons/src/img/icons/appstore/microsoft.dk.svg';
 
 export default {
     components: {
-        AndroidIconDk,
-        AndroidIconEn,
-        AndroidIconEs,
-        AndroidIconIt,
-        AndroidIconNo,
-        IosIconDk,
-        IosIconEn,
-        IosIconEs,
-        IosIconIt,
-        IosIconNo,
-        MicrosoftIconDk
+        AndroidIconDaDk,
+        AndroidIconEnGb,
+        AndroidIconEnIe,
+        AndroidIconEnAu,
+        AndroidIconEnNz,
+        AndroidIconEsEs,
+        AndroidIconItIt,
+        AndroidIconNbNo,
+        IosIconDaDk,
+        IosIconEnGb,
+        IosIconEnIe,
+        IosIconEnAu,
+        IosIconEnNz,
+        IosIconEsEs,
+        IosIconItIt,
+        IosIconNbNo,
+        MicrosoftIconDaDk
     },
     props: {
         name: {
             type: String,
             required: true
         },
-        language: {
+        locale: {
             type: String,
-            default: 'en'
+            required: false,
+            default: 'en-GB'
         }
     },
     computed: {
         iconComponent () {
-            return `${this.name}-icon-${this.language}`;
+            return `${this.name}-icon-${this.locale.toLowerCase()}`;
         }
     }
 };
