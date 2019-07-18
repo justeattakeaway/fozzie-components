15880c0d95e79f3dbe26052a1a63658426eee53a
Xander Marjoram <xander.marjoram@just-eat.com>
2019-07-18 11:44:42 +0100

    f-footer@2.0.0-beta.6 - Remove SVG config

--- packages/f-footer/vue.config.js
+++ packages/f-footer/vue.config.js
@@ -13,18 +13,5 @@ module.exports = {
                 // eslint-disable-next-line quotes
                 data: `@import "@/assets/scss/common.scss";`
             });
-
-        // Inline SVGs
-        const svgRule = config.module.rule('svg');
-
-        // clear all existing loaders.
-        // if you don't do this, the loader below will be appended to
-        // existing loaders of the rule.
-        svgRule.uses.clear();
-        // add replacement loader(s)
-        svgRule
-            .use('babel-loader')
-            .loader('babel-loader')
-            .end();
     }
 };
