ca516d12a73b9c297649a253ab4d96652e06c346
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-07-31 11:21:11 +0100

    f-vue-icons@0.15.1 Removed babel rule for ignoring polyfills (#26)
    * f-vue-icons@0.15.1 Removed babel rule for ignoring polyfills
    
    * js condition optimisation

--- packages/f-vue-icons/babel.config.js
+++ packages/f-vue-icons/babel.config.js
@@ -1,11 +1,20 @@
-module.exports = {
-    presets: [
-        [
-            '@vue/app',
-            {
-                useBuiltIns: 'false',
-                polyfills: 'false'
-            }
-        ]
-    ]
+module.exports = api => {
+    // Use isTest to determine what presets and plugins to use with jest
+    const isTest = api.env('test');
+    const isSettings = api.env('settings');
+    const presets = [];
+    const plugins = [];
+
+    if (!isTest && !isSettings) {
+        api.cache(true);
+        presets.push(['@vue/app']);
+    }
+
+    // use for both test and dev/live
+    presets.push('@babel/env');
+
+    return {
+        presets,
+        plugins
+    };
 };
