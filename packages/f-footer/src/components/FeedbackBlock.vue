d55cad54b71f9610e847aced12d43598ef5828d1
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-08-06 10:03:10 +0100

    f-footer@2.0.0-beta.10 feedback block styles and data-attr for gtm (#30)
    * f-footer@2.0.0-beta.10 feedback block style and data-attr for gtm
    
    * f-footer@2.0.0-beta.10 c-feedback instead of c-feedbackBlock
    
    * f-footer@2.0.0-beta.10 linting error fixes

--- packages/f-footer/src/components/FeedbackBlock.vue
+++ packages/f-footer/src/components/FeedbackBlock.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="c-footer-feedbackBlock">
+    <div
+        class="c-feedback"
+        data-gtm-feedback>
         <h2 class="c-footer-heading c-footer-heading--shortBelowWide">
             {{ title }}
         </h2>
-        <p>
+        <p class="c-feedback-text">
             {{ text }}
         </p>
-        <button class="o-btn--tertiary">
+        <button class="c-feedback-button">
             {{ buttonText }}
         </button>
     </div>
@@ -33,9 +35,47 @@ export default {
 
 <style lang="scss">
 
-.c-footer-feedbackBlock {
+.c-feedback {
     @include media('<wide') {
         order: 1;
+    }
+}
+
+.c-feedback-text {
+    margin: 0 0 spacing(x0.5) 0;
+}
+
+.c-feedback-button {
+    border: 0;
+    background-color: transparent;
+    padding: 0;
+    margin: 0;
+    color: $color-link-default;
+    font-weight: 500;
+    @include font-size(base);
+    text-decoration: underline;
+
+    @include theme(ml) {
+        color: $color-link-default--ml;
+    }
+
+    &:hover {
+        cursor: pointer;
+        color: $color-link-hover;
+        background-color: transparent;
+
+        @include theme(ml) {
+            color: $color-link-hover--ml;
+        }
+    }
+    &:active,
+    &:focus {
+        color: $color-link-active;
+        background-color: transparent;
+
+        @include theme(ml) {
+            color: $color-link-active--ml;
+        }
     }
 }
 
