a067f18a33d5a956c470403befe20a1ea5767e1e
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-09-04 16:29:18 +0100

    f-header@2.0.0 beta.0 (#41)
    * f-header@0.1.0 - Temporary local check-in of base code
    
    * f-header@2.0.0-beta.0 initian commit

---
+++ packages/f-header/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="c-header">
+        I am Header
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'VueHeader'
+};
+</script>
+
+<style lang="scss">
+
+</style>
