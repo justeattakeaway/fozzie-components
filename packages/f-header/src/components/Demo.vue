0330191494e07398a41b11b31d33225b81cdb896
Maryia Radchuk <maryia.radchuk@just-eat.com>
2019-09-05 15:52:17 +0100

    f-header@2.0.0-beta.1 logo component + tenants resource files

--- packages/f-header/src/components/Demo.vue
+++ packages/f-header/src/components/Demo.vue
@@ -7,7 +7,8 @@
             name="viewport"
             content="width=device-width, initial-scale=1">
         <vue-header
-            locale="en-GB" />
+            :is-transparent=true
+            locale="en-AU" />
     </div>
 </template>
 
---
+++ packages/f-header/src/components/Logo.vue
@@ -0,0 +1,113 @@
+<template>
+    <a
+        :aria-label="linkAltText"
+        href="/"
+        class="c-logo" >
+        <component
+            :is="iconComponent"
+            :class="[iconClassName, 'c-logo-img', {'je-icon--transparent': isTransparent}]" />
+    </a>
+</template>
+
+<script>
+import {
+    JeLogoNoColourIcon as JeLogo,
+    MlLogoIcon as MlLogo
+} from '@justeat/f-vue-icons';
+
+export default {
+    components: {
+        JeLogo,
+        MlLogo
+    },
+    props: {
+        theme: {
+            type: String,
+            required: true
+        },
+        isTransparent: {
+            type: Boolean,
+            required: true
+        },
+        companyName: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        iconComponent () {
+            return `${this.theme}-logo`;
+        },
+        iconClassName () {
+            return `${this.theme}-icon`;
+        },
+        linkAltText () {
+            return `Go to ${this.companyName} homepage`;
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+    // link with the logo
+    .c-logo {
+        display: block;
+        display: flex;
+        justify-content: center;
+        height: $header-height--narrow;
+        padding-top: 22px;
+
+
+        @include theme(ml) {
+            padding-top: 12px;
+        }
+
+        @include media('>=mid') {
+            justify-content: left;
+            height: $header-height;
+            padding-top: 24px;
+
+            @include theme(ml) {
+                padding-top: 10px;
+            }
+        }
+    }
+
+    .c-logo-img {
+        // default logo image height and width (as should be an inline SVG)
+        width: 82px;
+        height: 16px;
+
+        @include media('>=mid') {
+            width: 165px;
+            height: 32px;
+        }
+
+        // Menulog logo, as it has multiple fill values built in. We just hide the outline on transparent mode.
+        @include theme(ml) {
+            width: 118px;
+            height: 40px;
+            margin-left: -10.5px; //half of hamburger menu width
+
+            @include media('>=mid') {
+                width: 180px;
+                height: 61px;
+                margin-left: 0;
+            }
+
+            path:first-child {
+                .c-header--transparent & {
+                    display: none;
+                }
+            }
+        }
+    }
+
+    .je-icon g {
+        fill: $header-logo-color;
+    }
+
+    .je-icon--transparent g {
+        fill: $header-logo-color--transparent;
+    }
+</style>
