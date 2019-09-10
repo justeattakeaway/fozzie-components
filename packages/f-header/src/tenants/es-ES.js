9dcdf5b2ec9d16c8fb04638608fa0d6c880f40a0
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-09-10 12:01:39 +0100

    f-header@2.0.0-beta.1 logo component + tenants resource files (#43)
    * f-header@2.0.0-beta.1 logo component + tenants resource files
    
    * f-header@2.0.0-beta.1 jest tests for header and logo components
    
    * f-header@2.0.0-beta.1 rename some css classes
    
    * f-header@2.0.0-beta.1 Rename css class from c-icon-je--transparentBckgrnd to c-icon-je--transparentBg

---
+++ packages/f-header/src/tenants/es-ES.js
@@ -0,0 +1,15 @@
+export default {
+    locale: 'es-ES',
+    companyName: 'Just Eat',
+    openMenuText: 'Open Menu',
+    loginLinkText: 'Inicia sesión',
+    loginNoScriptLinkText: 'Cuenta',
+    helpLinkText: 'Ayuda',
+    logoutLinkText: 'Salir',
+    skipToMainContentText: 'Ir al contenido principal',
+    accountInfo: 'Información de la cuenta',
+    accountOrderHistory: 'Pedidos',
+    accountCredit: 'Crédito de la cuenta',
+    paymentMethods: 'Métodos de pago',
+    deliveryAddresses: 'Direcciones de reparto'
+};
