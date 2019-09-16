bc1010f9277b8153a5012c701c1f5fa06c06bdcc
Mariya Radchuk <maryia.radchuk@just-eat.com>
2019-09-16 10:13:38 +0100

    f-header@2.0.0 beta.3: Navigation component (#51)
    * f-header@2.0.0-beta.3 transfered css and html for navigation from live version
    
    * f-header@2.0.0-beta.3 Add profile icon + some html which was missing
    
    * f-header@2.0.0-beta.3 updated translations and added tracking (#47)
    
    * f-header@2.0.0-beta.3 Tenants translations and correct links from live version
    
    * f-header@2.0.0-beta.3 pass gtm label for logo component through the props
    
    * f-header@2.0.0-beta.3 Add ml css variable ovewrite
    
    * WCP-149: Added the new link to header and updated content and tests (#50)
    
    * added the new link to header and updated content and tests
    
    * removed test prop values
    
    * added computed property for logic
    
    * updated snapshot and icon version
    
    * removed class for icon and changed string to var
    
    * changed to GB
    
    * removed translations for unused locales
    
    * removed logic from component level and toggle with one prop
    
    * changed back to false
    
    * changed width to match live
    
    * added logic to toggle delivery content
    
    * added getter to only show when there is content
    
    * update snapshot and make sure tests pass
    
    * updated snapshot again
    
    * f-header@2.0.0-beta.3 Accessibility tabbing functionality

--- packages/f-header/src/tenants/it-IT.js
+++ packages/f-header/src/tenants/it-IT.js
@@ -2,14 +2,58 @@ export default {
     locale: 'it-IT',
     companyName: 'Just Eat',
     openMenuText: 'Open Menu',
-    loginLinkText: 'Accedi',
     loginNoScriptLinkText: 'Account',
-    helpLinkText: 'Aiuto?',
-    logoutLinkText: 'Esci',
     skipToMainContentText: 'Vai al contenuto principale',
-    accountInfo: 'Account',
-    accountOrderHistory: 'Ordini',
-    accountCredit: 'Credito dell\'account',
-    paymentMethods: 'Metodi di pagamento',
-    deliveryAddresses: 'Indirizzi di consegna'
+
+    navLinks: {
+        accountInfo: {
+            text: 'Account',
+            url: '/account/info',
+            gtm: 'click_account_your_account'
+        },
+        orderHistory: {
+            text: 'Ordini',
+            url: '/order-history',
+            gtm: 'click_account_your_orders'
+        },
+        accountCredit: {
+            text: 'Credito dell\'account',
+            url: '/account/credit',
+            gtm: 'click_account_credit'
+        },
+        savedCards: {
+            text: 'Metodi di pagamento',
+            url: '/member/savedcards',
+            gtm: 'click_account_saved_cards'
+        },
+        addressBook: {
+            text: 'Indirizzi di consegna',
+            url: '/account/addressbook',
+            gtm: 'click_account_address_books'
+        },
+        contactPreferences: {
+            text: 'Modalità di contatto',
+            url: '/account/contact-preferences/',
+            gtm: 'click_account_contact_preferences'
+        }
+    },
+
+    logo: {
+        gtm: 'click_logo'
+    },
+    help: {
+        text: 'Aiuto?',
+        url: '/help',
+        gtm: 'click_help'
+    },
+    accountLogin: {
+        text: 'Accedi',
+        url: '/account/login?returnurl=',
+        gtm: 'click_login'
+    },
+    accountLogout: {
+        text: 'Esci',
+        url: '/account/logout?returnurl=',
+        gtm: 'click_logout'
+    }
 };
