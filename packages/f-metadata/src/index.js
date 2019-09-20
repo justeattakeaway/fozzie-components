58a393ed7ef4c53a7f648e7811464d04d7b95b3e
Christa-Awa Kollen <cak8@users.noreply.github.com>
2019-09-20 12:02:34 +0100

    f-metadata@v0.1.0 – Initial Setup (#57)
    *  intialise braze code
    
    *  serverside rendering issues
    
    * undefined window error removed
    
    * PR feedback revision
    
    * PR revision
    
    * final

---
+++ packages/f-metadata/src/index.js
@@ -0,0 +1,10 @@
+const initialiseBraze = (apiKey, enableLogging = false) => {
+    if (typeof window !== 'undefined') {
+        const appboy = require('appboy-web-sdk'); // eslint-disable-line
+        appboy.initialize(apiKey, { enableLogging });
+        appboy.display.automaticallyShowNewInAppMessages();
+        appboy.openSession();
+    }
+};
+
+export default initialiseBraze;
