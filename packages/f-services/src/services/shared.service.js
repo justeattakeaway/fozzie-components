84b55081bf72901a5c16316a423b2b59a8532f83
Sean Milburn <seanmilburn1990@gmail.com>
2019-09-27 09:32:28 +0100

    f-services@0.7.0-remove-method (#65)
    * updated package and versions
    
    * removed conflict
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update shared.service.js
    
    * Update CHANGELOG.md

--- packages/f-services/src/services/shared.service.js
+++ packages/f-services/src/services/shared.service.js
@@ -13,18 +13,15 @@
  * @returns {String}
  */
 const getLocale = (tenantConfigs, tenantString, globalTenant) => {
-    const locale = tenantString === '' ? globalTenant?.locale : tenantString;
-
-    // makes sure the locale is the same format as the tenants - 'en-GB'
-    let transformedLocale = `${locale.slice(0, -3)}${locale.slice(locale.lastIndexOf('-')).toUpperCase()}`;
+    let locale = tenantString === '' ? globalTenant?.locale : tenantString;
 
     // if the locale is either
     // a) not set
     // or b) set to a country code that this component does not recognise
     // it will be set to 'en-GB'
-    if (!tenantConfigs[transformedLocale]) transformedLocale = 'en-GB';
+    if (!tenantConfigs[locale]) locale = 'en-GB';
 
-    return transformedLocale;
+    return locale;
 };
 
 /**
