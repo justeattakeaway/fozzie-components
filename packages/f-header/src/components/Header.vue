2dc1687eabe99fd2cb8317e83f199f5b3e6bc22b
Sean Milburn <seanmilburn1990@gmail.com>
2019-10-01 16:29:11 +0100

    updates from PR comments

--- packages/f-header/src/components/Header.vue
+++ packages/f-header/src/components/Header.vue
@@ -1,37 +1,35 @@
 <template>
-    <section>
+    <header
+        :data-theme="theme"
+        :class="['c-header', { 'c-header--transparent c-header--gradient': showTransparentHeader }]">
         <input
             data-order-count-supported
             type="hidden"
             :value="isOrderCountSupported">
-        <header
-            :data-theme="theme"
-            :class="['c-header', { 'c-header--transparent c-header--gradient': showTransparentHeader }]">
-            <skip-to-main
-                :text="copy.skipToMainContentText"
-                :transparent-bg="showTransparentHeader" />
-            <div class="c-header-container">
-                <logo
-                    :theme="theme"
-                    :is-transparent="showTransparentHeader"
-                    :company-name="copy.companyName"
-                    :logo-gtm-label="copy.logo.gtm" />
-                <navigation
-                    :nav-links="copy.navLinks"
-                    :help="copy.help"
-                    :account-logout="copy.accountLogout"
-                    :account-login="copy.accountLogin"
-                    :open-menu-text="copy.openMenuText"
-                    :delivery-enquiry="copy.deliveryEnquiry"
-                    :show-delivery-enquiry="showDeliveryEnquiryWithContent"
-                    :just-log="justLog"
-                    :user-info-prop="userInfoProp"
-                    :user-info-url="userInfoUrl"
-                    :order-count-url="orderCountUrl"
-                    @onMobileNavToggle="mobileNavToggled" />
-            </div>
-        </header>
-    </section>
+        <skip-to-main
+            :text="copy.skipToMainContentText"
+            :transparent-bg="showTransparentHeader" />
+        <div class="c-header-container">
+            <logo
+                :theme="theme"
+                :is-transparent="showTransparentHeader"
+                :company-name="copy.companyName"
+                :logo-gtm-label="copy.logo.gtm" />
+            <navigation
+                :nav-links="copy.navLinks"
+                :help="copy.help"
+                :account-logout="copy.accountLogout"
+                :account-login="copy.accountLogin"
+                :open-menu-text="copy.openMenuText"
+                :delivery-enquiry="copy.deliveryEnquiry"
+                :show-delivery-enquiry="showDeliveryEnquiryWithContent"
+                :just-log="justLog"
+                :user-info-prop="userInfoProp"
+                :user-info-url="userInfoUrl"
+                :order-count-url="orderCountUrl"
+                @onMobileNavToggle="mobileNavToggled" />
+        </div>
+    </header>
 </template>
 
 <script>
