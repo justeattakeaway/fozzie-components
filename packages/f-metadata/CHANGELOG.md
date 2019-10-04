2ab0523f6cfb39255c6ec6f19ff7a2a7ae87301a
ashleynolan <nolly00@gmail.com>
2019-10-04 15:55:32 +0100

    f-metadata@v0.4.0 – Changing Braze to be a dynamic import

--- packages/f-metadata/CHANGELOG.md
+++ packages/f-metadata/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.4.0
+------------------------------
+*October 07, 2019*
+
+ ### Changed
+- Changing Braze to be a dynamic import (to save on bundle size of `f-metadata`)
+
+
 v0.3.0
 ------------------------------
 *October 04, 2019*
--- packages/f-metadata/src/index.js
+++ packages/f-metadata/src/index.js
@@ -1,10 +1,13 @@
 const initialiseBraze = (apiKey, enableLogging = false, disableComponent = false) => {
     if (typeof window !== 'undefined') {
         if (!disableComponent) {
-            const appboy = require('appboy-web-sdk'); // eslint-disable-line
-            appboy.initialize(apiKey, { enableLogging });
-            appboy.display.automaticallyShowNewInAppMessages();
-            appboy.openSession();
+            import(/* webpackChunkName: "appboy-web-sdk" */ 'appboy-web-sdk')
+                .then(({ default: appboy }) => {
+                    appboy.initialize(apiKey, { enableLogging });
+                    appboy.display.automaticallyShowNewInAppMessages();
+                    appboy.openSession();
+                })
+                .catch(error => `An error occurred while loading the component: ${error}`);
         }
     }
 };
