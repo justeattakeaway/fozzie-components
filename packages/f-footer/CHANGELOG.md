e197e7981a15be3fc955995e682a85816417d8fb
Sean Milburn <seanmilburn1990@gmail.com>
2019-10-24 11:31:04 +0100

    f-footer@2.0.0-beta.25-add-courier-links-nz (#89)
    * updated content for tenants
    
    * added some gtm for DK
    
    * added new links and updated logic for NZ

--- packages/f-footer/CHANGELOG.md
+++ packages/f-footer/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v2.0.0-beta.25
+------------------------------
+*October 21, 2019*
+
+### Added
+- Updated `NZ`, `DK` & `NO` tenant copy.
+
+
 v2.0.0-beta.24
 ------------------------------
 *October 17, 2019*
--- packages/f-footer/src/tenants/en-NZ.js
+++ packages/f-footer/src/tenants/en-NZ.js
@@ -121,7 +121,38 @@ export default {
             ]
         }
     },
-    linkButtonList: [],
+    linkButtonList: [
+        {
+            title: 'Partner with Us',
+            buttons: [
+                {
+                    title: 'List your restaurant',
+                    url: '/info/join-menulog',
+                    gtm: 'click_about_restaurant_signup'
+                },
+                {
+                    title: 'Become a courier',
+                    url: 'https://couriers.menulog.co.nz/application/',
+                    gtm: 'click_about_couriers'
+                }
+            ]
+        },
+        {
+            title: 'Already a partner',
+            buttons: [
+                {
+                    title: 'Partner centre',
+                    url: 'https://partner.menulog.co.nz/',
+                    gtm: 'click_about_partner_centre'
+                },
+                {
+                    title: 'Courier portal',
+                    url: 'https://couriers.menulog.co.nz/login',
+                    gtm: 'click_about_courier_portal'
+                }
+            ]
+        }
+    ],
     downloadOurApps: 'Download our apps',
     appStoreIcons: [
         {
