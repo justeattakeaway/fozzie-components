ab7753a72ada4e87243c915dacd024c079f6ee51
Sean Milburn <seanmilburn1990@gmail.com>
2019-10-24 11:31:04 +0100

    f-footer@2.0.0-beta.25-add-courier-links-nz (#89)
    * updated content for tenants
    
    * added some gtm for DK
    
    * added new links and updated logic for NZ

--- packages/f-footer/src/components/Demo.vue
+++ packages/f-footer/src/components/Demo.vue
@@ -7,7 +7,7 @@
             name="viewport"
             content="width=device-width, initial-scale=1">
         <vue-footer
-            locale="en-GB" />
+            locale="en-AU" />
     </div>
 </template>
 
--- packages/f-footer/src/components/Footer.vue
+++ packages/f-footer/src/components/Footer.vue
@@ -13,7 +13,8 @@
         <div class="c-footer-light">
             <div class="c-footer-container">
                 <div
-                    v-if="copy.linkButtonList.length"
+                    v-if="copy.linkButtonList.length && locale !== 'en-NZ'"
+                    data-test-id="courierLinks-wrapper"
                     class="c-footer-row c-footer-row--noBottomPad">
                     <button-list
                         v-for="(buttonList, i) in copy.linkButtonList"
