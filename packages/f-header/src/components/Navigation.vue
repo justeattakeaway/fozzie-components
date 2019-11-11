e5163dda8f8ca2c8d0e15d00c7efc3d5d58d41ef
Carlos Rodrigues <carlos.rodrigues@just-eat.com>
2019-11-11 15:32:13 +0000

    fix: wrong class being appended to the document (#102)

--- packages/f-header/src/components/Navigation.vue
+++ packages/f-header/src/components/Navigation.vue
@@ -339,7 +339,8 @@ export default {
         onNavToggle () {
             this.navIsOpen = !this.navIsOpen;
             // This is added to remove the ability to scroll the page content when the mobile navigation is open
-            document.documentElement.classList.toggle(this.isTransparent ? 'is-navInView is-navInView--noPad' : 'is-navInView');
+            document.documentElement.classList.toggle('is-navInView', this.navIsOpen);
+            document.documentElement.classList.toggle('is-navInView--noPad', this.navIsOpen && this.isTransparent);
         },
 
         closeNav () {
