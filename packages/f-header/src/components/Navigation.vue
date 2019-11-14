16305ea6142ead2071208e2a258df7823514a15d
Xander Marjoram <xander.marjoram@just-eat.com>
2019-11-14 16:29:29 +0000

    f-header@2.0.0-beta.29 - Fixing returnUrl (#103)
    * f-header@2.0.0-beta.29 - Fixing returnUrl
    
    * f-header@2.0.0-beta.29 - Fixing returnUrl

--- packages/f-header/src/components/Navigation.vue
+++ packages/f-header/src/components/Navigation.vue
@@ -286,9 +286,7 @@ export default {
 
         returnUrl () {
             if (this.$route) {
-                const { name } = this.$route;
-                const { href } = this.$router.resolve({ name });
-                return encodeURIComponent(href);
+                return encodeURIComponent(this.$route.path);
             }
             if (typeof document !== 'undefined') {
                 return encodeURIComponent(document.location.pathname);
