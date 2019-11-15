36576d3d9bca65892df957cd115beef1e719dfc7
Ashley Watson-Nolan <nolly00@gmail.com>
2019-11-15 15:38:19 +0000

    f-metadata@v1.0.0 - Dynamic bundling, content card support and adding userID (#105)

--- packages/f-metadata/CHANGELOG.md
+++ packages/f-metadata/CHANGELOG.md
@@ -4,19 +4,33 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v1.0.0
+------------------------------
+*November 15, 2019*
+
+### Added
+- Support for handling content cards through SDK methods
+- User ID to `initialiseBraze` function
+- Support for GTM with Braze
+
+### Changed
+- Changing Braze to be a dynamic import
+- Don't bundle `f-metadata`, leave that to the consuming component.
+
+
 v0.3.0
 ------------------------------
-*October 04, 2019*
+*October 4, 2019*
 
- ### Added
+### Added
 - Babel config (as wasn't transpiling ES functionality)
 
 
 v0.2.0
 ------------------------------
-*October 02, 2019*
+*October 2, 2019*
 
- ### Added
+### Added
 - `disable` toggle added to disable Braze
 
 
@@ -24,6 +38,6 @@ v0.1.0
 ------------------------------
 *September 19, 2019*
 
- ### Added
+### Added
 - Created package
 - Enabling initialisation of Braze
--- packages/f-metadata/src/index.js
+++ packages/f-metadata/src/index.js
@@ -1,10 +1,40 @@
-const initialiseBraze = (apiKey, enableLogging = false, disableComponent = false) => {
+const initialiseBraze = (options = {
+    apiKey: null,
+    userId: null,
+    enableLogging: false,
+    disableComponent: false,
+    callbacks: {
+        handleContentCards: null
+    }
+}) => {
     if (typeof window !== 'undefined') {
-        if (!disableComponent) {
-            const appboy = require('appboy-web-sdk'); // eslint-disable-line
-            appboy.initialize(apiKey, { enableLogging });
-            appboy.display.automaticallyShowNewInAppMessages();
-            appboy.openSession();
+        window.dataLayer = window.dataLayer || [];
+
+        if (!options.disableComponent) {
+            import(/* webpackChunkName: "appboy-web-sdk" */ 'appboy-web-sdk')
+                .then(({ default: appboy }) => {
+                    if (options.apiKey && options.apiKey.length && options.userId && options.userId.length) {
+                        appboy.initialize(options.apiKey, { enableLogging: options.enableLogging });
+
+                        appboy.display.automaticallyShowNewInAppMessages();
+
+                        appboy.changeUser(options.userId, () => {
+                            window.dataLayer.push({
+                                event: 'appboyReady'
+                            });
+                        });
+
+                        window.appboy = appboy;
+                        appboy.openSession();
+
+                        appboy.requestContentCardsRefresh();
+                        const contentCards = appboy.getCachedContentCards();
+                        if (contentCards && contentCards.cards.length) {
+                            options.callbacks.handleContentCards(contentCards.cards);
+                        }
+                    }
+                })
+                .catch(error => `An error occurred while loading the component: ${error}`);
         }
     }
 };
