8850036f6466eee5bc8dc8997bcbdd8fb92b7fcb
Ashley Watson-Nolan <nolly00@gmail.com>
2020-02-25 11:11:18 +0000

    fozzie-components@v0.5.1 – Remove isSettings env check (#146)

--- packages/f-metadata/babel.config.js
+++ packages/f-metadata/babel.config.js
@@ -1,13 +1,12 @@
 module.exports = api => {
     // Use isTest to determine what presets and plugins to use with jest
     const isTest = api.env('test');
-    const isSettings = api.env('settings');
     const presets = [];
     const plugins = [
         '@babel/plugin-proposal-optional-chaining'
     ];
 
-    if (!isTest && !isSettings) {
+    if (!isTest) {
         api.cache(true);
     }
 
