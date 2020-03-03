c3b3e9efab7c6f443a77bb2dd53f5acbd24300be
Asher Stoppard <asher@asherstoppard.com>
2020-03-03 10:50:21 +0000

    f-metadata@2.3.0: Change callback and return early (#148)
    * f-metadata@2.3.0 - Bump package version and update change log
    
    * f-metadata@2.3.0 - Callback with null and avoid import if deps are unavailable
    
    * f-metadata@2.3.0 - Update formatting
    
    * f-metadata@2.3.0 - Remove return

--- packages/f-metadata/src/index.js
+++ packages/f-metadata/src/index.js
@@ -12,32 +12,33 @@ const initialiseBraze = (options = {}) => new Promise((resolve, reject) => {
     } = options;
     const { handleContentCards = noop } = callbacks;
 
-    if (disableComponent) return resolve(null);
+    if (disableComponent || !apiKey || !userId) {
+        handleContentCards(null);
+        return resolve(null);
+    }
 
     window.dataLayer = window.dataLayer || [];
 
     return import(/* webpackChunkName: "appboy-web-sdk" */ 'appboy-web-sdk')
         .then(({ default: appboy }) => {
-            if (apiKey && apiKey.length && userId && userId.length) {
-                appboy.initialize(apiKey, { enableLogging });
+            appboy.initialize(apiKey, { enableLogging });
 
-                appboy.display.automaticallyShowNewInAppMessages();
+            appboy.display.automaticallyShowNewInAppMessages();
 
-                appboy.openSession();
-                window.appboy = appboy;
+            appboy.openSession();
+            window.appboy = appboy;
 
-                appboy.changeUser(userId, () => {
-                    window.dataLayer.push({
-                        event: 'appboyReady'
-                    });
+            appboy.changeUser(userId, () => {
+                window.dataLayer.push({
+                    event: 'appboyReady'
                 });
+            });
 
-                appboy.requestContentCardsRefresh();
+            appboy.requestContentCardsRefresh();
 
-                appboy.subscribeToContentCardsUpdates(handleContentCards);
+            appboy.subscribeToContentCardsUpdates(handleContentCards);
 
-                resolve(appboy);
-            }
+            resolve(appboy);
         })
         .catch(error => reject(new Error(`An error occurred while loading the component: ${error}`)));
 });
