1b516d05154c12b6f72331e6add7cbb08bc7acdb
Zlatin Ivanov <zlatin.ivanov@just-eat.com>
2020-03-27 10:40:58 +0000

    f-user-message Use of f-vue-icons instead

--- packages/f-user-message/src/components/UserMessage.vue
+++ packages/f-user-message/src/components/UserMessage.vue
@@ -8,13 +8,7 @@
             <div
                 :class="$style['c-userMessageContainer']"
                 class="l-container--inner">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 28 28">
-                    <path
-                        fill-rule="nonzero"
-                        d="M14 3c6.09 0 11 4.99 11 11s-4.99 11-11 11c-6.01.078-11-4.911-11-11C3 7.91 7.871 3 14 3zm2.16 11.785V7.832h-2.867v5.5H7.95v2.867h6.796c.825 0 1.415-.589 1.415-1.414z" />
-                </svg>
+                <clock-icon />
                 <p :class="$style['c-userMessageText']">
                     {{ userMessageText }}
                 </p>
@@ -25,11 +19,15 @@
 
 <script>
 import sharedServices from '@justeat/f-services';
+import { ClockIcon } from '@justeat/f-vue-icons';
 import tenantConfigs from '../tenants';
 import UserMessageApi from '../services/UserMessageApi';
 
 export default {
     name: 'UserMessage',
+    components: {
+        ClockIcon
+    },
     props: {
         locale: {
             type: String,
