a82a8fdc7a458b32f05f83fe13247a27ad494893
Zlatin Ivanov <zlatin.ivanov@just-eat.com>
2020-04-01 09:43:30 +0100

    f-user-message@v0.1.0 - Initial Component implementation (#140)
    * Merge up master
    
    * f-user-message Reworked due to local build issues
    
    * f-user-message Replaces demo prop for proper dev stub
    
    * f-user-message Use axios mock adapter in tests
    
    * f-user-message Updated Changelog and tests
    
    * f-user-message Applyed correct font
    
    * f-user-message Tests update
    
    * f-user-message Removed redundant code
    
    * f-user-message Fix issues with eslint
    
    * f-user-message update and lock dependencies
    
    * Date update as per comment
    
    * f-user-message Update registry reference
    
    * f-user-message Use of f-vue-icons instead
    
    * f-user-message Updated as per comment
    
    * f-user-message Removed unused icon file
    
    * f-user-message Packages update

---
+++ packages/f-user-message/README.md
@@ -0,0 +1,79 @@
+<!-- markdownlint-disable -->
+<div align="center">
+    <h1>f-user-message</h1>
+
+    <img width="125" alt="Fozzie Bear" src="../../bear.png" />
+
+    <p>Global User message Component for Vue.js.</p>
+</div>
+
+---
+
+[![Coverage Status](https://coveralls.io/repos/github/justeat/f-user-message/badge.svg)](https://coveralls.io/github/justeat/f-user-message)
+[![CircleCI](https://circleci.com/gh/justeat/fozzie-components.svg?style=svg&circle-token=4c77c1990b98c8e06e01b497bc80f376346f609d)](https://circleci.com/gh/justeat/workflows/fozzie-components)
+[![Known Vulnerabilities](https://snyk.io/test/github/justeat/f-user-message/badge.svg?targetFile=package.json)](https://snyk.io/test/github/justeat/f-user-message?targetFile=package.json)
+<!-- markdownlint-enable -->
+
+<!-- markdownlint-disable MD002 -->
+## Usage
+<!-- markdownlint-enable -->
+
+1. Install the module using NPM or Yarn:
+
+    ```bash
+    yarn add @justeat/f-user-message
+    # Alternatively if using npm
+    npm install @justeat/f-user-message
+    ```
+
+2. Import the component
+
+    You can import it in your Vue SFC like this (please note that styles have
+    to be imported separately):
+
+    ```JavaScript
+    import UserMessage from '@justeat/f-user-message';
+    import '@justeat/f-user-message/dist/f-user-message.css';
+    export default {
+        components: {
+            UserMessage
+        }
+    }
+    ```
+
+    If you are using Webpack, you can import the component dynamically to
+    separate the user message bundle from the main `bundle.client.js`:
+
+    <!-- markdownlint-disable MD013 -->
+    ```JavaScript
+    import '@justeat/f-user-message/dist/f-user-message.css';
+    export default {
+        components: {
+            ...
+            UserMessage: () => import(/* webpackChunkName: "vue-user-message" */ '@justeat/f-user-message')
+        }
+    }
+    ```
+    <!-- markdownlint-enable MD013 -->
+
+3. Properties which component accepts:
+
+    `locale` - If there is a vue-i18n plugin in the project, the user message
+     component can be called without locale property, otherwise it accepts it 
+     in format like 'en-GB'
+
+## Development
+
+Running below `yarn` commands from the component folder, starts a development
+server displaying a preview example of the component implementation.
+
+```bash
+# cd /packages/f-user-message
+yarn install
+# followed by
+yarn demo
+```
+
+A development server is set to provide a stubbed response for the api call, when running locally. 
+
+See [Demo folder](./src/demo/) for more detail.
---
+++ packages/f-user-message/src/components/tests/UserMessage.test.js
@@ -0,0 +1,212 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import UserMessage from '../UserMessage.vue';
+import userMessageService from '../../services/UserMessageApi';
+
+const localVue = createLocalVue();
+const axiosMock = new MockAdapter(axios);
+const _locale = 'en-GB';
+const propsData = {
+    locale: _locale
+};
+
+let spy;
+let wrapper;
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('UserMessage component', () => {
+    it('has a mounted hook', () => {
+        expect(typeof UserMessage.mounted).toBe('function');
+    });
+
+    it('calls populateUserMessage on mount', async () => {
+        // Arrange
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: true
+            }
+        });
+
+        const mockPopulateUserMessage = jest.fn();
+
+        // Act
+        shallowMount(UserMessage, {
+            localVue,
+            propsData,
+            methods: {
+                populateUserMessage: mockPopulateUserMessage
+            }
+        });
+
+        // Assert
+        expect(mockPopulateUserMessage).toHaveBeenCalledTimes(1);
+    });
+
+    it('calls getUserMessage on mount', async () => {
+        // Arrange
+        spy = jest.spyOn(userMessageService, 'getUserMessage');
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: true
+            }
+        });
+
+        // Act
+        shallowMount(UserMessage, { propsData });
+
+        // Assert
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('should be displayed when', () => {
+    it('user message is enabled and not empty', async () => {
+        // Arrange
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: true
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageEnabled).toBe(true);
+    });
+});
+
+describe('should contain message when', () => {
+    it('user message is enabled and not empty', async () => {
+        // Arrange
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: true
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageText).toEqual('test message');
+    });
+});
+
+describe('should not contain message when', () => {
+    it('user message is present but disabled', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: false
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageText).toEqual('');
+    });
+
+    it('user message is enabled but empty', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: '',
+                userMessageEnabled: true
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageText).toEqual('');
+    });
+
+    it('user message is enabled and empty', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: '',
+                userMessageEnabled: true
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageText).toEqual('');
+    });
+});
+
+describe('should not be displayed when', () => {
+    it('user message is present but disabled', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: false
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageEnabled).toBe(false);
+    });
+
+    it('user message is enabled but empty', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: '',
+                userMessageEnabled: true
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageEnabled).toBe(false);
+    });
+
+    it('user message is enabled and empty', async () => {
+        axiosMock.onGet('/user-message.json').reply(200, {
+            [_locale]: {
+                userMessageText: 'test message',
+                userMessageEnabled: false
+            }
+        });
+
+        wrapper = shallowMount(UserMessage, { propsData });
+
+        // Act
+        await wrapper.vm.populateUserMessage();
+
+        // Assert
+        expect(wrapper.vm.userMessageEnabled).toBe(false);
+    });
+});
