9f7bf8a6c554d7481809cc7dc14f9cda131e8615
Ashley Watson-Nolan <nolly00@gmail.com>
2020-04-14 12:08:17 +0100

    f-registration@v0.1.0 - initial component structure and config (#161)

---
+++ packages/f-registration/src/demo/Index.vue
@@ -0,0 +1,32 @@
+// Demo component for testing `f-registration` independently via `vue serve --open src/demo/Index.vue`
+// Fonts were added for beter comparison with the designs.
+
+<template>
+    <div>
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+        >
+        <registration
+            locale="en-GB"
+        />
+    </div>
+</template>
+
+<script>
+import Registration from '@/components/Registration.vue';
+
+export default {
+    components: { Registration }
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Ubuntu:300,500&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Hind+Vadodara:300,500&display=swap');
+
+
+body {
+    margin: 0;
+}
+</style>
