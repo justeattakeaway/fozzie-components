588a90d19bd61b6efa6c386604baed2d63c7dd99
Ben Siggery <ben.siggery@just-eat.com>
2020-04-21 11:14:22 +0100

    webpack fixes

---
+++ packages/storybook/vue.config.js
@@ -0,0 +1,18 @@
+const magicImporter = require('node-sass-magic-importer');
+
+// vue.config.js
+module.exports = {
+    chainWebpack: config => {
+        // console.log(config.module.rules);
+        config.module
+            .rule('scss-importer')
+            .test(/\.scss$/)
+            .use('importer')
+            .loader('sass-loader')
+            .options({
+                importer: magicImporter(),
+                // eslint-disable-next-line quotes
+                data: `@import "../assets/scss/common.scss";`
+            });
+    }
+};
