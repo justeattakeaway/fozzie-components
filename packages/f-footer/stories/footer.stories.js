e422a1a8c934bfe2deee05c2635657c77705b30f
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2020-04-22 09:33:43 +0100

    storybook@v0.1.0 (#163)
    * vue-cli-storybook test
    
    * vue-cli-storybook test
    
    * Storybook Implementation WIP
    
    * PR suggestions
    
    * PR suggestions and hooked in more props
    
    * File formatting
    
    * webpack fixes
    
    * webpack fixes
    
    * formatting changes
    
    * missed file formatting...
    
    * Fix unit test
    
    * PR suggestions
    
    Co-authored-by: Ben Siggery <ben.siggery@just-eat.com>

---
+++ packages/f-footer/stories/footer.stories.js
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/vue';
+import { select, boolean } from '@storybook/addon-knobs';
+import VueFooter from '../src/components/Footer.vue';
+
+storiesOf('Shared', module)
+  .add('f-footer', () => ({
+    components: { VueFooter },
+    props: {
+      locale: {
+        default: select('Locale', ['en-GB', 'en-AU'])
+      },
+      showCourierLinks: {
+        default: boolean('Show courier links', true)
+      }
+    },
+    template: `<vue-footer :showCourierLinks="showCourierLinks" :locale="locale" />`
+  })
+);
