3defbe95c6ef75dba2805725ee5bc387a34acb36
Ashley Watson-Nolan <nolly00@gmail.com>
2020-04-28 16:45:37 +0100

    f-card@v0.2.0 – Updating component with markup and core functionality (#168)
    * f-card@v0.2.0 – Updating component with markup and core functionality
    
    * Updating test descriptions and README

--- packages/f-card/src/components/tests/Card.test.js
+++ packages/f-card/src/components/tests/Card.test.js
@@ -3,8 +3,121 @@ import Card from '../Card.vue';
 
 describe('Card', () => {
     it('should be defined', () => {
+        // Arrange
         const propsData = {};
+
+        // Act
         const wrapper = shallowMount(Card, { propsData });
+
+        // Assert
         expect(wrapper.exists()).toBe(true);
+    });
+
+    describe('cardTitle', () => {
+        it('should not be visible if it’s not set in props', () => {
+            // Arrange
+            const propsData = {};
+
+            // Act
+            const wrapper = shallowMount(Card, { propsData });
+            const cardTitleElement = wrapper.find('[data-test="card-heading"]');
+
+            // Assert
+            expect(cardTitleElement.exists()).toBe(false);
+        });
+
+        it('should be visible if it’s set in props', () => {
+            // Arrange
+            const propsData = {
+                cardHeading: 'Test card title'
+            };
+
+            // Act
+            const wrapper = shallowMount(Card, { propsData });
+            const cardTitleElement = wrapper.find('[data-test="card-heading"]');
+
+            // Assert
+            expect(cardTitleElement.text()).toBe(propsData.cardHeading);
+        });
+    });
+
+    describe('props', () => {
+        describe('isRounded', () => {
+            it('should default to `false` if it is not set', () => {
+                // Arrange
+                const propsData = {};
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.isRounded).toBe(false);
+            });
+
+            it('should be set to `true` if the `isRounded` prop is passed in as `true`', () => {
+                // Arrange
+                const propsData = {
+                    isRounded: true
+                };
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.isRounded).toBe(true);
+            });
+        });
+
+        describe('hasOutline', () => {
+            it('should default to `false` if it is not set', () => {
+                // Arrange
+                const propsData = {};
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.hasOutline).toBe(false);
+            });
+
+            it('should be set to `true` if the `hasOutline` prop is passed in as `true`', () => {
+                // Arrange
+                const propsData = {
+                    hasOutline: true
+                };
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.hasOutline).toBe(true);
+            });
+        });
+
+        describe('isPageContentWrapper', () => {
+            it('should default to `false` if it is not set', () => {
+                // Arrange
+                const propsData = {};
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.isPageContentWrapper).toBe(false);
+            });
+
+            it('should be set to `true` if the `isPageContentWrapper` prop is passed in as `true`', () => {
+                // Arrange
+                const propsData = {
+                    isPageContentWrapper: true
+                };
+
+                // Act
+                const wrapper = shallowMount(Card, { propsData });
+
+                // Assert
+                expect(wrapper.vm.isPageContentWrapper).toBe(true);
+            });
+        });
     });
 });
