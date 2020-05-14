1c0c08b1e69f31f5ff25ad6310eb873c48519ab1
Adam Ashton <mail@adamashton.com>
2020-05-14 14:56:31 +0100

    f-registration@0.3.0 (#182)
    * Adding data-test-id
    
    * Fixing has a button unit test
    
    Co-authored-by: Adam Ashton <adam.ashton@just-eat.com>

--- packages/f-registration/src/components/tests/Registration.test.js
+++ packages/f-registration/src/components/tests/Registration.test.js
@@ -8,6 +8,13 @@ describe('Registration', () => {
         expect(wrapper.exists()).toBe(true);
     });
 
+    it('has a button', () => {
+        const propsData = {};
+        const wrapper = shallowMount(Registration, { propsData });
+        const button = wrapper.find("[data-test-id='create-account-submit-button']");
+        expect(button.exists()).toBe(true);
+    });
+
     describe(': props :', () => {
         it('if `value` is specified, should assign the input field a value attribute', () => {
             const propsData = {};
