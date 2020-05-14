0f4ad8e9269fbbe8121e0fd880d15ed98f394940
Adam Ashton <adam.ashton@just-eat.com>
2020-05-14 11:05:58 +0100

    Adding data-test-id

--- packages/f-registration/src/components/tests/Registration.test.js
+++ packages/f-registration/src/components/tests/Registration.test.js
@@ -8,6 +8,13 @@ describe('Registration', () => {
         expect(wrapper.exists()).toBe(true);
     });
 
+    it('has a button', () => {
+        const propsData = {};
+        const wrapper = shallowMount(Registration, { propsData });
+        const button = wrapper.find('button[data-test-id="create-account-submit-button"]')
+        expect(button.exists());
+    })
+
     describe(': props :', () => {
         it('if `value` is specified, should assign the input field a value attribute', () => {
             const propsData = {};
