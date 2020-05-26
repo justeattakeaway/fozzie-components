915c507d3690fa5e8425cb6b1569ff98283300b2
ashleynolan <nolly00@gmail.com>
2020-05-26 11:20:10 +0100

    f-form-field@v0.4.0 – emits value and allows for passed-in error messages

--- packages/f-registration/CHANGELOG.md
+++ packages/f-registration/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+Latest (to be added to next release)
+------------------------------
+*May 26, 2020*
+
+### Changed
+- Stubbed `.css` file module imports as was causing build error.
+
+
 v0.4.0
 ------------------------------
 *May 18, 2020*
--- packages/f-registration/jest.config.js
+++ packages/f-registration/jest.config.js
@@ -15,9 +15,22 @@ module.exports = {
         'node_modules/(?!(lodash-es)/)'
     ],
 
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^~include-media/(.*)$': '<rootDir>../../node_modules/include-media/$1',
+        '^~@justeat/(.*)$': '<rootDir>../../node_modules/@justeat/$1',
+        '\\.(css|scss)$': 'jest-transform-stub'
+    },
+
     snapshotSerializers: [
         'jest-serializer-vue'
     ],
 
-    testURL: 'http://localhost/'
+    testURL: 'http://localhost/',
+
+    globals: {
+        'vue-jest': {
+            hideStyleWarn: true // We hide style warnings given the first time we run the tests it complains about some styles. The second time the tests are run, the warning disappears. https://github.com/vuejs/vue-jest/issues/178#issuecomment-529175129
+        }
+    }
 };
