662c031e63b7ad7a7f1354e2185ff2674c6372bb
Adam Ashton <adam.ashton@just-eat.com>
2020-06-03 12:59:18 +0100

    Updating dev dependencies to remove auto minor patch updates Using new babel config for unit tests

--- packages/f-registration/src/components/Registration.vue
+++ packages/f-registration/src/components/Registration.vue
@@ -133,7 +133,7 @@ export default {
             type: String,
             default: ''
         },
-        tenant: {
+        tenant: { // TODO Compute tenant value from local config instead of using this prop
             type: String,
             required: true
         },
