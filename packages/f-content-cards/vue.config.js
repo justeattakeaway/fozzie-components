005da49068525931cb0069eeb55cc84b63b075ae
Asher Stoppard <asher@asherstoppard.com>
2020-06-09 14:59:52 +0100

    f-content-cards@0.1.0 (#197)
    * f-braze-content-cards@0.1.0 - Add initial configuration and placeholder component
    
    * f-braze-content-cards@0.1.0 - Update changelog
    
    * f-braze-content-cards@0.1.0 - Remove eslint ignore and change quotation marks
    
    * f-braze-content-cards@0.1.0 - Clean up installation scripts
    
    * f-braze-content-cards@0.1.0 - Add core js and utilise globalThis
    
    * f-braze-content-cards@0.1.0 - Add global key
    
    * f-braze-content-cards@0.1.0 - Address PR observations
    
    * f-braze-content-cards@0.1.0 - Change package name in docs
    
    * f-braze-content-cards@0.1.0 - Change package name in badges
    
    * f-braze-content-cards@0.1.0 - Update storybook configuration
    
    * f-braze-content-cards@0.1.0 - Clean up optional chaining
    
    * f-braze-content-cards@0.1.0 - Fix typo
    
    * f-braze-content-cards@0.1.0 - Fix typo v2

---
+++ packages/f-content-cards/vue.config.js
@@ -0,0 +1,16 @@
+const magicImporter = require('node-sass-magic-importer');
+
+// vue.config.js
+module.exports = {
+    chainWebpack: config => {
+        config.module
+            .rule('scss-importer')
+            .test(/\.scss$/)
+            .use('importer')
+            .loader('sass-loader')
+            .options({
+                importer: magicImporter(),
+                data: '@import "@/assets/scss/common.scss";'
+            });
+    }
+};
