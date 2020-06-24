f89e4d396bd0b981f5a463e8b43bd6b4cf9d0e2f
Asher Stoppard <asher@asherstoppard.com>
2020-06-24 13:16:00 +0100

    f-metadata@2.7.0 - Change promises to async/await (#210)
    * f-metadata@2.6.1 - Change promises to async/await
    
    * f-metadata@2.6.1 - Bump package version and add changelog entry
    
    * f-metadata@2.6.1 - Add test to check for return value
    
    * f-metadata@2.6.1 - Change isAppboyInitialised to a Promise
    
    * f-metadata@2.7.0 - Bump minor version and update changelog

--- packages/f-metadata/src/utils/isAppboyInitialised.js
+++ packages/f-metadata/src/utils/isAppboyInitialised.js
@@ -2,16 +2,11 @@
  * Checks if a previous version of appboy has been initialised
  * @param appboy - Appboy SDK instance
  * @param {function} appboy.getUser - Appboy SDK getUser callback
- * @returns {boolean} - has appboy been initialised
+ * @returns {promise<boolean>} - has appboy been initialised
  */
-const isAppboyInitialised = appboy => {
-    let result = false;
-    if (!appboy) return result;
-
-    appboy.getUser().getUserId(id => {
-        result = !!id;
-    });
-    return result;
-};
+const isAppboyInitialised = appboy => new Promise(resolve => {
+    if (!appboy) return resolve(false);
+    return appboy.getUser().getUserId(id => resolve(!!id));
+});
 
 export default isAppboyInitialised;
