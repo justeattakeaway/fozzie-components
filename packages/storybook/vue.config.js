659f04788b0cb2316b25f5a24d0c93222eb1857f
Benjamin R. White <ben.white@just-eat.com>
2020-07-09 14:48:23 +0100

    storybook@0.5.0 🐻 Use fozzie styles in storybook

--- packages/storybook/vue.config.js
+++ packages/storybook/vue.config.js
@@ -1,12 +1,28 @@
 const magicImporter = require('node-sass-magic-importer');
 const path = require('path');
+const eyeglass = require('eyeglass');
+
+const rootDir = path.join(__dirname, '..', '..');
+
+const sassOptions = eyeglass({
+    root: rootDir,
+    includePaths: ['node_modules/'],
+    sourceMap: true
+});
+
+sassOptions.importer = [
+    magicImporter({
+        cwd: rootDir
+    }),
+    sassOptions.importer
+];
 
 // vue.config.js
 module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                importer: magicImporter(),
+                ...sassOptions,
                 /**
                  * Requires sass-loader 7.3.1 - works out the relative path for the common.scss file for each component
                  *
@@ -21,7 +37,7 @@ module.exports = {
                     );
                     const relPath = path.relative(path.dirname(resourcePath), absPath)
                         .replace(new RegExp(path.sep.replace('\\', '\\\\'), 'g'), '/');
-                    return `@import "${relPath}";`;
+                    return `@import "@justeat/fozzie/src/scss/fozzie"; @import "${relPath}";`;
                 }
             }
         },
