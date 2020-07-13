753a22bbc49b71e758dd08b37f61d549a1c9b077
Ben Siggery <bensiggery28@gmail.com>
2020-07-13 22:18:12 +0100

    Update version number

--- packages/f-registration/stories/Registration.stories.js
+++ packages/f-registration/stories/Registration.stories.js
@@ -3,29 +3,29 @@ import Registration from '../src/components/Registration.vue';
 import { withA11y } from '@storybook/addon-a11y';
 
 export default {
-    title: 'Components/Organisms',
-    decorators: [withKnobs, withA11y]
+title: 'Components/Organisms',
+decorators: [withKnobs, withA11y]
 };
 
- export const RegistrationComponent = () => ({
-     components: { Registration },
-     props: {
-         locale: {
-             default: select('Locale', ['en-GB', 'en-AU'])
-         },
-         title: {
-             default: text('Title', 'Create Account')
-         },
-         buttonText: {
-             default: text('Button Text', 'Create Account')
-         }
-     },
-     parameters: {
-         notes: 'some documentation here'
-     },
-     template: '<registration :locale="locale" :title="title" :button-text="buttonText" />'
- });
+export const RegistrationComponent = () => ({
+    components: { Registration },
+    props: {
+        locale: {
+            default: select('Locale', ['en-GB', 'en-AU'])
+        },
+        title: {
+            default: text('Title', 'Create Account')
+        },
+        buttonText: {
+            default: text('Button Text', 'Create Account')
+        }
+    },
+    parameters: {
+        notes: 'some documentation here'
+    },
+    template: '<registration :locale="locale" :title="title" :button-text="buttonText" />'
+});
 
- RegistrationComponent.story = {
-     name: 'f-registration'
- };
+RegistrationComponent.story = {
+    name: 'f-registration'
+};
