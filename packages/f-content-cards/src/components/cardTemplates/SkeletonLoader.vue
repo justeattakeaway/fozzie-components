8171b4f81c27bb61e14df3edf5cbef72144b9277
Asher Stoppard <asher@asherstoppard.com>
2020-07-16 09:22:20 +0100

    f-content-cards@0.11.0 - Address PR observations

--- packages/f-content-cards/src/components/cardTemplates/SkeletonLoader.vue
+++ packages/f-content-cards/src/components/cardTemplates/SkeletonLoader.vue
@@ -1,14 +1,12 @@
 <template>
-    <div>
-        <div class="offers-preloading-wrap">
-            <template v-for="(skeletons, index) in count">
-                <div
-                    :key="index"
-                    class="offers-preloading"
-                    data-test-id="contentCard-skeletonLoader"
-                />
-            </template>
-        </div>
+    <div class="offers-preloading-wrap">
+        <template v-for="(skeletons, index) in count">
+            <div
+                :key="index"
+                class="offers-preloading"
+                data-test-id="contentCard-skeletonLoader"
+            />
+        </template>
     </div>
 </template>
 
@@ -23,3 +21,88 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+    .offers-preloading-wrap {
+        margin-top: 40px;
+
+        @include media('>=narrowMid') {
+            display: flex;
+            flex-flow: wrap;
+            flex-direction: row;
+        }
+    }
+    .offers-preloading {
+        border-radius: $contentCardRadius;
+        display: flex;
+        flex-direction: column;
+        flex: 0 0 40%;
+        margin: 0 spacing() spacing(x3) 0;
+        width: 100%;
+        padding: spacing(x3) spacing(x2);
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+
+        @include media('>=narrowMid') {
+            flex-direction: row;
+            max-width: 370px;
+            flex: 0 0 40%;
+        }
+    }
+
+    /**
+     * 1. Magic number to match card height
+     * 2. Using radial and linear gradients to create greyed out placeholder elements in preloading component
+     */
+    .offers-preloading:empty {
+        height: 403px; // 1
+        background-color: $white;
+
+        // 2
+        background-image: radial-gradient(0 at 0 0, lightgray 99%, transparent 0),
+                          linear-gradient(100deg, rgba(255, 255, 255, 0) 0%,
+                                                  rgba(255, 255, 255, 0) 30%,
+                                                  rgba(255, 255, 255, 0.5) 50%,
+                                                  rgba(255, 255, 255, 0) 70%), // shine
+                          linear-gradient(to left, gray 23px, transparent 0), // restaurant thumbnail placeholder left
+                          linear-gradient(to right, gray 23px, transparent 0), // restaurant thumbnail placeholder right
+                          linear-gradient(lightgray 170px, transparent 0), // restaurant hero image placeholder
+                          linear-gradient(lightgray 20px, transparent 0), // line one
+                          linear-gradient(lightgray 20px, transparent 0), // line two
+                          linear-gradient(lightgray 20px, transparent 0); // line three
+
+        background-repeat: no-repeat;
+
+        background-size: 100px 457px,
+        100px 657px,
+        50% 46px,
+        50% 46px,
+        100% 170px,
+        80% 20px,
+        40% 20px,
+        65% 20px;
+
+        background-position: 0 0,
+        -100px -100px,
+        0 147px,
+        100% 147px,
+        0 0,
+        20px 230px,
+        20px 260px,
+        20px 300px;
+
+        animation: shine 1.5s linear 1s infinite;
+    }
+
+    @keyframes shine {
+        33%, to {
+            background-position: 0 0,
+            140% -100px,
+            0 147px,
+            100% 147px,
+            0 0,
+            20px 230px,
+            20px 260px,
+            20px 300px;
+        }
+    }
+</style>
