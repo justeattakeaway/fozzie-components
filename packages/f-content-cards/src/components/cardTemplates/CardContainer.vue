fbadbe23263de442a62f7a42b6af6c3a71b5e936
Asher Stoppard <asher@asherstoppard.com>
2020-07-16 16:34:23 +0100

    f-content-cards@0.14.0 - Open external links in a new tab

--- packages/f-content-cards/src/components/cardTemplates/CardContainer.vue
+++ packages/f-content-cards/src/components/cardTemplates/CardContainer.vue
@@ -2,6 +2,8 @@
     <component
         :is="ctaUrl && ctaEnabled ? 'a' : 'div'"
         :href="ctaEnabled && ctaUrl"
+        :target="target.attribute"
+        :rel="target.rel"
         :class="['c-contentCard', { 'c-contentCard--isolateHeroImage': isAnniversaryCard }]"
         :data-test-id="testId"
         @click="onClickContentCard"
@@ -117,6 +119,25 @@ export default {
 
         isBackgroundImage () {
             return this.type !== 'Post_Order_Card_1';
+        },
+
+        target () {
+            try {
+                const url = new URL(this.ctaUrl);
+                const internalDomains = ['just-eat', 'justeat', 'menulog'];
+                const openInNewWindow = !internalDomains.some(partial => url.hostname.indexOf(partial) > -1);
+
+                return openInNewWindow ? {
+                    attribute: '_blank',
+                    rel: 'noopener noreferrer'
+                } : {
+                    attribute: '_self'
+                };
+            } catch (err) {
+                return {
+                    attribute: '_self'
+                };
+            }
         }
     },
 
