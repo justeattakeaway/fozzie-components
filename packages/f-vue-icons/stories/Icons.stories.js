f1a36859e93386f2dd50c7ed03d97d9990da0a83
Benjamin R. White <ben.white@just-eat.com>
2020-07-23 10:02:36 +0100

    f-vue-icons@1.2.0 🎨 Fixes styles and changes for PR review

--- packages/f-vue-icons/stories/Icons.stories.js
+++ packages/f-vue-icons/stories/Icons.stories.js
@@ -1,5 +1,7 @@
 import { withA11y } from '@storybook/addon-a11y';
+
 import * as IconsComponents from '../dist/f-vue-icons.es';
+import './Icons.scss';
 
 const icons = {};
 
@@ -8,31 +10,21 @@ Object.keys(IconsComponents).forEach(icon => {
 });
 
 export default {
-    title: 'Components/Atoms/Icons',
+    title: 'Components/Atoms',
     decorators: [withA11y]
 };
 
 export const Icons = () => ({
     components: icons,
-    template: `<div style="
-  display: grid;
-  grid-template-columns: 50% 50%;
-  justify-items: stretch;
-">
+    template: `<div class="s-icons">
         <template v-for="(component, componentName) in icons">
-            <div style="border-bottom: solid 1px lightgrey; min-height: 32px;"><span>{{ componentName }}</span></div>
-            <div style="border-bottom: solid 1px lightgrey; min-height: 32px;"><component :is="component" style="max-width: 50%; max-height: 32px" /></div>
+            <div class="s-icons-cell"><span>{{ componentName }}</span></div>
+            <div class="s-icons-cell"><component :is="component" class="s-icons-icon" /></div>
         </template>
     </div>`,
     data: () => ({
         icons
-    }),
-    methods: {
-        log: item => {
-            console.log(item);
-            return 'IconsLogged';
-        }
-    }
+    })
 });
 
 Icons.story = {
