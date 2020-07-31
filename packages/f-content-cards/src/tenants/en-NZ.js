e5a55cd5eccc61945f326814af8d468f776988f9
Benjamin White <ben.white@just-eat.com>
2020-07-31 12:03:01 +0100

    f-content-cards@1.5.0 🎟 Imports voucher/anniversary cards from homeweb (#262)
    🎫 Adds VoucherCard i18n config
    📖 Adds Card stories and categorization
    📋 Adds VoucherCard and copy handler tests
    🔂 Dedups the Post-Order card and rearranges stories
    🔻 Adds vue as devDependency

---
+++ packages/f-content-cards/src/tenants/en-NZ.js
@@ -0,0 +1,4 @@
+export default {
+    locale: 'en-NZ',
+    copyCodeLabel: 'Copy Code'
+};
--- packages/f-content-cards/stories/PostOrderCard.stories.js
+++ packages/f-content-cards/stories/PostOrderCard.stories.js
@@ -1,13 +1,15 @@
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 import PostOrderCard from '../src/components/cardTemplates/PostOrderCard.vue';
+import jeIcon from './images/je-icon.png';
+import jeBackground from './images/je-marketing.png';
 
 export default {
-    title: 'Components/Atoms',
+    title: 'Components/Atoms/f-content-cards',
     decorators: [withKnobs, withA11y]
 };
 
-export const ContentCardscomponent = () => ({
+export const PostOrderCardcomponent = () => ({
     components: { PostOrderCard },
     props: {
         title: {
@@ -20,10 +22,10 @@ export const ContentCardscomponent = () => ({
             default: text('Card Description', 'Whether you want to treat Mum to her Friday night favourite, or surprise your mate with a ‘KFC on me’, show them you care – the tasty way.')
         },
         image: {
-            default: text('Card Image', 'https://appboy-images.com/appboy/communication/marketing/content_cards_message_variations/images/5edf97b6141af454f8197e93/f43c54c99f2deca37600fa16331d7080c51717ef/original.png?1591711674')
+            default: text('Card Image', jeBackground)
         },
         icon: {
-            default: text('Card Icon', 'https://appboy-images.com/appboy/communication/assets/image_assets/images/5ed7aac3967e180c25132d24/original.png?1591192259')
+            default: text('Card Icon', jeIcon)
         },
         cta: {
             default: text('Card CTA', 'Purchase now')
@@ -69,6 +71,6 @@ export const ContentCardscomponent = () => ({
     template: '<post-order-card :card="card" :title="title" />'
 });
 
-ContentCardscomponent.story = {
-    name: 'f-post-order-card'
+PostOrderCardcomponent.story = {
+    name: 'post-order-card'
 };
