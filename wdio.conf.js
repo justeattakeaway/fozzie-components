734648ff40e9ff90587c8e4c092f20cd1400dce1
Ben Siggery <bensiggery28@gmail.com>
2020-08-16 13:28:55 +0100

    add baseurl and yarn lock

--- wdio.conf.js
+++ wdio.conf.js
@@ -86,6 +86,12 @@ exports.config = {
     // bail (default is 0 - don't bail, run all tests).
     bail: 0,
     //
+    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
+    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
+    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
+    // gets prepended directly.
+    baseUrl: 'http://localhost:8080',
+    //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
     //
