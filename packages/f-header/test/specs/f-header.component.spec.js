a1bf8a4df4ab12dd7c8ccbca53d7e6f77b8cd233
Ben Siggery <ben.siggery@just-eat.com>
2020-09-01 14:12:28 +0100

    Add assert

--- packages/f-header/test/specs/f-header.component.spec.js
+++ packages/f-header/test/specs/f-header.component.spec.js
@@ -1,10 +1,11 @@
+import assert from 'assert';
 import HeaderComponent from '../../test-utils/component-objects/f-header.component';
 
 describe('f-header component tests', () => {
     it('should display the f-header component', () => {
         browser.url('http://localhost:8080');
 
-        HeaderComponent.isLogoDisplayed();
+        assert.ok(HeaderComponent.isLogoDisplayed());
     });
 });
 
