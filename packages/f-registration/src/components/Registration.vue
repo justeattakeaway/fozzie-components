2f25a8471d7e210a8700391b99591e516031430d
Adam Ashton <mail@adamashton.com>
2020-09-04 13:39:53 +0100

    f-registration@0.18.0 Adding legal text and relevant links (#298)
    * Adding legal text and relevant links
    
    * Adding some usability tests
    
    * Adding target blank
    
    Co-authored-by: Adam Ashton <adam.ashton@just-eat.com>

--- packages/f-registration/src/components/Registration.vue
+++ packages/f-registration/src/components/Registration.vue
@@ -148,6 +148,23 @@
                 {{ buttonText }}
             </form-button>
         </form>
+        <p :class="$style['c-legal-hyperlinks']">
+            {{ copy.navLinks.termsAndConditions.prefix }}
+            <a
+                data-test-id="ts-and-cs-link"
+                :href="copy.navLinks.termsAndConditions.url"
+                target="_blank">{{ copy.navLinks.termsAndConditions.text }}</a>{{ copy.navLinks.termsAndConditions.suffix }}
+            {{ copy.navLinks.privacyPolicy.prefix }}
+            <a
+                data-test-id="privacy-policy-link"
+                :href="copy.navLinks.privacyPolicy.url"
+                target="_blank">{{ copy.navLinks.privacyPolicy.text }}</a>
+            {{ copy.navLinks.cookiesPolicy.prefix }}
+            <a
+                data-test-id="cookies-policy-link"
+                :href="copy.navLinks.cookiesPolicy.url"
+                target="_blank">{{ copy.navLinks.cookiesPolicy.text }}</a>{{ copy.navLinks.cookiesPolicy.suffix }}
+        </p>
     </card>
 </template>
 
@@ -373,6 +390,13 @@ export default {
 
 .c-loginLink {
     text-align: center;
+    a {
+        color: $blue;
+        text-decoration: none;
+    }
+}
+
+.c-legal-hyperlinks {
     a {
         color: $blue;
         text-decoration: none;
