c5878812a32d333d215cbcf294374a07688463a8
Ben Siggery <ben.siggery@just-eat.com>
2020-09-08 18:39:52 +0100

    Write integration tests

---
+++ packages/f-registration/test/specs/integration/f-registration.integration.spec.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import RegistrationServiceApi from '../../../src/services/RegistrationServiceApi';
+
+const axiosMock = new MockAdapter(axios);
+
+const tenant = 'uk';
+const endpoint = `consumers/${tenant}`
+const CONSUMERS_REQUEST_DATA =
+    {
+        "emailAddress": "test-user@justeat.com",
+        "firstName": "Test",
+        "lastName": "User",
+        "password": "test123"
+    };
+
+describe('Registration API service', () => {
+
+    it('responds with 201 when request is made with valid details', async () => {
+
+        // Arrange
+        axiosMock.onPost(endpoint, {
+
+            firstName: CONSUMERS_REQUEST_DATA.firstName,
+            lastName: CONSUMERS_REQUEST_DATA.lastName,
+            emailAddress: CONSUMERS_REQUEST_DATA.emailAddress,
+            password: CONSUMERS_REQUEST_DATA.password
+
+        }).reply(201);
+
+        await RegistrationServiceApi.createAccount(endpoint, tenant, CONSUMERS_REQUEST_DATA)
+        .then(response => {
+            expect(response.status).toEqual(201);
+        })
+    });
+
+    it('responds with 409 when request is made with valid details', async () => {
+
+        // Arrange
+        axiosMock.onPost(endpoint, {
+
+            firstName: CONSUMERS_REQUEST_DATA.firstName,
+            lastName: CONSUMERS_REQUEST_DATA.lastName,
+            emailAddress: CONSUMERS_REQUEST_DATA.emailAddress,
+            password: CONSUMERS_REQUEST_DATA.password
+
+        }).reply(409, {
+
+            faultId: "e2ea5f11-f771-487a-9f80-5c6f0981890b",
+            traceId: "80000806-0000-fd00-b63f-84710c7967bb",
+            errors: [{
+              description:"The specified email already exists",
+              errorCode:"409"
+            }]
+
+        });
+
+        await RegistrationServiceApi.createAccount(endpoint, tenant, CONSUMERS_REQUEST_DATA)
+        .then()
+        .catch(error => {
+
+            expect(error.response.status).toBe(409);
+            expect(error.response.data.errors.length).toBe(1);
+            expect(error.response.data.errors[0].description).toBe('The specified email already exists');
+
+        })
+    });
+
+});
