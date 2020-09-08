9285ed73b20ff5972b23ed5879b846fc77fa085e
Ben Siggery <ben.siggery@just-eat.com>
2020-09-08 13:27:46 +0100

    409 conflict pact test

--- packages/f-registration/test/specs/consumer/emailInUse.spec.js
+++ packages/f-registration/test/specs/consumer/emailInUse.spec.js
@@ -25,7 +25,7 @@ describe("Pact setup", () => {
   beforeAll(async () => {
     await provider.setup().then(() => {
          provider.addInteraction({
-            state: 'i have a new customer email address for a tenant',
+            state: 'i have an existing customer email address for a tenant',
             uponReceiving: 'a POST request to create a consumer',
             withRequest: {
                 method: 'POST',
@@ -42,20 +42,34 @@ describe("Pact setup", () => {
                 }
             },
             willRespondWith: {
-                status: 201,
+                status: 409,
                 headers: {
                     "date": Matchers.like("Fri, 04 Oct 2019 11:59:09 GMT"),
                     "Content-Type": "application/json; charset=utf-8"
+                },
+                body: {
+                  faultId: Matchers.like("e2ea5f11-f771-487a-9f80-5c6f0981890b"),
+                  traceId: Matchers.like("80000806-0000-fd00-b63f-84710c7967bb"),
+                  errors: [{
+                    description:"The specified email already exists",
+                    errorCode:"409"
+                  }]
                 }
             },
           });
     });
   });
-  test("successfully creates a consumer", async () => {
+
+  test("consumer email is already in use", async () => {
 
     await RegistrationServiceApi.createAccount(`${provider.mockService.baseUrl}/consumers/uk`, 'uk', CONSUMERS_REQUEST_DATA)
-    .then(repsonse => {
-      expect(repsonse.status).toBe(201);
+    .then(() => { 
+      fail()
+    })
+    .catch(error => {
+      expect(error.response.status).toBe(409)
+      expect(error.response.data.errors.length).toBe(1);
+      expect(error.response.data.errors[0].description).toBe('The specified email already exists');
     })
   })
   afterEach(async () => {
