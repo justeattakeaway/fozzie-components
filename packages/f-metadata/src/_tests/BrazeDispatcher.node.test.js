e3d7f3abf2b3396118d05d148a7667ae3a545ced
Benjamin White <ben.white@just-eat.com>
2020-09-15 17:12:32 +0100

    f-metadata@3.0.0-beta.1 〰 Begins decoupling of f-content-cards (#312)
    🚢 Ports contentcard service to f-metadata
    📦 Adds relevant packages and jest-config
    💔 Implements decoupling
    🌇 Removes noop
    📜 Documents BrazeDispatcher.js and updates README

--- packages/f-metadata/src/_tests/BrazeDispatcher.node.test.js
+++ packages/f-metadata/src/_tests/BrazeDispatcher.node.test.js
@@ -2,22 +2,31 @@
  * @jest-environment node
  */
 import appboy from 'appboy-web-sdk';
-import { initialise } from '../index';
+import GetDispatcher from '../BrazeDispatcher';
 
 jest.mock('appboy-web-sdk', () => ({
     initialize: jest.fn()
 }));
+jest.mock('../utils/isAppboyInitialised');
 
-describe('f-metadata › node', () => {
+const apiKey = '__API_KEY__';
+const userId = '__USER_ID__';
+
+describe('BrazeDispatcher › node', () => {
     beforeEach(() => {
         jest.resetAllMocks();
     });
 
     it('should not call initialise if window is undefined', async () => {
-        // Arrange & Act
+        // Arrange
         expect.assertions(2);
+
         try {
-            await initialise();
+            const Dispatcher = GetDispatcher(0);
+            await Dispatcher.configure({
+                userId,
+                apiKey
+            });
         } catch (error) {
             // Assert
             expect(appboy.initialize).not.toHaveBeenCalled();
