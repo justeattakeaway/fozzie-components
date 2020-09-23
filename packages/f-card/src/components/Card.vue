242bae3339259b86410db67608f5182f33a13cb2
Thomas William McClean <thomas.mcclean@just-eat.com>
2020-09-23 09:46:26 +0100

    v0.6.0 - Use suggested syntax

--- packages/f-card/src/components/Card.vue
+++ packages/f-card/src/components/Card.vue
@@ -107,7 +107,7 @@ $card--pageContentWrapper-width           : 460px;
         transition: 250ms padding ease-in-out;
         padding-left: 6%;
         padding-right: 6%;
-        margin: spacing() * 5 0;
+        margin: spacing(x5) 0;
 
         @include media('>=narrow') {
             padding-left: 10%;
@@ -116,9 +116,9 @@ $card--pageContentWrapper-width           : 460px;
 
         @include media('>=#{$card--pageContentWrapper-width}') {
             width: $card--pageContentWrapper-width;
-            margin: spacing() * 5 auto;
-            padding-left: spacing() * 10;
-            padding-right: spacing() * 10;
+            margin: spacing(x5) auto;
+            padding-left: spacing(x10);
+            padding-right: spacing(x10);
         }
     }
 
