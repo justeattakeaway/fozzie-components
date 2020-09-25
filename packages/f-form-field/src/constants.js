58e3df62087d3c897ed6741572dc5c2fc2affb7c
Faye Carter <faye.carter@just-eat.com>
2020-09-25 10:56:56 +0100

    WCB-817 - Add 'inlineNarrow' to VALID_LABEL_STYLES constant

--- packages/f-form-field/src/constants.js
+++ packages/f-form-field/src/constants.js
@@ -1,4 +1,4 @@
 export const VALID_INPUT_TYPES = ['text', 'email', 'password', 'radio', 'checkbox'];
 export const DEFAULT_INPUT_TYPE = 'text';
 
-export const VALID_LABEL_STYLES = ['default', 'inline'];
+export const VALID_LABEL_STYLES = ['default', 'inline', 'inlineNarrow'];
