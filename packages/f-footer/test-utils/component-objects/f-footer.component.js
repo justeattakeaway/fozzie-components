fc861a5df3c5e829a102e7af325e13f59a0823a0
Lizzie Turney <lizzie.turney@just-eat.com>
2020-09-28 10:25:40 +0100

    WCB-1182 Added footer-test component

---
+++ packages/f-footer/test-utils/component-objects/f-footer.component.js
@@ -0,0 +1,6 @@
+// const footerIcons = () => $$('[data-test-id="footerIcon"]');
+// const iosIcon = () => footerIcons[0]
+// const androidIcon = () => footerIcons[1]
+// const facebookIcon = () => footerIcons[2]
+// const twitterIcon = () => footerIcons[3]
+// const instagramIcon = () => footerIcons[4]
---
+++ packages/f-footer/test/specs/component/f-footer.component.spec.js
@@ -0,0 +1,15 @@
+// import FooterComponent from '../../../test-utils/component-objects/f-footer.component';
+
+// describe('f-header component tests', () => {
+//     beforeEach(() => {
+//         // Arrange
+//         browser.url('http://localhost:8080');
+//     })
+
+//     it('should display the f-footer component', () => {
+//         //Assert
+//         expect(FooterComponent.isLogoDisplayed()).toBe(true);
+//     })
+
+    
+// });
