72fcb4a92594712f9848ddb7c03f55a2bcd24f7c
Faye Carter <faye.carter@just-eat.com>
2020-09-29 16:40:51 +0100

    v0.7.1 - Move timeout variable out of return statment

--- packages/f-form-field/src/services/debounce.js
+++ packages/f-form-field/src/services/debounce.js
@@ -1,6 +1,7 @@
 export default function debounce (fn, delay, ...args) {
+    let timeoutId = null;
+
     return () => {
-        let timeoutId = null;
         clearTimeout(timeoutId);
 
         timeoutId = setTimeout(() => {
