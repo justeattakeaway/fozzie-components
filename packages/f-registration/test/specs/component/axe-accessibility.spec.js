8ea04bab457c821037c2f0ba48f26ec3b53222f9
Adam Ashton <adam.ashton@just-eat.com>
2020-09-30 14:35:11 +0100

    Updating CHANGELOGs

--- packages/f-registration/test/specs/component/axe-accessibility.spec.js
+++ packages/f-registration/test/specs/component/axe-accessibility.spec.js
@@ -1,13 +1,35 @@
 import AxeReports from 'axe-reports';
 import { getAccessibilityTestResults } from '../../../../../test/utils/axe-helper.js';
+import RegistrationComponent from '../../../test-utils/component-objects/f-registration.component';
 
-describe('Axe accessibility tests', () => {
+describe('Accessibility tests', () => {
     beforeEach(() => {
         // Arrange
         browser.url('http://localhost:8081');
     });
 
-    it('should test f-registration component WCAG compliance', () => {
+    it('a11y - should test f-registration component WCAG compliance', () => {
+        // Act
+        const axeResults = getAccessibilityTestResults();
+
+        // Assert
+        if (axeResults.violations.length > 0) {
+            AxeReports.processResults(axeResults, 'csv', 'registration-violations');
+            console.error(`Expected no accessibility violations. Found: ${axeResults.violations.length}`);
+        }
+        expect(axeResults.violations.length).toBe(0);
+    });
+
+    it('a11y - should test f-registration component error messages are WCAG compliance', () => {
+        // Arrange
+        const userInfo = {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    password: ''
+                };
+        RegistrationComponent.submitRegistrationForm(userInfo);
+
         // Act
         const axeResults = getAccessibilityTestResults();
 
