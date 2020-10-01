ace971617435bffd34461822703559186d7debc7
Damian Mullins <89059+DamianMullins@users.noreply.github.com>
2020-10-01 13:52:59 +0100

    Revert "Hack 1 pie (#347)" (#349)
    This reverts commit 2afb6dc8be4b7f329832c6b3826d045df552c5b6.
    
    Co-authored-by: gurps321-je <36697833+gurps321-je@users.noreply.github.com>

--- packages/storybook/config/storybook/main.js
+++ packages/storybook/config/storybook/main.js
@@ -1,14 +1,13 @@
 module.exports = {
     stories: [
         '../../../**/*.stories.@(js|mdx)',
-        '../../../../stories/**/*.stories.@(js|mdx)'
+        '../../../../stories/*.stories.@(js|mdx)'
     ],
     addons: [
         '@storybook/addon-essentials',
         '@storybook/addon-knobs',
         '@storybook/addon-links',
         '@storybook/addon-a11y',
-        '@storybook/addon-controls',
-        'storybook-addons-abstract'
+        '@storybook/addon-controls'
     ]
 };
--- packages/storybook/config/storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import jetTheme from './jetTheme';
-
-addons.setConfig({
-    theme: jetTheme
-});
