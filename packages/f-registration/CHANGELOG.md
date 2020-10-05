65b05b2765c682ddffe96acd7cf63b492c3013ab
Adam Ashton <mail@adamashton.com>
2020-10-05 15:12:00 +0100

    f-registration@0.31.0 Adding accessibility tests (#345)
    * Adding accessibility tests
    
    * Moving test helper into root component
    
    * Add axe-reports
    
    * create CSV file in CI before processing results
    
    * Delete f-registration-a11y-violations.csv
    
    Co-authored-by: Adam Ashton <adam.ashton@just-eat.com>
    Co-authored-by: Ben Siggery <ben.siggery@just-eat.com>
    Co-authored-by: Ben Siggery <14013357+siggerzz@users.noreply.github.com>

--- packages/f-registration/CHANGELOG.md
+++ packages/f-registration/CHANGELOG.md
@@ -3,6 +3,14 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+v0.31.0
+------------------------------
+*September 30, 2020*
+
+### Added
+- Accessibility tests that run as part of the pipeline
+
+
 v0.30.0
 ------------------------------
 *September 29, 2020*
---
+++ test/utils/axe-helper.js
@@ -0,0 +1,69 @@
+const { source } = require('axe-core');
+const AxeReports = require('axe-reports');
+const { exec } = require('child_process');
+
+/**
+ * Runs the WCAG accessibility tests on the curent page of the global browser
+ */
+exports.getAccessibilityTestResults = () => {
+    browser.execute(source);
+
+    // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md
+
+    return browser.executeAsync(done => {
+        const options = {
+            runOnly: {
+                type: 'tag',
+                values: ['wcag2a']
+            },
+            rules: {
+                'duplicate-id': { enabled: false }
+            }
+        };
+
+        axe.run(document, options, (err, results) => {
+            if (err) throw err;
+            done(results);
+        });
+    });
+};
+
+/**
+ * Creates a CSV build artifact of the pages accessibility violations.
+ * @param {Object} results - The accessibility violations.
+ * @param {String} componentName - Name of the component that was tested.
+ */
+exports.processResults = (results, componentName) => {
+    console.log('Creating .CSV artifact for Axe violations');
+
+    const fileName = `${componentName}-a11y-violations`;
+    const localFilePath = `${__dirname}../../../axe-violations/${fileName}`;
+    // axe-reports can't create the CSV in CI due to permissions so we have to create the file ourselves.
+    if (process.env.CIRCLECI) {
+        const ciFileName = `/home/circleci/project/axe-violations/${fileName}`;
+
+        try {
+            if (!fs.existsSync(ciFileName)) {
+                exec(`touch ${ciFileName}`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
+        AxeReports.processResults(results, 'csv', ciFilePath, false);
+    } else {
+        AxeReports.processResults(results, 'csv', localFilePath, false);
+    }
+
+    console.error(`Expected no accessibility violations. Found: ${results.violations.length}`);
+};
