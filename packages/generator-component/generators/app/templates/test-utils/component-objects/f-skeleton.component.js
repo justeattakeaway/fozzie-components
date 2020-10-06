809ad943e79e5bd5cf6d7ad1c894d947e35e527d
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2020-10-06 16:15:57 +0100

    fixed generator template + added API service (#355)
    Co-authored-by: Ben Siggery <ben.siggery@just-eat.com>

--- packages/generator-component/generators/app/templates/test-utils/component-objects/f-skeleton.component.js
+++ packages/generator-component/generators/app/templates/test-utils/component-objects/f-skeleton.component.js
@@ -1,9 +1,5 @@
 const <%= name.class %>Component = () => $('[data-test-id="<%= name.class %>-component"]');
 
-exports.waitFor<%= name.filename%>Component = () => {
-    <%= name.class%>Component().waitForExist();
-};
+exports.waitFor<%= name.filename%>Component = () => <%= name.class%>Component().waitForExist();
 
-exports.is<%= name.filename %>ComponentDisplayed = () => {
-    <%= name.class %>Component().isDisplayed();
-};
+exports.is<%= name.filename %>ComponentDisplayed = () => <%= name.class %>Component().isDisplayed();
