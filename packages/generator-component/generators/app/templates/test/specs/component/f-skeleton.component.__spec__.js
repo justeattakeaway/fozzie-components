a7009238bfaeec2518376b2b28c77dc23b3e9d44
Ben Siggery <ben.siggery@just-eat.com>
2020-10-06 12:01:59 +0100

    PR fix

--- packages/generator-component/generators/app/templates/test/specs/component/f-skeleton.component.__spec__.js
+++ packages/generator-component/generators/app/templates/test/specs/component/f-skeleton.component.__spec__.js
@@ -7,7 +7,6 @@ describe('f-<%= name.class %> component tests', () => {
     })
 
     it('should display the f-<%= name.class %> component', () => {
-
         // Assert
         expect(<%= name.filename %>Component.is<%= name.filename %>ComponentDisplayed()).toBe(true);
     });
