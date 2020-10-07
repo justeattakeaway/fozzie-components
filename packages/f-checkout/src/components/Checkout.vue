979868bf01b5fa9258de488ebabd9aff86a77988
Faye Carter <faye.carter@just-eat.com>
2020-10-07 11:38:57 +0100

    Add button

--- packages/f-checkout/src/components/Checkout.vue
+++ packages/f-checkout/src/components/Checkout.vue
@@ -69,6 +69,27 @@
                         {{ time }}
                     </option>
                 </select>
+
+                <div>
+                    <h3>Leave a note</h3>
+                    <p>
+                        {{ noteText }}
+                    </p>
+
+                    <textarea
+                        id=""
+                        name=""
+                        cols="30"
+                        rows="10"
+                        :placeholder="notePlaceholder" />
+                </div>
+
+                <button
+                    type='submit'
+                    :class="$style['o-btn--primary']"
+                >
+                    Go to Payment
+                </button>
             </form>
         </card>
     </div>
@@ -119,6 +140,14 @@ export default {
         deliveryTimes: {
             type: Array,
             default: () => []
+        },
+        noteText: {
+            type: String,
+            default: "Leave a note for the restaurant with anything they need to know (e.g. the doorbell doesn't work). Do not include details about any allergies here."
+        },
+        notePlaceholder: {
+            type: String,
+            default: "e.g. the dorbell doesn't work. Do not include details about any allergies here. \n\n We're working with resturants to cut waste. Please don't as for plastic cutlery."
         }
     },
     data () {
@@ -136,16 +165,32 @@ export default {
 
 <style lang="scss" module>
 
-.c-checkout {
-    display: flex;
-    justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid $red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
-}
+$btn-default-bgColor                : $grey--lighter;
+$btn-default-text-colour            : $grey--dark;
+$btn-default-weight                 : 500;
+$btn-default-height                 : 2.6;
+$btn-default-font-size              : body-l;
+$btn-default-font-family            : $font-family-base;
+$btn-default-bgColor--hover         : $grey--light;
+$btn-default-borderRadius           : 2px;
+$btn-default-hozPadding             : 1em;
+$btn-default-vertPadding            : 11px;
+
+$btn-primary-bgColor                : $orange;
+$btn-primary-bgColor--hover         : $orange--dark;
+$btn-primary-bgColor--focus         : $orange--darkest;
+$btn-primary-textColor              : $white;
+
+// .c-checkout {
+//     display: flex;
+//     justify-content: center;
+//     min-height: 80vh;
+//     width: 80vw;
+//     margin: auto;
+//     border: 1px solid $red;
+//     font-family: $font-family-base;
+//     @include font-size(heading-m);
+// }
 
 .c-card-padding {
     padding-top: 30px;
@@ -153,5 +198,50 @@ export default {
 
 .o-form {
     @include font-size(body-l);
+}
+
+.o-btn--primary {
+    background-color: $btn-primary-bgColor;
+    display: inline-block;                      /* [1] */
+    vertical-align: middle;                     /* [2] */
+    font-family: $btn-default-font-family;      /* [3] */
+    @include font-size($btn-default-font-size); /* [3] */
+    cursor: pointer;                            /* [4] */
+    margin: 0;                                  /* [5] */
+    padding: $btn-default-vertPadding $btn-default-hozPadding; /* [5, 6] */
+    overflow: visible;                          /* [7] */
+    text-align: center;
+    font-weight: $btn-default-weight;
+    line-height: 1;
+
+    // You may want to change this
+    // background-color: $btn-default-bgColor;
+    border-radius: $btn-default-borderRadius;
+    border: 1px solid transparent;
+    user-select: none;
+
+    color: $btn-default-text-colour;
+    text-decoration: none;
+
+    &,
+    &:link,
+    &:visited {
+        color: $btn-primary-textColor;
+    }
+
+    &:hover,
+    &:active,
+    &:focus {
+        color: $btn-primary-textColor;
+    }
+
+    &:hover {
+        background-color: $btn-primary-bgColor--hover;
+    }
+
+    &:active,
+    &:focus {
+        background-color: $btn-primary-bgColor--focus;
+    }
 }
 </style>
