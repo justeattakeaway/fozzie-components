d62fae4a3b551626c948ad5938d3792ad6313937
Faye Carter <faye.carter@just-eat.com>
2020-10-07 10:30:06 +0100

    Add f-card component

--- packages/f-checkout/src/components/Checkout.vue
+++ packages/f-checkout/src/components/Checkout.vue
@@ -1,23 +1,37 @@
 <template>
     <div
-        :data-theme="theme"
         :class="$style['c-checkout']"
         data-test-id='checkout-component'>
-        {{ copy.text }}
+        <card
+            :data-theme="theme"
+            :card-heading="title"
+            has-outline
+            is-page-content-wrapper
+            card-heading-position="center"
+            data-test-id="checkout-card-component"
+            :class="$style['c-card-padding']" />
     </div>
 </template>
 
 <script>
 import { globalisationServices } from '@justeat/f-services';
+import Card from '@justeat/f-card';
+import '@justeat/f-card/dist/f-card.css';
 import tenantConfigs from '../tenants';
 
 export default {
     name: 'VueCheckout',
-    components: {},
+    components: {
+        Card
+    },
     props: {
         locale: {
             type: String,
             default: ''
+        },
+        title: {
+            type: String,
+            default: '#name, confirm your details'
         }
     },
     data () {
@@ -44,6 +58,10 @@ export default {
     border: 1px solid $red;
     font-family: $font-family-base;
     @include font-size(heading-m);
+}
+
+.c-card-padding {
+    padding-top: 30px;
 }
 
 </style>
