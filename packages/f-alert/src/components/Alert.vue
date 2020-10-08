aea335e45050f36cf7f87cec9fed2fe04977a92e
Sacha Zvetelman <sacha.zvetelman@just-eat.com>
2020-10-08 08:51:54 +0100

    f-alert@0.1.0 - Some progress around the basic alert.

--- packages/f-alert/src/components/Alert.vue
+++ packages/f-alert/src/components/Alert.vue
@@ -1,23 +1,59 @@
 <template>
     <div
         :data-theme="theme"
-        :class="$style['c-alert']"
+        :class="[$style['c-alert'],
+                 $style[`c-alert--${type}`],
+                 (isRounded ? $style['c-alert--rounded'] : '')]"
         data-test-id='alert-component'>
-        {{ copy.text }}
+        <button
+            v-if="isDismissable"
+            type="button"
+            :class="[$style['c-alert-dismiss'], 'o-btn o-btn--icon']"
+            @click="dismiss">
+            <cross-icon
+                :class="[$style['c-icon--cross'], $style['c-alert-dismiss-icon']]" />
+        </button>
+        <!-- TODO: make this dynamic. See https://skipthedishes.atlassian.net/browse/WCB-1219 -->
+        <h3
+            :class="$style['c-alert-heading']">
+            <slot name="heading" />
+        </h3>
+        <p
+            :class="$style['c-alert-content']">
+            <slot name="content" />
+        </p>
     </div>
 </template>
 
 <script>
+import { CrossIcon } from '@justeat/f-vue-icons';
 import { globalisationServices } from '@justeat/f-services';
 import tenantConfigs from '../tenants';
 
 export default {
     name: 'VueAlert',
-    components: {},
+    components: { CrossIcon },
     props: {
         locale: {
             type: String,
             default: ''
+        },
+        type: {
+            type: String,
+            default: 'success',
+            validator: value => ['success', 'warning', 'info', 'danger'].indexOf(value) !== -1
+        },
+        isRounded: {
+            type: Boolean,
+            default: false
+        },
+        isDismissable: {
+            type: Boolean,
+            default: false
+        },
+        isFullWidth: {
+            type: Boolean,
+            default: false
         }
     },
     data () {
@@ -29,21 +65,79 @@ export default {
             copy: { ...localeConfig },
             theme
         };
+    },
+    methods: {
+        dismiss () {
+            this.$destroy();
+            this.$el.parentNode.removeChild(this.$el);
+        }
     }
 };
 </script>
 
 <style lang="scss" module>
 
+$alert-borderRadius: $border-radius;
+
 .c-alert {
-    display: flex;
-    justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid $red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
+    position: relative;
+    padding: spacing();
+    margin-top: spacing(x2);
+    border: 0;
+    min-height: 132px;
+
+    &:first-child {
+        margin-top: 0;
+    }
+
+    & + * {
+        margin-top: spacing(x2);
+    }
 }
 
+    .c-alert--rounded {
+        border-radius: $alert-borderRadius;
+    }
+
+    .c-alert--success {
+        @include alert-variant($color-bg--accept, $color-text);
+    }
+
+    .c-alert--warning {
+        @include alert-variant($color-bg--notification, $color-text);
+    }
+
+    .c-alert--danger {
+        @include alert-variant($color-bg--error, $color-text);
+    }
+
+    .c-alert--info {
+        @include alert-variant($color-bg--info, $color-text);
+    }
+
+    .c-alert-heading {
+        @include font-size(subheading-s, true, narrow);
+        margin-top: spacing(x0.5);
+    }
+
+    .c-alert-content {
+        @include font-size(body-l);
+    }
+
+    button.c-alert-dismiss { // TODO: Needed more specificity here.
+        padding: spacing();
+        text-indent: 0;
+        position: absolute;
+        right: spacing();
+        top: spacing();
+        z-index: zIndex(high);
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+        .c-alert-dismiss-icon {
+            height: 13px;
+        }
 </style>
--- packages/f-alert/stories/Alert.stories.js
+++ packages/f-alert/stories/Alert.stories.js
@@ -1,9 +1,33 @@
 import { storiesOf } from '@storybook/vue';
 import VueAlert from '../src/components/Alert.vue';
+import VueCard from '../../f-card/src/components/Card.vue';
+
 
 storiesOf('Components', module)
   .add('f-alert', () => ({
-    components: { VueAlert },
-    template: `<vue-alert />`
+    components: { VueAlert, VueCard },
+    template: `
+      <vue-card
+        locale="en-GB"
+        is-rounded
+        has-outline
+        is-page-content-wrapper>
+        <vue-alert
+            locale="en-GB"
+            :type="success"
+            :isRounded="false"
+            :isDismissable="true">
+            <template v-slot:heading>Title of the alert</template>
+            <template v-slot:content>You can put any HTML here, buddy.</template>
+        </vue-alert>
+        <p>
+            Mollit aliqua labore excepteur voluptate consequat ut dolore
+            ipsum nostrud magna elit proident laboris. Irure do nulla
+            eiusmod aliquip enim qui deserunt Lorem. Dolore eu exercitation
+            fugiat tempor laborum consequat eu dolor voluptate reprehenderit tempor incididunt in.
+            Fugiat cillum laborum irure ea id deserunt quis est deserunt reprehenderit magna ea consectetur.
+            Magna ullamco eu et Lorem.
+        </p>
+      </vue-card>`
   })
 );
