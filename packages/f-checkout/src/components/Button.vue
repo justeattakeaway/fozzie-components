7f5e79a9152a2868fbd890e0407fb6cffbea085b
Faye Carter <faye.carter@just-eat.com>
2020-10-09 10:17:39 +0100

    Add additional styles from button

--- packages/f-checkout/src/components/Button.vue
+++ packages/f-checkout/src/components/Button.vue
@@ -1,11 +1,7 @@
 <template>
     <button
         type='submit'
-        :class="[
-            $style['o-btn'],
-            $style[buttonStyleClass],
-            (isFullWidth ? $style['o-btn--block'] : '')
-        ]"
+        :class="$style['o-btn']"
     >
         <slot />
     </button>
@@ -37,15 +33,10 @@ export default {
 
 <style lang="scss" module>
 
-// Button styling (should be pulled out as a separate component)
-
-$btn-default-bgColor                : $grey--lighter;
-$btn-default-text-colour            : $grey--dark;
 $btn-default-weight                 : 500;
 $btn-default-height                 : 2.6;
 $btn-default-font-size              : body-l;
 $btn-default-font-family            : $font-family-base;
-$btn-default-bgColor--hover         : $grey--light;
 $btn-default-borderRadius           : 2px;
 $btn-default-hozPadding             : 2em;
 $btn-default-vertPadding            : 12px;
@@ -57,52 +48,36 @@ $btn-primary-textColor              : $white;
 
 $btn-disabled-bgColor               : $grey--light;
 
-
-/**
- * Base button styles – Based on csswizardry.com/beautons
- *
- * 1. Allow us to better style box model properties.
- * 2. Line different sized buttons up a little nicer.
- * 3. Make buttons inherit font styles.
- * 4. Force all elements using beautons to appear clickable.
- * 5. Normalise box model styles.
- * 6. If the button’s text is 1em, and the button is (3 * font-size) tall, then
- *    there is 1em of space above and below that text. We therefore apply 1em
- *    of space to the left and right, as padding, to keep consistent spacing.
- * 7. Fixes odd inner spacing in IE7.
- */
-
 .o-btn {
-    display: inline-block;                      /* [1] */
-    vertical-align: middle;                     /* [2] */
-    font-family: $btn-default-font-family;      /* [3] */
-    @include font-size($btn-default-font-size); /* [3] */
-    cursor: pointer;                            /* [4] */
-    margin: 0;                                  /* [5] */
-    padding: $btn-default-vertPadding $btn-default-hozPadding; /* [5, 6] */
-    overflow: visible;                          /* [7] */
+    display: inline-block;
+    vertical-align: middle;
+    font-family: $btn-default-font-family;
+    @include font-size($btn-default-font-size);
+    cursor: pointer;
+    margin: 0;
+    padding: $btn-default-vertPadding $btn-default-hozPadding;
+    overflow: visible;
     text-align: center;
     font-weight: $btn-default-weight;
     line-height: 1;
     display: flex;
     margin: 0 auto;
 
-    // You may want to change this
-    background-color: $btn-default-bgColor;
+    background-color: $btn-primary-bgColor;
     border-radius: $btn-default-borderRadius;
     border: 1px solid transparent;
     user-select: none;
 
-    color: $btn-default-text-colour;
+    color: $btn-primary-textColor;
     text-decoration: none;
 
     &:hover,
     &:active,
     &:focus {
-        background-color: $btn-default-bgColor--hover;
+        background-color: $btn-primary-bgColor--hover;
 
         &:not(.o-btnLink) {
-            outline: none; // no need as already has a focus/active state
+            outline: none;
         }
     }
 
@@ -114,7 +89,6 @@ $btn-disabled-bgColor               : $grey--light;
         text-decoration: none;
     }
 
-    // Disabled state
     &.disabled,
     &[disabled] {
         cursor: default;
@@ -129,56 +103,4 @@ $btn-disabled-bgColor               : $grey--light;
         margin-top: spacing(x2);
     }
 }
-
-/**
- * Modifier – .o-btn--primary
- *
- * Sets the btn colour to site primary colour
- */
-
-.o-btn--primary {
-    background-color: $btn-primary-bgColor;
-
-    &,
-    &:link,
-    &:visited {
-        color: $btn-primary-textColor;
-    }
-
-    &:hover,
-    &:active,
-    &:focus {
-        color: $btn-primary-textColor;
-    }
-
-    &:hover {
-        background-color: $btn-primary-bgColor--hover;
-    }
-
-    &:active,
-    &:focus {
-        background-color: $btn-primary-bgColor--focus;
-    }
-}
-
-/**
- * Modifier – btn-block
- *
- * Makes the btn full width
- */
-
-.o-btn--block {
-    display: block;
-    width: 100%;
-    padding-left: 0;
-    padding-right: 0;
-
-    // Vertically space out multiple block buttons
-    // same as .o-btn--block + .o-btn--block
-    & + & {
-        margin-top: 10px;
-    }
-}
-
-
 </style>
