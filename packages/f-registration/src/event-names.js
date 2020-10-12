32876254258f4bfef6af5aad218c43ebecf86c6b
Luke Parry <luke.parry@gmail.com>
2020-10-12 15:53:11 +0100

    f-registration@0.34.0 (#358)
    * Emit event on first form interaction
    
    * Add additional inline error to satisfy tracking requirements
    
    * Remove CMDER_ROOT directory
    
    * ensure exact version for f-form-field component
    
    Co-authored-by: Luke Parry <luke.parry@just-eat.com>

--- packages/f-registration/src/event-names.js
+++ packages/f-registration/src/event-names.js
@@ -1,7 +1,11 @@
 const CreateAccountSuccess = 'registration-create-account-success';
 const CreateAccountFailure = 'registration-create-account-failure';
+const CreateAccountStart = 'registration-create-account-start';
+const CreateAccountInlineError = 'registration-create-account-inline-error';
 
 export default {
     CreateAccountSuccess,
-    CreateAccountFailure
+    CreateAccountFailure,
+    CreateAccountStart,
+    CreateAccountInlineError
 };
