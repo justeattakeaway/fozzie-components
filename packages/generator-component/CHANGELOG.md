47c9525b466dc3f2c186cdca07e5c89864c3506e
Lizzie Turney <lizzie.turney@just-eat.com>
2020-10-13 14:17:01 +0100

    generator-component@1.2.0 - added accessibility test template

--- packages/generator-component/CHANGELOG.md
+++ packages/generator-component/CHANGELOG.md
@@ -3,6 +3,13 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+v1.2.0
+------------------------------
+*October 13, 2020*
+
+### Added
+- Axe Accessibility Spec template
+
 v1.1.1
 ------------------------------
 *October 6, 2020*
---
+++ packages/generator-component/generators/app/templates/test/specs/accessibility/axe-accessibility.spec.js
@@ -0,0 +1,21 @@
+import { getAccessibilityTestResults, processResults } from '../../../../../test/utils/axe-helper';
+
+describe('Accessibility tests', () => {
+    beforeEach(() => {
+        // Arrange
+        browser.url('http://localhost:8081');
+    });
+
+    it('a11y - should test f-registration component WCAG compliance', () => {
+        // Act
+        const axeResults = getAccessibilityTestResults();
+
+        // Assert
+        if (axeResults.violations.length > 0) {
+            // only create artifact if there are accessibility errors to show
+            processResults(axeResults, 'f-<%= name.class %>'); 
+        }
+        // We want the build to fail if there are accessibility violations
+        expect(axeResults.violations.length).toBe(0);
+    });
+});
