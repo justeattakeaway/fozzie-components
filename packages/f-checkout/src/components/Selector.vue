35bdc4c21e8d68de62e2c36e23ce7f3b2c9081f9
FayeCarter <faye.carter@just-eat.com>
2020-10-15 16:49:52 +0100

    f-checkout@0.2.0 - Replicate ConsumerWeb layout (#356)
    * Add f-card component
    
    * Add input fields
    
    * Fix props
    
    * Fix placeholder text
    
    * Add delivery time select
    
    * Add button
    
    * Import f-form-field css
    
    * Create Selector component
    
    * Button from f-registration
    
    * Test button exists
    
    * Center button
    
    * Add style to textarea
    
    * Group address fields
    
    * Add padding to selector component
    
    * Add basic style to note text
    
    * Add Allergy Text
    
    * Update class names
    
    * Create UserNote component
    
    * Update allergy class names
    
    * Update userNote class names
    
    * Update Selector className
    
    * Remove duplicate props
    
    * Create computed title
    
    * Start styling Selector
    
    * Style selector
    
    * Update spacing
    
    * Update styles
    
    * Update version
    
    * Add focus styling to Allergies
    
    * Add additional styles from button
    
    * Remove conditions from button component
    
    * Update userNote
    
    * Fix typo
    
    * Add spacing to template
    
    * Update card dimensions
    
    * Update padding
    
    * Remove hats
    
    * Remove style variables
    
    * Update field height
    
    * Remove names from script
    
    * Update padding and sizes
    
    * Use spacing
    
    * Update padding
    
    * Update colours
    
    * Update Changelog
    
    * Remove extra button component
    
    * Remove duplicate
    
    * Update storybook
    
    * Remove Allergy Component
    
    * Update data test ids
    
    * Move styling import statements
    
    * Remove typos
    
    * Remove axios
    
    * Update scss names
    
    * Update styles
    
    * Update margins
    
    * Update colours
    
    * Update imports
    
    * Fix import error
    
    * Update variable names
    
    * Update changelog date
    
    * Move data out of props
    
    * Remove scss variables
    
    * Update label font size
    
    * Update class list
    
    * Increase font weight of text area
    
    * Update formField styling
    
    * Add class to UserNote text
    
    * Update card width in <wide
    
    * Update <wide button
    
    * Update <mid button
    
    * Fix <mid button
    
    * Capitalize name
    
    * Tests computed Name
    
    * Tests computed title
    
    * Update margin
    
    * Update Changelog
    
    * Fix test script
    
    * Update storybook
    
    * UPdate Storybook
    
    * Remove lint error
    
    * Remove json test file

---
+++ packages/f-checkout/src/components/Selector.vue
@@ -0,0 +1,70 @@
+<template>
+    <div :class="$style['o-selector']">
+        <label
+            for="delivery-time"
+            :class="$style['o-selector-label']">
+            Delivery time
+        </label>
+        <select
+            id="delivery-time"
+            :class="$style['o-selector-input']">
+            <option
+                v-for="(time, index) in deliveryTimes"
+                :key="index"
+                :value="time">
+                {{ time }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        deliveryTimes: {
+            type: Array,
+            default: () => ['', 'As soon as possible', 'Today in 5 minutes']
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+$form-label-colour                        : $grey--dark;
+$form-input-colour                        : $color-text;
+$form-input-bg                            : $white;
+$form-input-borderRadius                  : 3px;
+$form-input-borderWidth                   : 1px;
+$form-input-borderColour                  : $grey--light;
+$form-input-borderColour--focus           : $grey--dark;
+
+.o-selector {
+    margin: spacing(x2) 0;
+    position: relative;
+    padding: 0;
+    font-family: $font-family-base;
+    color: $form-input-colour;
+    font-weight: $font-weight-base;
+    background-color: $form-input-bg;
+    @include font-size(body-l);
+
+    .o-selector-label {
+        display: block;
+        color: $form-label-colour;
+        position: absolute;
+        top: 50%;
+        left: 0.5rem;
+        transform: translateY(-50%);
+        cursor: pointer;
+    }
+
+    .o-selector-input {
+        height: 53px;
+        width: 100%;
+        padding: 0.5rem;
+        border: $form-input-borderWidth solid $form-input-borderColour;
+        border-radius: $form-input-borderRadius;
+        cursor: pointer;
+    }
+}
+</style>
