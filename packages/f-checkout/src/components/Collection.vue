53c439b0dabf17868efce7fad1c20e7a949b57d8
Faye Carter <faye.carter@just-eat.com>
2020-10-20 14:36:05 +0100

    Create Collection component

---
+++ packages/f-checkout/src/components/Collection.vue
@@ -0,0 +1,41 @@
+<template>
+    <div data-test-id='collection-component'>
+        <form-field
+            v-model="mobileNumber"
+            :class="$style['c-formField']"
+            name="mobile-number"
+            data-test-id="input-mobile-number"
+            label-text="Mobile number"
+            input-type="text"
+            label-style="inline" />
+    </div>
+</template>
+
+<script>
+import FormField from '@justeat/f-form-field';
+import '@justeat/f-form-field/dist/f-form-field.css';
+
+export default {
+    components: { FormField },
+
+    props: {
+        mobileNumber: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+
+.c-formField {
+    input {
+        height: 50px;
+    }
+
+    label {
+        @include font-size(body-l);
+    }
+}
+</style>
