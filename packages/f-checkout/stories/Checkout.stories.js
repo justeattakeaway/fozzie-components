7c8a43dc6f47ececa96ddc779b434735eaff4072
Faye Carter <faye.carter@just-eat.com>
2020-10-20 14:36:50 +0100

    Update storybook

--- packages/f-checkout/stories/Checkout.stories.js
+++ packages/f-checkout/stories/Checkout.stories.js
@@ -1,16 +1,22 @@
+import { withKnobs, select } from '@storybook/addon-knobs';
 import { withTests } from '@storybook/addon-jest';
 import VueCheckout from '../src/components/Checkout.vue';
 import results from '../src/components/tests/.jest-test-results.json';
 
 export default {
     title: 'Components/Organisms',
-    decorators: [withTests({ results })]
+    decorators: [withKnobs, withTests({ results })]
 };
 
 export const Checkout = () => ({
     components: { VueCheckout },
+    props: {
+        type: {
+            default: select('type', ['Collection', 'Delivery'])
+        }
+    },
     template:
-        '<vue-checkout />'
+        '<vue-checkout :type="type" />'
 });
 
 Checkout.parameters = {
