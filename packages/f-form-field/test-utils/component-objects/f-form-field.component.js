39dcc76b019ffe671de8de7728dba51aad08c187
Lizzie Turney <lizzie.turney@just-eat.com>
2020-10-21 17:32:32 +0100

    f-form-field@v0.8.0 - resolved comments from PR

--- packages/f-form-field/test-utils/component-objects/f-form-field.component.js
+++ packages/f-form-field/test-utils/component-objects/f-form-field.component.js
@@ -3,19 +3,17 @@ const testLabel = () => $('[data-test-id="testLabel"]');
 const fieldInput = () => $('input');
 
 /**
- * @description
- * Inputs user's first name into the form-field component.
- *
  * @param {Object} userInput
  * @param {String} userInput.firstName The user's first name
+ * @description
+ * The below function adds and displays the user's first name into the form-field component.
  */
-
-exports.waitForFormField = () => formFieldComponent().waitForExist();
-
 exports.displayUserInput = (userInput) => {
     fieldInput().setValue(userInput.firstName);
-    fieldInput().getValue()
+    fieldInput().getValue();
 };
+
+exports.waitForFormField = () => formFieldComponent().waitForExist();
 
 exports.isLabelDisplayed = () => testLabel().isDisplayed();
 exports.isFormFieldDisplayed = () => formFieldComponent().isDisplayed(); 
