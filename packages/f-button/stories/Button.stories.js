5b2f8a64e07b69220cb06c3dd2ae660896049703
Ashley Watson-Nolan <nolly00@gmail.com>
2020-10-21 15:53:42 +0100

    fozzie-components@v2.5.0 & f-button@v0.1.0 – fozzie package updates and initial f-button component setup (#380)
    * fozzie-components@v2.6.0 & f-button@v0.1.0
    
    fozzie package updates and initial f-button component setup
    
    * f-button@v0.1.0 - PR comments

---
+++ packages/f-button/stories/Button.stories.js
@@ -0,0 +1,28 @@
+import {
+    withKnobs, select, boolean
+} from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import VueButton from '../src/components/Button.vue';
+
+export default {
+    title: 'Components/Atoms',
+    decorators: [withKnobs, withA11y]
+};
+
+export const ButtonComponent = () => ({
+    components: { VueButton },
+    props: {
+        buttonType: {
+            default: select('Button Type', ['primary', 'secondary', 'outline', 'ghost', 'link'])
+        },
+        buttonSize: {
+            default: select('Button Size', ['xsmall', 'small', 'medium', 'large'], 'medium')
+        },
+        fullWidth: {
+            default: boolean('fullWidth', false)
+        }
+    },
+    template: '<vue-button :buttonType="buttonType" :buttonSize="buttonSize" :fullWidth="fullWidth">Default Button Text</vue-button>'
+});
+
+ButtonComponent.storyName = 'f-button';
