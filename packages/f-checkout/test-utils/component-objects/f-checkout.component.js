1cad8a5ac8751c473ceac6291150ea077da342df
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2020-10-26 15:31:11 +0000

    f-checkout@0.5.0 - Add f-checkout component tests (#376)
    * Add f-checkout component tests
    
    * minor changes
    
    * fix typos
    
    * add linebreaks
    
    Co-authored-by: Ben Siggery <ben.siggery@just-eat.com>

--- packages/f-checkout/test-utils/component-objects/f-checkout.component.js
+++ packages/f-checkout/test-utils/component-objects/f-checkout.component.js
@@ -1,4 +1,68 @@
 const checkoutComponent = () => $('[data-test-id="checkout-component"]');
 
+// Form Fields
+const mobileNumberInput = () => $('[data-test-id="input-mobile-number"]');
+const addressLine1Input = () => $('[data-test-id="input-address-line-1"]');
+const addressLine2Input = () => $('[data-test-id="input-address-line-2"]');
+const addressCityInput = () => $('[data-test-id="input-address-city"]');
+const addressPostcodeInput = () => $('[data-test-id="input-address-postcode"]');
+
+const userNoteInput = () => $('[data-test-id="user-note"] textarea');
+
+const allergenLink = () => $('[data-test-id="allergy-button"]');
+
+const deliveryTimeDropdown = () => $('[data-test-id="delivery-time"]');
+
+const goToPaymentButton = () => $('[data-test-id="confirm-payment-submit-button"]');
+
 exports.waitForCheckoutComponent = () => checkoutComponent().waitForExist();
 exports.isCheckoutComponentDisplayed = () => checkoutComponent().isDisplayed();
+exports.isAllergenLinkDisplayed = () => allergenLink().isDisplayed();
+
+/**
+ * @description
+ * Inputs address details into the checkout component.
+ *
+ * @param {Object} addressInfo
+ * @param {String} addressInfo.mobileNumber The user's mobile number
+ * @param {String} addressInfo.line1 First line of the user's address
+ * @param {String} addressInfo.line2 Second line of the user's address
+ * @param {String} addressInfo.city City of the user's address
+ * @param {String} addressInfo.postcode Postcode of the user's address
+ */
+exports.submitCheckoutForm = (addressInfo) => {
+    exports.waitForCheckoutComponent();
+    mobileNumberInput().setValue(addressInfo.mobileNumber);
+    addressLine1Input().setValue(addressInfo.line1);
+    addressLine2Input().setValue(addressInfo.line2);
+    addressCityInput().setValue(addressInfo.city);
+    addressPostcodeInput().setValue(addressInfo.postcode);
+};
+
+/**
+ * @description
+ * Sets the value of the delivery time dropdown based on visible text.
+ *
+ * @param {String} deliveryTimeText The text visible text value of the delivery time
+ */
+exports.selectDeliveryTime = (deliveryTimeText) => {
+    deliveryTimeDropdown().selectByVisibleText(deliveryTimeText);
+}
+
+/**
+ * @description
+ * Sets the value of the user note.
+ *
+ * @param {String} userNote The user note value to be entered
+ */
+exports.inputUserNote = (userNote) => {
+    userNoteInput().setValue(userNote);
+}
+
+/**
+ * @description
+ * Submits the checkout form.
+ */
+exports.submit = () => {
+    goToPaymentButton().click();
+}
--- packages/f-checkout/test/specs/component/f-checkout.component.spec.js
+++ packages/f-checkout/test/specs/component/f-checkout.component.spec.js
@@ -4,10 +4,35 @@ describe('f-checkout component tests', () => {
     beforeEach(() => {
         // Arrange
         browser.url('http://localhost:8080');
+        CheckoutComponent.waitForCheckoutComponent();
     })
 
     it('should display the f-checkout component', () => {
         // Assert
         expect(CheckoutComponent.isCheckoutComponentDisplayed()).toBe(true);
     });
+
+    it('should display the allergen link'), () => {
+        // Assert
+        expect(CheckoutComponent.isAllergenLinkDisplayed()).toBe(true);
+    }
+
+    // Skip until we have something to assert on
+    it.skip('should submit the checkout form', () => {
+
+        // Arrange
+        const addressInfo = {
+            mobileNumber: '07777777779',
+            line1: 'Test House',
+            line2: 'High Street',
+            city: 'Test City',
+            postcode: 'AR51 1AA'
+        }
+
+        // Act
+        CheckoutComponent.submitCheckoutForm(addressInfo);
+        CheckoutComponent.inputUserNote('No mushrooms!');
+        CheckoutComponent.selectDeliveryTime('As soon as possible');
+        CheckoutComponent.submit();
+    })
 });
