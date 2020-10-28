c73605a5085b8b7becb7b2f2550d713397707948
Josh Ng <joshua.ng2332@gmail.com>
2020-10-28 12:40:23 +0000

    Updated wdio config file and package.json.

--- wdio.conf.js
+++ wdio.conf.js
@@ -122,7 +122,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    // reporters: ['dot'],
 
 
 
