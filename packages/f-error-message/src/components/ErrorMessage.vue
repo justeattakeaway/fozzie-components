1618f71bfd932b99860e0a9302846d91ad905125
Ross Gratton <rossgratton@gmail.com>
2020-10-29 09:20:40 +0000

    f-error-message@v0.1.0 (#357)
    * f-error-message@v0.1.0: initial generation of f-error-message component
    
    * f-error-message@v0.1.0: created error message component and related storybook usage file
    
    * f-error-message@v0.1.0: adding unit tests
    
    * f-error-message@v0.1.0: Adjusting babel config in line with new method
    
    * f-error-message@v0.1.0: Removed old font imports in demo index.vue
    
    * f-error-message@v0.1.0: corrected css classes to match convention
    
    * f-error-message:v0.1.0: updated changelog
    
    * f-error-message@v0.1.0: Changed red to color-text--danger variable
    
    * f-error-message@v0.1.0: Added f-vue-icons dependency
    
    * f-error-message@v0.1.0: fixed formatting issues
    
    * f-error-message@v0.1.0: Removed test as we had coverage overlap
    
    * f-error-message@v0.2.0: updated version and added stylelint config
    
    * f-error-message@v0.2.0: fixed incorrect prop name and incorrect changelog format
    
    Co-authored-by: Ross Gratton <ross.gratton@just-eat.com>

---
+++ packages/f-error-message/src/components/ErrorMessage.vue
@@ -0,0 +1,52 @@
+<template>
+    <p
+        v-if="hasSlotData"
+        :class="$style['c-errorMessage']"
+        data-test-id="error-message"
+        :data-test-title="dataTestTitle">
+        <warning-icon :class="$style['c-errorMessage-icon']" />
+        <span data-test-id="content">
+            <slot />
+        </span>
+    </p>
+</template>
+
+<script>
+import { WarningIcon } from '@justeat/f-vue-icons';
+
+export default {
+    name: 'ErrorMessage',
+    components: {
+        WarningIcon
+    },
+    props: {
+        dataTestTitle: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        hasSlotData () {
+            return !!this.$slots.default;
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+
+.c-errorMessage {
+    display: flex;
+    align-items: center;
+    color: $color-text--danger;
+    @include font-size(base);
+    margin-top: spacing();
+}
+
+.c-errorMessage-icon {
+    width: 16px;
+    height: 16px;
+    margin-right: spacing(x0.5);
+}
+
+</style>
