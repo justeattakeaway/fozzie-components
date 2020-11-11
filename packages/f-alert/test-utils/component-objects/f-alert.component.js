7cd396d27cee04fc21d8c51810f243f9219e9fb9
Lizzie Turney <lizzie.turney@just-eat.com>
2020-11-11 10:26:03 +0000

    reverted f-form-field and added variables to webdriver p-object

--- packages/f-alert/test-utils/component-objects/f-alert.component.js
+++ packages/f-alert/test-utils/component-objects/f-alert.component.js
@@ -1,7 +1,8 @@
-const IDs = require('./data-test-ids');
-
+const IDS = require('./data-test-ids');
+// import IDS from './data-test-ids';
+console.log(IDS.heading)
 const alertComponent = () => $(`[data-test-id="${IDS.component.id}"]`);
-const exitAlertButton = () => $(`[data-test-id="${IDS.button.cancel}"]`); 
+const exitAlertButton = () => $(`[data-test-id="${IDS.buttons.cancel}"]`); 
 
 exports.isAlertDisplayed = () => alertComponent().isDisplayed();
 exports.waitForAlert = () => alertComponent().waitForExist();
