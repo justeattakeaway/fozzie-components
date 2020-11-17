13ddce55ca4ab29ec27b252e3b65d43d20304c8e
Pawel Kedziora <6950110+pkedziora@users.noreply.github.com>
2020-11-17 14:54:43 +0000

    f-checkout@0.9.0 - Wire up the form with response from get checkout (#443)
    * Wire up checkout with api response
    
    * Wire up address
    
    * Wire up mobile number
    
    * Tests
    
    * eslint fix
    
    * CR fixes
    
    * Test file for collection
    
    * Remove unused prop
    
    * Remove unused prop
    
    Co-authored-by: Pawel Kedziora <pawel.kedziora@just-eat.com>

--- packages/f-checkout/src/constants.js
+++ packages/f-checkout/src/constants.js
@@ -1,4 +1,15 @@
-export const CHECKOUT_METHOD_COLLECTION = 'Collection';
-export const CHECKOUT_METHOD_DELIVERY = 'Delivery';
+export const CHECKOUT_METHOD_COLLECTION = 'collection';
+export const CHECKOUT_METHOD_DELIVERY = 'delivery';
 
 export const VALID_CHECKOUT_METHOD = [CHECKOUT_METHOD_COLLECTION, CHECKOUT_METHOD_DELIVERY];
+
+export const TENANT_MAP = {
+    'en-GB': 'uk',
+    'en-AU': 'au',
+    'en-NZ': 'nz',
+    'da-DK': 'dk',
+    'es-ES': 'es',
+    'en-IE': 'ie',
+    'it-IT': 'it',
+    'nb-NO': 'no'
+};
