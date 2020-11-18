ec7a9c4761385dfeeb2ef9eb9f56e9f10edc4fa8
Ashley Watson-Nolan <nolly00@gmail.com>
2020-11-18 15:09:43 +0000

    f-registration@v0.42.0 - Adding box-shadow and spacing tweaks (#451)
    * f-registration@v0.42.0 - Adding box-shadow and spacing tweaks
    
    * f-registration@v0.42.0 - PR comments

--- packages/f-registration/src/components/Registration.vue
+++ packages/f-registration/src/components/Registration.vue
@@ -12,7 +12,10 @@
             <bag-celebrate-icon :class="$style['c-registration-icon']" />
             <p
                 v-if="showLoginLink"
-                :class="$style['c-registration-link']"
+                :class="[
+                    $style['c-registration-link'],
+                    $style['c-registration-link--subtitle']
+                ]"
                 data-test-id="create-account-login-link"
                 @click="visitLoginPage">
                 <a :href="copy.navLinks.login.url">{{ copy.navLinks.login.text }}</a>
@@ -440,16 +443,23 @@ $registration-icon-height--narrow : 74px;
 
 // Form styling
 .c-registration {
-    margin-top: 100px;
+    margin-top: 112px;
 }
 
     .c-registration-card {
         position: relative;
-        padding-top: spacing(x5);
+        padding-top: spacing(x7);
         padding-bottom: spacing(x6);
 
         @include media('<mid') {
             padding-bottom: spacing(x4);
+        }
+
+        @include media('>=narrow') {
+            // TODO: box shadow value will eventually come from PIE design tokens, but hard coding here for now
+            box-shadow: 0 1px 1px 0 rgba($black, 0.03),
+                    0 2px 1px -1px rgba($black, 0.07),
+                    0 1px 3px 0 rgba($black, 0.06);
         }
     }
 
@@ -468,12 +478,12 @@ $registration-icon-height--narrow : 74px;
     }
 
     .c-registration-form {
-        margin-top: spacing(x2);
+        margin-top: spacing(x3);
     }
 
     .c-registration-submit {
         margin-top: spacing(x4);
-        margin-bottom: spacing(x3);
+        margin-bottom: spacing(x4);
     }
 
     .c-registration-link {
@@ -490,5 +500,9 @@ $registration-icon-height--narrow : 74px;
             }
         }
     }
+
+        .c-registration-link--subtitle {
+            margin-top: - spacing(); // shift the subtitle link closer to the main title
+        }
 
 </style>
