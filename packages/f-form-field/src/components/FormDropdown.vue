f7fab1f6ef2f3325cb2d1b745ad32ce136a5f949
FayeCarter <faye.carter@just-eat.com>
2020-11-19 12:06:08 +0000

    f-form-field@1.2.0 - Add dropdown module (#449)
    * Initial changes
    
    * Create FormDropdown component & test
    
    * Update class name
    
    * Update version
    
    * Temp test fix
    
    * Fix tests
    
    * Remove comment
    
    * Updates to tests and styling from review
    
    * Change date
    
    * Update inputType validator

---
+++ packages/f-form-field/src/components/FormDropdown.vue
@@ -0,0 +1,69 @@
+<template>
+    <div
+        :class="$style['c-formDropdown']"
+        data-test-id="formDropdown">
+        <caret-icon
+            :class="$style['c-formDropdown-icon']"
+            data-test-id="formDropdown-icon" />
+        <select
+            id="time-selection"
+            :class="$style['c-formDropdown-select']"
+            data-test-id="formDropdown-select"
+            @change="updateOption">
+            <option
+                v-for="(option, index) in dropdownOptions"
+                :key="index"
+                :data-test-id="`formDropdown-option-${index}`"
+                :value="option">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { CaretIcon } from '@justeat/f-vue-icons';
+
+export default {
+    name: 'FormField',
+
+    components: {
+        CaretIcon
+    },
+
+    props: {
+        dropdownOptions: {
+            type: Array,
+            default: () => null
+        }
+    },
+
+    methods: {
+        updateOption (event) {
+            this.$emit('update', event.target.value);
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+.c-formDropdown-icon {
+    width: spacing(x1.5);
+    position: absolute;
+    right: spacing(x3);
+    bottom: 20px;
+    transform: rotate(180deg);
+}
+
+.c-formDropdown-select {
+    padding: spacing(x1.5) spacing(x2);
+    height: 100%;
+    width: 100%;
+
+    /* Remove default styling */
+    border: none;
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+}
+</style>
