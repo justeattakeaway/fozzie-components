9887beff580a6d0032cb17a9f291c8cccacc09fe
FayeCarter <faye.carter@just-eat.com>
2020-11-25 13:31:49 +0000

    f-services@1.5.0 - Add phone validation (#464)
    * Add phone validation
    
    * Remove extra line
    
    * Add locale
    
    * Update test
    
    * Update Regex
    
    Co-authored-by: Benjamin White <ben.white@just-eat.com>
    
    Co-authored-by: Benjamin White <ben.white@just-eat.com>

--- packages/f-services/src/validations.js
+++ packages/f-services/src/validations.js
@@ -36,8 +36,13 @@ const isValidPostcode = postcode => POSTCODE_REGEX.test(postcode);
  */
 const meetsCharacterValidationRules = value => /^[\u0060\u00C0-\u00F6\u00F8-\u017Fa-zA-Z-' ]*$/.test(value);
 
+const PHONE_REGEX = { 'en-GB': /^(\+(44))?[0-9]{10,11}$/ };
+
+const isValidPhoneNumber = (number, locale) => PHONE_REGEX[locale].test(number);
+
 export default {
     getFormValidationState,
     isValidPostcode,
-    meetsCharacterValidationRules
+    meetsCharacterValidationRules,
+    isValidPhoneNumber
 };
