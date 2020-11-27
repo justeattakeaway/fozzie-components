2aceb3684e0cacc0ebcd4e8cbc7c5d79de476a42
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2020-11-27 13:44:25 +0000

    Update f-header.component.js

--- packages/f-header/test-utils/component-objects/f-header.component.js
+++ packages/f-header/test-utils/component-objects/f-header.component.js
@@ -1,5 +1,5 @@
 const loginLink = () => $('[data-test-id="login-link"]');
-const offersLink = () =>$$('[data-test-id="offers-link"]');
+const offersLink = () => $$('[data-test-id="offers-link"]');
 const deliveryEnquiryLink = () => $('[data-test-id="delivery-link"]');
 const helpLink = () => $('[data-test-id="help-link"]');
 const headerLogo = () => $('[data-test-id="header-logo"]');
