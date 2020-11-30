febc24bf446bd6d64d1455d5298a43cf9a6a9fa0
Sacha Zvetelman <sacha.zvetelman@just-eat.com>
2020-11-30 15:07:29 +0000

    f-service-validationstate - Removing empty lines.

--- packages/f-services/src/validations.js
+++ packages/f-services/src/validations.js
@@ -35,8 +35,6 @@ const getFormValidationState = $v => {
     };
 };
 
-
-
 // https://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive#164994
 const POSTCODE_REGEX = /^([Gg][Ii][Rr]\s?0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$/;
 
