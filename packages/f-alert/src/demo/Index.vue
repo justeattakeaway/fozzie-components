9276e633b2c6c3d7d9e8109028d3d08682c88ac5
Ben Siggery <ben.siggery@just-eat.com>
2020-11-30 15:36:34 +0000

    Add prop control to f-alert demo file for WebDriverIO component tests

--- packages/f-alert/src/demo/Index.vue
+++ packages/f-alert/src/demo/Index.vue
@@ -6,11 +6,34 @@
         <meta
             name="viewport"
             content="width=device-width, initial-scale=1">
+        <div data-test-id="component-controls">
+            <h1>Component Controls</h1>
+            <label for='locale'>Locale</label>
+            <select name='locale' data-test-id='control-locale' v-model="controls.locale">
+                <option value='en-GB'>en-GB</option>
+                <option value='en-AU'>en-AU</option>
+            </select>
+            <br>
+            <label for='alert-type'>Alert Type</label>
+            <select name='alert-type' data-test-id='control-alertType' v-model="controls.alertType">
+                <option value='success'>success</option>
+                <option value='warning'>warning</option>
+                <option value='danger'>danger</option>
+                <option value='info'>info</option>
+            </select>
+            <br>
+            <label for="is-dismissable">Is Dismissable</label>
+            <input id="is-dismissable" type="checkbox" data-test-id='control-isDismissable' v-model="controls.isDismissable">
+            <br>
+            <label for="heading">Heading</label>
+            <input id="heading" type="text" data-test-id='control-heading' v-model='controls.heading'> 
+        </div>
+
         <vue-alert
-            locale="en-GB"
-            type="success"
-            :is-dismissable="true"
-            heading="Title of the alert">
+            :locale='controls.locale'
+            :type="controls.alertType"
+            :is-dismissable="controls.isDismissable"
+            :heading="controls.heading">
             You can put any HTML here!
         </vue-alert>
     </div>
@@ -20,12 +43,26 @@
 import VueAlert from '@/components/Alert.vue';
 
 export default {
-    components: { VueAlert }
+    components: { VueAlert },
+    data: () => ({
+        controls: {
+            locale: 'en-GB',
+            alertType: 'success',
+            isDismissable: false,
+            heading: 'Title of the alert'
+        }
+    })
 };
 </script>
 
 <style>
 body {
     margin: 0;
+}
+
+.component-controls {
+    color: #ffffff;
+    font-family: 'Helvetica Neue', sans-serif;
+    background-color:#ffa500;
 }
 </style>
---
+++ packages/f-alert/test-utils/component-objects/demo-controls.js
@@ -0,0 +1,32 @@
+const componentControls = () => $('[data-test-id="component-controls"]');
+
+const controlAlertType = () => componentControls().$('[data-test-id="control-alertType"]');
+const controlIsDismissable = () => componentControls().$('[data-test-id="control-isDismissable"]');
+const controlHeading = () => componentControls().$('[data-test-id="control-heading"]');
+
+/**
+ * Sets the alert type of a component based off the 'Alert Type' control dropdown value
+ * @param {string} alertType
+ */
+exports.selectAlertTypeByValue = alertType => {
+    controlAlertType().selectByAttribute('value', alertType);
+};
+
+/**
+ * Sets the alert type of a component based off the 'Alert Type' control dropdown value
+ * @param {string} alertType
+ */
+exports.isDissmiable = () => {
+    controlIsDismissable().click();
+};
+
+/**
+ * Sets the alert heading of a component based off the 'Heading' control input value
+ * @param {string} headerText
+ */
+exports.setHeaderTextValue = headerText => {
+    controlHeading().setValue(headerText);
+};
+
+
+
