8802ed22f5c3d037e6d7d340a4c75429c552cbd0
Ben Siggery <ben.siggery@just-eat.com>
2020-12-01 16:52:31 +0000

    Failing build test

--- packages/f-alert/src/components/Alert.vue
+++ packages/f-alert/src/components/Alert.vue
@@ -18,8 +18,7 @@
                 data-test-id="alert-heading">
                 {{ heading }}
             </h2>
-            <button
-                v-if="isDismissable"
+            <button v-if="isDismissable"
                 type="button"
                 :class="[$style['c-alert-dismiss'], 'o-btn o-btn--icon']"
                 data-test-id="alert-dismiss"
