5b2d83f6050d7ad7116ee195dc3e675c86421650
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2020-12-03 14:13:08 +0000

    changes to the callback function remove brackets put in by IDE

--- packages/f-content-cards/src/components/ContentCards.vue
+++ packages/f-content-cards/src/components/ContentCards.vue
@@ -550,9 +550,9 @@ export default {
          */
         handleLogging (logger) {
             // eslint-disable-next-line func-names
-            return ((type, logMessage, payload) => {
+            return (type, logMessage, payload) => {
                 if (logger && logger[type]) logger[type](logMessage, null, payload);
-            });
+            };
         }
     }
 };
