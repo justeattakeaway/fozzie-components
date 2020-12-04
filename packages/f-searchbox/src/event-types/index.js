dfb6689d8e254cfa78a4fd00ebed8bd01301cc79
Kevin Rodrigues <kevrodrigues116@gmail.com>
2020-12-04 14:50:08 +0000

    f-searchbox@v4.0.0-beta.8 - Emit custom events for consuming applicat… (#496)
    * f-searchbox@v4.0.0-beta.8 - Emit custom events for consuming applications.
    
    * f-searchbox@v4.0.0-beta.8 - Fix up test description.
    
    * f-searchbox@v4.0.0-beta.8 - Update readme.
    
    * f-searchbox@v4.0.0-beta.8 - Update readme.
    
    * f-searchbox@v4.0.0-beta.8 - Update readme.

---
+++ packages/f-searchbox/src/event-types/index.js
@@ -0,0 +1,7 @@
+export const ADDRESS_SEARCH_FOCUS = 'address-search-focus';
+export const SUBMIT_SAVED_ADDRESS = 'submit-saved-address';
+export const SUBMIT_VALID_ADDRESS = 'submit-valid-address';
+export const SEARCHBOX_ERROR = 'searchbox-error';
+export const TRACK_POSTCODE_CHANGED = 'track-postcode-changed';
+
+
