e3cf8a862004121863e8ace56d021df0017eea26
Thomas William McClean <thomas.mcclean@just-eat.com>
2020-12-04 11:38:10 +0000

    Extract Vuex Context into Context Builder

--- packages/f-checkout/src/demo/demo.js
+++ packages/f-checkout/src/demo/demo.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import VueCheckout from '../components/Checkout.vue';
 import CheckoutMock from './checkoutMock';
 
 import i18n from '../../../storybook/context/i18n.context';
+import store from '../../../storybook/context/vuex.context';
 import '../../../storybook/context/logger.context';
 import '../../../storybook/context/cookie.context';
 
 CheckoutMock.setupDelivery('/checkout-delivery.json');
 
 Vue.config.productionTip = false;
-Vue.use(Vuex);
-
 
 /* eslint-disable no-new */
 new Vue({
     i18n,
-    store: new Vuex.Store({}),
+    store,
     render: h => h(VueCheckout, {
         props: {
             checkoutUrl: '/checkout-delivery.json'
