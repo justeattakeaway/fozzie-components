2755f70d227d88501525b20663ec641fd61ddead
Thomas William McClean <thomas.mcclean@just-eat.com>
2020-12-07 11:31:55 +0000

    f-development-context@v1.0.0 - Create service

---
+++ services/f-development-context/src/logger.context.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+
+const appendStandardProperties = (logPayload, message, level, mode) => {
+    logPayload.message = message || 'No message provided';
+    logPayload.Level = level || 'error'; // Intentionally uppercased property
+    logPayload.mode = mode || 'client';
+};
+
+const logger = {
+    logError: (logMessage, store, logPayload = {}) => {
+        appendStandardProperties(logPayload, logMessage, 'Error', 'client');
+        console.error(logMessage, logPayload);
+    },
+    logWarn: (logMessage, store, logPayload = {}) => {
+        appendStandardProperties(logPayload, logMessage, 'Warn', 'client');
+        console.warn(logMessage, logPayload);
+    },
+    logInfo: (logMessage, store, logPayload = {}) => {
+        appendStandardProperties(logPayload, logMessage, 'Info', 'client');
+        console.log(logMessage, logPayload);
+    }
+};
+
+Vue.prototype.$logger = logger;
