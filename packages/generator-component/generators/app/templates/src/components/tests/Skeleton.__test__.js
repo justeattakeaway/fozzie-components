6764aeee3e6d27f4f2f53f315a4d9b6c4867c5be
Thomas William McClean <thomas.mcclean@just-eat.com>
2020-12-07 16:39:03 +0000

    generator@v1.9.0 - Wire up Context

--- packages/generator-component/generators/app/templates/src/components/tests/Skeleton.__test__.js
+++ packages/generator-component/generators/app/templates/src/components/tests/Skeleton.__test__.js
@@ -1,10 +1,26 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { VueI18n } from '@justeat/f-globalisation';
 import <%= name.component %> from '../<%= name.filename %>.vue';
+import tenantConfigs from '../../tenants';
+
+const localVue = createLocalVue();
+
+localVue.use(VueI18n);
+
+const i18n = {
+    locale: 'en-GB',
+    messages: tenantConfigs['en-GB']
+};
 
 describe('<%= name.filename %>', () => {
+    allure.feature('<%= name.filename %>');
+
     it('should be defined', () => {
-        const propsData = {};
-        const wrapper = shallowMount(<%= name.component %>, { propsData });
+        const wrapper = shallowMount(<%= name.component %>, {
+            i18n,
+            localVue
+        });
+
         expect(wrapper.exists()).toBe(true);
     });
 });
---
+++ packages/generator-component/generators/app/templates/src/demo/demo.js
@@ -0,0 +1,7 @@
+import initialise from '@justeat/f-development-context';
+
+import <%= name.component %> from '../components/<%= name.filename %>.vue';
+
+const props = {};
+
+initialise(<%= name.component %>, props);
