74c0e8719c42e922c1caa00505a36461b56d680f
FayeCarter <faye.carter@just-eat.com>
2020-12-09 16:32:12 +0000

    f-form-field@v1.6.1 - Fix styling for grouped fields with errors (#526)
    * Fix styling
    
    * Use a beter name
    
    * Update yarn
    
    * Update yarn

--- packages/f-form-field/src/components/FormField.vue
+++ packages/f-form-field/src/components/FormField.vue
@@ -4,7 +4,7 @@
         :class="[
             $style['c-formField'], {
                 [$style['c-formField--invalid']]: hasError,
-                [$style['c-formField--grouped']]: isGrouped
+                [$style['c-formField--grouped']]: isFieldGrouped
             }
         ]"
         :data-test-id="testId.container">
@@ -196,6 +196,10 @@ export default {
 
         isSelectionControl () {
             return !(this.inputType === 'radio' || this.inputType === 'checkbox');
+        },
+
+        isFieldGrouped () {
+            return this.isGrouped && !this.hasError;
         }
     },
 
