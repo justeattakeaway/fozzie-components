0c29a6e7461aa57a5797e26485025be20d1624c2
Thomas William McClean <thomas.mcclean@just-eat.com>
2020-12-09 11:01:24 +0000

    f-registration@v0.44.0 - Remove unsupported tenants

--- packages/f-registration/src/tenants/index.js
+++ packages/f-registration/src/tenants/index.js
@@ -1,21 +1,11 @@
 import uk from './en-GB';
 import au from './en-AU';
 import nz from './en-NZ';
-import dk from './da-DK';
-import es from './es-ES';
-import ie from './en-IE';
-import it from './it-IT';
-import no from './nb-NO';
 
 const tenantConfigs = {
     'en-GB': uk,
     'en-AU': au,
-    'en-NZ': nz,
-    'da-DK': dk,
-    'es-ES': es,
-    'en-IE': ie,
-    'it-IT': it,
-    'nb-NO': no
+    'en-NZ': nz
 };
 
 export default tenantConfigs;
