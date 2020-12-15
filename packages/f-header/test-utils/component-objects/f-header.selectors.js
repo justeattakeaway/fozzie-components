6b9516aee3461d87538485fc5f5ec62f9b1ab164
Lizzie Turney <lizzie.turney@just-eat.com>
2020-12-15 11:57:14 +0000

    f-header selectors

--- packages/f-header/test-utils/component-objects/f-header.selectors.js
+++ packages/f-header/test-utils/component-objects/f-header.selectors.js
@@ -1,8 +1,7 @@
 export const HEADER_COMPONENT = '[data-test-id="header-component"]'
-
-const loginLink = () => $('[data-test-id="login-link"]');
-const offersLink = () => $$('[data-test-id="offers-link"]');
-const deliveryEnquiryLink = () => $('[data-test-id="delivery-link"]');
-const helpLink = () => $('[data-test-id="help-link"]');
-const headerLogo = () => $('[data-test-id="header-logo"]');
-const mobileNavigation = () => $('[data-test-id="nav-toggle"]');
+export const LOGIN_LINK = '[data-test-id="login-link"]'
+export const OFFERS_LINK = '[data-test-id="offers-link"]'
+export const DELIVERY_ENQUIRY_LINK = '[data-test-id="offers-link"]'
+export const HELP_LINK = '[data-test-id="help-link"]'
+export const HEADER_LOGO = '[data-test-id="header-logo"]'
+export const MOBILE_NAVIGATION = '[data-test-id="nav-toggle"]'
