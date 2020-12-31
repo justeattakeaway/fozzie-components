f334f66859fc8f18a75ea2e08f1b2715408966a3
ashleynolan <nolly00@gmail.com>
2020-12-31 15:00:40 +0000

    fozzie-components@v3.0.0 – Big folder structure change

---
+++ packages/components/molecules/f-searchbox/vue.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const rootDir = path.join(__dirname, '..', '..');
+const sassOptions = require('../../../../config/sassOptions')(rootDir);
+
+// vue.config.js
+module.exports = {
+    chainWebpack: config => {
+        config.module
+            .rule('scss-importer')
+            .test(/\.scss$/)
+            .use('importer')
+            .loader('sass-loader')
+            .options({
+                ...sassOptions,
+                /**
+                 * Requires sass-loader 7.3.1 - works out the relative path for the common.scss file for each component
+                 *
+                 * @param resourcePath
+                 * @returns {string}
+                 */
+                additionalData (content, { resourcePath }) {
+                    const levelsUpToSrc = resourcePath.split(path.sep).reverse().indexOf('src');
+
+                    const absPath = path.join(
+                        resourcePath,
+                        ...(new Array(levelsUpToSrc).fill('..')),
+                        'assets/scss/common.scss'
+                    );
+                    const relPath = path.relative(path.dirname(resourcePath), absPath)
+                        .replace(new RegExp(path.sep.replace('\\', '\\\\'), 'g'), '/');
+
+                    return `@import "${relPath}";
+                            ${content}`;
+                }
+            });
+    },
+    pluginOptions: {
+        lintStyleOnBuild: true
+    }
+};
