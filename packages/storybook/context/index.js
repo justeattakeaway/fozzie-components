7405cf47e26f77eb3ae93bbfeff221f976bbc3f8
Thomas William McClean <thomas.mcclean@just-eat.com>
2021-01-05 10:40:47 +0000

    #trivial - Prevent vuex store from being overridden

--- packages/storybook/context/index.js
+++ packages/storybook/context/index.js
@@ -1,11 +1,9 @@
 import i18nContext from './i18n.context';
-import stateContext from './vuex.context';
 import cookieContext from './cookie.context';
 import loggerContext from './logger.context';
 
 const initialise = () => {
     i18nContext();
-    stateContext();
     cookieContext();
     loggerContext();
 };
