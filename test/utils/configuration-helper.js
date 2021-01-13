bcd9fcdf9fcd006d53cf97ec4608af405b89b2a3
Ben Siggery <ben.siggery@just-eat.com>
2021-01-13 15:11:38 +0000

    Fix local spec folder paths

--- test/utils/configuration-helper.js
+++ test/utils/configuration-helper.js
@@ -1,15 +1,41 @@
 const CIRCLE_CI = process.env.CIRCLECI;
 
-exports.local = () => ({
-    specs: [
-        './test/specs/component/*.spec.js',
-        './test/specs/accessibility/*.spec.js'
-    ],
-    bail: 0,
-    allureOutputFolder: '../../../../allure-results',
-    maxinstances: 1,
-    loglevel: 'info'
-});
+
+exports.localPaths = () => {
+    if (process.env.ROOT_DIRECTORY) {
+        return {
+            specs: [
+                './packages/components/atoms/**/test/specs/component/*.component.spec.js',
+                './packages/components/molecules/**/test/specs/component/*.component.spec.js',
+                './packages/components/organisms/**/test/specs/component/*.component.spec.js',
+                './packages/components/atoms/**/test/specs/accessibility/axe-accessibility.spec.js',
+                './packages/components/molecules/**/test/specs/accessibility/axe-accessibility.spec.js',
+                './packages/components/organisms/**/test/specs/accessibility/axe-accessibility.spec.js'
+            ],
+            allureOutputFolder: './allure-results'
+        };
+    }
+
+    return {
+        specs: [
+            './test/specs/component/*.spec.js',
+            './test/specs/accessibility/*.spec.js'
+        ],
+        allureOutputFolder: '../../../../allure-results'
+    };
+};
+
+exports.local = () => {
+    const localPaths = exports.localPaths();
+
+    return {
+        specs: localPaths.specs,
+        bail: 0,
+        allureOutputFolder: localPaths.allureOutputFolder,
+        maxinstances: 1,
+        loglevel: 'info'
+    };
+};
 
 exports.ci = () => ({
     specs: [
