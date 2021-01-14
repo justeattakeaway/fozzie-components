a3010796678e0ee17dab60355e524b04751254a0
FayeCarter <faye.carter@just-eat.com>
2021-01-14 14:41:40 +0000

    f-checkout@v0.37.0 - Add guest collection and delivery views (#582)
    * Add Header and Guest block
    
    * Add Header and Guest to Checkout
    
    * Add Header tests and T&C
    
    * Add test file
    
    * Update Checkout/Guest/Header styles
    
    * Add Header and Guest tests, cleanup props
    
    * Clean up translation file
    
    * Add todos
    
    * Update version
    
    * Update class and testID names
    
    * Fix linting
    
    * Updates from review
    
    * Update version and T&C styling
    
    * Fix usernote axe-core violation
    
    * Update class names
    
    * Update date
    
    * Update changelog and test descriptions
    
    * Update test ids
    
    * Change tenant names
    
    * Update yarn.lock
    
    * Update date
    
    * Updates from review
    
    * Update comment
    
    * Updates from review

---
+++ packages/components/organisms/f-checkout/src/components/TermsAndConditions.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+        data-test-id="checkout-terms-and-conditions-component"
+        :class="$style['c-checkoutTermsAndConditions']">
+        <i18n
+            path="checkoutTermsAndConditions">
+            <template #termsAndConditions>
+                <a
+                    :href="$t('termsAndConditionsLinkUrl')"
+                    target="_blank">
+                    <span>{{ $t('termsAndConditionsLinkText') }}</span>
+                </a>
+            </template>
+            <template #privacyPolicy>
+                <a
+                    :href="$t('privacyPolicyLinkUrl')"
+                    target="_blank">
+                    <span>{{ $t('privacyPolicyLinkText') }}</span>
+                </a>
+            </template>
+            <template #cookiePolicy>
+                <a
+                    :href="$t('cookiePolicyLinkUrl')"
+                    target="_blank">
+                    <span>{{ $t('cookiePolicyLinkText') }}</span>
+                </a>
+            </template>
+        </i18n>
+    </div>
+</template>
+
+<style lang="scss" module>
+.c-checkoutTermsAndConditions {
+    width: Calc($checkout-width - 2px); // -2px for border width
+    background: $grey--offWhite;
+    margin-left: -(spacing(x10));
+    margin-bottom: -(spacing(x6));
+    margin-top: spacing(x6);
+    border-radius: 0 0 $border-radius $border-radius;
+    padding: spacing(x2);
+    @include font-size('body-s');
+    text-align: center;
+    color: $grey--dark;
+    font-weight: $font-weight-bold;
+
+    a {
+        text-decoration: none;
+        font-weight: $font-weight-bold;
+        margin-right: none;
+
+        &:hover,
+        &:focus {
+            text-decoration: underline;
+        }
+    }
+}
+</style>
