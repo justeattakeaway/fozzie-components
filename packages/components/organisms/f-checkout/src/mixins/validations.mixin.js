89d78fd6612394ba3d57e2b740cb25d5ec66cc17
Faye Carter <faye.carter@just-eat.com>
2021-01-18 15:49:57 +0000

    Add validations mixin

---
+++ packages/components/organisms/f-checkout/src/mixins/validations.mixin.js
@@ -0,0 +1,27 @@
+export default {
+    methods: {
+        /**
+        * Returns true if `field` has been touched and if it is still empty
+        * The $dirty boolean changes to true when the user has focused/lost
+        * focus on the input field.
+        *
+        * @param component {string} The component being validated. (`Address` or `Guest`)
+        * @param field {string} The field to be checked.
+        */
+        isFieldEmpty (component, field) {
+            const type = `${component}Validations`;
+            return this.$v[type][field].$dirty && !this.$v[type][field].required;
+        },
+
+        /**
+        * Dispatches action `updateAddressDetails` or `updateCustomerDetails` to update input fields values in Vuex store.
+        *
+        * @param component {string} The component being updated. (`Address` or `Guest`)
+        * @param field {string} The field to be being updated. e.g. `firstName`, `line1`
+        * @param value {string} The new value.
+        */
+        updateFulfilmentDetails (component, field, value) {
+            this.$store.dispatch(`checkout/update${component}Details`, { [field]: value });
+        }
+    }
+};
