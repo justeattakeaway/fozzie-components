605145cab0e1b6533584542e926daa73d0f653c7
Josh Ng <joshua.ng2332@gmail.com>
2021-01-18 14:52:48 +0000

    f-footer@v4.7.0 - Updated footer component with tenant tests (#604)
    * Updated footer component with tenant tests and refactored.
    
    * Changelog edit.
    
    * Updated file name.
    
    * Updated test naming.
    
    * Updated test naming.
    
    Co-authored-by: Joshua Ng <joshua.ng@just-eat.com>

---
+++ packages/components/organisms/f-footer/test/specs/component/f-footer-au.component.spec.js
@@ -0,0 +1,24 @@
+import FooterComponent from '../../../test-utils/component-objects/f-footer.component';
+const path = 'iframe.html?id=components-organisms--' // storybook url for all components - could move to config.
+const locale = '&knob-Locale=en-AU'
+const links = '&knob-Show%20courier%20links=true'
+
+describe('f-footer component tests', () => {
+    beforeEach(() => {
+        browser.url(`${path}footer-component${locale}`);
+        FooterComponent.waitForFooter();
+    });
+
+    it('Should not show courier links on en-AU if courier links is set to false', () => {
+        // Assert
+        expect(FooterComponent.isCourierLinksDisplayed()).toBe(false);
+    });
+
+    it('Should show courier links on en-AU locale', () => {
+        // Act
+        browser.url(`${path}footer-component${locale}${links}`);
+
+        // Assert
+        expect(FooterComponent.isCourierLinksDisplayed()).toBe(true);
+    });
+});
