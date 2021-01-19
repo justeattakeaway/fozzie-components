6baa0e431b9d1572356c5fad8cd46a3f4d24b8fa
Ben Siggery <ben.siggery@just-eat.com>
2021-01-19 15:22:20 +0000

    circleci change

--- packages/components/organisms/f-checkout/stories/Checkout.stories.js
+++ packages/components/organisms/f-checkout/stories/Checkout.stories.js
@@ -47,7 +47,7 @@ export const CheckoutComponent = () => ({
             default: select('Auth token', ['authToken', null], 'authToken')
         },
         loginUrl: {
-            default: text('Login Url', '/login')
+            default: text('foo Url', '/login')
         }
     },
     store: new Vuex.Store({}),
