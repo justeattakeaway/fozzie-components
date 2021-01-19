2372f5ea6c3acd923e408e1e2329d97151008aaa
Ben Siggery <ben.siggery@just-eat.com>
2021-01-19 15:25:04 +0000

    Undo storybook file change

--- packages/components/organisms/f-checkout/stories/Checkout.stories.js
+++ packages/components/organisms/f-checkout/stories/Checkout.stories.js
@@ -47,7 +47,7 @@ export const CheckoutComponent = () => ({
             default: select('Auth token', ['authToken', null], 'authToken')
         },
         loginUrl: {
-            default: text('foo Url', '/login')
+            default: text('Login Url', '/login')
         }
     },
     store: new Vuex.Store({}),
