e23e5273dbce621019a084875490cb7125e365d9
Simon Sanders <simon.sanders@just-eat.com>
2021-01-28 11:18:36 +0000

    Correct typo in IT.

--- packages/components/organisms/f-cookie-banner/src/tenants/it-IT.js
+++ packages/components/organisms/f-cookie-banner/src/tenants/it-IT.js
@@ -5,7 +5,7 @@ const messages = {
     acceptButtonText: 'Accetta tutti i cookie',
     textLine1: 'Utilizziamo i cookie necessari (e tecnologie simili) per far funzionare la nostra piattaforma.',
     textLine2: 'Vorremmo anche utilizzare cookies facoltativi che ci aiutano a migliorare la nostra piattaforma raccogliendo informazioni su come la utilizzi.',
-    textLine3: 'Per i dettagli sui cookie e sulle tecnologie che utilizziamo puoi leggere {cookiePolicy}. L\'utilizzo di questo banner imposter&aacute; un cookie sul tuo dispositivo per ricordare le tue preferenze.',
+    textLine3: 'Per i dettagli sui cookie e sulle tecnologie che utilizziamo puoi leggere {cookiePolicy}. L\'utilizzo di questo banner imposterá; un cookie sul tuo dispositivo per ricordare le tue preferenze.',
     cookiePolicyLinkText: 'la nostra informativa sui cookie',
     cookiePolicyLinkUrl: 'https://www.justeat.it/informazioni/politica-dei-cookie'
 };
