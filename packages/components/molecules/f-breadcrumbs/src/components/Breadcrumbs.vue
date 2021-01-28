264ad7600ea8e9c0606cb5d3a8264e49f61d25f8
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-01-28 16:53:34 +0000

    first version of the breadcrumbs

--- packages/components/molecules/f-breadcrumbs/src/components/Breadcrumbs.vue
+++ packages/components/molecules/f-breadcrumbs/src/components/Breadcrumbs.vue
@@ -1,8 +1,22 @@
 <template>
-    <div
-        :class="$style['c-breadcrumbs']"
-        data-test-id="breadcrumbs-component">
-        {{ copy.text }}
+    <div :class="$style['c-breadcrumbs']">
+        <ul :class="$style['c-breadcrumbs-list']">
+            <li :class="$style['c-breadcrumbs-item']">
+                <a :class="$style['c-breadcrumbs-link']">Home</a>
+            </li>
+            <li :class="$style['c-breadcrumbs-separator']">
+                >
+            </li>
+            <li :class="$style['c-breadcrumbs-item']">
+                <a :class="$style['c-breadcrumbs-link']">For You</a>
+            </li>
+            <li :class="$style['c-breadcrumbs-separator']">
+                >
+            </li>
+            <li :class="$style['c-breadcrumbs-item']">
+                <a :class="[$style['c-breadcrumbs-link'], $style['c-breadcrumbs-link--active']]">Stamp Cards</a>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -17,6 +31,10 @@ export default {
         locale: {
             type: String,
             default: ''
+        },
+        links: {
+            type: Array,
+            default: () => []
         }
     },
     data () {
@@ -32,15 +50,58 @@ export default {
 
 <style lang="scss" module>
 
+$breadcrumbs-text-colour: $white;
+$breadcrumbs-background-colour: rgba($black, 0.6);
+$breadcrumbs-border-radius: 16px;
+$breadcrumbs-not-active-font-weight: $font-weight-bold;
+$breadcrumbs-active-font-weight: $font-weight-base;
+
 .c-breadcrumbs {
     display: flex;
-    justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid $red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
+}
+.c-breadcrumbs-list {
+    list-style: none;
+    background-color: $breadcrumbs-background-colour;
+    border-radius: $breadcrumbs-border-radius;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+}
+.c-breadcrumbs-item {
+    padding: spacing(x0.5) spacing(x2);
+    @include media('<narrowMid') {
+        padding: spacing(x0.5) spacing(x2) spacing(x0.5) spacing();
+    }
+}
+.c-breadcrumbs-item, .c-breadcrumbs-separator {
+    @include media('<narrowMid'){
+        display: none;
+        &:nth-last-child(-n+4):not(:nth-last-child(-n+2)) {
+            display: block;
+        }
+    }
+}
+.c-breadcrumbs-link {
+    color: $breadcrumbs-text-colour;
+    font-weight: $breadcrumbs-not-active-font-weight;
+    cursor: pointer;
+    &:hover {
+        text-decoration: underline;
+    }
+}
+.c-breadcrumbs-separator {
+    color: $breadcrumbs-text-colour;
+    transform: scale(0.6, 1.2);
+    @include media('<narrowMid') {
+        margin-left: spacing(x1.5);
+        transform: scale(0.6, 1.2) rotate(180deg);
+        margin-top: 2px;
+    }
+}
+.c-breadcrumbs-link--active {
+    font-weight: $breadcrumbs-active-font-weight;
 }
 
 </style>
