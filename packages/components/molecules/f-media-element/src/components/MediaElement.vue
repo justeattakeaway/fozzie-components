dba3d801636afa7199387f2597eb734f7bdb1325
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-01-29 10:58:31 +0000

    generated ouput for media element component

---
+++ packages/components/molecules/f-media-element/src/components/MediaElement.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+        :class="$style['c-mediaElement']"
+        data-test-id="mediaElement">
+        {{ copy.text }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MediaElement',
+    components: {},
+    props: {
+    },
+    data () {
+    }
+};
+</script>
+
+<style lang="scss" module>
+
+.c-mediaElement {
+    display: flex;
+    justify-content: center;
+    min-height: 80vh;
+    width: 80vw;
+    margin: auto;
+    border: 1px solid $red;
+    font-family: $font-family-base;
+    @include font-size(heading-m);
+}
+
+</style>
