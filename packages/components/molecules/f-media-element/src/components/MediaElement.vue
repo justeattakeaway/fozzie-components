0532064d4e3d6eb9cff01522154ced48938c48f6
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-01-29 15:19:32 +0000

    changes to add styles and base component

--- packages/components/molecules/f-media-element/src/components/MediaElement.vue
+++ packages/components/molecules/f-media-element/src/components/MediaElement.vue
@@ -1,18 +1,93 @@
 <template>
     <div
-        :class="$style['c-mediaElement']"
-        data-test-id="mediaElement-component">
-        {{ componentName }}
+        data-test-id="mediaElement-component"
+        :class="[$style['c-mediaElement'], { [$style['c-mediaElement--stack']]: stacked }, { [$style['c-mediaElement--reverse']]: reverse }]">
+        <div :class="[$style['c-mediaElement-content'], contentAlignClass]">
+            <h3 :class="$style['c-mediaElement-title']">
+                {{ title }}
+            </h3>
+            <p :class="$style['c-mediaElement-text']">
+                {{ text }}
+            </p>
+        </div>
+        <div :class="[$style['c-mediaElement-imgWrapper'], imageAlignClass]">
+            <img
+                :class="$style['c-mediaBlock-img']"
+                src="https://via.placeholder.com/250"
+                alt="test image">
+        </div>
     </div>
 </template>
 
 <script>
 
+const ALIGN = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    CENTER: 'center'
+};
+
 export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        imageUrl:  {
+            type: String,
+            required: true
+        },
+        stacked: {
+            type: Boolean,
+            default: false
+        },
+        reverse: {
+            type: Boolean,
+            default: false
+        },
+        contentAlign: {
+            type: String,
+            default: ALIGN.LEFT
+        },
+        imageAlign: {
+            type: String,
+            default: ALIGN.LEFT
+        }
+    },
     data () {
         return {
             componentName: 'MediaElement'
         };
+    },
+    computed: {
+        contentAlignClass () {
+            switch (this.contentAlign) {
+                case ALIGN.LEFT:
+                    return this.$style['c-mediaElement-content--left'];
+                case ALIGN.RIGHT:
+                    return this.$style['c-mediaElement-content--right'];
+                case ALIGN.CENTER:
+                    return this.$style['c-mediaElement-content--center'];
+                default:
+                    return this.$style['c-mediaElement-content--left'];
+            }
+        },
+        imageAlignClass () {
+            switch (this.imageAlign) {
+                case ALIGN.LEFT:
+                    return this.$style['c-mediaElement-imgWrapper--left'];
+                case ALIGN.RIGHT:
+                    return this.$style['c-mediaElement-imgWrapper--right'];
+                case ALIGN.CENTER:
+                    return this.$style['c-mediaElement-imgWrapper--center'];
+                default:
+                    return this.$style['c-mediaElement-imgWrapper--left'];
+            }
+        }
     }
 };
 </script>
@@ -21,13 +96,69 @@ export default {
 
 .c-mediaElement {
     display: flex;
+    width: 100%;
+}
+.c-mediaElement--stack {
+    flex-direction: column;
+}
+.c-mediaElement--stack.c-mediaElement--reverse {
+    flex-direction: column-reverse;
+}
+
+.c-mediaElement-content {
+    display: flex;
+    flex-direction: column;
     justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid $red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
+    flex: 1 1 0;
+
+    &--center {
+        align-items: center;
+        text-align: center;
+    }
+    &--left {
+        align-items: flex-start;
+    }
+    &--right {
+        align-items: flex-end;
+    }
+}
+
+.c-mediaElement-title {
+    @include font-size(subheading-l);
+}
+
+.c-mediaElement--stack .c-mediaElement-title {
+    margin-top: spacing(x3);
+}
+
+.c-mediaElement-text {
+    @include font-size(body-l);
+    margin-top: spacing(x0.5);
+}
+
+.c-mediaElement-imgWrapper {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0;
+
+    &--center {
+        align-items: center;
+        .c-mediaBlock-img {
+            align-self: center;
+        }
+    }
+    &--left {
+        align-items: flex-start;
+        .c-mediaBlock-img {
+            align-self: flex-start;
+        }
+    }
+    &--right {
+        align-items: flex-end;
+        .c-mediaBlock-img {
+            align-self: flex-end;
+        }
+    }
 }
 
 </style>
--- packages/components/molecules/f-media-element/stories/MediaElement.stories.js
+++ packages/components/molecules/f-media-element/stories/MediaElement.stories.js
@@ -10,11 +10,20 @@ export default {
     decorators: [withA11y]
 };
 
-export const MediaElementComponent = () => ({
+export const MediaElementComponent = (args, { argTypes }) => ({
     components: { MediaElement },
-    props: {
-    },
-    template: `<media-element />`
+    props: Object.keys(argTypes),
+    template: '<media-element v-bind="$props" />'
 });
 
 MediaElementComponent.storyName = 'f-media-element';
+
+MediaElementComponent.args = {
+    title: 'Earn',
+    text: 'Each stamp you collect from a restaurant is worth up to 15% of your order’s value and will be saved under its own StampCard.',
+    stacked: true,
+    reverse: true,
+    contentAlign: 'center',
+    imageAlign: 'center',
+    imageUrl: 'https://via.placeholder.com/250'
+};
