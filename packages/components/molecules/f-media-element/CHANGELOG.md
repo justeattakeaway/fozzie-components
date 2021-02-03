3824c78f149f371f163e366e1cb975c94be2c438
matthewhardern <matthewhardern@gmail.com>
2021-02-03 15:31:41 +0000

    f-media-element@0.2.0 - layout +  styles + components (#667)
    * changes to add styles and base component
    
    * version change
    
    * changes to media element
    
    * change the way the text sizing works
    
    * changelog addition plus rebase from master
    
    * font size change
    
    * fixes based on PR suggestions
    
    * update to the method of handling the font sizes, added alt tag prop and added some modifier descriptions
    
    * change to address PR suggestions
    
    Co-authored-by: Matthew Hardern <matthew.hardern@justeattakeaway.com>

--- packages/components/molecules/f-media-element/CHANGELOG.md
+++ packages/components/molecules/f-media-element/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.2.0
+------------------------------
+*February 1, 2021*
+
+### Added
+- Add the initial start of the component, which includes styles, base component and story changes
+
+
 v0.1.0
 ------------------------------
 *January 29, 2021*
--- packages/components/molecules/f-media-element/stories/MediaElement.stories.js
+++ packages/components/molecules/f-media-element/stories/MediaElement.stories.js
@@ -4,17 +4,41 @@
 // } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 import MediaElement from '../src/components/MediaElement.vue';
+import { ALIGN, FONT_SIZE } from '../src/constants';
 
 export default {
     title: 'Components/Molecules',
     decorators: [withA11y]
 };
 
-export const MediaElementComponent = () => ({
+export const MediaElementComponent = (args, { argTypes }) => ({
     components: { MediaElement },
-    props: {
-    },
-    template: `<media-element />`
+    props: Object.keys(argTypes),
+    template: '<media-element v-bind="$props" />'
 });
 
 MediaElementComponent.storyName = 'f-media-element';
+
+MediaElementComponent.args = {
+    title: 'Stampcards',
+    text: 'See the stamps you’ve collected and any discounts you’ve earned.',
+    stacked: false,
+    reverse: true,
+    altText: 'Test alt text',
+    imageUrl: 'https://via.placeholder.com/250',
+};
+
+MediaElementComponent.argTypes = {
+    textSize: {
+        control: { type: 'select', options: [...Object.entries(FONT_SIZE).map(([, value]) => value)] },
+        defaultValue: 'xxl'
+    },
+    contentAlign: {
+        control: { type: 'select', options: [...Object.entries(ALIGN).map(([, value]) => value)] },
+        defaultValue: 'left'
+    },
+    imageAlign: {
+        control: { type: 'select', options: [...Object.entries(ALIGN).map(([, value]) => value)] },
+        defaultValue: 'center'
+    }
+};
