2c35dae4579f161914a555a0155d51bc8fc8fac5
Xander Marjoram <xander.marjoram@just-eat.com>
2021-02-04 14:58:54 +0000

    f-checkout@0.48.0 - Include user note in PATCH request (#671)
    * - Reduce nesting within state by moving address and time out of fulfilment
    - Update checkout post URL
    - Fill out checkout post data
    - Use latest f-form-field with separate text/value
    - Update tests
    
    * Update checkout method from POST to PATCH
    and remove redundant `method` property from request configs
    
    * Versioning for PR
    
    * Minor: Add missing changes
    
    * Fix checkout path, and add checkoutId prop
    
    * Fix component test
    
    * Update tests, address PR comments and add mutation-types
    
    * Minor: Update changelog date
    
    * Re-enable test
    
    * Replace notes with userNote in state
    
    * Pull creation of checkout patch object into a mapper service
    And fix tests
    
    * Fix test that I thought I had already fixed
    
    * Fix import and test mapper
    
    * Add user note state tests, and call action directly
    
    * Pass in built-up object rather than passing `this` directly into the mapper
    
    * JSdoc updates
    
    * Version updates
    
    * Clearer name for mapper function
    
    * Wrap test in describe block

---
+++ packages/components/organisms/f-checkout/src/services/tests/mapper.test.js
@@ -0,0 +1,97 @@
+import mapUpdateCheckoutRequest from '../mapper';
+
+const defaultParams = {
+    address: {},
+    customer: {},
+    isCheckoutMethodDelivery: true,
+    time: {},
+    userNote: ''
+};
+
+describe('checkout mapper', () => {
+    it('should map customer details correctly', () => {
+        // Arrange
+        const customer = {
+            firstName: 'Test',
+            lastName: 'Tester',
+            mobileNumber: '07890123456'
+        };
+
+        // Act
+        const requestBody = mapUpdateCheckoutRequest({
+            ...defaultParams,
+            customer
+        });
+
+        // Assert
+        expect(requestBody.customer.firstName).toBe(customer.firstName);
+        expect(requestBody.customer.lastName).toBe(customer.lastName);
+        expect(requestBody.customer.phoneNumber).toBe(customer.mobileNumber);
+        expect(requestBody.customer.dateOfBirth).toBe(null);
+    });
+
+    it('should map address correctly', () => {
+        // Arrange
+        const address = {
+            line1: '1 Bristol Road',
+            line2: 'Flat 1',
+            city: 'Bristol',
+            postcode: 'BS1 1AA'
+        };
+
+        // Act
+        const requestBody = mapUpdateCheckoutRequest({
+            ...defaultParams,
+            address
+        });
+
+        // Assert
+        expect(requestBody.fulfilment.location.address.postalCode).toBe(address.postcode);
+        expect(requestBody.fulfilment.location.address.lines).toStrictEqual([
+            address.line1,
+            address.line2,
+            '',
+            address.city,
+            ''
+        ]);
+    });
+
+    describe('when checkout method is not delivery', () => {
+        const isCheckoutMethodDelivery = false;
+
+        it('should not map the address', () => {
+            // Arrange
+            const address = {
+                line1: '1 Bristol Road',
+                line2: 'Flat 1',
+                city: 'Bristol',
+                postcode: 'BS1 1AA'
+            };
+
+            // Act
+            const requestBody = mapUpdateCheckoutRequest({
+                ...defaultParams,
+                address,
+                isCheckoutMethodDelivery
+            });
+
+            // Assert
+            expect(requestBody.fulfilment.location.address).toBeUndefined();
+        });
+    });
+
+    it('should map user note correctly', () => {
+        // Arrange
+        const userNote = 'Beware of the dachshund';
+
+        // Act
+        const requestBody = mapUpdateCheckoutRequest({
+            ...defaultParams,
+            userNote
+        });
+
+        // Assert
+        expect(requestBody.notes.length).toBe(1);
+        expect(requestBody.notes[0].note).toBe(userNote);
+    });
+});
