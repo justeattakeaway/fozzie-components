22da870bc7f9163035687999e02d81e619d1133d
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-02-04 11:29:02 +0000

    begining of unit tests

---
+++ packages/components/molecules/f-tabs/src/components/tests/Tab.test.js
@@ -0,0 +1,27 @@
+
+
+describe('Tab.vue', () => {
+    it('should be defined', () => {
+
+    });
+    it('should call the register callback method ', () => {
+
+    });
+    it('should supply the correct object to the callback method', () => {
+
+    });
+    it('should show only if isActive is true', () => {
+
+    });
+    describe('Animation', () => {
+        it('should only animate when animateTab is true', () => {
+
+        });
+        it('should apply the fade-in-right class when animationDirection is LEFT', () => {
+
+        });
+        it('should apply the fade-in-left class when animationDirection is RIGHT', () => {
+
+        });
+    });
+});
--- packages/components/molecules/f-tabs/src/components/tests/Tabs.test.js
+++ packages/components/molecules/f-tabs/src/components/tests/Tabs.test.js
@@ -7,4 +7,26 @@ describe('Tabs', () => {
         const wrapper = shallowMount(VueTabs, { propsData });
         expect(wrapper.exists()).toBe(true);
     });
+
+    describe('Tabs registration', () => {
+        it('should register a tab when addTab method is called', () => {
+
+        });
+        it('should display a button for each registered tab', () => {
+
+        });
+    });
+
+    describe('Tabs selection', () => {
+        it('should apply a active class to the selected tab when selectTabIndex method is called', () => {
+
+        });
+        it('should set the direction to RIGHT when new index > old index', () => {
+
+        });
+        it('should set the direction to LEFT when new index <= old index', () => {
+
+        });
+    });
+
 });
