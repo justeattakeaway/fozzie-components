7a56026b9d13146c86a5f4086d8c02ec8723a57f
Sacha <sacha.zvetelman@just-eat.com>
2021-02-05 09:28:10 +0000

    f-checkout@0.49.0 - Initial APIs failure user-friendly error page. (#672)
    * f-checkout - Adding a basic error page.
    
    * f-checkout - Adding tests and refactoring existing code.
    
    * f-checkout - Refactoring, tests, and more!
    
    * f-checkout - Updating version.
    
    * f-checkout - Fixing linting issues.
    
    * f-checkout - Code review improvements.
    
    * f-checkout - Refactoring to be more specific on class names and override the default styles.
    
    * f-checkout - Updated version

--- packages/components/organisms/f-checkout/src/components/Checkout.vue
+++ packages/components/organisms/f-checkout/src/components/Checkout.vue
@@ -1,70 +1,75 @@
 <template>
-    <div
-        data-theme="jet"
-        data-test-id="checkout-component">
-        <alert
-            v-if="genericErrorMessage"
-            type="danger"
-            :class="$style['c-checkout-alert']"
-            :heading="$t('errorMessages.errorHeading')">
-            {{ genericErrorMessage }}
-        </alert>
+    <div>
+        <div
+            v-if="hasCheckoutLoadedSuccessfully"
+            data-theme="jet"
+            data-test-id="checkout-component">
+            <alert
+                v-if="genericErrorMessage"
+                type="danger"
+                :class="$style['c-checkout-alert']"
+                :heading="$t('errorMessages.errorHeading')">
+                {{ genericErrorMessage }}
+            </alert>
 
-        <card
-            is-rounded
-            has-outline
-            is-page-content-wrapper
-            card-heading-position="center"
-            data-test-id="checkout-card-component"
-            :class="$style['c-checkout']">
-            <checkout-header
-                :login-url="loginUrl" />
+            <card
+                is-rounded
+                has-outline
+                is-page-content-wrapper
+                card-heading-position="center"
+                data-test-id="checkout-card-component"
+                :class="$style['c-checkout']">
+                <checkout-header
+                    :login-url="loginUrl" />
 
-            <form
-                method="post"
-                :class="$style['c-checkout-form']"
-                @submit.prevent="onFormSubmit">
-                <guest-block v-if="!isLoggedIn" />
+                <form
+                    method="post"
+                    :class="$style['c-checkout-form']"
+                    @submit.prevent="onFormSubmit">
+                    <guest-block v-if="!isLoggedIn" />
 
-                <form-field
-                    :value="customer.mobileNumber"
-                    name="mobile-number"
-                    :label-text="$t('labels.mobileNumber')"
-                    :has-error="!isMobileNumberValid"
-                    @input="updateCustomerDetails({ mobileNumber: $event })">
-                    <template #error>
-                        <error-message
-                            v-if="!isMobileNumberValid"
-                            data-test-id="error-mobile-number">
-                            {{ $t('validationMessages.mobileNumber.requiredError') }}
-                        </error-message>
-                    </template>
-                </form-field>
+                    <form-field
+                        :value="customer.mobileNumber"
+                        name="mobile-number"
+                        :label-text="$t('labels.mobileNumber')"
+                        :has-error="!isMobileNumberValid"
+                        @input="updateCustomerDetails({ mobileNumber: $event })">
+                        <template #error>
+                            <error-message
+                                v-if="!isMobileNumberValid"
+                                data-test-id="error-mobile-number">
+                                {{ $t('validationMessages.mobileNumber.requiredError') }}
+                            </error-message>
+                        </template>
+                    </form-field>
 
-                <address-block
-                    v-if="isCheckoutMethodDelivery"
-                    data-test-id="address-block" />
+                    <address-block
+                        v-if="isCheckoutMethodDelivery"
+                        data-test-id="address-block" />
 
-                <form-selector />
+                    <form-selector />
 
-                <user-note
-                    data-test-id="user-note"
-                    @input="updateUserNote($event.target.value)" />
+                    <user-note
+                        data-test-id="user-note"
+                        @input="updateUserNote($event.target.value)" />
 
-                <f-button
-                    :class="$style['c-checkout-submitButton']"
-                    button-type="primary"
-                    button-size="large"
-                    is-full-width
-                    action-type="submit"
-                    data-test-id="confirm-payment-submit-button">
-                    {{ $t('buttonText') }}
-                </f-button>
-            </form>
+                    <f-button
+                        :class="$style['c-checkout-submitButton']"
+                        button-type="primary"
+                        button-size="large"
+                        is-full-width
+                        action-type="submit"
+                        data-test-id="confirm-payment-submit-button">
+                        {{ $t('buttonText') }}
+                    </f-button>
+                </form>
 
-            <checkout-terms-and-conditions
-                v-if="!isLoggedIn" />
-        </card>
+                <checkout-terms-and-conditions
+                    v-if="!isLoggedIn" />
+            </card>
+        </div>
+
+        <error-page v-else />
     </div>
 </template>
 
@@ -93,6 +98,7 @@ import CheckoutTermsAndConditions from './TermsAndConditions.vue';
 import FormSelector from './Selector.vue';
 import GuestBlock from './Guest.vue';
 import UserNote from './UserNote.vue';
+import ErrorPage from './Error.vue';
 
 import { CHECKOUT_METHOD_DELIVERY, TENANT_MAP, VALIDATIONS } from '../constants';
 import checkoutValidationsMixin from '../mixins/validations.mixin';
@@ -110,6 +116,7 @@ export default {
         Card,
         CheckoutHeader,
         CheckoutTermsAndConditions,
+        ErrorPage,
         ErrorMessage,
         FormField,
         FormSelector,
@@ -182,7 +189,8 @@ export default {
         return {
             tenantConfigs,
             genericErrorMessage: null,
-            shouldDisableCheckoutButton: false
+            shouldDisableCheckoutButton: false,
+            hasCheckoutLoadedSuccessfully: true
         };
     },
 
@@ -343,6 +351,7 @@ export default {
                 this.$emit(EventNames.CheckoutGetSuccess); // TODO: Check these emitted events.
             } catch (thrownErrors) {
                 this.$emit(EventNames.CheckoutGetFailure, thrownErrors); // TODO: Check these emitted events.
+                this.hasCheckoutLoadedSuccessfully = false;
             }
         },
 
@@ -362,6 +371,7 @@ export default {
                 this.$emit(EventNames.CheckoutBasketGetSuccess); // TODO: Check these emitted events.
             } catch (thrownErrors) {
                 this.$emit(EventNames.CheckoutBasketGetFailure, thrownErrors); // TODO: Check these emitted events.
+                this.hasCheckoutLoadedSuccessfully = false;
             }
         },
 
@@ -379,6 +389,7 @@ export default {
                 this.$emit(EventNames.CheckoutAvailableFulfilmentGetSuccess); // TODO: Check these emitted events.
             } catch (thrownErrors) {
                 this.$emit(EventNames.CheckoutAvailableFulfilmentGetFailure, thrownErrors); // TODO: Check these emitted events.
+                this.hasCheckoutLoadedSuccessfully = false;
             }
         },
 
