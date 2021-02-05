56e895075aef40572ec4326ef97b129ffea24e47
neha.dhanuka <neha.dhanuka@je-labs.com>
2021-02-05 13:07:54 +0000

    suggestion

--- packages/tools/f-vue-icons/icons/MoreVerticalIcon.js
+++ packages/tools/f-vue-icons/icons/MoreVerticalIcon.js
@@ -9,12 +9,14 @@ export default {
     return h("svg", _mergeJSXProps([{
       attrs: {
         xmlns: "http://www.w3.org/2000/svg",
-        viewBox: "0 0 20 20"
+        viewBox: "0 0 24 24"
       },
-      "class": "c-ficon c-ficon--moreverticalicon"
+      "class": "c-ficon c-ficon--more-vertical"
     }, ctx.data]), [h("path", {
       attrs: {
-        d: "M12 8a2 2 0 100-4 2 2 0 000 4zm0 2a2 2 0 100 4 2 2 0 000-4zm0 6a2 2 0 100 4 2 2 0 000-4z"
+        d: "M12 8a2 2 0 100-4 2 2 0 000 4zm0 2a2 2 0 100 4 2 2 0 000-4zm0 6a2 2 0 100 4 2 2 0 000-4z",
+        fill: "#000",
+        "fill-rule": "evenodd"
       }
     })]);
   }
