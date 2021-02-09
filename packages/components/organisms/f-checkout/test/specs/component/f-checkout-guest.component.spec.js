5d4a2f7f3cd221403b01b9c2411862305e55474f
Nicholas "Jonkers" John <Nicholas.John@just-eat.com>
2021-02-09 09:15:01 +0000

    Added tests to guest checkout

--- packages/components/organisms/f-checkout/test/specs/component/f-checkout-guest.component.spec.js
+++ packages/components/organisms/f-checkout/test/specs/component/f-checkout-guest.component.spec.js
@@ -47,7 +47,6 @@ describe('f-checkout component tests', () => {
         expect(CheckoutComponent.isFieldErrorDisplayed('emailAddress')).toBe(true);
     });
 
-
     it('should navigate to correct url when the login link is clicked', () => {
         // Arrange
         const loginPath = '/login';
@@ -58,5 +57,58 @@ describe('f-checkout component tests', () => {
 
         // Assert
         expect(pathname).toEqual(loginPath);
+    });
+
+    it('should display times in ascending order, with default text "As soon as possible" showing first', () => {
+        // Act
+        CheckoutComponent.selectOrderTime('As soon as possible');
+
+        // Assert
+        expect(CheckoutComponent.isOrderTimeDropdownDisplayed()).toBe(true);
+        expect(CheckoutComponent.getOrderTimeOptionText(0)).toBe('As soon as possible');
+        expect(CheckoutComponent.getOrderTimeOptionText(1)).toBe('Wednesday 00:45');
+        expect(CheckoutComponent.getOrderTimeOptionText(2)).toBe('Wednesday 01:00');
+    });
+
+    it('should display a "mobileNumber" error message when an unsupported country code is used in the mobile number field', () => {
+        // Arrange
+        const addressDetails = {
+            mobileNumber: '+8112345678911'
+        };
+
+        // Act
+        CheckoutComponent.populateCheckoutForm(addressDetails);
+        CheckoutComponent.goToPayment();
+
+        // Assert
+        expect(CheckoutComponent.isFieldErrorDisplayed('mobileNumber')).toBe(true);
+    });
+
+    it('should not display a "mobileNumber" error message when a number is formatted with a supported country code', () => {
+        // Arrange
+        const addressDetails = {
+            mobileNumber: '+4412345678911'
+        };
+
+        // Act
+        CheckoutComponent.populateCheckoutForm(addressDetails);
+        CheckoutComponent.goToPayment();
+
+        // Assert
+        expect(CheckoutComponent.isFieldErrorDisplayed('mobileNumber')).toBe(false);
+    });
+
+    it('should prevent a user from writing a note of over 200 characters', () => {
+        // Arrange
+        const userNote = 'A';
+        const addressInfo = {
+            note: userNote.repeat(300)
+        };
+
+        // Act
+        CheckoutComponent.inputUserNote(addressInfo);
+
+        // Assert
+        expect(CheckoutComponent.userNoteMaxCharacterCount()).toEqual('200');
     });
 });
