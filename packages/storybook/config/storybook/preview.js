a6525517405b74e404145edb1b2257ab71e7c689
Ashley Watson-Nolan <nolly00@gmail.com>
2021-02-12 15:55:03 +0000

    fozzie-components@v3.13.0 – Contributing and Dependency guides (#720)

--- packages/storybook/config/storybook/preview.js
+++ packages/storybook/config/storybook/preview.js
@@ -19,6 +19,7 @@ export const parameters = {
                     [
                         'Intro',
                         'Development Principles',
+                        'Contributing',
                         'Troubleshooting'
                     ],
                     'Standards',
