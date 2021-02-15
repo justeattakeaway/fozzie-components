897321ddf52375911a1a299b52460c87f420fa60
Pawel Kedziora <6950110+pkedziora@users.noreply.github.com>
2021-02-15 14:34:35 +0000

    f-checkout@0.57.0: Logic to choose closest address for uk tenant (#722)
    * f-checkout: addressService
    
    * f-checkout: Align address choosing logic
    
    * f-checkout: Tests
    
    * f-checkout: addressService fixes
    
    * f-checkout: More address tests
    
    * f-checkout: Fix
    
    * f-checkout: changelog
    
    * f-checkout: Case fix
    
    Co-authored-by: Pawel Kedziora <pawel.kedziora@just-eat.com>

--- packages/components/organisms/f-checkout/src/store/checkout.module.js
+++ packages/components/organisms/f-checkout/src/store/checkout.module.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import addressService from '../services/addressService';
 
 import {
     UPDATE_AUTH,
@@ -190,6 +191,7 @@ export default {
          */
         getAddress: async ({ commit, state }, {
             url,
+            tenant,
             language,
             timeout
         }) => {
@@ -207,15 +209,7 @@ export default {
 
             const { data } = await axios.get(url, config);
 
-            // TODO: Implement logic to select best address. For now, select the first address
-            const [selectedAddress] = data.Addresses;
-
-            const addressDetails = {
-                line1: selectedAddress.Line1,
-                line2: selectedAddress.Line2,
-                city: selectedAddress.City,
-                postcode: selectedAddress.ZipCode
-            };
+            const addressDetails = addressService.getClosestAddress(data.Addresses, tenant);
 
             commit(UPDATE_FULFILMENT_ADDRESS, addressDetails);
         },
