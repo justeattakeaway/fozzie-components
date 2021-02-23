e4b609aa885eba19700fb0e5cf8f3bb7f0d9b642
Lizzie Turney <lizzie.turney@just-eat.com>
2021-02-23 09:04:32 +0000

    refactored accessibility tests

--- packages/components/organisms/f-checkout/test/specs/accessibility/axe-accessibility.spec.js
+++ packages/components/organisms/f-checkout/test/specs/accessibility/axe-accessibility.spec.js
@@ -7,21 +7,36 @@ describe('Accessibility tests', () => {
 
     it('a11y - should test f-checkout component (delivery) WCAG compliance', () => {
         // Act
-        checkout.open('delivery');
+        const checkoutData = {
+            type: 'delivery', 
+            isAuthenticated: true, 
+            isValid: true
+        }
+        checkout.open(checkoutData);
         checkout.waitForComponent();
         const axeResults = getAccessibilityTestResults('f-checkout-delivery');
     });
 
     it('a11y - should test f-checkout component (collection) WCAG compliance', () => {
         // Act
-        checkout.open('collection');
+        const checkoutData = {
+            type: 'collection', 
+            isAuthenticated: true, 
+            isValid: true
+        }
+        checkout.open(checkoutData);
         checkout.waitForComponent();
         const axeResults = getAccessibilityTestResults('f-checkout-collection');
     });
 
     it('a11y - should test f-checkout component (guest) WCAG compliance', () => {
         // Act
-        checkout.open('guest');
+        const checkoutData = {
+            type: 'delivery', 
+            isAuthenticated: false, 
+            isValid: true
+        }
+        checkout.open(checkoutData);
         checkout.waitForComponent();
         const axeResults = getAccessibilityTestResults('f-checkout-guest');
     });
