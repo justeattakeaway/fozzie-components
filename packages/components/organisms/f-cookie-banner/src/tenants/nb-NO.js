c1d848e28032f9a1747a6d4221296be5ef54f35a
simonsandersje <simon.sanders@just-eat.com>
2021-02-25 15:15:52 +0000

    f-cookie-banner@v0.3.0 - remove f-globalisation mixin (#747)
    * Update tests.
    
    * f-cookie-banner@v0.1.1 - Remove f-globalisation mixin.
    
    * f-cookie-banner@v0.3.0 - Bump to minor version.

--- packages/components/organisms/f-cookie-banner/src/tenants/nb-NO.js
+++ packages/components/organisms/f-cookie-banner/src/tenants/nb-NO.js
@@ -5,7 +5,8 @@ const messages = {
     acceptButtonText: 'Aksepter alle informasjonskapsler',
     textLine1: 'Vi bruker nødvendige informasjonskapsler (og lignende teknologier) for å få plattformen vår til å fungere.',
     textLine2: 'Vi ønsker også å bruke valgfrie informasjonskapsler for å hjelpe oss med å forbedre plattformen vår ved å samle inn informasjon om hvordan du bruker den.',
-    textLine3: 'For detaljer om hvilke informasjonskapsler og teknologier vi bruker, se vår {cookiePolicy}. Ved å bruke dette banneret, plasseres en informasjonskapsel på enheten din for å huske dine preferanser.',
+    textLine3: 'For detaljer om hvilke informasjonskapsler og teknologier vi bruker, se vår ',
+    textLine4: '. Ved å bruke dette banneret, plasseres en informasjonskapsel på enheten din for å huske dine preferanser.',
     cookiePolicyLinkText: 'melding om informasjonskapsler',
     cookiePolicyLinkUrl: 'https://www.just-eat.no/info/cookie-politikk'
 };
