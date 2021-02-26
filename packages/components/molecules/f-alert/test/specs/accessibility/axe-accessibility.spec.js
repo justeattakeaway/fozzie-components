878c09e876d5234120f909400dac116d70c5c481
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2021-02-26 14:23:36 +0000

    updated component test files in molecules folder #trivial (#751)
    * added tests for f-alert
    
    * added the rest of molecule tests
    
    * linting fixed
    
    * fixed date in changelog
    
    * fixed typo
    
    * updated package.json
    
    Co-authored-by: Lizzie Turney <lizzie.turney@just-eat.com>

--- packages/components/molecules/f-alert/test/specs/accessibility/axe-accessibility.spec.js
+++ packages/components/molecules/f-alert/test/specs/accessibility/axe-accessibility.spec.js
@@ -1,11 +1,11 @@
 import { getAccessibilityTestResults } from '../../../../../../../test/utils/axe-helper';
-import AlertComponent from '../../../test-utils/component-objects/f-alert.component';
+const Alert = require('../../../test-utils/component-objects/f-alert.component');
+const alert = new Alert();
 
 describe('Accessibility tests', () => {
     beforeEach(() => {
-        browser.url('?path=/story/components-molecules--alert-component');
-        browser.switchToFrame(0);
-        AlertComponent.waitForAlert();
+        alert.open();
+        alert.waitForComponent();
     });
 
     it('a11y - should test f-alert component WCAG compliance', () => {
--- packages/components/molecules/f-mega-modal/test-utils/component-objects/f-mega-modal.component.js
+++ packages/components/molecules/f-mega-modal/test-utils/component-objects/f-mega-modal.component.js
@@ -1,3 +1,13 @@
-const megaModalComponent = () => $('[data-test-id="mega-modal-component"]');
+const Page = require('@justeat/f-wdio-utils/src/page.object');
 
-exports.waitForMegaModal = () => megaModalComponent().waitForExist();
+module.exports = class MegaModal extends Page {
+    get component () { return $('[data-test-id="mega-modal-component"]'); }
+
+    open () {
+        super.openComponent('molecule', 'mega-modal-component');
+    }
+
+    waitForComponent () {
+        super.waitForComponent(this.component);
+    }
+};
