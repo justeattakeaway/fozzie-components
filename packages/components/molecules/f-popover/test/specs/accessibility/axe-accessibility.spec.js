9006966e6dca641d6cdde8431c3c21dd943ff44f
Maryia Radchuk <maryia.radchuk@just-eat.com>
2021-03-02 15:43:55 +0000

    f-popover@0.1.0 - moved to atoms instead of molecules

--- packages/components/molecules/f-popover/test/specs/accessibility/axe-accessibility.spec.js
+++
@@ -1,8 +0,0 @@
-import { getAccessibilityTestResults } from '../../../../../../../test/utils/axe-helper';
-
-describe('Accessibility tests', () => {
-    it('a11y - should test f-popover component WCAG compliance', () => {
-        // Act
-        const axeResults = getAccessibilityTestResults('f-popover');
-    });
-});
