5e4e6a976dc514ee8ed0e6039088a67b6a3a8cc2
Mariya Radchuk <radchuk.mary@gmail.com>
2021-03-03 14:13:35 +0000

    f-popover@0.2.0 - component markup (#763)
    * f-popover@0.1.0 - generator-output
    
    * f-popover@0.1.0 - moved to atoms instead of molecules
    
    * f-popover@0.2.0 - component markup
    
    * f-popover@0.2.0 - remove commented import
    
    * f-popover@0.2.0 - style ammends
    
    * f-popover@0.2.0 - linting errors fix
    
    Co-authored-by: Maryia Radchuk <maryia.radchuk@just-eat.com>

--- packages/components/atoms/f-popover/src/components/Popover.vue
+++ packages/components/atoms/f-popover/src/components/Popover.vue
@@ -2,7 +2,7 @@
     <div
         :class="$style['c-popover']"
         data-test-id="popover">
-        I am a VPopover Component (GB)
+        <slot />
     </div>
 </template>
 
@@ -18,15 +18,31 @@ export default {
 
 <style lang="scss" module>
 
+$popover-padding               : spacing(x2);
+$tooltip-width                 : 10px;
+
 .c-popover {
-    display: flex;
-    justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid $red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
+    @include media('>=mid') {
+        background-color: $color-bg--component;
+        box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.03),
+                    0 1px 10px 1px rgba(0, 0, 0, 0.07),
+                    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        border-radius: $border-radius;
+        padding: 0 $popover-padding;
+        width: auto;
+
+        // tooltip arrow
+        &:before {
+            bottom: 100%;
+            border: $tooltip-width solid transparent;
+            border-bottom: $tooltip-width solid $color-bg--component;
+            content: '';
+            height: 0;
+            width: 0;
+            position: absolute;
+            pointer-events: none;
+        }
+    }
 }
 
 </style>
