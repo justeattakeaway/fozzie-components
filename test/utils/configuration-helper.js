b15ae6a34762feb82788a0914265c7ef0bd6ca19
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2021-03-03 10:38:28 +0000

    fozzie-components@v3.19.0 - Junit circleci test results (#761)
    * Test config changes
    
    * Convert env var to int
    
    * Fix folder path
    
    * Update changelog

--- test/utils/configuration-helper.js
+++ test/utils/configuration-helper.js
@@ -1,24 +1,55 @@
+const video = require('wdio-video-reporter');
 const CIRCLE_CI = process.env.CIRCLECI;
 const TEST_TYPE = process.env.TEST_TYPE;
 
 exports.local = () => ({
     bail: 0,
     maxinstances: 1,
-    loglevel: 'info'
+    loglevel: 'info',
+    reporters: [
+        [video, {
+            saveAllVideos: false, // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
+            outputDir: `${global.baseDir}/_results_`
+        }],
+        ['allure', {
+            outputDir: `${global.baseDir}/allure-results`,
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false
+        }],
+    ]
 });
 
 exports.ci = () => ({
     bail: 1,
-    maxinstances: 2,
-    loglevel: 'info'
+    maxinstances: parseInt(process.env.WDIO_MAX_INSTANCES) || 2,
+    loglevel: process.env.WDIO_LOG_LEVEL || 'info',
+    reporters: [
+        [video, {
+            saveAllVideos: false, // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
+            outputDir: `${global.baseDir}/_results_`
+        }],
+        ['allure', {
+            outputDir: `${global.baseDir}/allure-results`,
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false
+        }],
+        ['junit', {
+            outputDir: `${global.baseDir}/test/results/ci`,
+            outputFileFormat: function(options) { // optional
+                return `${options.cid}-${options.capabilities.browserName}-results.xml`
+            }
+        }]
+    ]
 });
 
 exports.a11y = () => ({
-    specs: ['./test/specs/accessibility/axe-accessibility.spec.js']
+    specs: [`${global.baseDir}/test/specs/accessibility/axe-accessibility.spec.js`]
 });
 
 exports.component = () => ({
-    specs: ['./test/specs/component/*.component.spec.js']
+    specs: [`${global.baseDir}/test/specs/component/*.component.spec.js`]
 });
 
 exports.setTestEnvironment = () => (CIRCLE_CI ? exports.ci() : exports.local());
