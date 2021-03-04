955d8fb76539b0d4b2674a27910932a25fd182f2
Zlatin Ivanov <zlatin.ivanov@just-eat.com>
2021-03-04 11:08:18 +0000

    Again comment update

--- packages/components/organisms/f-footer/src/components/LinkList.vue
+++ packages/components/organisms/f-footer/src/components/LinkList.vue
@@ -121,7 +121,7 @@ export default {
         },
         /**
          * Hanles `resize` window events.
-         * Screen width is the only factor that affects responsive layout.
+         * Screen width is the only factor that affects Links List presentation.
          * Note: Devices trigger `resize` on scroll when address bar collapses
          */
         onResize () {
