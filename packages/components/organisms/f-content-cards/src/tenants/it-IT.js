9764865412ece1291eb8d75c53a802d87ea63710
matthewhardern <matthewhardern@gmail.com>
2021-03-05 09:44:03 +0000

    f-content-cards@v4.1.0 - stamp cards accessibility (#772)
    * f-content-cards@xxx 👁 Implements accessibility of StampCard1
    
    * adds accessibility handling for stamp card content card plus adds small helper directive for some screen readers
    
    * added changelog changes and package json change
    
    * adding alt tag back and change to the directive to include more sentence endings
    
    * fix to test and remove directive where not needed
    
    * made PR recomendation changes
    
    * change the way we load the accessibility directive to be only on this card for now
    
    * change to jsdoc to show multiple return types for date-fns locale method
    
    * change to comment block
    
    Co-authored-by: Benjamin R. White <ben.white@just-eat.com>
    Co-authored-by: Matthew Hardern <matthew.hardern@justeattakeaway.com>

--- packages/components/organisms/f-content-cards/src/tenants/it-IT.js
+++ packages/components/organisms/f-content-cards/src/tenants/it-IT.js
@@ -5,5 +5,13 @@ export default {
     loggedInTitle: 'Te lo sei meritato',
     loggedInSubtitle: 'Queste offerte sono fatte su misura per te, non lasciartele sfuggire.',
     loggedInTermsText: 'Termini e condizioni qui',
-    loggedInTermsUrl: '/informazioni/termini-e-condizioni'
+    loggedInTermsUrl: '/informazioni/termini-e-condizioni',
+    stampCardStatus: {
+        0: 'Zero stamps earned out of five.',
+        1: 'Un punto guadagnato su cinque.',
+        2: 'Due punti guadagnati su cinque.',
+        3: 'Tre punti guadagnati su cinque.',
+        4: 'Quattro punti guadagnati su cinque.',
+        5: 'Five stamps earned out of five.'
+    }
 };
