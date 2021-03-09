31b8641ed96894a0ff898e37c9b16533ab967696
Joshua Ng <joshua.ng@just-eat.com>
2021-03-09 17:03:34 +0000

    Updated to support browserstack tests.

---
+++ test/configuration/browserstack.js
@@ -0,0 +1,13 @@
+// Learn How Configuration Works: https://skipthedishes.atlassian.net/l/c/ViFW6SaY
+// To set browserstack capabilities, please visit: https://www.browserstack.com/automate/capabilities
+const configuration = () => ({
+    capabilities: {
+        os: 'android',
+        osVersion: '11.0',
+        browserName: 'chrome',
+        device: 'Google Pixel 4',
+        browserVersion: ''
+    }
+});
+
+export default configuration;
---
+++ test/utils/browserstack-helper.js
@@ -0,0 +1,52 @@
+/* eslint camelcase: ["error", { allow: [
+    "bs_local",
+    "os_version",
+    "browser_version"
+] }]
+*/
+
+const configuration = require('../configuration/browserstack').default();
+
+const CIRCLE_CI = process.env.CIRCLECI;
+let browserstackName = `Local - ${new Date().toLocaleTimeString()}`;
+
+/**
+ * Check if we're running on a TeamCity agent,
+ * so we can set the Browserstack build name.
+ */
+
+if (CIRCLE_CI) {
+    const buildNumber = process.env.BUILD_NUMBER;
+
+    browserstackName = `CircleCI - ${buildNumber}`;
+}
+
+exports.circleCICapabilities = () => ({
+    os: process.env.BROWSERSTACK_OS,
+    os_version: process.env.BROWSERSTACK_VERSION,
+    browserName: process.env.BROWSERSTACK_BROWSER_NAME,
+    browser_version: process.env.BROWSERSTACK_BROWSER_VERSION,
+    device: process.env.BROWSERSTACK_DEVICE,
+    project: 'Fozzie-Components',
+    build: browserstackName,
+    'browserstack.networkLogs': true
+});
+
+exports.localCapabilities = () => ({
+    maxInstances: 1,
+    os: configuration.capabilities.os,
+    os_version: configuration.capabilities.osVersion,
+    browserName: configuration.capabilities.browserName,
+    browser_version: configuration.capabilities.browserVersion,
+    device: configuration.capabilities.device,
+    project: 'Fozzie-Components',
+    build: browserstackName,
+    'browserstack.networkLogs': true
+});
+
+exports.browserStackCapabilities = () => {
+    if (CIRCLE_CI) {
+        return exports.circleCICapabilities();
+    }
+    return exports.localCapabilities();
+};
