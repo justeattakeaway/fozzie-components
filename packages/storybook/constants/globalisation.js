6d43f7405fa75447538bd4a85d10b6ae9c1125c3
Charles Waite <charles.waite@justeattakeaway.com>
2021-03-09 14:06:08 +0000

    Add more locales for Storybook

--- packages/storybook/constants/globalisation.js
+++ packages/storybook/constants/globalisation.js
@@ -1,4 +1,10 @@
+export const AUSTRALIAN_LOCALE = 'en-AU';
+export const DANISH_LOCALE = 'da-DK';
 export const ENGLISH_LOCALE = 'en-GB';
+export const IRISH_LOCALE = 'en-IE';
+export const ITALIAN_LOCALE = 'it-IT';
+export const NEW_ZEALAND_LOCALE = 'en-NZ';
+export const NORWEIGAN_LOCALE = 'nb-NO';
 export const SPANISH_LOCALE = 'es-ES';
 
 export const VALID_LOCALES = [ENGLISH_LOCALE, SPANISH_LOCALE];
