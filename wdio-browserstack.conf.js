d9783350bc432bef111247223b0a9d29b0a437b4
Joshua Ng <joshua.ng@just-eat.com>
2021-03-10 11:12:29 +0000

    Updated capabilities.

--- wdio-browserstack.conf.js
+++ wdio-browserstack.conf.js
@@ -4,11 +4,11 @@ const browserstack = require('browserstack-local');
 global.baseDir = __dirname;
 
 const { setTestEnvironment, setTestType, getBaseUrl } = require('./test/utils/configuration-helper');
-const { browserStackCapabilities } = require('./test/utils/browserstack-helper');
+// const { browserStackCapabilities } = require('./test/utils/browserstack-helper');
 
 const testEnvironment = setTestEnvironment();
 const testType = setTestType();
-const bsCapabilities = browserStackCapabilities();
+// const bsCapabilities = browserStackCapabilities();
 
 exports.config = {
 
@@ -72,7 +72,15 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [].concat(bsCapabilities),
+    capabilities: [{
+        os: 'android',
+        os_version: '11.0',
+        browserName: 'chrome',
+        device: 'Google Pixel 4',
+        project: 'Fozzie-Components',
+        build: 'local-browserstack',
+        'browserstack.networkLogs': true
+    }],
     //
     // ===================
     // Test Configurations
@@ -120,7 +128,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [],
+    services: ['browserstack'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
