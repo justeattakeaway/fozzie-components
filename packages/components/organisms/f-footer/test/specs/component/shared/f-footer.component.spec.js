b0ca66d679e70d4b25eed12c1f29900244bfd1c6
Lizzie Turney <lizzie.turney@just-eat.com>
2021-03-15 17:08:16 +0000

    small refactor of f-footer tests

--- packages/components/organisms/f-footer/test/specs/component/shared/f-footer.component.spec.js
+++ packages/components/organisms/f-footer/test/specs/component/shared/f-footer.component.spec.js
@@ -1,27 +1,57 @@
-// const Footer = require('../../../../test-utils/component-objects/f-footer.component');
-// const footer = new Footer();
+const Footer = require('../../../../test-utils/component-objects/f-footer.component');
+const footer = new Footer();
 
-// describe('Shared - f-footer component tests', () => {
-//     beforeEach(() => {
-//         footer.open('gb');
-//         footer.waitForComponent();
-//     });
+describe('Shared - f-footer component tests', () => {
+    beforeEach(() => {
+        const footerData = {
+            locale: 'gb',
+            courierLinks: false,
+            countrySelector: true
+        };
 
-//     it('should display the footer', () => {
-//         // Assert
-//         expect(footer.isComponentDisplayed()).toBe(true);
-//     });
+        footer.open(footerData);
+        footer.waitForComponent();
+    });
 
-//     it('Should not show courier links on en-GB locale if courier links is set to false', () => {
-//         // Assert
-//         expect(footer.areCourierLinksDisplayed()).toBe(false);
-//     });
+    it('should display the footer', () => {
+        // Assert
+        expect(footer.isComponentDisplayed()).toBe(true);
+    });
 
-//     it('Should not show courier links on en-GB locale if courier links is set to true', () => {
-//         // Act
-//         footer.openGBWithExtraFeatures();
+    it('Should not show courier links on en-GB locale if courier links is set to false', () => {
+        // Assert
+        expect(footer.areCourierLinksDisplayed()).toBe(false);
+    });
 
-//         // Assert
-//         expect(footer.areCourierLinksDisplayed()).toBe(false);
-//     });
-// });
+    it('Should display country selector when set to true', () => {
+        // Assert
+        expect(footer.isCountrySelectorDisplayed()).toBe(true);
+    });
+
+    it('Should not show courier links on en-GB locale if courier links is set to true', () => {
+        // Arrange
+        const footerData = {
+            locale: 'gb',
+            courierLinks: true
+        };
+        // Act
+        footer.open(footerData);
+
+        // Assert
+        expect(footer.areCourierLinksDisplayed()).toBe(false);
+    });
+
+    it('Should not show country selector on en-GB locale if unselected', () => {
+        // Arrange
+        const footerData = {
+            locale: 'gb',
+            courierLinks: true,
+            countrySelector: false
+        };
+        // Act
+        footer.open(footerData);
+
+        // Assert
+        expect(footer.isCountrySelectorDisplayed()).toBe(false);
+    });
+});
