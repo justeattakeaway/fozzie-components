1f049e154eaab341722560dd512edfb777f9c3d4
Lizzie Turney <lizzie.turney@just-eat.com>
2021-03-18 10:01:13 +0000

    refactored files

--- packages/components/organisms/f-footer/test/specs/component/shared/f-footer.component.spec.js
+++ packages/components/organisms/f-footer/test/specs/component/shared/f-footer.component.spec.js
@@ -1,8 +1,9 @@
 const forEach = require('mocha-each');
 const Footer = require('../../../../test-utils/component-objects/f-footer.component');
+
 const footer = new Footer();
 
-describe('Shared - f-footer component tests', () => {
+describe('Shared - f-footer component tests for tenants: AU, IE, NZ, ES, IT, NO', () => {
     beforeEach(() => {
         const footerData = {
             locale: 'gb',
@@ -14,51 +15,100 @@ describe('Shared - f-footer component tests', () => {
         footer.waitForComponent();
     });
 
+    forEach(['gb', 'au', 'ie', 'nz', 'dk', 'es', 'it', 'no'])
+    .it('should not show courier links and country selector when options are unselected', expectedLocale => {
+        // Arrange
+        const footerData = {
+            locale: expectedLocale,
+            courierLinks: false,
+            countrySelector: false
+        };
+
+        // Act
+        footer.open(footerData);
+        footer.waitForComponent();
+
+        // Assert
+        expect(footer.areCourierLinksDisplayed()).toBe(false);
+        expect(footer.isCountrySelectorDisplayed()).toBe(false);
+    });
+
     it('should display the footer', () => {
         // Assert
         expect(footer.isComponentDisplayed()).toBe(true);
     });
 
-    it('Should not show courier links on en-GB locale if courier links is set to false', () => {
+    forEach(['au', 'ie', 'nz'])
+    .it('should show courier links when option is selected', expectedLocale => {
+        // Arrange
+        const footerData = {
+            locale: expectedLocale,
+            courierLinks: true,
+            countrySelector: false
+        };
+
+        // Act
+        footer.open(footerData);
+        footer.waitForComponent();
+
+        // Assert
+        expect(footer.areCourierLinksDisplayed()).toBe(true);
+    });
+
+    forEach(['gb', 'es', 'it', 'no'])
+    .it('should never show courier links, even when option is selected', expectedLocale => {
+        // Arrange
+        const footerData = {
+            locale: expectedLocale,
+            courierLinks: true,
+            countrySelector: false
+        };
+
+        // Act
+        footer.open(footerData);
+        footer.waitForComponent();
+
         // Assert
         expect(footer.areCourierLinksDisplayed()).toBe(false);
     });
 
-    it('Should display country selector when set to true', () => {
+    forEach(['au', 'ie', 'nz', 'dk', 'es', 'it', 'no'])
+    .it('should always show country selector when selected', expectedLocale => {
+        // Arrange
+        const footerData = {
+            locale: expectedLocale,
+            courierLinks: false,
+            countrySelector: true
+        };
+
+        // Act
+        footer.open(footerData);
+        footer.waitForComponent();
+
         // Assert
         expect(footer.isCountrySelectorDisplayed()).toBe(true);
     });
 
-    it('Should not show courier links on en-GB locale if courier links is set to true', () => {
+    forEach(['au', 'gb', 'nz', 'ie', 'dk', 'es', 'it'])
+    .it('should display the corresponding icon for each locale selected', expectedLocale => {
         // Arrange
         const footerData = {
-            locale: 'gb',
-            courierLinks: true
+            locale: expectedLocale,
+            courierLinks: false,
+            countrySelector: true
         };
+
         // Act
         footer.open(footerData);
+        footer.waitForComponent();
 
         // Assert
-        expect(footer.areCourierLinksDisplayed()).toBe(false);
+        expect(footer.isCurrentCountryIconDisplayed(expectedLocale)).toBe(true);
     });
 
-    it('Should not show country selector on en-GB locale if unselected', () => {
-        // Arrange
-        const footerData = {
-            locale: 'gb',
-            courierLinks: true,
-            countrySelector: false
-        };
-        // Act
-        footer.open(footerData);
-
-        // Assert
-        expect(footer.isCountrySelectorDisplayed()).toBe(false);
-    });
-
-    forEach([['au', 'au'], ['at', 'at'], ['be', 'be-en'], ['bg', 'bg'], ['ca_en', 'skipthedishes.com'], ['ca_fr', 'skipthedishes.com/fr'], ['dk', '.dk'], ['jet_fr', '.fr'], ['de', '.de'], ['ie', '.ie'], ['il', '.il'], ['it', '.it'], 
-    ['lu', 'lu-en'], ['nl', '.nl'], ['nz', '.nz'], ['no', '.no'], ['pl', '.pl'], ['pt', '/pt'], ['ro', '/ro'], ['es', '.es'], ['ch_ch', '.ch'], ['ch_en', '/en'], ['ch_fr', '/fr'] ])
-    .it.skip('should display all countries and redirect to correct URL', (country, expectedUrl) => {
+    forEach([['au', 'au'], ['at', 'at'], ['be', 'be-en'], ['bg', 'bg'], ['ca_en', 'skipthedishes.com'], ['ca_fr', 'skipthedishes.com/fr'], ['dk', '.dk'], ['jet_fr', '.fr'], ['de', '.de'], ['ie', '.ie'], ['il', '.il'], ['it', '.it'],
+        ['lu', 'lu-en'], ['nl', '.nl'], ['nz', '.nz'], ['no', '.no'], ['pl', '.pl'], ['pt', '/pt'], ['ro', '/ro'], ['es', '.es'], ['ch_ch', '.ch'], ['ch_en', '/en'], ['ch_fr', '/fr']])
+    .it('should display all countries and redirect to correct URL', (country, expectedUrl) => {
         // Act
         footer.clickCountrySelectorButton();
         footer.expectedCountry = country;
