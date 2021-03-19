0f70d42965f49a7d1fb5d8fb3c32ee2daea0889a
Josh Ng <joshua.ng2332@gmail.com>
2021-03-19 14:10:04 +0000

    f-cookiebanner@v0.6.0 - Add component and A11y tests (#804)
    * f-cookie-banner tests
    
    * Updated tests file structure and test placement
    
    * Updated accessbility file and config.yml for chrome driver.
    
    * Updated naming for cookie banner files and updated version number.
    
    Co-authored-by: Ben Siggery <ben.siggery@just-eat.com>
    Co-authored-by: Joshua Ng <joshua.ng@just-eat.com>

---
+++ packages/components/organisms/f-cookie-banner/test-utils/component-objects/f-cookieBanner-legacy.component.js
@@ -0,0 +1,32 @@
+const Page = require('@justeat/f-wdio-utils/src/page.object');
+
+module.exports = class CookieBanner extends Page {
+
+    get component () { return $('[data-test-id="legacyCookieBanner-component"]')}
+    get cookiePolicyLink () { return this.component.$('[data-test-id="cookie-policy-link"]')}
+    get closeButton () { return this.component.$('[data-test-id="cookieBanner-close-button"]')}
+
+    open(tenant = 'en-gb') {
+        super.openComponent('organism', `cookie-banner-component&knob-Locale=en-${tenant.toUpperCase()}`);
+    }
+
+    waitForComponent() {
+        this.component.waitForExist();
+    }
+
+    isCookieBannerComponentDisplayed() {
+        return this.component.isDisplayed();
+    }
+
+    clickCookiePolicyLink() {
+        this.cookiePolicyLink.click();
+    }
+
+    close() {
+        this.closeButton.click();
+
+        return this;
+    }
+
+
+}
