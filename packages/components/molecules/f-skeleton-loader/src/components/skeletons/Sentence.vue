82677ed4f2c570c91271b89e7e9da74ac99078a1
Andy May <andy.may@just-eat.com>
2021-03-19 16:22:33 +0000

    Child component prop, structure and css

---
+++ packages/components/molecules/f-skeleton-loader/src/components/skeletons/Sentence.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="c-skeleton-loader-sentence c-skeleton-loader-bone">
+        <text-block />
+        <text-block />
+    </div>
+</template>
+
+<script>
+import TextBlock from './Text.vue';
+
+export default {
+    name: 'Sentence',
+    components: { TextBlock }
+};
+</script>
+
+<style lang="scss" scoped>
+  .c-skeleton-loader-sentence .c-skeleton-loader-text:nth-child(2) {
+    max-width: 70%;
+  }
+  .c-skeleton-loader-sentence .c-skeleton-loader-text:not(:last-child) {
+    margin-bottom: 6px;
+  }
+</style>
---
+++ packages/components/molecules/f-skeleton-loader/src/components/skeletons/index.js
@@ -0,0 +1,3 @@
+export { default as textBlock } from './Text.vue';
+export { default as heading } from './Heading.vue';
+export { default as sentence } from './Sentence.vue';
