a61d83b2628b1b7b118b66de1441122c1f579fb6
Andy May <andy.may@just-eat.com>
2021-03-20 10:46:40 +0000

    Add skeleton components and animation

--- packages/components/molecules/f-skeleton-loader/src/components/_tests/SkeletonLoader.test.js
+++ packages/components/molecules/f-skeleton-loader/src/components/_tests/SkeletonLoader.test.js
@@ -1,10 +1,33 @@
 import { shallowMount } from '@vue/test-utils';
 import SkeletonLoader from '../SkeletonLoader.vue';
 
+const defaultProps = {};
+
 describe('SkeletonLoader', () => {
+    const makeWrapper = ({ propsData = defaultProps } = {}) => shallowMount(SkeletonLoader, {
+        propsData
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should be defined', () => {
-        const propsData = {};
-        const wrapper = shallowMount(SkeletonLoader, { propsData });
+        // Arrange & Act
+        const wrapper = makeWrapper();
+
+        // Assert
         expect(wrapper.exists()).toBe(true);
+    });
+
+    describe('skeletonType validator', () => {
+        it.each([[true, 'Heading'], [true, 'TextBlock'], [true, 'Sentence'], [false, 'NotAValidType']])('should return %p when %s is passed', (expected, skeletonTypeValue) => {
+            // Arrange & Act
+            const wrapper = makeWrapper();
+            const prop = wrapper.vm.$options.props.skeletonType;
+
+            // Assert
+            expect(prop.validator(skeletonTypeValue)).toBe(expected);
+        });
     });
 });
