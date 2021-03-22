fd88026185544708988d4eaf1d3d81ee3ebd9331
Andy May <andy.may@just-eat.com>
2021-03-22 09:04:41 +0000

    Clean styles up

--- packages/components/molecules/f-skeleton-loader/src/components/skeletons/Sentence.vue
+++ packages/components/molecules/f-skeleton-loader/src/components/skeletons/Sentence.vue
@@ -15,10 +15,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .c-skeleton-loader-sentence .c-skeleton-loader-text:nth-child(2) {
+.c-skeleton-loader-text:nth-child(2) {
     max-width: 70%;
-  }
-  .c-skeleton-loader-sentence .c-skeleton-loader-text:not(:last-child) {
+}
+.c-skeleton-loader-text:not(:last-child) {
     margin-bottom: 6px;
-  }
+}
 </style>
