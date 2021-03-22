77d123e5d1afbdd64d66c19690c409b0ef12c070
Andy May <andy.may@just-eat.com>
2021-03-22 08:41:59 +0000

    Add tests for isAnimated prop

--- packages/components/molecules/f-skeleton-loader/src/components/_tests/SkeletonLoader.test.js
+++ packages/components/molecules/f-skeleton-loader/src/components/_tests/SkeletonLoader.test.js
@@ -20,14 +20,42 @@ describe('SkeletonLoader', () => {
         expect(wrapper.exists()).toBe(true);
     });
 
-    describe('skeletonType validator', () => {
-        it.each([[true, 'Heading'], [true, 'TextBlock'], [true, 'Sentence'], [false, 'NotAValidType']])('should return %p when %s is passed', (expected, skeletonTypeValue) => {
-            // Arrange & Act
-            const wrapper = makeWrapper();
-            const prop = wrapper.vm.$options.props.skeletonType;
+    describe('props', () => {
+        describe('skeletonType validator', () => {
+            it.each([[true, 'Heading'], [true, 'TextBlock'], [true, 'Sentence'], [false, 'NotAValidType']])('should return %p when %s is passed', (expected, skeletonTypeValue) => {
+                // Arrange & Act
+                const wrapper = makeWrapper();
+                const prop = wrapper.vm.$options.props.skeletonType;
 
-            // Assert
-            expect(prop.validator(skeletonTypeValue)).toBe(expected);
+                // Assert
+                expect(prop.validator(skeletonTypeValue)).toBe(expected);
+            });
+        });
+
+        describe('isAnimated', () => {
+            it('should add add animation modifier class to component root', () => {
+                // Arrange
+                const propsData = {
+                    isAnimated: true
+                };
+                // Act
+                const wrapper = makeWrapper({ propsData });
+
+                // Assert
+                expect(wrapper.classes()).toContain('c-skeleton-loader--animated');
+            });
+
+            it('should add not add animation modifier class when false', () => {
+                // Arrange
+                const propsData = {
+                    isAnimated: false
+                };
+                // Act
+                const wrapper = makeWrapper({ propsData });
+
+                // Assert
+                expect(wrapper.classes()).not.toContain('c-skeleton-loader--animated');
+            });
         });
     });
 });
