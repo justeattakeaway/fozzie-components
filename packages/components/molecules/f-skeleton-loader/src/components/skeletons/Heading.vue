d847c451b87f83dfd3bc71302562f067e77c1d47
AndyMayJE <andy.may@just-eat.com>
2021-03-22 10:33:48 +0000

    f-skeleton-loader@0.2.0 Animation, skeleton component by prop (#812)

---
+++ packages/components/molecules/f-skeleton-loader/src/components/skeletons/Heading.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+        class="c-skeleton-loader-heading c-skeleton-loader-bone" />
+</template>
+
+<script>
+export default {
+    name: 'Heading'
+};
+</script>
+
+<style lang="scss" scoped>
+.c-skeleton-loader-heading {
+    height: $skeleton-loader-heading-height;
+    width: 65%;
+}
+</style>
