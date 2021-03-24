d8f43f5e836546744a4ba8020fe29ca9ef85c5e0
Lizzie Turney <lizzie.turney@just-eat.com>
2021-03-24 16:34:54 +0000

    unnoted other tests

--- packages/components/organisms/f-checkout/test/specs/component/f-checkout-collection.component.spec.js
+++ packages/components/organisms/f-checkout/test/specs/component/f-checkout-collection.component.spec.js
@@ -1,24 +1,24 @@
-// import forEach from 'mocha-each';
+import forEach from 'mocha-each';
 
-// const Checkout = require('../../../test-utils/component-objects/f-checkout.component');
+const Checkout = require('../../../test-utils/component-objects/f-checkout.component');
 
-// const checkout = new Checkout();
+const checkout = new Checkout();
 
-// describe('f-checkout "collection" component tests', () => {
-//     beforeEach(() => {
-//         const checkoutData = {
-//             type: 'collection',
-//             isAuthenticated: true,
-//             isValid: true
-//         };
+describe('f-checkout "collection" component tests', () => {
+    beforeEach(() => {
+        const checkoutData = {
+            type: 'collection',
+            isAuthenticated: true,
+            isValid: true
+        };
 
-//         checkout.open(checkoutData);
-//         checkout.waitForComponent();
-//     });
+        checkout.open(checkoutData);
+        checkout.waitForComponent();
+    });
 
-//     forEach(['addressLine1', 'addressLine2', 'addressLocality', 'addressPostcode'])
-//     .it('should check that address fields should not exist', field => {
-//         // Assert
-//         expect(checkout.doesFieldExist(field)).toBe(false);
-//     });
-// });
+    forEach(['addressLine1', 'addressLine2', 'addressLocality', 'addressPostcode'])
+    .it('should check that address fields should not exist', field => {
+        // Assert
+        expect(checkout.doesFieldExist(field)).toBe(false);
+    });
+});
