5f2990abe15e531a1732e837ce522b754c6194b6
Nicholas John <NicholasIvorJohn@googlemail.com>
2021-03-26 09:44:06 +0000

    f-content-cards@4.4.1 - Fixed Page Component Object Model for content cards (#831)
    * Fixed Page Component Object Model for content cards to use base page object package.
    
    * Updating content card tests to use latest version of utils component
    
    Co-authored-by: Nicholas "Jonkers" John <Nicholas.John@just-eat.com>

--- packages/components/organisms/f-content-cards/test-utils/component-objects/f-content-cards-home-promotion-card1.component.js
+++ packages/components/organisms/f-content-cards/test-utils/component-objects/f-content-cards-home-promotion-card1.component.js
@@ -1,6 +1,4 @@
-const Page = require('../../../../../services/f-wdio-utils/src/page.object');
-const HomePromotionCard2 = require('./f-content-cards-home-promotion-card2.component');
-const card2 = new HomePromotionCard2();
+const Page = require('@justeat/f-wdio-utils/src/page.object');
 
 const {
     PROMOTION_CARD_1_COMPONENT,
@@ -11,16 +9,19 @@ const {
 } = require('./f-content-cards-home-promotion.selectors');
 
 module.exports = class HomePromotionCard1 extends Page {
+    get component () { return $(PROMOTION_CARD_1_COMPONENT); }
 
-    get component () { return $(PROMOTION_CARD_1_COMPONENT) }
-    get innerContainer () { return $(INNER_CONTAINER) }
-    get header () { return $(HEADER) }
-    get image () { return $(IMAGE) }
-    get subtitle () { return $(SUBTITLE) }
+    get innerContainer () { return $(INNER_CONTAINER); }
 
-    open() {
+    get header () { return $(HEADER); }
+
+    get image () { return $(IMAGE); }
+
+    get subtitle () { return $(SUBTITLE); }
+
+    open () {
         super.openComponent('molecule-folder', 'f-content-cards--home-promotion-card-1-component');
-    };
+    }
 
     waitForComponent () {
         super.waitForComponent(this.component);
@@ -45,24 +46,4 @@ module.exports = class HomePromotionCard1 extends Page {
     isSubtitleDisplayed () {
         return this.subtitle.isDisplayed();
     }
-
-    isInnerCardDisplayed () {
-        return card2.isComponentDisplayed();
-    }
-
-    isInnerCardCallToActionDisplayed () {
-        return card2.isCallToActionDisplayed();
-    }
-
-    isInnerCardTextDisplayed () {
-        return card2.isTextDisplayed();
-    }
-
-    isInnerCardTitleDisplayed () {
-        return card2.isTitleDisplayed();
-    }
-
-    isInnerCardBackgroundImageDisplayed () {
-        return card2.isBackgroundImageDisplayed();
-    }
-}
+};
