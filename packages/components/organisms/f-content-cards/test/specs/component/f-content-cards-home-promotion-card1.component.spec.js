4641b69e115158420a0cde30a3e39ff70e98e1f9
Nicholas John <NicholasIvorJohn@googlemail.com>
2021-03-26 09:44:06 +0000

    f-content-cards@4.4.1 - Fixed Page Component Object Model for content cards (#831)
    * Fixed Page Component Object Model for content cards to use base page object package.
    
    * Updating content card tests to use latest version of utils component
    
    Co-authored-by: Nicholas "Jonkers" John <Nicholas.John@just-eat.com>

--- packages/components/organisms/f-content-cards/test/specs/component/f-content-cards-home-promotion-card1.component.spec.js
+++ packages/components/organisms/f-content-cards/test/specs/component/f-content-cards-home-promotion-card1.component.spec.js
@@ -1,5 +1,8 @@
 const HomePromotionCard1 = require('../../../test-utils/component-objects/f-content-cards-home-promotion-card1.component');
+const HomePromotionCard2 = require('../../../test-utils/component-objects/f-content-cards-home-promotion-card2.component');
+
 const card = new HomePromotionCard1();
+const card2 = new HomePromotionCard2();
 
 describe('home promotion card 1 component tests', () => {
     beforeEach(() => {
@@ -16,16 +19,16 @@ describe('home promotion card 1 component tests', () => {
         // Assert
         expect(card.isInnerContainerDisplayed()).toBe(true);
         expect(card.isHeaderDisplayed()).toBe(true);
-        expect(card.isImageDisplayed()).toBe(true); 
+        expect(card.isImageDisplayed()).toBe(true);
         expect(card.isSubtitleDisplayed()).toBe(true);
     });
 
     it('should display the elements of the inner card', () => {
         // Assert
-        expect(card.isInnerCardDisplayed()).toBe(true);
-        expect(card.isInnerCardCallToActionDisplayed()).toBe(true);
-        expect(card.isInnerCardTextDisplayed()).toBe(true);
-        expect(card.isInnerCardTitleDisplayed()).toBe(true); 
-        expect(card.isInnerCardBackgroundImageDisplayed()).toBe(true);
+        expect(card2.isComponentDisplayed()).toBe(true);
+        expect(card2.isCallToActionDisplayed()).toBe(true);
+        expect(card2.isTextDisplayed()).toBe(true);
+        expect(card2.isTitleDisplayed()).toBe(true);
+        expect(card2.isBackgroundImageDisplayed()).toBe(true);
     });
 });
