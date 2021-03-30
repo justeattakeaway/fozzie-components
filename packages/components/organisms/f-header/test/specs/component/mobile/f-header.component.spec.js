69f4f89572c0db7909bafec9b547287e0c72f864
Lizzie Turney <lizzie.turney@just-eat.com>
2021-03-30 15:02:08 +0100

    refactored mobile tests slightly

--- packages/components/organisms/f-header/test/specs/component/mobile/f-header.component.spec.js
+++ packages/components/organisms/f-header/test/specs/component/mobile/f-header.component.spec.js
@@ -19,52 +19,25 @@ describe('Mobile - f-header component tests', () => {
         }
     });
 
-    // forEach(['offersLink', 'delivery', 'userAccount', 'help', 'countrySelector'])
-    // .it('should hide all navigation links, except offersIcon link, when in mobile mode', field => {
-    //     // Arrange
-    //     const locales = ['gb', 'au', 'ie', 'nz'];
+    forEach(['gb', 'au', 'ie', 'nz'])
+    .it('should hide all navigation links, except offersIcon link, when in mobile mode', expectedLocale => {
+        // Arrange
+        const headerData = {
+            locale: expectedLocale,
+            offers: true,
+            delivery: true
+        };
 
-    //     for (let i = 0; i < locales.length; i++) {
-    //         // Arrange
-    //         const headerData = {
-    //             locale: locales[i],
-    //             offers: true,
-    //             delivery: true
-    //         };
+        // Act
+        ['offersLink', 'delivery', 'userAccount', 'help', 'countrySelector'].forEach(link => {
+            header.open(headerData);
 
-    //         // Act
-    //         header.open(headerData);
-    //         console.log(headerData);
-    //     }
-
-    //     // Assert
-    //     expect(header.isMobileNavigationBarDisplayed()).toBe(true);
-    //     expect(header.isFieldLinkDisplayed(field)).toBe(false);
-    //     expect(header.isFieldLinkDisplayed('offersIcon')).toBe(true);
-    // });
-
-    // forEach(['offersLink', 'delivery', 'userAccount', 'help', 'countrySelector'])
-    // .it('should display navigation fields when burger menu has been opened', field => {
-    //     // Arrange
-    //     const locales = ['gb', 'au', 'ie', 'nz'];
-
-    //     for (let i = 0; i < locales.length; i++) {
-    //         // Arrange
-    //         const headerData = {
-    //             locale: locales[i],
-    //             offers: true,
-    //             delivery: true
-    //         };
-
-    //         // Act
-    //         header.open(headerData);
-    //         console.log('heyyy', headerData);
-    //         header.openMobileNavigation();
-    //     }
-
-    //     // Assert
-    //     expect(header.isFieldLinkDisplayed(field)).toBe(true);
-    // });
+            // Assert
+            expect(header.isMobileNavigationBarDisplayed()).toBe(true);
+            expect(header.isFieldLinkDisplayed(link)).toBe(false);
+            expect(header.isFieldLinkDisplayed('offersIcon')).toBe(true);
+        });
+    });
 
     forEach(['gb', 'au', 'ie', 'nz'])
     .it('should display navigation links when burger menu is opened', expectedLocale => {
@@ -74,75 +47,45 @@ describe('Mobile - f-header component tests', () => {
             offers: true,
             delivery: true
         };
-        let navigationLink = '';
+
         ['offersLink', 'delivery', 'userAccount', 'help', 'countrySelector'].forEach(link => {
-            navigationLink = link;
-        });
-
-        // Act
-        header.open(headerData);
-        header.openMobileNavigation();
-
-        // Assert
-        expect(header.isFieldLinkDisplayed(navigationLink)).toBe(true);
-    });
-
-    forEach(['userAccount', 'help', 'countrySelector'])
-    .it('should display all navigation links, except for offers and delivery', field => {
-        // Arrange
-        const locales = ['it', 'es', 'dk', 'no'];
-
-        for (let i = 0; i < locales.length; i++) {
-            // Arrange
-            const headerData = {
-                locale: locales[i],
-                offers: true,
-                delivery: true
-            };
-
-            // Act
             header.open(headerData);
             header.openMobileNavigation();
-        }
 
-        // Assert
-        expect(header.isFieldLinkDisplayed(field)).toBe(true);
-        expect(header.isFieldLinkDisplayed('offersLink')).toBe(false);
-        expect(header.isFieldLinkDisplayed('delivery')).toBe(false);
+            // Assert
+            expect(header.isFieldLinkDisplayed(link)).toBe(true);
+        });
     });
 
     forEach(['it', 'es', 'dk', 'no'])
-    .it('should display all navigation links, except for offers and delivery', expectedLocale => {
+    .it('should display all navigation links, except for offers and delivery, when burger menu has been opened', expectedLocale => {
         // Arrange
         const headerData = {
             locale: expectedLocale,
             offers: true,
             delivery: true
         };
-        const navigation = ['userAccount', 'help', 'countrySelector'];
-        let navigationLink = '';
 
         // Act
-        for (let i = 0; i < navigation.length; i++) {
-            navigationLink = navigation[i];
+        ['userAccount', 'help', 'countrySelector'].forEach(link => {
             header.open(headerData);
             header.openMobileNavigation();
-        }
 
-        // Assert
-        expect(header.isFieldLinkDisplayed(navigationLink)).toBe(true);
-        expect(header.isFieldLinkDisplayed('offersLink')).toBe(false);
-        expect(header.isFieldLinkDisplayed('delivery')).toBe(false);
+            // Assert
+            expect(header.isFieldLinkDisplayed(link)).toBe(true);
+            expect(header.isFieldLinkDisplayed('offersLink')).toBe(false);
+            expect(header.isFieldLinkDisplayed('delivery')).toBe(false);
+        });
     });
 
-    // forEach(['au', 'gb', 'nz', 'ie', 'dk', 'es', 'it'])
-    // .it('should display all countries when in mobile mode', country => {
-    //     // Act
-    //     header.openMobileNavigation();
-    //     header.openCountrySelector();
-    //     header.expectedCountry = country;
+    forEach(['au', 'gb', 'nz', 'ie', 'dk', 'es', 'it'])
+    .it('should display all countries when in mobile mode', country => {
+        // Act
+        header.openMobileNavigation();
+        header.openCountrySelector();
+        header.expectedCountry = country;
 
-    //     // Assert
-    //     expect(header.isCountryLinkDisplayed()).toBe(true);
-    // });
+        // Assert
+        expect(header.isCountryLinkDisplayed()).toBe(true);
+    });
 });
