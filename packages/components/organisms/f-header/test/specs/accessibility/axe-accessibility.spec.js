b76e9464ee5921c360794f9661fad9083480ecfb
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2021-04-01 13:21:38 +0100

    f-header - component tests for different locales #trivial (#847)
    * clean up of offers and delivery links
    
    * experimenting with locale structure
    
    * other ways of iteration explored
    
    * refactored mobile tests slightly
    
    * tenants completed for mobile
    
    * tests done for tenants
    
    * removed noted code
    
    * naming clean up
    
    * added changelog entry
    
    * small change to changelog
    
    * refactored mobile spec
    
    * deleted locales getter in component obj
    
    * small change to header
    
    * small typo change
    
    * removed line gap
    
    * added wait function to f-header test
    
    * small change to url test
    
    * accessibility test refactored
    
    * small change to accessibility test
    
    Co-authored-by: Lizzie Turney <lizzie.turney@just-eat.com>

--- packages/components/organisms/f-header/test/specs/accessibility/axe-accessibility.spec.js
+++ packages/components/organisms/f-header/test/specs/accessibility/axe-accessibility.spec.js
@@ -1,15 +1,20 @@
+const forEach = require('mocha-each');
 const { getAccessibilityTestResults } = require('../../../../../../../test/utils/axe-helper');
 const Header = require('../../../test-utils/component-objects/f-header.component');
 const header = new Header();
 
 describe('Accessibility tests', () => {
-    beforeEach(() => {
-        header.open();
-        header.waitForComponent();
-    });
-
-    it('a11y - should test f-header component WCAG compliance', () => {
+    forEach(['gb', 'au', 'nz', 'ie', 'it', 'es', 'dk', 'no'])
+    .it('a11y - should test f-header component WCAG compliance', expectedLocale => {
         // Act
+        const headerData = {
+            locale: expectedLocale,
+            offers: true,
+            delivery: true
+        };
+
+        header.open(headerData);
+        header.waitForComponent();
         const axeResults = getAccessibilityTestResults('f-header');
 
         // Assert
