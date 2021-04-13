2c9a4d857d383150a1f38382bac6296fd56790f0
Charles Waite <charles.waite@justeattakeaway.com>
2021-04-13 15:14:32 +0100

    Added a test to ensure line 2 isn't mapped

--- packages/components/organisms/f-checkout/src/services/tests/mapper.test.js
+++ packages/components/organisms/f-checkout/src/services/tests/mapper.test.js
@@ -64,6 +64,29 @@ describe('checkout mapper', () => {
         ]);
     });
 
+    it('should not map the second line of the address when it\'s not provided', () => {
+        // Act
+        const oneLineAddress = {
+            line1: '1 Bristol Road',
+            locality: 'Bristol',
+            postcode: 'BS1 1AA'
+        };
+
+        const requestBody = mapUpdateCheckoutRequest({
+            ...defaultParams,
+            address: oneLineAddress
+        });
+
+        const locationRequest = requestBody[1].value.location;
+
+        // Assert
+        expect(locationRequest.address.postalCode).toBe(address.postcode);
+        expect(locationRequest.address.locality).toBe(address.locality);
+        expect(locationRequest.address.lines).toStrictEqual([
+            address.line1
+        ]);
+    });
+
     it('should map time correctly', () => {
         // Arrange
         const time = {
