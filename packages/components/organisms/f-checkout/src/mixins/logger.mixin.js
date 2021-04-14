28e0c4902cafed297e60914187fd08eb989fbd85
Billy Oliver <billy.oliver@gmail.com>
2021-04-14 10:37:53 +0100

    f-checkout@0.91.0 - Logger alignment changes (#880)
    * WCB-1759 - Aligned all uses of the logger plus added support for exception props to be logged as fields
    
    * f-checkout - Removed redundancy
    
    * f-checkout - Refactored the Error handling
    
    * f-checkout - Updated the version & readme
    
    * f-checkout - Fix up test file
    
    * f-checkout - Fixed up tests
    
    * f-checkout - Updated version
    
    * f-checkout - version
    
    * f-checkout - Reviewer changes, refactored logInvoker method signature and fixed up tests
    
    * f-checkout - typo
    
    * f-checkout - version bump
    
    * f-checkout - formatting
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

---
+++ packages/components/organisms/f-checkout/src/mixins/logger.mixin.js
@@ -0,0 +1,29 @@
+function buildErrorLogFields (error) {
+    return {
+        ...(error && error instanceof Error && {
+            exception: error.name,
+            exceptionMessage: error.message,
+            exceptionStackTrace: error.stack
+        })
+    };
+}
+
+export default {
+    methods: {
+        /**
+         * @function logInvoker
+         * @param  {string} message   - Text that will appear in kibana log.
+         * @param  {object} data    - Bag of objects that will attempt to be displayed
+         *                              as labels and text in the kibana logs.
+         * @param  {method} logMethod - The `this.$logger` method that will be executed.
+         * @param  {Error} error      - Spread into key/value pairs using the method `buildErrorLogFields`
+         *                              and appear in the kibana logs as labels and text.
+         */
+        logInvoker ({
+            message, data, logMethod, error
+        }) {
+            const errorFields = buildErrorLogFields(error);
+            logMethod(message, this.$store, { data, ...errorFields });
+        }
+    }
+};
