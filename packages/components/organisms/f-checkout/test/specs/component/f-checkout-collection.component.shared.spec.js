34c8fbf4da08cafb18f73dbd7fe926f459b13b28
Ben Siggery <bensiggery28@gmail.com>
2021-04-16 10:53:30 +0100

    test(white space to test specs): added white space

--- packages/components/organisms/f-checkout/test/specs/component/f-checkout-collection.component.shared.spec.js
+++ packages/components/organisms/f-checkout/test/specs/component/f-checkout-collection.component.shared.spec.js
@@ -2,6 +2,8 @@ import forEach from 'mocha-each';
 
 const Checkout = require('../../../test-utils/component-objects/f-checkout.component');
 
+
+
 const checkout = new Checkout();
 
 describe('f-checkout "collection" component tests - @browserstack', () => {
