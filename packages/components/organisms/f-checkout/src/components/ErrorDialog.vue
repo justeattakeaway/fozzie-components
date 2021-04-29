a10bc4304587c997e3fab73f2102a22e89788b13
FayeCarter <faye.carter@justeattakeaway.com>
2021-04-29 08:41:12 +0100

    Update error dialog to display modal or alert

--- packages/components/organisms/f-checkout/src/components/ErrorDialog.vue
+++ packages/components/organisms/f-checkout/src/components/ErrorDialog.vue
@@ -1,58 +1,106 @@
 <template>
-    <mega-modal
-        :is-open="isOpen"
-        data-test-id="checkout-issue-modal"
-        has-overlay
-        v-bind="$attrs"
-        v-on="$listeners">
-        <h3
-            data-test-id="checkout-issue-modal-title"
-            class="u-noSpacing">
-            {{ $t(`errorMessages.checkoutIssues.${errorCode}.title`) }}
-        </h3>
+    <div>
+        <mega-modal
+            data-test-id="checkout-issue-modal"
+            has-overlay
+            :is-open="shouldShowModal"
+            @close="handleErrorDialogButtonClick"
+        >
+            <h3
+                data-test-id="checkout-issue-modal-title"
+                class="u-noSpacing"
+            >
+                {{ $t(`errorMessages.checkoutIssues.${errorCode}.title`) }}
+            </h3>
 
-        <p data-test-id="checkout-issue-modal-message">
-            {{ $t(`errorMessages.checkoutIssues.${errorCode}.message`) }}
-        </p>
+            <p data-test-id="checkout-issue-modal-message">
+                {{ $t(`errorMessages.checkoutIssues.${errorCode}.message`) }}
+            </p>
 
-        <f-button
-            :class="$style['c-checkout-redirectButton']"
-            button-size="large"
-            data-test-id="redirect-to-menu-button"
-            @click.native="handleButtonClick">
-            {{ $t(`errorMessages.checkoutIssues.${errorCode}.buttonText`) }}
-        </f-button>
-    </mega-modal>
+            <f-button
+                :class="$style['c-checkout-redirectButton']"
+                button-size="large"
+                data-test-id="redirect-to-menu-button"
+                @click.native="handleErrorDialogButtonClick"
+            >
+                {{ $t(`errorMessages.checkoutIssues.${errorCode}.buttonText`) }}
+            </f-button>
+        </mega-modal>
+
+        <alert
+            v-if="shouldShowAlert"
+            type="danger"
+            :class="$style['c-checkout-alert']"
+            :heading="$t('errorMessages.errorHeading')"
+        >
+            {{ errorCode }}
+        </alert>
+    </div>
 </template>
 
 <script>
+import Alert from '@justeat/f-alert';
+import '@justeat/f-alert/dist/f-alert.css';
 import MegaModal from '@justeat/f-mega-modal';
 import '@justeat/f-mega-modal/dist/f-mega-modal.css';
 import FButton from '@justeat/f-button';
 import '@justeat/f-button/dist/f-button.css';
+import { mapActions, mapState } from 'vuex';
+import {
+    VUEX_CHECKOUT_MODULE
+} from '../constants';
 
 import EventNames from '../event-names';
 
 export default {
     components: {
+        Alert,
         MegaModal,
         FButton
     },
 
-    props: {
-        isOpen: {
-            type: Boolean,
-            default: false
+    computed: {
+        ...mapState(VUEX_CHECKOUT_MODULE, [
+            'displayError',
+            'restaurant'
+        ]),
+
+        errorCode () {
+            return this.displayError && this.displayError.code;
         },
-        errorCode: {
-            type: String,
-            default: ''
+
+        shouldShowModal () {
+            return this.displayError ? this.displayError.shouldShowInDialog : false;
+        },
+
+        shouldShowAlert () {
+            return this.displayError ? this.displayError.shouldShowInAlert : false;
+        },
+
+        restaurantMenuPageUrl () {
+            return `restaurant-${this.restaurant.seoName}/menu`;
         }
     },
 
     methods: {
+        ...mapActions(VUEX_CHECKOUT_MODULE, [
+            'updateDisplayError'
+        ]),
+
         handleButtonClick () {
             this.$emit(EventNames.CheckoutErrorDialogButtonClicked);
+        },
+
+        handleErrorDialogClose () {
+            this.updateDisplayError(null);
+        },
+
+        handleErrorDialogButtonClick () {
+            if (this.displayError && this.displayError.shouldRedirectToMenu) {
+                window.location.assign(this.restaurantMenuPageUrl);
+            }
+
+            this.handleErrorDialogClose();
         }
     }
 };
@@ -61,5 +109,11 @@ export default {
 <style lang="scss" module>
 .c-checkout-redirectButton {
     margin: spacing(x4) 0 spacing(x0.5);
+}
+
+.c-checkout-alert {
+    width: $checkout-width;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
