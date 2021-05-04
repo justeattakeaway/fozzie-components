935bd7422e794a1f69ca88f8142264b853b7d5b4
Lizzie Turney <49618712+LTurns@users.noreply.github.com>
2021-05-04 11:26:16 +0100

    fozzie-components@v3.30.1 - Add error handling and removed extra `spec` folder  #globalconfig (#916)
    * removed first folder
    
    * removed 'spec' folder for atoms
    
    * finished changing test folder structure for components
    
    * clean-up of checkout
    
    * edited jest config
    
    * refactored path in browserstack settings
    
    * added error handling
    
    * small test description changes
    
    * tiny change in test description
    
    * added changelong entry
    
    * added line space
    
    * removed space
    
    Co-authored-by: Lizzie Turney <lizzie.turney@just-eat.com>

---
+++ packages/components/organisms/f-status-banner/test/accessibility/axe-accessibility.spec.js
@@ -0,0 +1,19 @@
+import { getAccessibilityTestResults } from '../../../../../../test/utils/axe-helper';
+
+const StatusBanner = require('../../test-utils/component-objects/f-statusBanner.component');
+
+const statusBanner = new StatusBanner();
+
+describe('Accessibility tests', () => {
+    beforeEach(() => {
+        statusBanner.open();
+        statusBanner.waitForComponent();
+    });
+    it('a11y - should test f-statusBanner component WCAG compliance', () => {
+        // Act
+        const axeResults = getAccessibilityTestResults('f-statusBanner');
+
+        // Assert
+        expect(axeResults.violations.length).toBe(0);
+    });
+});
