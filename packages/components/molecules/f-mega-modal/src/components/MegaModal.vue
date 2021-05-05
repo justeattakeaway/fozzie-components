3abe4450e194e9c744c7fce279857add1911b12f
Kevin Rodrigues <kevrodrigues116@gmail.com>
2021-05-05 16:36:49 +0100

    Revert "f-mega-modal@0.7.0 (#923)" (#929)
    This reverts commit 2dd9f13d154070e4a631f921642d9c980d2a6c57.

--- packages/components/molecules/f-mega-modal/src/components/MegaModal.vue
+++ packages/components/molecules/f-mega-modal/src/components/MegaModal.vue
@@ -15,8 +15,7 @@
                 [$style['c-megaModal-content--narrow']]: isNarrow,
                 [$style['c-megaModal-content--wide']]: isWide,
                 [$style['c-megaModal-content--flush']]: isFlush,
-                [$style['is-fullHeight']]: isFullHeight,
-                [$style['is-positioned-bottom']]: isPositionedBottom
+                [$style['is-fullHeight']]: isFullHeight
             }]"
             role="dialog">
             <div
@@ -101,11 +100,6 @@ export default {
         },
 
         isCloseRounded: {
-            type: Boolean,
-            default: false
-        },
-
-        isPositionedBottom: {
             type: Boolean,
             default: false
         },
@@ -305,22 +299,6 @@ export default {
     top: 50%;
     transform: translate(50%, -50%);
     width: 75%;
-
-    &.is-positioned-bottom {
-        border-radius: 0;
-        bottom: -100vh;
-        box-shadow: 0 -8px 16px 0 rgba(0, 0, 0, 0.22), 0 16px 16px 0 rgba(0, 0, 0, 0.24);
-        display: block;
-        left: 0;
-        top: auto;
-        transform: none;
-        width: 100%;
-        max-width: inherit;
-
-        &.c-megaModal-content--visible {
-            bottom: 0;
-        }
-    }
 
     @include media('<mid') {
         min-width: em(22);
