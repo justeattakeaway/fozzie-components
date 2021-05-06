cdbb150818ca25fa1e07207e050bd42dd6807099
Ben Siggery <bensiggery28@gmail.com>
2021-05-06 12:49:59 +0100

    Fix export format

--- test/constants/viewport-widths.js
+++ test/constants/viewport-widths.js
@@ -1,9 +1,4 @@
 // Widths defined - https://github.com/justeat/fozzie/blob/master/src/scss/settings/_variables.scss#L76
-const DESKTOP_HUGE_WIDTH = 1280;
-const MOBILE_NARROW_WIDTH = 414;
+exports.DESKTOP_HUGE_WIDTH = 1280;
+exports.MOBILE_NARROW_WIDTH = 414;
 
-
-exports.default = {
-    DESKTOP_HUGE_WIDTH,
-    MOBILE_NARROW_WIDTH
-};
