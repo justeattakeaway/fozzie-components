72652aca2f42667377ef61fbbdbad3a80449b320
FayeCarter <faye.carter@just-eat.com>
2021-05-14 12:25:27 +0100

    f-link@0.1.0 - Generate component (#955)
    * Generate component
    
    * Fix component tests
    
    * Remove NO and DA, fix component tests
    
    Co-authored-by: FayeCarter <faye.carter@justeattakeaway.com>

---
+++ packages/components/atoms/f-link/src/components/Link.vue
@@ -0,0 +1,45 @@
+<template>
+    <div
+        :class="$style['o-link']"
+        data-test-id="link"
+    >
+        {{ copy.text }}
+    </div>
+</template>
+
+<script>
+import { globalisationServices } from '@justeat/f-services';
+import tenantConfigs from '../tenants';
+
+export default {
+    name: 'VLink',
+    components: {},
+    props: {
+        locale: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        const locale = globalisationServices.getLocale(tenantConfigs, this.locale, this.$i18n);
+        const localeConfig = tenantConfigs[locale];
+
+        return {
+            copy: { ...localeConfig }
+        };
+    }
+};
+</script>
+
+<style lang="scss" module>
+.o-link {
+    display: flex;
+    justify-content: center;
+    min-height: 80vh;
+    width: 80vw;
+    margin: auto;
+    border: 1px solid $red;
+    font-family: $font-family-base;
+    @include font-size(heading-m);
+}
+</style>
