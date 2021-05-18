6fec761d3cf1cc5b99b5dd3be4c77bbfb1a84f2a
billy.oliver <billy.oliver@just-eat.com>
2021-05-18 16:38:01 +0100

    f-http - Remove error capture for now as it breaks tests relying on the current logic/error shape (may put back in at a later stage)

--- packages/services/f-http/src/interceptors.js
+++ packages/services/f-http/src/interceptors.js
@@ -8,37 +8,28 @@ const outputToConsole = response => {
  * Attach interceptors to the axios client to record response time from an API
  *
  * @param {AxiosInstance} axiosInstance
+ * @param {Boolean} `isDevelopment` option flag - if provided and is true then the api stats are outputted to the console
  */
 const addTimingInterceptor = ({ interceptors }, isDevelopment) => {
-    interceptors.request.use(
-        req => {
-            req.meta = req.meta || {};
-            req.meta.requestStartedAt = new Date().getTime();
+    interceptors.request.use(req => {
+        req.meta = req.meta || {};
+        req.meta.requestStartedAt = new Date().getTime();
 
-            return req;
-        },
-        error => {
-            Promise.reject(error);
+        return req;
+    });
+
+    interceptors.response.use(res => {
+        const timeTakenMs = new Date().getTime() - res.config.meta.requestStartedAt;
+        res.responseTimeMs = timeTakenMs;
+
+        if (isDevelopment) {
+            outputToConsole(res);
+        } else {
+            // TODO : Output to stats engine
         }
-    );
 
-    interceptors.response.use(
-        res => {
-            const timeTakenMs = new Date().getTime() - res.config.meta.requestStartedAt;
-            res.responseTimeMs = timeTakenMs;
-
-            if (isDevelopment) {
-                outputToConsole(res);
-            } else {
-                // TODO : Output to stats engine
-            }
-
-            return res;
-        },
-        error => {
-            Promise.reject(error);
-        }
-    );
+        return res;
+    });
 };
 
 export default {
