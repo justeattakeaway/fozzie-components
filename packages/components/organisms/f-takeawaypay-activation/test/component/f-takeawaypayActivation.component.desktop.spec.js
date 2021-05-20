962111a5a73bb038e7af8a47a71c2b0893da8e35
oleksiishevchukje <oleksii.shevchuk@just-eat.com>
2021-05-20 11:10:22 +0300

    f-takeawaypay-activation@0.1.0 - Generate component (#958)
    * f-takeawaypay-activation@0.1.0 - Generate component
    
    * Code cleanup
    
    * Fix tests
    
    * Fixed component name in tests
    
    * Fix typo in the component name
    
    * Fix component name
    
    * Renamed component
    
    * Added name explicitly
    
    * Renamed back to TakeawaypayActivation name
    
    * Renamed
    
    Co-authored-by: Oleksii <oleksii.shevchuk@justeattakeaway.com>

---
+++ packages/components/organisms/f-takeawaypay-activation/test/component/f-takeawaypayActivation.component.desktop.spec.js
@@ -0,0 +1,14 @@
+const TakeawaypayActivation = require('../../test-utils/component-objects/f-takeawaypayActivation.component');
+const takeawaypayActivation = new TakeawaypayActivation();
+
+describe('f-takeawaypayActivation component tests', () => {
+    beforeEach(() => {
+        takeawaypayActivation.open();
+        takeawaypayActivation.waitForComponent();
+    });
+
+    it('should display the f-takeawaypayActivation component', () => {
+        // Assert
+        expect(takeawaypayActivation.isComponentDisplayed()).toBe(true);
+    });
+});
