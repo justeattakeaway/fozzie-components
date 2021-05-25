9f38a03347e061cece9e761be4d8f6b36c2527d4
Lizzie Turney <lizzie.turney@just-eat.com>
2021-05-25 13:52:57 +0100

    removed commented code

--- packages/components/organisms/f-header/test/accessibility/axe-accessibility.spec.js
+++ packages/components/organisms/f-header/test/accessibility/axe-accessibility.spec.js
@@ -1,24 +1,24 @@
-// import forEach from 'mocha-each';
+import forEach from 'mocha-each';
 
-// const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions.js');
-// const { getAccessibilityTestResults } = require('../../../../../../test/utils/axe-helper');
-// const Header = require('../../test-utils/component-objects/f-header.component');
+const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions.js');
+const { getAccessibilityTestResults } = require('../../../../../../test/utils/axe-helper');
+const Header = require('../../test-utils/component-objects/f-header.component');
 
-// const header = new Header('organism', 'header-component');
+const header = new Header('organism', 'header-component');
 
-// describe('Accessibility tests', () => {
-//     forEach(['en-GB', 'en-AU', 'en-NZ', 'en-IE', 'it-IT', 'es-ES', 'da-DK', 'nb-NO'])
-//     .it('a11y - should test f-header component WCAG compliance for "%s"', tenant => {
-//         // Act
-//         header.withQuery('&knob-Locale', tenant);
-//         header.withQuery('&knob-Show offers link', 'true');
-//         header.withQuery('&knob-Show delivery enquiry', 'true');
-//         const pageUrl = buildUrl(header.componentType, header.componentName, header.path);
-//         header.open(pageUrl);
-//         header.waitForComponent();
-//         const axeResults = getAccessibilityTestResults('f-header');
+describe('Accessibility tests', () => {
+    forEach(['en-GB', 'en-AU', 'en-NZ', 'en-IE', 'it-IT', 'es-ES', 'da-DK', 'nb-NO'])
+    .it('a11y - should test f-header component WCAG compliance for "%s"', tenant => {
+        // Act
+        header.withQuery('&knob-Locale', tenant);
+        header.withQuery('&knob-Show offers link', 'true');
+        header.withQuery('&knob-Show delivery enquiry', 'true');
+        const pageUrl = buildUrl(header.componentType, header.componentName, header.path);
+        header.open(pageUrl);
+        header.waitForComponent();
+        const axeResults = getAccessibilityTestResults('f-header');
 
-//         // Assert
-//         expect(axeResults.violations.length).toBe(0);
-//     });
-// });
+        // Assert
+        expect(axeResults.violations.length).toBe(0);
+    });
+});
