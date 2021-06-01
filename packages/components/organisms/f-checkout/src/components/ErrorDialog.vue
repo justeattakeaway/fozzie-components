ea5ecd41448383d6f20f1098003f84b31af5d916
Kevin Rodrigues <kevrodrigues116@gmail.com>
2021-06-01 10:15:31 +0100

    f-checkout@v0.124.0 - Fix modal breaking page scrolling. (#995)
    * f-checkout@v0.121.0 - Fix modal breaking page scrolling.
    
    * f-checkout@v0.121.0 - Update version.
    
    * f-checkout@v0.121.0 - Fix error.
    
    * f-checkout@v0.121.0 - Add tests.
    
    * f-checkout@v0.121.0 - Update changelog and package file.

--- packages/components/organisms/f-checkout/src/components/ErrorDialog.vue
+++ packages/components/organisms/f-checkout/src/components/ErrorDialog.vue
@@ -1,5 +1,6 @@
 <template>
     <mega-modal
+        ref="errorModal"
         data-test-id="checkout-issue-modal"
         has-overlay
         :title="$t(`errorMessages.checkoutIssues.${errorCode}.title`, { serviceType: serviceTypeText })"
@@ -39,7 +40,7 @@ export default {
 
     data () {
         return {
-            isOpen: false
+            isOpen: true
         };
     },
 
@@ -64,7 +65,9 @@ export default {
     },
 
     mounted () {
-        this.isOpen = true;
+        const modalContext = this.getModalContext();
+
+        if (modalContext) modalContext.open();
     },
 
     methods: {
@@ -72,12 +75,25 @@ export default {
             'updateMessage'
         ]),
 
+        getModalContext () {
+            const { errorModal } = this.$refs;
+            const { megaModal } = errorModal.$refs;
+
+            if (megaModal) return errorModal;
+
+            return null;
+        },
+
         closeErrorDialog () {
+            const modalContext = this.getModalContext();
+
             if (this.message && this.message.shouldRedirectToMenu) {
                 window.location.assign(this.restaurantMenuPageUrl);
             }
 
             this.updateMessage();
+
+            if (modalContext) modalContext.close();
         }
     }
 };
