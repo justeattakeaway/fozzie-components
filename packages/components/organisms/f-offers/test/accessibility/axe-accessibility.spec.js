dabc15f38d3cec853d205f985d84f2dd6687d58b
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-06-02 13:10:49 +0100

    changes to fix broken acessibility tests

--- packages/components/organisms/f-offers/test/accessibility/axe-accessibility.spec.js
+++ packages/components/organisms/f-offers/test/accessibility/axe-accessibility.spec.js
@@ -1,11 +1,16 @@
 import { getAccessibilityTestResults } from '../../../../../../test/utils/axe-helper';
 
+const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions.js');
+
 const Offers = require('../../test-utils/component-objects/f-offers.component');
-const offers = new Offers();
+
+const offers = new Offers('organism', 'v-offers-component');
 
 describe('Accessibility tests', () => {
     beforeEach(() => {
-        offers.open();
+        const pageUrl = buildUrl(offers.componentType, offers.componentName, offers.path);
+
+        offers.open(pageUrl);
         offers.waitForComponent();
     });
     it('a11y - should test f-offers component WCAG compliance', () => {
