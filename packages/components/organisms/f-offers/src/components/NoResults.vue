7ce34326350e191b08e9f407044b3bbaee43c512
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-06-14 11:38:32 +0100

    adding f-button to no results component

--- packages/components/organisms/f-offers/src/components/NoResults.vue
+++ packages/components/organisms/f-offers/src/components/NoResults.vue
@@ -8,12 +8,22 @@
             image-url="https://d30v2pzvrfyzpo.cloudfront.net/a/hw/img/decoration/oi_no-results-image.svg"
             :image-align="imageAlign"
             :text-size="textSize"
-        />
+        >
+            <f-button
+                :class="$style['c-noResults-link']"
+                button-type="link"
+                button-size="xsmall"
+                href="/terms&conditions"
+            >
+                Full terms and conditions here
+            </f-button>
+        </media-element>
     </div>
 </template>
 
 <script>
 import MediaElement from '@justeat/f-media-element';
+import FButton from '@justeat/f-button';
 import '@justeat/f-card/dist/f-card.css';
 import '@justeat/f-media-element/dist/f-media-element.css';
 import { ALIGN, FONT_SIZE } from '@justeat/f-media-element/src/constants';
@@ -22,12 +32,13 @@ export default {
     name: 'OffersNoResults',
 
     components: {
-        MediaElement
+        MediaElement,
+        FButton
     },
 
     data: () => ({
         imageAlign: ALIGN.RIGHT,
-        textSize: FONT_SIZE.XL
+        textSize: FONT_SIZE.LG
     })
 };
 </script>
@@ -37,5 +48,10 @@ export default {
     width: 100%;
     background: $color-grey-20;
     padding: spacing(x4) spacing(x5);
+
+    & .c-noResults-link {
+        padding: spacing(x2) 0;
+    }
+
 }
 </style>
