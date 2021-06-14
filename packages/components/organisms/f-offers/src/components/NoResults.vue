57bebf685e1cba797f7e1d001ce946690c57e3e3
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-06-14 10:02:02 +0100

    changes to implement first part of no results component

---
+++ packages/components/organisms/f-offers/src/components/NoResults.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+        :class="$style['c-noResults-wrapper']"
+    >
+        <media-element
+            title="Can’t find anything?"
+            text="There might still be restaurants near you with a tasty discount or deal on."
+            image-url="https://d30v2pzvrfyzpo.cloudfront.net/a/hw/img/decoration/oi_no-results-image.svg"
+            :image-align="imageAlign"
+            :text-size="textSize"
+        />
+    </div>
+</template>
+
+<script>
+import MediaElement from '@justeat/f-media-element';
+import '@justeat/f-card/dist/f-card.css';
+import '@justeat/f-media-element/dist/f-media-element.css';
+import { ALIGN, FONT_SIZE } from '@justeat/f-media-element/src/constants';
+
+export default {
+    name: 'OffersNoResults',
+
+    components: {
+        MediaElement
+    },
+
+    data: () => ({
+        imageAlign: ALIGN.RIGHT,
+        textSize: FONT_SIZE.XL
+    })
+};
+</script>
+
+<style lang="scss" module>
+.c-noResults-wrapper {
+    width: 100%;
+    background: $color-grey-20;
+    padding: spacing(x4) spacing(x5);
+}
+</style>
--- packages/components/organisms/f-offers/src/components/Offers.vue
+++ packages/components/organisms/f-offers/src/components/Offers.vue
@@ -2,23 +2,21 @@
     <div
         :class="$style['c-offers']"
         data-test-id="offers">
-        <offers-search-box />
+        <no-results />
     </div>
 </template>
 
 <script>
 import { globalisationServices } from '@justeat/f-services';
 import tenantConfigs from '../tenants';
-import OffersSearchBox from './SearchBox.vue';
+import NoResults from './NoResults.vue';
 import '@justeat/f-searchbox/dist/f-searchbox.css';
 
 export default {
     name: 'VOffers',
-
     components: {
-        OffersSearchBox
+        NoResults
     },
-
     props: {
         locale: {
             type: String,
@@ -42,12 +40,8 @@ export default {
 .c-offers {
     display: flex;
     justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
+    width: 100%;
     margin: auto;
-    border: 1px solid $color-red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
 }
 
 </style>
