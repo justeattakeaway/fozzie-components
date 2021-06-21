4568e6fc33f5738e08a3ded564c0c8edf27de969
matthewhardern <matthewhardern@gmail.com>
2021-06-21 12:50:48 +0100

    f-offers@v0.4.0 - NoResults component (#1053)
    * changes to implement first part of no results component
    
    * adding f-button to no results component
    
    * changes to add globalisation plugin and tests
    
    * changes to add searchbox to no results
    
    * version and changelog update
    
    * changes based on PR recomendations
    
    Co-authored-by: Matthew Hardern <matthew.hardern@justeattakeaway.com>

--- packages/components/organisms/f-offers/src/components/_tests/Offers.test.js
+++ packages/components/organisms/f-offers/src/components/_tests/Offers.test.js
@@ -1,10 +1,27 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { VueI18n } from '@justeat/f-globalisation/src';
+import tenantConfigs from '../../tenants';
 import VOffers from '../Offers.vue';
+
+const localVue = createLocalVue();
+
+localVue.use(VueI18n);
+
+const i18n = {
+    locale: 'en-GB',
+    messages: {
+        'en-GB': tenantConfigs['en-GB'].messages
+    }
+};
 
 describe('Offers', () => {
     it('should be defined', () => {
         const propsData = {};
-        const wrapper = shallowMount(VOffers, { propsData });
+        const wrapper = shallowMount(VOffers, {
+            propsData,
+            i18n,
+            localVue
+        });
         expect(wrapper.exists()).toBe(true);
     });
 });
