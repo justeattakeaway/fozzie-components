db7b0b19bdcf917e20c43584371ff1b873071ade
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-06-23 12:34:07 +0100

    removing override style

--- packages/components/molecules/f-media-element/src/components/MediaElement.vue
+++ packages/components/molecules/f-media-element/src/components/MediaElement.vue
@@ -24,7 +24,6 @@
         <div
             :class="[
                 $style['c-mediaElement-imgWrapper'],
-                'c-mediaElement-imgWrapper--override',
                 imageAlignClass
             ]">
             <img
