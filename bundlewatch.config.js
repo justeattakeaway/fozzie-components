a069d017f9027786868476833177b35065ac88f9
Xander Marjoram <xander.marjoram@just-eat.com>
2021-07-01 11:23:44 +0100

    fozzie-components@3.43.0 - More granular package size limits for bundlewatch #globalconfig (#1088)
    * fozzie-components@3.43.0 - Use different package size limits for atoms/molecules/organisms, etc
    
    * fozzie-component@3.43.0 - Changelog updates
    
    * fozzie-components@3.43.0 - Add changelog entry
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

--- bundlewatch.config.js
+++ bundlewatch.config.js
@@ -3,12 +3,17 @@
 const { readdirSync } = require('fs');
 
 const packageFolders = [
-    'packages/components/atoms',
-    'packages/components/molecules',
-    'packages/components/organisms',
-    'packages/services',
-    'packages/tools'
+    { name: 'packages/components/atoms', maxSize: '15kB' },
+    { name: 'packages/components/molecules', maxSize: '60kB' },
+    { name: 'packages/components/organisms', maxSize: '100kB' },
+    { name: 'packages/services', maxSize: '100kB' },
+    { name: 'packages/tools', maxSize: '100kB' }
 ];
+
+const getMaxSizeForPackage = packageName => {
+    const folder = packageFolders.find(f => packageName.startsWith(f.name));
+    return (folder && folder.maxSize) ? folder.maxSize : '100kB';
+};
 
 const excludedPackages = [
     'packages/services/f-braze-adapter',
@@ -23,22 +28,21 @@ const excludedPackages = [
  * @param {String} source – Path string to check the contents of.
  * @returns {Array} – A set of subfolder paths to each package found
  */
-const getDirectories = source =>
-    readdirSync(source, { withFileTypes: true })
+const getDirectories = source => readdirSync(source, { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
         .map(dirent => `${source}/${dirent.name}`);
 
 let packageNames = [];
 
 packageFolders.forEach(folder => {
-    packageNames = packageNames.concat(getDirectories(folder));
+    packageNames = packageNames.concat(getDirectories(folder.name));
 });
 
-const filteredPackages = packageNames.filter(package => !excludedPackages.includes(package));
+const filteredPackages = packageNames.filter(packageName => !excludedPackages.includes(packageName));
 
-const files = filteredPackages.map(package => ({
-    path: `${package}/dist/*+(.min|.min.umd|.es).js`,
-    maxSize: '100kB'
+const files = filteredPackages.map(packageName => ({
+    path: `${packageName}/dist/*+(.min|.min.umd|.es).js`,
+    maxSize: getMaxSizeForPackage(packageName)
 }));
 
 module.exports = {
