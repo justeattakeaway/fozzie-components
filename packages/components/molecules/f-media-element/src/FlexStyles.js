a607952b37ebc8d273c160332612ecaefcc63688
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-07-02 08:49:58 +0100

    modification to how the css classes are created

--- packages/components/molecules/f-media-element/src/FlexStyles.js
+++ packages/components/molecules/f-media-element/src/FlexStyles.js
@@ -3,50 +3,97 @@ import { MODIFIER_OPPOSITES_RULES_MAP, MODIFIER_RULES_MAP } from './constants';
 class FlexStyles {
     flexDirection = '--row';
 
-    flexReverse = '';
+    flexReverse = undefined;
 
     defaultFlexDirection = '--row';
 
     defaultFlexReverse = '';
 
-    operator = '';
+    operator = undefined;
 
-    operatorOpposite = '';
+    operatorOpposite = undefined;
 
-    mediaSize = '';
+    mediaSize = undefined;
 
+    /**
+     * Adds the specific part of css class string
+     * @param value
+     * @returns {string}
+     */
     static returnReverseClassSegment (value) {
         return value ? '--reverse' : '';
     }
 
+    /**
+     * Adds the specific part of css class string
+     * @param value
+     * @returns {string}
+     */
     static returnDirectionClassSegment (value) {
         return value ? '--col' : '--row';
     }
 
-    get styles () {
-        return [
-            this._createDefaultStyle(),
-            this._createOppositeStyle(),
-            this._createStyle()
-        ];
+    /**
+     * Function creates a string (css class name) using the previously set class properties
+     * @returns {*}
+     * @private
+     */
+    static createStyle (...values) {
+        if (!values.some(value => value === undefined)) {
+            return values.reduce((a, c) => `${a}${c}`, 'c-mediaElement');
+        }
+        return undefined;
     }
 
+    /**
+     * Compiles a list of css classes to be applied
+     * @returns {*[]}
+     */
+    get styles () {
+        return [
+            FlexStyles.createStyle(this.defaultFlexDirection, this.defaultFlexReverse),
+            FlexStyles.createStyle(this.operatorOpposite, this.mediaSize, this.defaultFlexDirection, this.defaultFlexReverse),
+            FlexStyles.createStyle(this.operator, this.mediaSize, this.flexDirection, this.flexReverse)
+        ].filter(value => value !== undefined);
+    }
+
+    /**
+     * Sets the flex direction css string part value
+     * @param value
+     */
     set column (value) {
         this.flexDirection = FlexStyles.returnDirectionClassSegment(value);
     }
 
+    /**
+     * Sets the flex reverse css string part value
+     * @param value
+     */
     set reverse (value) {
         this.flexReverse = FlexStyles.returnReverseClassSegment(value);
     }
 
+    /**
+     * Sets the default flex direction css string part value
+     * @param value
+     */
     set defaultColumn (value) {
         this.flexDirection = FlexStyles.returnDirectionClassSegment(value);
     }
 
+    /**
+     * Sets the default flex reverse css string part value
+     * @param value
+     */
     set defaultReverse (value) {
         this.flexReverse = FlexStyles.returnReverseClassSegment(value);
     }
 
+    /**
+     * This sets the operators within the css class string and the media size on which to apply
+     * @param operator
+     * @param size
+     */
     set rule ([operator, size]) {
         this.operator = `--${MODIFIER_RULES_MAP[operator]}`;
         this.operatorOpposite = `--${MODIFIER_OPPOSITES_RULES_MAP[operator]}`;
@@ -59,23 +106,11 @@ class FlexStyles {
     }) {
         this.defaultColumn = defaultColumn;
         this.defaultReverse = defaultReverse;
-        if (modifier) {
-            this.column = modifier?.column;
-            this.reverse = modifier?.reverse;
+        this.column = modifier?.column;
+        this.reverse = modifier?.reverse;
+        if (modifier?.rule) {
             this.rule = modifier?.rule;
         }
-    }
-
-    _createDefaultStyle () {
-        return `c-mediaElement${this.defaultFlexDirection}${this.defaultFlexReverse}`;
-    }
-
-    _createOppositeStyle () {
-        return `c-mediaElement${this.operatorOpposite}${this.mediaSize}${this.defaultFlexDirection}${this.defaultFlexReverse}`;
-    }
-
-    _createStyle () {
-        return `c-mediaElement${this.operator}${this.mediaSize}${this.flexDirection}${this.flexReverse}`;
     }
 }
 
