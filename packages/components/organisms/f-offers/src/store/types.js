dd5c142f009b4b2b3610ad0996db337428536cfe
Matthew Hardern <matthewhardern@gmail.com>
2021-07-07 13:29:43 +0100

    f-offers@v0.6.0 - adding header component (#1096)
    * change to f-offers to add in header component made with new f-media-element
    
    * added tests for Header.vue
    
    * changed changelog and updated package version
    
    * adding in PR recomendations and additional test for name being null when authenticated

--- packages/components/organisms/f-offers/src/store/types.js
+++ packages/components/organisms/f-offers/src/store/types.js
@@ -4,7 +4,13 @@ export const ACTION_INVALID_ADDRESS = 'invalidAddress';
 export const ACTION_UNKNOWN_ERROR = 'unknownError';
 export const ACTION_VALID_SAVED_ADDRESS_SEARCH = 'validSavedAddressSearch';
 export const ACTION_VALID_SEARCH = 'validSearch';
+export const ACTION_INITIALISE_OFFERS = 'initialise';
+export const ACTION_HANDLE_CUSTOMER_DATA = 'handleCustomerData';
 
 export const MUTATION_EMIT_SEARCH_START = 'searchbox-focus';
+export const MUTATION_SET_AUTH_TOKEN = 'setAuthToken';
+export const MUTATION_SET_CUSTOMER_DATA = 'setCustomerData';
+export const MUTATION_SET_BRAZE_KEY = 'setBrazeKey';
 
 export const VUEX_MODULE_NAMESPACE_OFFERS_SEARCH = 'offersSearch';
+export const VUEX_MODULE_NAMESPACE_OFFERS = 'offers';
