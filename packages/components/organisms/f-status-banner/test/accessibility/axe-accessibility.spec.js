012eb56bab6ad5d2f9fb726b8df44db1079309ca
Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>
2021-07-12 16:53:29 +0100

    fozzie-components@v3.44.6 – Linting and formatting fixes (#1111)

--- packages/components/organisms/f-status-banner/test/accessibility/axe-accessibility.spec.js
+++ packages/components/organisms/f-status-banner/test/accessibility/axe-accessibility.spec.js
@@ -1,4 +1,4 @@
-const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions.js');
+const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions');
 const { getAccessibilityTestResults } = require('../../../../../../test/utils/axe-helper');
 
 const StatusBanner = require('../../test-utils/component-objects/f-statusBanner.component');
