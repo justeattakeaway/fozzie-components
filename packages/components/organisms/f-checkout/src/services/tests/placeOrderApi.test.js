027bc0b589dfd4575fb00c69bfc41bfeba672267
Lizzie Turney <lizzie.turney@just-eat.com>
2021-07-16 13:28:46 +0100

    added order place API test

---
+++ packages/components/organisms/f-checkout/src/services/tests/placeOrderApi.test.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import placeOrderApi from '../orderPlacementApi';
+import { version as applicationVersion } from '../../../package.json';
+import { mockAuthToken } from '../../components/_tests/helpers/setup';
+
+const authToken = mockAuthToken;
+
+
+describe('BasketApi', () => {
+    let payload;
+    let config;
+    let state;
+
+    beforeEach(() => {
+        // Arrange
+        payload = {
+            url: 'http://localhost/account/checkout',
+            timeout: 10000,
+            data: ''
+        };
+
+        state = {
+            authToken
+        };
+
+        config = {
+            headers: {
+                'Content-Type': 'application/json;v=2',
+                'x-je-application-id': 7, // Responsive Web
+                'x-je-application-version': applicationVersion,
+                Authorization: `Bearer ${authToken}`
+            },
+            timeout: payload.timeout
+        };
+
+        axios.post = jest.fn(() => Promise.resolve({ status: 200 }));
+    });
+    describe('getBasket ::', () => {
+        it('should get the basket details from the backend', async () => {
+            // Act
+            await placeOrderApi.placeOrder(payload.url, payload.data, payload.timeout, state);
+
+            // Assert
+            expect(axios.post).toHaveBeenCalledWith(payload.url, payload.data, config);
+        });
+    });
+});
