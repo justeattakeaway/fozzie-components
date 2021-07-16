1c8c99a6b533d836c1e6fdfa20d029837a6d2668
Lizzie Turney <lizzie.turney@just-eat.com>
2021-07-16 12:04:46 +0100

    tests for checkoutApi

--- packages/components/organisms/f-checkout/src/services/tests/checkoutApi.test.js
+++ packages/components/organisms/f-checkout/src/services/tests/checkoutApi.test.js
@@ -1,142 +1,98 @@
-// import axios from 'axios';
-// import checkoutApi from '../checkoutApi';
-// import CheckoutModule from '../../store/checkout.module';
-// import checkoutDelivery from '../../demo/checkout-delivery.json';
-// import { mockAuthToken } from '../../components/_tests/helpers/setup';
-// // import basketDelivery from '../../demo/get-basket-delivery.json';
+import axios from 'axios';
+import checkoutApi from '../checkoutApi';
+import { mockAuthToken } from '../../components/_tests/helpers/setup';
 
+const mobileNumber = '+447111111111';
+const authToken = mockAuthToken;
 
-it('should get the checkout details from the backend', () => {
-    // Act
-    // await checkoutApi.getCheckout(payload.url, state, payload.timeout);
+describe('CheckoutApi', () => {
+    let payload;
+    let rootGetters;
+    let state;
 
-    // Assert
-    expect(1).toEqual(1);
+    beforeEach(() => {
+        rootGetters = {};
+        payload = {
+            url: 'http://localhost/account/checkout',
+            getCustomerUrl: 'http://localhost/customer',
+            tenant: 'uk',
+            language: 'en-GB',
+            timeout: 10000
+        };
+        state = {
+            authToken
+        };
+    });
+
+    describe('getCheckout ::', () => {
+        let config;
+
+        beforeEach(() => {
+            config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${state.authToken}`
+                },
+                timeout: payload.timeout
+            };
+
+            axios.get = jest.fn(() => Promise.resolve({}));
+        });
+
+        it('should get the checkout details from the backend', async () => {
+            // Act
+            await checkoutApi.getCheckout(payload.url, state, payload.timeout);
+
+            // Assert
+            expect(axios.get).toHaveBeenCalledWith(payload.url, config);
+        });
+    });
+    describe('updateCheckout ::', () => {
+        let config;
+
+        beforeEach(() => {
+            payload = {
+                mobileNumber
+            };
+            config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${state.authToken}`
+                },
+                timeout: payload.timeout
+            };
+
+            axios.patch = jest.fn(() => Promise.resolve({ }));
+        });
+
+        it('should patch the checkout details to the backend', async () => {
+            // Act
+            await checkoutApi.updateCheckout(payload.url, state, rootGetters, payload.data, payload.timeout);
+
+            // Assert
+            expect(axios.patch).toHaveBeenCalledWith(payload.url, payload.data, config);
+        });
+    });
+    describe('getAvailableFulfilment ::', () => {
+        let config;
+
+        beforeEach(() => {
+            config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                timeout: payload.timeout
+            };
+
+            axios.get = jest.fn(() => Promise.resolve({ }));
+        });
+
+        it('should get the checkout available fulfilment details from the backend', async () => {
+            // Act
+            await checkoutApi.getAvailableFulfilment(payload.url, payload.timeout);
+
+            // Assert
+            expect(axios.get).toHaveBeenCalledWith(payload.url, config);
+        });
+    });
 });
-
-// const mobileNumber = '+447111111111';
-
-// const customerDetails = {
-//     mobileNumber
-// };
-
-// const authToken = mockAuthToken;
-
-// const address = {
-//     line1: 'line 1',
-//     line2: 'line 2',
-//     locality: 'locality',
-//     postcode: 'postcode'
-// };
-
-// const locationData = {
-//     addressLines: [
-//         '1 Jazz Avenue',
-//         'Strange Town',
-//         'JZ1 1AA'
-//     ]
-// };
-
-// const basketId = 'newbasketid0001-v1';
-
-// const time = {
-//     from: 'fromTime',
-//     to: 'toTime'
-// };
-
-// const isFulfillable = false;
-
-// const issues = [
-//     {
-//         code: 'RESTAURANT_UNAVAILABLE'
-//     }
-// ];
-
-// const userNote = 'Beware of the dachshund';
-// const message = {
-//     code: 'DuplicateOrder',
-//     shouldRedirectToMenu: false,
-//     shouldShowInDialog: true
-// };
-
-// const defaultState = {
-//     id: '',
-//     serviceType: '',
-//     tableIdentifier: '',
-//     restaurant: {
-//         id: '',
-//         seoName: ''
-//     },
-//     basket: {
-//         id: '',
-//         total: 0
-//     },
-//     customer: {
-//         firstName: '',
-//         lastName: '',
-//         email: '',
-//         mobileNumber: ''
-//     },
-//     orderId: '',
-//     time: {
-//         from: '',
-//         to: ''
-//     },
-//     address: {
-//         line1: '',
-//         line2: '',
-//         locality: '',
-//         postcode: ''
-//     },
-//     isFulfillable: true,
-//     errors: [],
-//     notices: [],
-//     message: null,
-//     messages: [],
-//     availableFulfilment: {
-//         times: [],
-//         isAsapAvailable: false
-//     },
-//     authToken: '',
-//     isLoggedIn: false,
-//     isGuestCreated: false,
-//     userNote: '',
-//     geolocation: null,
-//     hasAsapSelected: false
-// };
-
-// const state = CheckoutModule.state();
-
-// describe('getCheckout ::', () => {
-//     let payload;
-//     let checkoutDeliveryCopy;
-//     let config;
-
-//     beforeEach(() => {
-//         payload = {
-//             url: 'http://localhost/account/checkout',
-//             tenant: 'uk',
-//             language: 'en-GB',
-//             timeout: 10000,
-//             postData: null
-//         };
-
-//         config = {
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             timeout: payload.timeout
-//         };
-
-//         checkoutDeliveryCopy = Object.assign(checkoutDelivery);
-//         axios.get = jest.fn(() => Promise.resolve({ checkoutDeliveryCopy, config }));
-//     });
-
-//     it('should get the checkout details from the backend', async () => {
-//         // Act
-//         await checkoutApi.getCheckout(payload.url, state, payload.timeout);
-
-//         // Assert
-//         expect(axios.get).toHaveBeenCalledWith(payload.url, state, payload.timeout);
-//     });
-// });
