7ff1dc8fde2f3f220a8c021cf5a46990f94f1437
Lizzie Turney <lizzie.turney@just-eat.com>
2021-07-16 12:25:26 +0100

    updated tests

--- packages/components/organisms/f-checkout/src/services/tests/addressGeocodingApi.test.js
+++ packages/components/organisms/f-checkout/src/services/tests/addressGeocodingApi.test.js
@@ -1,93 +1,49 @@
 import axios from 'axios';
-import checkoutApi from '../checkoutApi';
+import addressGeocodingApi from '../addressGeocodingApi';
 import { mockAuthToken } from '../../components/_tests/helpers/setup';
 
-const mobileNumber = '+447111111111';
+const locationData = {
+    addressLines: [
+        '1 Jazz Avenue',
+        'Strange Town',
+        'JZ1 1AA'
+    ]
+};
+
 const authToken = mockAuthToken;
 
 describe('AddressGeocodingApi', () => {
     let payload;
     let state;
+    let config;
 
     beforeEach(() => {
+        // Arrange
         payload = {
             url: 'http://localhost/account/checkout',
-            timeout: 10000
+            timeout: 10000,
+            postData: locationData
+        };
+        config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${authToken}`
+            },
+            timeout: payload.timeout
         };
         state = {
             authToken
         };
-    });
 
-    describe('getCheckout ::', () => {
-        let config;
-
-        beforeEach(() => {
-            config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${state.authToken}`
-                },
-                timeout: payload.timeout
-            };
-
-            axios.get = jest.fn(() => Promise.resolve({}));
-        });
-
-        it('should get the checkout details from the backend', async () => {
-            // Act
-            await checkoutApi.getCheckout(payload.url, state, payload.timeout);
-
-            // Assert
-            expect(axios.get).toHaveBeenCalledWith(payload.url, config);
-        });
-    });
-    describe('updateCheckout ::', () => {
-        let config;
-
-        beforeEach(() => {
-            payload = {
-                mobileNumber
-            };
-            config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${state.authToken}`
-                },
-                timeout: payload.timeout
-            };
-
-            axios.patch = jest.fn(() => Promise.resolve({ }));
-        });
-
-        it('should patch the checkout details to the backend', async () => {
-            // Act
-            await checkoutApi.updateCheckout(payload.url, state, rootGetters, payload.data, payload.timeout);
-
-            // Assert
-            expect(axios.patch).toHaveBeenCalledWith(payload.url, payload.data, config);
-        });
+        axios.post = jest.fn(() => Promise.resolve({ status: 200 }));
     });
     describe('getAvailableFulfilment ::', () => {
-        let config;
-
-        beforeEach(() => {
-            config = {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                timeout: payload.timeout
-            };
-
-            axios.get = jest.fn(() => Promise.resolve({ }));
-        });
-
         it('should get the checkout available fulfilment details from the backend', async () => {
             // Act
-            await checkoutApi.getAvailableFulfilment(payload.url, payload.timeout);
+            await addressGeocodingApi.getGeoLocation(payload.url, payload.postData, payload.timeout, state);
 
             // Assert
-            expect(axios.get).toHaveBeenCalledWith(payload.url, config);
+            expect(axios.post).toHaveBeenCalledWith(payload.url, payload.postData, config);
         });
     });
 });
