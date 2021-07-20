8bf8ee78e403f16b4503c264aa6617a72312b67d
Lizzie Turney <lizzie.turney@just-eat.com>
2021-07-20 13:47:02 +0100

    readded layout for checkout

--- packages/components/organisms/f-checkout/stories/Checkout.stories.js
+++ packages/components/organisms/f-checkout/stories/Checkout.stories.js
@@ -155,6 +155,7 @@ export const CheckoutComponent = () => ({
             getCustomerUrl
         };
     },
+    layout: 'fullscreen',
     props: {
         isLoggedIn: {
             default: boolean('Is User Logged In', false)
