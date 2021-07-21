ec4070b34c916c826929cc6043e6d11e8bfc2c58
Charles Waite <charles.waite@justeattakeaway.com>
2021-07-21 14:13:35 +0100

    Revert "Removal of test to debug issue"
    This reverts commit 6bedad7a59b48a380cfc76a1c96cb3f091cd0f59.

---
+++ packages/components/organisms/f-checkout/test/component/f-checkout-delivery.component.desktop.spec.js
@@ -0,0 +1,33 @@
+const { buildUrl } = require('@justeat/f-wdio-utils/src/storybook-extensions');
+const Checkout = require('../../test-utils/component-objects/f-checkout.component');
+
+
+let checkout;
+
+describe('f-checkout "delivery" component tests', () => {
+    beforeEach(() => {
+        checkout = new Checkout('organism', 'checkout-component');
+        checkout.withQuery('&knob-Service Type', 'delivery')
+            .withQuery('&knob-Is User Logged In', true)
+            .withQuery('&knob-Is ASAP available', true);
+
+        const pageUrl = buildUrl(checkout.componentType, checkout.componentName, checkout.path);
+
+        checkout.open(pageUrl);
+        checkout.waitForComponent();
+    });
+
+    it('should enable a user to submit a postcode with correct characters', () => {
+        // Arrange
+        const addressInfo = {
+            postcode: 'AR51 1AA'
+        };
+
+        // Act
+        checkout.populateCheckoutForm(addressInfo);
+        checkout.goToPayment();
+
+        // Assert
+        expect(checkout.isPostcodeTypeErrorDisplayed()).toBe(false);
+    });
+});
