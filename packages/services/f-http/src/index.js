7c0254d5ae70c9b61eda0f3d229cacf2681dd600
Thomas William McClean <spam@tommcclean.me>
2021-07-28 12:26:41 +0100

    f-http@v0.6.0 - Collect and Push Dependency Timings (#1123)
    * f-http@v0.6.0 - Utilise f-statistics
    
    * f-http@v0.6.0 - Correction
    
    * f-http@v0.6.0 - Rewrite property names
    
    * f-http@v0.6.0 - Don't wrap payload
    
    * f-http@v0.6.0 - Update uri stem to capture base url
    
    * f-http@v0.6.0 - Switch to CTOR for multiple instantiations
    
    * f-http@v0.6.0 - Version
    
    * f-http@v0.6.0 - Update changelog
    
    Co-authored-by: Thomas William McClean <thomas.mcclean@justeattakeaway.com>
    Co-authored-by: Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>

--- packages/services/f-http/src/index.js
+++ packages/services/f-http/src/index.js
@@ -1,22 +1,6 @@
-import createClient from './createClient';
-
-import {
-    createMockClient,
-    setupMockResponse
-} from './mockFactory';
-
+import CreateClient from './createClient';
+import MockFactory from './mockFactory';
 import httpVerbs from './httpVerbs';
-
-/**
- *  @class mockFactory
- *  @type {Object}
- *  @property {function} createMockClient Create a new HTTP client which supports mocking of the underlying connection
- *  @property {object} setupMockResponse Setup a fixed response for a HTTP request
- */
-const mockFactory = {
-    createMockClient,
-    setupMockResponse
-};
 
 /**
  *  @class httpModule
@@ -26,8 +10,8 @@ const mockFactory = {
  *  @property {object} httpVerbs Exposes HTTP Verbs as constants
  */
 const httpModule = {
-    createClient,
-    mockFactory,
+    CreateClient,
+    MockFactory,
     httpVerbs
 };
 
