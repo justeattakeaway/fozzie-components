2d3cc2121ed3229fb15f64b25fe53724b3ae17a3
Faye <faye.carter@justeattakeaway.com>
2021-07-28 13:40:09 +0100

    Update package version

---
+++ packages/components/atoms/f-form-field/src/components/_tests/FormFieldAfix.test.js
@@ -0,0 +1,110 @@
+import { shallowMount } from '@vue/test-utils';
+import FormFieldAfix from '../FormFieldAfix.vue';
+
+describe('FormFieldAfix', () => {
+    const attributes = {
+        disabled: null
+    };
+
+    const propsData = { attributes };
+
+    it('should be defined', () => {
+        const wrapper = shallowMount(FormFieldAfix, { propsData });
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    describe('props ::', () => {
+        describe('prefix ::', () => {
+            describe.each([
+                [null, false],
+                ['', false],
+                ['$', true]
+            ])('when prefix value is', (prefixValue, prefixVisible) => {
+                it(`should ${prefixVisible ? '' : 'not '}display 'c-formField-afix-prefix'`, () => {
+                    // Arrange & Act
+                    propsData.prefix = prefixValue;
+
+                    const wrapper = shallowMount(FormFieldAfix, { propsData });
+                    const formPrefix = wrapper.find('[data-test-id="formfield-afix-prefix"]');
+
+                    // Assert
+                    expect(formPrefix.exists()).toBe(prefixVisible);
+                });
+            });
+        });
+
+        describe('suffix ::', () => {
+            describe.each([
+                [null, false],
+                ['', false],
+                ['GBP', true]
+            ])('when suffix value is', (suffixValue, suffixVisible) => {
+                it(`should ${suffixVisible ? '' : 'not '}display 'c-formField-afix-suffix'`, () => {
+                    // Arrange & Act
+                    propsData.suffix = suffixValue;
+
+                    const wrapper = shallowMount(FormFieldAfix, { propsData });
+                    const formSuffix = wrapper.find('[data-test-id="formfield-afix-suffix"]');
+
+                    // Assert
+                    expect(formSuffix.exists()).toBe(suffixVisible);
+                });
+            });
+        });
+    });
+
+    describe('computed ::', () => {
+        describe('testId :: ', () => {
+            describe('when `attribues` has name value', () => {
+                it('should return testIds for form field', () => {
+                    // Arrange
+                    propsData.attributes = {
+                        name: 'formfield-4'
+                    };
+
+                    // Act
+                    const wrapper = shallowMount(FormFieldAfix, { propsData });
+
+                    // Assert
+                    expect(wrapper.vm.testId).toMatchSnapshot();
+                });
+            });
+
+            describe('when `attribues` does not have name value', () => {
+                it('should return default testIds', () => {
+                    // Arrange
+                    propsData.attributes = {
+                        name: null
+                    };
+
+                    // Act
+                    const wrapper = shallowMount(FormFieldAfix, { propsData });
+
+                    // Assert
+                    expect(wrapper.vm.testId).toMatchSnapshot();
+                });
+            });
+        });
+    });
+
+    describe('methods ::', () => {
+        describe('updateInput ::', () => {
+            it('should emit `update` when an option is selected', async () => {
+                // Arrange
+                const wrapper = shallowMount(FormFieldAfix, { propsData });
+
+                const event = {
+                    target: {
+                        value: 'test option'
+                    }
+                };
+
+                // Act
+                await wrapper.vm.updateInput(event);
+
+                // Assert
+                expect(wrapper.emitted('update').length).toBe(1);
+            });
+        });
+    });
+});
