28560a338ff3b6e929c6364f1ff8b0b83f8f1d2b
billy.oliver <billy.oliver@just-eat.com>
2021-07-30 15:24:23 +0100

    f-analytics - Move the preparation of the GTM tags into a Nuxt plugin. Expose a global object (placeholder for now).

---
+++ packages/services/f-analytics/src/plugins/defaults.js
@@ -0,0 +1,10 @@
+const defaults = {
+    namespace: 'f-analytics',
+    globalVarName: 'gtm',
+    id: 'GTM-UNSET',
+    auth: null,
+    preview: null,
+    cookiesWin: null
+};
+
+module.exports = defaults;
