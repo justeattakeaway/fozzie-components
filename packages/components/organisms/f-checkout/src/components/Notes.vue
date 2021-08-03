7a4a3cfc6b1f7ebbac1e733ae22798e4ddc5ad31
Charles Waite <charles.waite@justeattakeaway.com>
2021-08-03 12:17:31 +0100

    Use correct variable for second notes input name

--- packages/components/organisms/f-checkout/src/components/Notes.vue
+++ packages/components/organisms/f-checkout/src/components/Notes.vue
@@ -10,7 +10,7 @@
                 cols="30"
                 rows="7"
                 maxlength="200"
-                :name="`${noteTypeDeliveryOrRestaurant}-note`"
+                :name="`${noteTypeCourierOrOrder}-note`"
                 has-input-description
                 @input="updateUserNote({ note: $event, type: noteTypeCourierOrOrder })">
                 {{ $t(`userNote.${noteTypeCourierOrOrder}.${serviceType}.text`) }}
