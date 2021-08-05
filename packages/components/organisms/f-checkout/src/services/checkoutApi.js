4b427731451ce392c568ed57a919c90d6ae27f5d
Charles Waite <charles.waite@justeattakeaway.com>
2021-08-05 13:54:02 +0100

    Update headers for v2 checkout API

--- packages/components/organisms/f-checkout/src/services/checkoutApi.js
+++ packages/components/organisms/f-checkout/src/services/checkoutApi.js
@@ -7,6 +7,7 @@ export default {
         const config = {
             headers: {
                 'Content-Type': 'application/json',
+                Accept: 'application/json;v=2',
                 ...(state.authToken && {
                     Authorization: authHeader
                 })
@@ -23,7 +24,7 @@ export default {
         const experimentationHeaders = request.rootGetters[`${VUEX_CHECKOUT_EXPERIMENTATION_MODULE}/getExperimentsHeaders`];
         const config = {
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json-patch+json;v=2',
                 ...(request.state.authToken && {
                     Authorization: authHeader
                 }),
--- packages/components/organisms/f-checkout/src/tenants/en-GB.js
+++ packages/components/organisms/f-checkout/src/tenants/en-GB.js
@@ -163,9 +163,9 @@ const messages = {
     userNote: {
         courier: {
             delivery: {
-                title: 'Leave a note',
-                text: 'Leave a note for the restaurant with anything they need to know (e.g. the doorbell doesn\'t work). Do not include details about any allergies here.',
-                placeholder: 'e.g. the doorbell doesn’t work. Do not include details about any allergies here. \n\nWe\'re working with restaurants to cut waste. Please don\'t ask for plastic cutlery.'
+                title: 'Leave a note for the driver',
+                text: 'Leave a note for the driver with anything they need to know (e.g. the doorbell doesn\'t work).',
+                placeholder: 'e.g. the doorbell doesn’t work.'
             },
             collection: {
                 title: 'Leave a note',
@@ -187,7 +187,7 @@ const messages = {
             collection: {
                 title: 'Leave a note',
                 text: 'Leave a note for the restaurant with anything they need to know. Do not include details about any allergies here.',
-                placeholder: 'Do not include details about any allergies here. \n\nWe\'re working with restaurants to cut waste. Please don\'t ask for plastic cutlery.'
+                placeholder: 'Do not include details about any allergies here.'
             },
             dinein: {
                 title: 'Leave a note',
@@ -197,6 +197,16 @@ const messages = {
         },
         kitchen: {
             delivery: {
+                title: 'Leave a note for the kitchen',
+                text: 'Leave a note for the restaurant with anything they need to know.',
+                placeholder: 'We\'re working with restaurants to cut waste. Please don\'t ask for plastic cutlery.'
+            },
+            dinein: {
+                title: 'Leave a note for the kitchen',
+                text: 'Leave a note for the restaurant with anything they need to know.',
+                placeholder: 'We\'re working with restaurants to cut waste. Please don\'t ask for plastic cutlery.'
+            },
+            collection: {
                 title: 'Leave a note for the kitchen',
                 text: 'Leave a note for the restaurant with anything they need to know.',
                 placeholder: 'We\'re working with restaurants to cut waste. Please don\'t ask for plastic cutlery.'
