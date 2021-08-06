53b389344554bb6495f64e1a5f08f8f8c8b63d8d
David Banks <david.banks@just-eat.com>
2021-08-06 12:53:13 +0100

    Use object instead of literals for HTTP status codes.

--- packages/components/organisms/f-checkout/src/demo/checkoutMock.js
+++ packages/components/organisms/f-checkout/src/demo/checkoutMock.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+
 import getCheckoutDelivery from './checkout-delivery.json';
 import getCheckoutDeliveryAsapUrl from './checkout-delivery-user-selected-asap.json';
 import getCheckoutDeliveryLaterUrl from './checkout-delivery-user-selected-later.json';
@@ -34,57 +35,65 @@ const mock = new MockAdapter(axios);
 
 export default {
     setupCheckoutMethod (path) {
+        const httpStatusCodes = {
+            noResponse: 0,
+            ok: 200,
+            badRequest: 400,
+            forbidden: 403,
+            internalServerError: 500
+        };
+
         switch (path) {
             case '/checkout-delivery.json':
-                mock.onGet(path).reply(200, getCheckoutDelivery);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutDelivery);
                 break;
             case '/checkout-delivery-user-selected-asap.json':
-                mock.onGet(path).reply(200, getCheckoutDeliveryAsapUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutDeliveryAsapUrl);
                 break;
             case '/checkout-delivery-user-selected-later.json':
-                mock.onGet(path).reply(200, getCheckoutDeliveryLaterUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutDeliveryLaterUrl);
                 break;
             case '/checkout-delivery-user-selected-unavailable-time.json':
-                mock.onGet(path).reply(200, getCheckoutDeliveryUnavailableUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutDeliveryUnavailableUrl);
                 break;
             case '/checkout-collection.json':
-                mock.onGet(path).reply(200, getCheckoutCollection);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutCollection);
                 break;
             case '/checkout-collection-user-selected-asap.json':
-                mock.onGet(path).reply(200, getCheckoutCollectionAsapUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutCollectionAsapUrl);
                 break;
             case '/checkout-collection-user-selected-later.json':
-                mock.onGet(path).reply(200, getCheckoutCollectionLaterUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutCollectionLaterUrl);
                 break;
             case '/checkout-collection-user-selected-unavailable-time.json':
-                mock.onGet(path).reply(200, getCheckoutCollectionUnavailableUrl);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutCollectionUnavailableUrl);
                 break;
             case '/checkout-dinein.json':
-                mock.onGet(path).reply(200, getCheckoutDineIn);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCheckoutDineIn);
                 break;
             case '/checkout-timeout-get-error.json':
                 mock.onGet(path).timeout();
                 break;
             case '/checkout-available-fulfilment.json':
-                mock.onGet(path).reply(200, checkoutAvailableFulfilment);
+                mock.onGet(path).reply(httpStatusCodes.ok, checkoutAvailableFulfilment);
                 break;
             case '/checkout-available-fulfilment-no-time-available.json':
-                mock.onGet(path).reply(200, checkoutAvailableFulfilmentNoTimeAvailable);
+                mock.onGet(path).reply(httpStatusCodes.ok, checkoutAvailableFulfilmentNoTimeAvailable);
                 break;
             case '/checkout-available-fulfilment-preorder.json':
-                mock.onGet(path).reply(200, checkoutAvailableFulfilmentPreorder);
+                mock.onGet(path).reply(httpStatusCodes.ok, checkoutAvailableFulfilmentPreorder);
                 break;
             case '/create-guest.json':
-                mock.onPost(path).reply(200, createGuest);
+                mock.onPost(path).reply(httpStatusCodes.ok, createGuest);
                 break;
             case '/get-basket-delivery.json':
-                mock.onGet(path).reply(200, getBasketDelivery);
+                mock.onGet(path).reply(httpStatusCodes.ok, getBasketDelivery);
                 break;
             case '/get-basket-collection.json':
-                mock.onGet(path).reply(200, getBasketCollection);
+                mock.onGet(path).reply(httpStatusCodes.ok, getBasketCollection);
                 break;
             case '/get-basket-dinein.json':
-                mock.onGet(path).reply(200, getBasketDineIn);
+                mock.onGet(path).reply(httpStatusCodes.ok, getBasketDineIn);
                 break;
             case '/get-basket-delivery-age-restriction.json':
                 mock.onGet(path).reply(200, getBasketDeliveryAgeRestricted);
@@ -93,46 +102,46 @@ export default {
                 mock.onGet(path).timeout();
                 break;
             case '/update-checkout.json':
-                mock.onPatch(path).reply(200, updateCheckout);
+                mock.onPatch(path).reply(httpStatusCodes.ok, updateCheckout);
                 break;
             case '/update-checkout-restaurant-not-taking-orders.json':
-                mock.onPatch(path).reply(200, updateCheckoutRestaurantNotTakingOrders);
+                mock.onPatch(path).reply(httpStatusCodes.ok, updateCheckoutRestaurantNotTakingOrders);
                 break;
             case '/update-checkout-additional-items-required.json':
-                mock.onPatch(path).reply(200, updateCheckoutAdditionalItemsRequired);
+                mock.onPatch(path).reply(httpStatusCodes.ok, updateCheckoutAdditionalItemsRequired);
                 break;
             case '/update-checkout-403.json':
-                mock.onPatch(path).reply(403, updateCheckoutAccessForbidden);
+                mock.onPatch(path).reply(httpStatusCodes.forbidden, updateCheckoutAccessForbidden);
                 break;
             case '/update-checkout-time-unavailable.json':
-                mock.onPatch(path).reply(200, updateCheckoutUnavailableTimeUrl);
+                mock.onPatch(path).reply(httpStatusCodes.ok, updateCheckoutUnavailableTimeUrl);
                 break;
             case '/update-checkout-timeout.json':
                 mock.onPatch(path).timeout();
                 break;
             case '/get-address.json':
-                mock.onGet(path).reply(200, getAddress);
+                mock.onGet(path).reply(httpStatusCodes.ok, getAddress);
                 break;
             case '/place-order.json':
-                mock.onPost(path).reply(200, placeOrder);
+                mock.onPost(path).reply(httpStatusCodes.ok, placeOrder);
                 break;
             case '/place-order-duplicate.json':
-                mock.onPost(path).reply(400, placeOrderDuplicate);
+                mock.onPost(path).reply(httpStatusCodes.badRequest, placeOrderDuplicate);
                 break;
             case '/place-order-timeout.json':
                 mock.onPost(path).timeout();
                 break;
             case '/checkout-403-get-error.json':
-                mock.onGet(path).reply(403, accessForbiddenError);
+                mock.onGet(path).reply(httpStatusCodes.forbidden, accessForbiddenError);
                 break;
             case '/checkout-500-get-error.json':
-                mock.onGet(path).reply(500, getCheckoutError);
+                mock.onGet(path).reply(httpStatusCodes.internalServerError, getCheckoutError);
                 break;
             case '/get-geo-location.json':
-                mock.onPost(path).reply(200, getGeoLocation);
+                mock.onPost(path).reply(httpStatusCodes.ok, getGeoLocation);
                 break;
             case '/get-customer.json':
-                mock.onGet(path).reply(200, getCustomer);
+                mock.onGet(path).reply(httpStatusCodes.ok, getCustomer);
                 break;
             default:
                 throw new Error(`${path} is not valid`);
