aaff9ed7b0d88d261e95968248c67fe7daac8dd4
Matthew Hardern <matthewhardern@gmail.com>
2021-08-06 09:31:16 +0100

    minor change to hexcode casing

--- packages/storybook/config/storybook/jetPieThemeDocs.js
+++ packages/storybook/config/storybook/jetPieThemeDocs.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     colorPrimary: '#f36d00',
-    colorSecondary: '#4996FD',
+    colorSecondary: '#4996fd',
     fontBase: '"JustEatBasis", Arial, sans-serif'
 });
