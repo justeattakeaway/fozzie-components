13a3fbb0183b2a2b77a199f030e15d98f6922fb3
Ben Siggery <bensiggery28@gmail.com>
2021-08-09 15:12:11 +0100

    Fix f-checkout selectors

--- packages/components/organisms/f-checkout/CHANGELOG.md
+++ packages/components/organisms/f-checkout/CHANGELOG.md
@@ -3,6 +3,14 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+v0.168.1
+------------------------------
+*August 9, 2021*
+
+### Fixed
+- Exports in test selector file.
+
+
 
 v0.168.0
 ------------------------------
--- packages/components/organisms/f-checkout/test-utils/component-objects/f-checkout-selectors.js
+++ packages/components/organisms/f-checkout/test-utils/component-objects/f-checkout-selectors.js
@@ -1,24 +1,24 @@
-export const CHECKOUT_COMPONENT = '[data-test-id="checkout-component"]';
-export const ORDER_TIME_DROPDOWN = '[data-test-id="formfield-order-time-dropdown-select"]';
-export const ORDER_TIME_DROPDOWN_OPTIONS = '[data-test-id="formfield-order-time-dropdown-select"] option';
-export const USER_NOTE_INPUT = '[data-test-id="formfield-textarea"]';
-export const GO_TO_PAYMENT_BUTTON = '[data-test-id="confirm-payment-submit-button"]';
-export const KNOB_BUTTON = '[id$="tabbutton-knobs"]';
-export const KNOB_CHECKOUT_DROPDOWN = '[name="Checkout Url"]';
-export const SWITCH_USER_LINK = '[data-test-id="switch-user-link"]';
-export const GUEST_CHECKOUT_LOGIN_BUTTON = '[data-test-id="guest-login-button"]';
-export const GUEST_CHECKOUT_HEADER = '[data-test-id="guest-checkoutHeader"]';
-export const PRE_ORDER_WARNING = '[data-test-id="warning-pre-order"]';
-export const CHECKOUT_ERROR_MESSAGE = '[data-test-id="checkout-issue-modal"] div';
-export const RETRY_BUTTON = '[data-test-id="redirect-to-menu-button"]';
-export const CLOSE_MODAL = '[data-test-id="close-modal"]';
-export const DUP_ORDER_GO_TO_HISTORY_BUTTON = '[data-test-id="redirect-to-orderhistory-button"]';
-export const ERROR_PAGE_COMPONENT = '[data-test-id="checkout-error-page-component"]';
-export const ERROR_PAGE_HEADING = '[data-test-id="checkout-error-page-heading"]';
-export const ERROR_PAGE_DESCRIPTION = '[data-test-id="checkout-error-page-description"]';
-export const ERROR_PAGE_IMAGE = '[data-test-id="checkout-error-page-image"]';
+exports.CHECKOUT_COMPONENT = '[data-test-id="checkout-component"]';
+exports.ORDER_TIME_DROPDOWN = '[data-test-id="formfield-order-time-dropdown-select"]';
+exports.ORDER_TIME_DROPDOWN_OPTIONS = '[data-test-id="formfield-order-time-dropdown-select"] option';
+exports.USER_NOTE_INPUT = '[data-test-id="formfield-textarea"]';
+exports.GO_TO_PAYMENT_BUTTON = '[data-test-id="confirm-payment-submit-button"]';
+exports.KNOB_BUTTON = '[id$="tabbutton-knobs"]';
+exports.KNOB_CHECKOUT_DROPDOWN = '[name="Checkout Url"]';
+exports.SWITCH_USER_LINK = '[data-test-id="switch-user-link"]';
+exports.GUEST_CHECKOUT_LOGIN_BUTTON = '[data-test-id="guest-login-button"]';
+exports.GUEST_CHECKOUT_HEADER = '[data-test-id="guest-checkoutHeader"]';
+exports.PRE_ORDER_WARNING = '[data-test-id="warning-pre-order"]';
+exports.CHECKOUT_ERROR_MESSAGE = '[data-test-id="checkout-issue-modal"] div';
+exports.RETRY_BUTTON = '[data-test-id="redirect-to-menu-button"]';
+exports.CLOSE_MODAL = '[data-test-id="close-modal"]';
+exports.DUP_ORDER_GO_TO_HISTORY_BUTTON = '[data-test-id="redirect-to-orderhistory-button"]';
+exports.ERROR_PAGE_COMPONENT = '[data-test-id="checkout-error-page-component"]';
+exports.ERROR_PAGE_HEADING = '[data-test-id="checkout-error-page-heading"]';
+exports.ERROR_PAGE_DESCRIPTION = '[data-test-id="checkout-error-page-description"]';
+exports.ERROR_PAGE_IMAGE = '[data-test-id="checkout-error-page-image"]';
 
-export const FIELDS = {
+exports.FIELDS = {
     firstName: {
         input: '[data-test-id="formfield-guest-first-name-input"]',
         error: '[data-test-id="error-first-name-empty"]'
