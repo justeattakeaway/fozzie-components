04f64e8bd2e6b02265c561a4962c3d0ac50f0a3c
Billy Oliver <billy.oliver@gmail.com>
2021-08-16 14:01:07 +0100

    f-analytics@0.8.0 - Added global method to Push GTM ad-hoc events (#1178)
    * f-analytics - Added PushEvent() to service, moved prepare server side analytics to plugin & refactored store to perform push.
    
    * f-analytics - Updated README.md, CHANGELOG.md, package.json, added complex test & removed redundant code.
    
    * f-analytics - Updated CHANGELOG.md
    
    * f-analytics - Updated README.md
    
    * f-analytics - Increased bundle size
    
    * f-analytics - Updated default
    
    * Squashed commit of the following:
    
    commit a24b07a2bc9de9dfbc1b260c90f99199c43dc88a
    Author: Mariya Radchuk <radchuk.mary@gmail.com>
    Date:   Mon Aug 9 22:05:37 2021 +0100
    
        f-analytics@0.6.0 - userData (#1160)
    
        * f-analytics@0.6.0 - prepareUserData method
    
        * f-analytics@0.6.0 - increase bundle max size
    
        * f-analytics@0.6.0 - add test case for a guest user
    
    # Conflicts:
    #	packages/services/f-analytics/CHANGELOG.md
    #	packages/services/f-analytics/README.md
    #	packages/services/f-analytics/package.json
    #	packages/services/f-analytics/src/mixins/_tests/analytics.methods.test.js
    #	packages/services/f-analytics/src/mixins/_tests/analytics.mounted.test.js
    #	packages/services/f-analytics/src/mixins/_tests/analytics.watch.test.js
    #	packages/services/f-analytics/src/mixins/analytics.mixin.vue
    #	packages/services/f-analytics/src/store/_tests/__snapshots__/analytics.module.test.js.snap
    #	packages/services/f-analytics/src/store/_tests/analytics.module.test.js
    #	packages/services/f-analytics/src/store/analytics.module.js
    #	packages/services/f-analytics/src/tests/helpers/setup.js
    
    * f-analytics - Code review changes
    
    * f-analytics - Review changes
    
    * Squashed commit of the following:
    
    commit 326fc425bfec34adfe93fe8db50042a9c52e611a
    Author: AndyMayJE <andy.may@just-eat.com>
    Date:   Thu Aug 12 11:15:19 2021 +0100
    
        f-analytics@0.7.0 - pageData (#1177)
    
        * Merge upstream changes. Split group and name functions
    
        * Support empty pageData params and update tests inline with 0.6.0
    
        * Add tests for updatePageData and pushPageData to analytics.module.test.js
    
        * Update snapshots
    
        * Fix changelog after merge
    
        * Update tests
    
        * Update packages/services/f-analytics/src/utils/helpers.js
    
        Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
        * Update packages/services/f-analytics/src/mixins/_tests/analytics.methods.test.js
    
        Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
        * Update packages/services/f-analytics/README.md
    
        Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
        Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    # Conflicts:
    #	packages/services/f-analytics/README.md
    #	packages/services/f-analytics/package.json
    #	packages/services/f-analytics/src/mixins/_tests/analytics.created.test.js
    #	packages/services/f-analytics/src/mixins/analytics.mixin.vue
    #	packages/services/f-analytics/src/store/_tests/__snapshots__/analytics.module.test.js.snap
    #	packages/services/f-analytics/src/store/_tests/analytics.module.test.js
    #	packages/services/f-analytics/src/store/analytics.module.js
    #	packages/services/f-analytics/src/tests/helpers/setup.js
    
    * f-analytics - Reverted snapshot changes
    
    * f-analytics - Removed the prepare platform data from the mixin as it is now in the service.
    
    * f-analytics - Re-added f-services back as a dependancy
    
    * f-analytics - Moved logic out of store + renamed store methods
    
    * f-analytics - Moved some files, renamed some actions & fixed up unit tests
    
    * f-analytics - Review changes
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Thomas William McClean <spam@tommcclean.me>
    Co-authored-by: Kevin Rodrigues <kevrodrigues116@gmail.com>

---
+++ packages/services/f-analytics/src/services/_tests/analytics.service.test.js
@@ -0,0 +1,118 @@
+import AnalyticService from '@/services/analytics.service';
+import {
+    UPDATE_PLATFORM_DATA,
+    UPDATE_EVENTS,
+    CLEAR_EVENTS
+} from '@/store/mutation-types';
+import {
+    defaultState,
+    newEvent,
+    options
+} from '@/tests/helpers/setup';
+
+describe('Analytic Service ::', () => {
+    let store;
+    let storeDispatchSpy;
+    let service;
+    let req;
+    let windowsPushSpy;
+    let state;
+
+    beforeEach(() => {
+        // Arrange - store
+        storeDispatchSpy = jest.fn();
+        state = [`${options.namespace}`];
+        state[`${options.namespace}`] = { ...defaultState };
+        store = {
+            state,
+            dispatch: storeDispatchSpy
+        };
+        // Arrange - request
+        req = {
+            headers: jest.fn(() => ['user-agent'])
+        };
+        // Arrange - window state
+        jest.spyOn(window.navigator, 'userAgent', 'get').mockReturnValue('test-agent-string');
+        windowsPushSpy = jest.fn();
+        const originalWindow = { ...window };
+        jest.spyOn(global, 'window', 'get').mockImplementation(() => ({
+            ...originalWindow,
+            dataLayer: {
+                push: windowsPushSpy
+            }
+        }));
+        service = new AnalyticService(store, req, options);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should instantiate a new instance', () => {
+        // Act
+        const instance = () => new AnalyticService(store, req, options);
+
+        // Assert
+        expect(instance).not.toThrowError();
+        expect(instance).toBeDefined();
+    });
+
+    describe('When calling the pushEvent', () => {
+        it('should dispatch the `event` to the store', () => {
+            // Arrange
+            const expectedEvent = { ...newEvent };
+            store.state[`${options.namespace}`].events.push(expectedEvent);
+
+            // Act
+            service.pushEvent(newEvent);
+
+            // Assert
+            expect(windowsPushSpy).toHaveBeenCalledWith({ ...expectedEvent });
+            expect(storeDispatchSpy).toHaveBeenNthCalledWith(1, `${options.namespace}/${UPDATE_EVENTS}`, expectedEvent);
+            expect(storeDispatchSpy).toHaveBeenNthCalledWith(2, `${options.namespace}/${CLEAR_EVENTS}`);
+        });
+    });
+
+    describe('When calling the pushPlatformData', () => {
+        // 1 == locale, 2 == branding, 3 == country, 4 == currency, 5 == language
+        const cases = [
+            ['en-GB', 'justeat', 'uk', 'gbp', 'en'],
+            ['en-IE', 'justeat', 'ie', 'eur', 'en'],
+            ['it-IT', 'justeat', 'it', 'eur', 'it'],
+            ['es-ES', 'justeat', 'es', 'eur', 'es'],
+            ['en-AU', 'menulog', 'au', 'aud', 'en'],
+            ['en-NZ', 'menulog', 'nz', 'nzd', 'en']
+        ];
+
+        test.each(cases)(
+            'should dispatch the correct `plaformData` to the store given %p as the locale',
+            (localeArg, brandingExpected, countryExpected, currencyExpected, languageExpected) => {
+                // Arrange
+                const expectedPlatformData = {
+                    appType: 'web',
+                    applicationId: 7,
+                    branding: brandingExpected,
+                    country: countryExpected,
+                    currency: currencyExpected,
+                    environment: 'localhost',
+                    instancePosition: 'N/A',
+                    jeUserPercentage: undefined,
+                    language: languageExpected,
+                    name: options.featureName,
+                    userAgent: navigator.userAgent,
+                    version: '0.0.0.0'
+                };
+                store.state[`${options.namespace}`].platformData = expectedPlatformData;
+                options.locale = localeArg;
+                service = new AnalyticService(store, req, options);
+
+                // Act
+                service.pushPlatformData();
+
+                // Assert
+                expect(windowsPushSpy).toHaveBeenCalledWith({ platformData: { ...expectedPlatformData } });
+                expect(storeDispatchSpy).toHaveBeenLastCalledWith(`${options.namespace}/${UPDATE_PLATFORM_DATA}`, expectedPlatformData);
+            }
+        );
+    });
+});
