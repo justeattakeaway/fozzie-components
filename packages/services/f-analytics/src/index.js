d0785f01e463ecabebebdcfcee7dd3f362ec336e
Billy Oliver <billy.oliver@gmail.com>
2021-08-23 10:22:41 +0100

    f-analytics@0.10.0 - Moved push 'PageData' from mixin to service (#1206)
    * f-analytics - Migrated the preparePageData from the Mixin to the Service (pushPageData), removed the need to store Page Data, refactored the method signatures
    
    * Squashed commit of the following:
    
    commit 3774ddcf1112866ad8df1f9c036485b6cfb664ef
    Author: Sacha <sacha.zvetelman@just-eat.com>
    Date:   Thu Aug 19 14:38:07 2021 +0100
    
        f-cookie-banner@0.23.0 - Adding dk-DK to the list of locales in the banner's static version. (#1204)
    
        Co-authored-by: Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>
    
    commit d7fb0d0ea07d2f378fa30c14cab596ea50dd4052
    Author: Ben Siggery <14013357+siggerzz@users.noreply.github.com>
    Date:   Thu Aug 19 11:58:06 2021 +0100
    
        f-checkout@v0.173.1 - Fix f-checkout selector export (#1176)
    
        * Fix f-checkout selectors
    
        * Update f-wdio-utils
    
        * yarn bump
    
    * Update README.md
    
    * Update README.md
    
    * Update analytics.service.test.js
    
    * Squashed commit of the following:
    
    commit 6a4f3ed8aae41a5b5b27a73b9a0a8b52cb549fd5
    Author: Billy Oliver <billy.oliver@gmail.com>
    Date:   Fri Aug 20 10:04:06 2021 +0100
    
        Update analytics.service.test.js
    
    commit 47a0799c918df1ed4dd5db318ffbe50125aa1789
    Author: Billy Oliver <billy.oliver@gmail.com>
    Date:   Fri Aug 20 09:58:54 2021 +0100
    
        Update README.md
    
    commit 18ac2c2c47bd6f1ed1e6d7ba76a5552d6bae64c2
    Author: Billy Oliver <billy.oliver@gmail.com>
    Date:   Fri Aug 20 09:49:22 2021 +0100
    
        Update README.md
    
    commit 73518c26bda88527e41763c75cd840cbc9b5eff9
    Merge: a2bfeafe 3774ddcf
    Author: Billy Oliver <billy.oliver@gmail.com>
    Date:   Fri Aug 20 09:47:05 2021 +0100
    
        Merge branch 'master' into f-analytics/migrate-pagedata-service2
    
    commit 3774ddcf1112866ad8df1f9c036485b6cfb664ef
    Author: Sacha <sacha.zvetelman@just-eat.com>
    Date:   Thu Aug 19 14:38:07 2021 +0100
    
        f-cookie-banner@0.23.0 - Adding dk-DK to the list of locales in the banner's static version. (#1204)
    
        Co-authored-by: Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>
    
    commit d7fb0d0ea07d2f378fa30c14cab596ea50dd4052
    Author: Ben Siggery <14013357+siggerzz@users.noreply.github.com>
    Date:   Thu Aug 19 11:58:06 2021 +0100
    
        f-checkout@v0.173.1 - Fix f-checkout selector export (#1176)
    
        * Fix f-checkout selectors
    
        * Update f-wdio-utils
    
        * yarn bump
    
    * Update packages/services/f-analytics/README.md
    
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    * Update packages/services/f-analytics/README.md
    
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    * Update packages/services/f-analytics/README.md
    
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    * Update packages/services/f-analytics/src/services/analytics.service.js
    
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    * Update packages/services/f-analytics/src/services/analytics.service.js
    
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>
    
    * f-analytics - Tweaked README.md
    
    * f-analytics - Migrated to use Vite over WebPack + fixed up
    
    * f-analytics - updated yarn.lock
    
    * f-analytics - Tweaked README.md
    
    * Update package.json
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Mariya Radchuk <radchuk.mary@gmail.com>

--- packages/services/f-analytics/src/index.js
+++ packages/services/f-analytics/src/index.js
@@ -4,29 +4,8 @@
  * @module f-analytics
  */
 
-import AnalyticsMixin from './mixins/analytics.mixin.vue';
 import AnalyticsPlugin from './plugins/analytics.plugin';
 
-export function install (Vue) {
-    if (install.installed) {
-        return;
-    }
-    install.installed = true;
-    Vue.component('Analytics', AnalyticsMixin);
-}
+const AnalyticsModule = { AnalyticsPlugin };
 
-const plugin = {
-    install
-};
-
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-    GlobalVue.use(plugin);
-}
-
-export { AnalyticsMixin, AnalyticsPlugin };
+export default AnalyticsModule;
