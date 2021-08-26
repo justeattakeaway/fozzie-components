7b8be6a5c52cc284205a6393294ffe5b8301d446
Charles Waite <charles.waite@justeattakeaway.com>
2021-08-26 14:31:37 +0100

    Fix broken test (thanks Josh!)

--- packages/components/organisms/f-checkout/test/component/f-checkout-delivery.component.desktop.spec.js
+++ packages/components/organisms/f-checkout/test/component/f-checkout-delivery.component.desktop.spec.js
@@ -20,10 +20,12 @@ describe('f-checkout "delivery" component tests', () => {
     it('should enable a user to submit a postcode with correct characters', () => {
         // Arrange
         const addressInfo = {
+            line1: 'line 1 of address',
             postcode: 'AR51 1AA'
         };
 
         // Act
+        checkout.clearBlurField('addressPostcode');
         checkout.populateCheckoutForm(addressInfo);
         checkout.goToPayment();
 
