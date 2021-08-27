e6a476d6e12514ced7e0ee80be911f5a11536a25
Dan Bernardic <dan.bernardic@justeattakeaway.com>
2021-08-27 15:39:03 +0100

    Moved f-form-field component type and name into page.object

--- packages/components/atoms/f-form-field/test-utils/component-objects/f-form-field.component.js
+++ packages/components/atoms/f-form-field/test-utils/component-objects/f-form-field.component.js
@@ -1,6 +1,10 @@
 const Page = require('@justeat/f-wdio-utils/src/page.object');
 
 module.exports = class FormField extends Page {
+    constructor() {
+        super('atom-folder', 'f-form-field--form-field-component');
+    }
+
     get component () { return $('[data-test-id="formfield-container"]'); }
 
     get label () { return $('[data-test-id="formfield-label"]'); }
