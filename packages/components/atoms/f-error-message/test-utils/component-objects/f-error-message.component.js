d43fbbbdcecef9485a657d275695e8ad40104996
Dan Bernardic <dan.bernardic@justeattakeaway.com>
2021-08-27 15:37:08 +0100

    Moved f-error-message component type and name into page.object

--- packages/components/atoms/f-error-message/test-utils/component-objects/f-error-message.component.js
+++ packages/components/atoms/f-error-message/test-utils/component-objects/f-error-message.component.js
@@ -1,6 +1,10 @@
 const Page = require('@justeat/f-wdio-utils/src/page.object');
 
 module.exports = class ErrorMessage extends Page {
+    constructor() {
+        super('atom', 'error-message-component');
+    }
+
     get component () { return $('[data-test-id="error-message-component"]'); }
 
     open (url) {
