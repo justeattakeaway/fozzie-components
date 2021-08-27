078624ad5a91626f7eb223e5cf2f26fc6b3fcf49
Dan Bernardic <dan.bernardic@justeattakeaway.com>
2021-08-27 15:41:51 +0100

    Moved f-popover component type and name into page.object

--- packages/components/atoms/f-popover/test-utils/component-objects/f-popover.component.js
+++ packages/components/atoms/f-popover/test-utils/component-objects/f-popover.component.js
@@ -2,6 +2,9 @@ const Page = require('@justeat/f-wdio-utils/src/page.object');
 const { COMPONENT } = require('./f-popover-selectors');
 
 module.exports = class Popover extends Page {
+    constructor() {
+        super('atom', 'popover-component');
+    }
 
     get component () { return $(COMPONENT); }
 
