b6f79c06b67c366a32e0ca719a254239c84a148c
Sacha <sacha.zvetelman@just-eat.com>
2021-09-02 15:47:52 +0100

    f-form-field@2.2.0 - Restructuring our stories to match the designs. (#1235)
    * f-form-field - Text input and dropdown.
    
    * f-form-field - Fixed broken test.
    
    * f-form-field - Remaining stories.
    
    * f-form-field - CHANGELOG and version.
    
    * f-form-field - Small refactorings.
    
    * f-form-field - Fixing tests.
    
    * f-form-field - Updated root package.json and CHANGELOG.
    
    * f-form-field - Fixed accessibility test.
    
    Co-authored-by: Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>

--- packages/components/atoms/f-form-field/stories/Radio.stories.js
+++ packages/components/atoms/f-form-field/stories/Radio.stories.js
@@ -1,12 +1,10 @@
 import {
-    withKnobs, select, text, boolean, object
+    withKnobs, select, text, boolean
 } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
 import FormField from '../src/components/FormField.vue';
 import {
-    CUSTOM_INPUT_TYPES,
     DEFAULT_FIELD_SIZE,
-    VALID_INPUT_TYPES,
     VALID_FIELD_SIZES
 } from '../src/constants';
 
@@ -15,14 +13,14 @@ export default {
     decorators: [withKnobs, withA11y]
 };
 
-export const FormFieldComponent = () => ({
+export const RadioComponent = () => ({
     components: { FormField },
     props: {
         locale: {
             default: select('Locale', ['en-GB', 'en-AU'])
         },
         labelText: {
-            default: text('Label Text', 'First name')
+            default: text('Label Text', 'Radio Label')
         },
         fieldSize: {
             default: select('Field Size', VALID_FIELD_SIZES, DEFAULT_FIELD_SIZE)
@@ -30,32 +28,11 @@ export const FormFieldComponent = () => ({
         isDisabled: {
             default: select('isDisabled', [null, 'disabled'])
         },
-        inputType: {
-            default: select('Input Type', VALID_INPUT_TYPES.concat(CUSTOM_INPUT_TYPES))
-        },
         shouldShowLabelText: {
             default: boolean('shouldShowLabelText', true)
         },
         hasError: {
             default: boolean('hasError', false)
-        },
-        dropdownOptions: {
-            default: object('Dropdown Options', [
-                {
-                    disabled: true,
-                    selected: true,
-                    text: 'Disabled option',
-                    value: ''
-                },
-                {
-                    text: 'As soon as possible',
-                    value: '2021-01-01T01:00:00.000Z'
-                },
-                {
-                    text: 'Today in 5 minutes',
-                    value: '2021-01-01T01:05:00.000Z'
-                }
-            ])
         },
         isGrouped: {
             default: boolean('isGrouped', false)
@@ -68,9 +45,6 @@ export const FormFieldComponent = () => ({
         },
         assistiveText: {
             default: text('Assistive Text', '')
-        },
-        placeholder: {
-            default: text('Placeholder', '')
         }
     },
     parameters: {
@@ -80,20 +54,15 @@ export const FormFieldComponent = () => ({
         `<form-field
             :locale="locale"
             :label-text="labelText"
-            :input-type="inputType"
+            input-type="radio"
             :shouldShowLabelText="shouldShowLabelText"
             :field-size="fieldSize"
             :has-error="hasError"
-            :dropdown-options="dropdownOptions"
             :is-grouped="isGrouped"
             :disabled="isDisabled"
-            :cols="30"
-            :placeholder="placeholder"
-            :rows="7"
-            :maxlength="200"
             :labelDetails="labelDetails"
             :labelDescription="labelDescription"
             :assistiveText="assistiveText"/>`
 });
 
-FormFieldComponent.storyName = 'Form Field';
+RadioComponent.storyName = 'Radio';
