b767330ec68770ba554060908aaf30902dba4c0c
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2021-09-03 15:34:44 +0100

    f-consumer-oidc@1.2.0 - Add js-cookie to external output.

--- packages/services/f-consumer-oidc/vite.config.js
+++ packages/services/f-consumer-oidc/vite.config.js
@@ -7,6 +7,16 @@ export default {
             entry: path.resolve(__dirname, 'src/index.js'),
             name: 'f-consumer-oidc'
         },
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            external: ['js-cookie'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    'js-cookie': 'js-cookie'
+                }
+            }
+        }
     }
 };
