66475d774dce49f23e4a157010ac9a9ca2b11df9
Faye <faye.carter@justeattakeaway.com>
2021-09-03 15:13:45 +0100

    Add service files

---
+++ packages/components/organisms/f-checkout/src/services/tests/daysInMonth.test.js
@@ -0,0 +1,62 @@
+import getDaysInMonth from '../daysInMonth';
+
+describe('getDaysInMonth', () => {
+    it.each([
+        [31, 'January', 0],
+        [31, 'March', 2],
+        [30, 'April', 8],
+        [31, 'May', 4],
+        [30, 'June', 5],
+        [31, 'July', 6],
+        [31, 'August', 7],
+        [30, 'September', 8],
+        [31, 'October', 9],
+        [30, 'November', 10],
+        [31, 'December', 11]
+    ])('should return %s when month is %s', (expected, monthName, month) => {
+        // Arrange
+        const date = {
+            day: 1,
+            month,
+            year: 2020
+        };
+
+        // Act
+        const daysInMonth = getDaysInMonth(date);
+
+        // Assert
+        expect(daysInMonth).toEqual(expected);
+    });
+
+    describe('when month is February', () => {
+        it('should return 28', () => {
+            // Arrange
+            const date = {
+                day: 1,
+                month: 1,
+                year: 2010
+            };
+
+            // Act
+            const daysInMonth = getDaysInMonth(date);
+
+            // Assert
+            expect(daysInMonth).toEqual(28);
+        });
+
+        it('should return 29', () => {
+            // Arrange
+            const date = {
+                day: 1,
+                month: 1,
+                year: 2008
+            };
+
+            // Act
+            const daysInMonth = getDaysInMonth(date);
+
+            // Assert
+            expect(daysInMonth).toEqual(29);
+        });
+    });
+});
