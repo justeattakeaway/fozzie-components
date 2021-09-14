a9b98c99e3fea031eda949ad5a67d1fb1c2345e8
Matthew Hardern <matthewhardern@gmail.com>
2021-09-14 10:22:55 +0100

    f-braze-adapter@v4.0.0-beta.5 - adding in logging for better visibility (#1249)
    * adding in logging for better visibility
    
    * making some modifications to make sure logs can be sent even when during consumer being instantiated
    
    * description change
    
    * changes made to logging based on suggestions and best practices to get the best from the logged information
    
    * spelling correction
    
    Co-authored-by: Matthew Hardern <matthew.hardern@justeattakeaway.com>

--- packages/services/f-braze-adapter/src/BrazeAdapter.js
+++ packages/services/f-braze-adapter/src/BrazeAdapter.js
@@ -1,4 +1,9 @@
 import GetConsumerRegistry from './services/BrazeConsumerRegistry';
+import dispatcherEventStream from './services/DispatcherEventStream';
+import {
+    LOGGER
+} from './services/types/events';
+import { LOG_INFO } from './services/types/logger';
 
 export default class BrazeAdapter {
     constructor ({
@@ -12,8 +17,13 @@ export default class BrazeAdapter {
         interceptInAppMessages,
         interceptInAppMessageClickEvents,
         customFilters,
-        loggerCallbacks
+        logger,
+        tags = 'global'
     }) {
+        // create a key to identify the section from which the logs reference for later lookup if needed if no tags
+        // apply global keyword
+        const loggingAdapterKey = `${tags}--${userId}`;
+
         const consumerOptions = {
             enabledCardTypes,
             brands,
@@ -21,17 +31,33 @@ export default class BrazeAdapter {
             interceptInAppMessages,
             interceptInAppMessageClickEvents,
             customFilters,
-            loggerCallbacks
+            logger,
+            userId,
+            tags
         };
 
         this._consumerRegistry = GetConsumerRegistry({
             apiKey,
             userId,
             enableLogging,
-            sessionTimeout
+            sessionTimeout,
+            tags
         });
 
         this._consumer = this._consumerRegistry.register(consumerOptions);
+
+        dispatcherEventStream.publish(LOGGER, {
+            type: LOG_INFO,
+            message: `Braze Adapter Section: (Adapter) Key: (BrazeAdapter--adapter--${loggingAdapterKey}): Braze Adapter has been initialised by the consumer.`,
+            data: {
+                key: loggingAdapterKey,
+                enableLogging,
+                sessionTimeout,
+                enabledCardTypes,
+                brands,
+                customFilters
+            }
+        });
     }
 
     get dispatcher () {
