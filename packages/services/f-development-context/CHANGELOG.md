884a728806c7742551819e4769767587dca3c807
Billy Oliver <billy.oliver@gmail.com>
2021-09-14 12:34:26 +0100

    f-development-context@v1.1.0 - + Storybook@v0.43.0 - Add in mock for `$gtm` (#1266)
    * Storybook - Add in mock for $gtm
    f-development-context - Add in mock for $gtm
    
    * f-development-context - Updated version + README
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Kevin Rodrigues <kevrodrigues116@gmail.com>

--- packages/services/f-development-context/CHANGELOG.md
+++ packages/services/f-development-context/CHANGELOG.md
@@ -3,6 +3,15 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+
+v1.1.0
+------------------------------
+*September 13, 2021*
+
+### Added
+- Extended f-development-context Package to support $gtm (aka f-analytics)
+
+
 v1.1.0
 ------------------------------
 *February 1, 2020*
---
+++ packages/storybook/context/analytics.context.js
@@ -0,0 +1,80 @@
+import Vue from 'vue';
+
+// Note this was upto date at the time of publishing
+// but does not need to be aligned/maintained as only
+// a placeholder.
+const platformData = {
+    environment: 'storybook',
+    name: 'storybook',
+    appType: 'web',
+    applicationId: 7,
+    userAgent: undefined,
+    branding: undefined,
+    country: undefined,
+    language: undefined,
+    jeUserPercentage: undefined,
+    currency: undefined,
+    version: undefined,
+    instancePosition: undefined,
+    isPilot: undefined
+};
+
+const userData = {
+    'a-UserId': 'storybook',
+    authType: undefined,
+    email: undefined,
+    globalUserId: undefined,
+    signinType: undefined,
+    signupDate: undefined
+};
+
+const pageData = {
+    group: 'storybook',
+    name: undefined,
+    httpStatusCode: 200,
+    conversationId: undefined,
+    requestId: undefined,
+    orientation: undefined,
+    display: undefined
+};
+
+// Just placeholder for Storybook to use in place of the global `$gtm`
+// It merely console logs what it can under its limited capacity
+export default () => {
+    const gtm = {
+        pushPlatformData: ({
+            featureName, locale, customFields
+        } = {}) => {
+            let data = { ...platformData, name: featureName, language: locale };
+            if (customFields) {
+                data = { ...data, ...customFields };
+            }
+            console.log('pushPlatformData', { platformData: { ...data } });
+        },
+        pushUserData: ({
+            authToken, customFields
+        } = {}) => {
+            let data = { ...userData, authType: authToken };
+            if (customFields) {
+                data = { ...data, ...customFields };
+            }
+            console.log('pushUserData', { userData: { ...data } });
+        },
+        pushPageData: ({
+            pageName, requestId, httpStatusCode, customFields
+        } = {}) => {
+            let data = {
+                ...pageData, name: pageName, requestId, httpStatusCode
+            };
+            if (customFields) {
+                data = { ...data, ...customFields };
+            }
+            console.log('pushPageData', { pageData: { ...data } });
+        },
+        pushEvent: event => {
+            console.log('pushEvent', event);
+        }
+    };
+
+    Vue.prototype.$gtm = gtm;
+};
