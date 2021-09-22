b2d4c71d8abade44faef80e94f804ffc8ee37b79
billy.oliver <billy.oliver@just-eat.com>
2021-09-22 18:06:15 +0100

    f-analytics - Fixed minor linting issue in diff. component

--- packages/services/f-feature-management-vue/test/createFeatureManagementForVue.test.js
+++ packages/services/f-feature-management-vue/test/createFeatureManagementForVue.test.js
@@ -1,6 +1,6 @@
-import createFeatureManagementForVue from '../src/createFeatureManagementForVue';
 import createFeatureManagement from '@justeat/f-feature-management';
 import { reactive } from '@vue/reactivity';
+import createFeatureManagementForVue from '../src/createFeatureManagementForVue';
 
 jest.mock('@justeat/f-feature-management', () => {
     const fmMock = {
@@ -10,45 +10,40 @@ jest.mock('@justeat/f-feature-management', () => {
     return {
         __esModule: true,
         default: () => fmMock
-    }
+    };
 });
 
 describe('When we get a mock Feature Management object', () => {
-    //NOTE: this would fail if our devDependency was Vue2
+    // NOTE: this would fail if our devDependency was Vue2
     it('should be a reactive proxy', async () => {
-
-        //Act
+        // Act
         const fm = await createFeatureManagementForVue({});
 
-        //Assert
-        //Vue3 tracks reactive proxies, so calling reactive on the same object twice will yield the same return object
+        // Assert
+        // Vue3 tracks reactive proxies, so calling reactive on the same object twice will yield the same return object
         expect(fm).toBe(reactive(createFeatureManagement()));
-
     });
 
     it('should have a property called reactive with a getValue function on it', async () => {
-
-        //Act
+        // Act
         const fm = await createFeatureManagementForVue({});
 
-        //Assert
+        // Assert
         expect(fm.reactive).toBeInstanceOf(Object);
         expect(fm.reactive.getValue).toBeInstanceOf(Function);
-
     });
 
     it('calling reactive.getValue() should delegate to function on base', async () => {
-        //Arrange
+        // Arrange
         const mockFm = createFeatureManagement();
         mockFm.getValue.mockReturnValue('return value');
 
-        //Act
+        // Act
         const fm = await createFeatureManagementForVue({});
         const val = fm.reactive.getValue('parameter');
 
-        //Assert
+        // Assert
         expect(mockFm.getValue).toHaveBeenCalledWith('parameter');
-        expect(val).toBe('return value')
+        expect(val).toBe('return value');
     });
-
 });
