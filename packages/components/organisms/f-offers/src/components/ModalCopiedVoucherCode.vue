cacf359cff65f291e4dac47c2eacf98fae443a97
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-09-23 09:17:36 +0100

    minor change based on PR feedback

--- packages/components/organisms/f-offers/src/components/ModalCopiedVoucherCode.vue
+++ packages/components/organisms/f-offers/src/components/ModalCopiedVoucherCode.vue
@@ -11,7 +11,7 @@
             v-if="hasLink"
             :href="url"
             :data-test-id="testIds.confirm"
-            :is-full-width="true">
+            is-full-width>
             {{ $t('authenticated.voucherCodeModal.confirmButton') }}
         </f-button>
         <f-button
