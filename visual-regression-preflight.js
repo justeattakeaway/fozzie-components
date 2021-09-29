37d8e9977e760470e2a93e488a57b9d7e743fb5d
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2021-09-29 14:32:15 +0100

    Minor: Merge preflight- and visual regression steps

--- visual-regression-preflight.js
+++ visual-regression-preflight.js
@@ -2,20 +2,18 @@ const fetch = require('node-fetch');
 
 const PULL_REQUEST_ID = process.env.CIRCLE_PULL_REQUEST.split('/').pop();
 const LABELS_TO_STOP = ['wip', 'do not merge'];
-let details = {};
 
-fetch(`https://api.github.com/repos/justeat/fozzie-components/pulls/${PULL_REQUEST_ID}`)
-    .then(res => res.json())
-    .then(json => {
-        details = {
-            draft: json.draft,
-            labels: json.labels.map(l => l.name)
-        };
+(async () => {
+    const request = fetch(`https://api.github.com/repos/justeat/fozzie-components/pulls/${PULL_REQUEST_ID}`);
+    const response = await request;
+    const json = await response.json();
+    const details = {
+        draft: json.draft,
+        labels: json.labels.map(l => l.name)
+    };
 
-        console.log(details);
 
-        // If PR is draft or has given labels, skip visual regression tests to save credits
-        const shouldRunTests = !details.draft && !LABELS_TO_STOP.some(label => details.labels.includes(label));
-        process.env.RUN_VISUAL_REGRESSION_TESTS = shouldRunTests;
-        console.log('Should run visual regression tests?', process.env.RUN_VISUAL_REGRESSION_TESTS);
-    });
+    // If PR is draft or has given labels, skip visual regression tests to save credits
+    const shouldRunTests = !details.draft && !LABELS_TO_STOP.some(label => details.labels.includes(label));
+    console.log(shouldRunTests);
+})();
