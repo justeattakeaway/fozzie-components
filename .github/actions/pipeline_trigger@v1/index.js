a13a1d866a89787794f301d2e8e2173994edf63e
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2021-09-30 15:51:49 +0100

    Try using octokit client for Circle CI POST

--- .github/actions/pipeline_trigger@v1/index.js
+++ .github/actions/pipeline_trigger@v1/index.js
@@ -1,19 +1,17 @@
-const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require('@octokit/core');
 
 try {
-    const headers = {
-        authorization: core.getInput('CIRCLE_CI_TOKEN')
-    };
-
     const CIRCLECI_API_URL = 'https://circleci.com/api/v2/project/gh';
     const { owner, repo } = github.context.repo;
 
-    fetch(`${CIRCLECI_API_URL}/${owner}/${repo}/pipeline`, {
-        headers,
-        method: 'POST'
+    // Can't seem to use node-fetch so trying to force Github's own Octokit client to work for us...
+    const client = new Octokit({
+        auth: core.getInput('CIRCLE_CI_TOKEN'),
+        baseUrl: CIRCLECI_API_URL
     });
+    client.request('POST /{owner}/{repo}/pipeline', { owner, repo });
 } catch (error) {
   core.setFailed(error.message);
 }
