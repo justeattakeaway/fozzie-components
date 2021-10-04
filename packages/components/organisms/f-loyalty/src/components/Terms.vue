cf7fa5377b5cad3367a5997d1ab5656959767208
Matthew Hardern <matthew.hardern@justeattakeaway.com>
2021-10-04 11:45:37 +0100

    adding terms component into loyalty, plus included unit tests

---
+++ packages/components/organisms/f-loyalty/src/components/Terms.vue
@@ -0,0 +1,36 @@
+<template functional>
+    <div :class="$style['c-loyalty-terms']">
+        <a
+            :class="$style['c-loyalty-terms-link']"
+            data-test-id="terms-and-conditions"
+            :href="parent.$t('termsUrl')">
+            {{ parent.$t("termsText") }}
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StampCardsTerms'
+};
+</script>
+
+<style lang="scss" module>
+.c-loyalty-terms {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+.c-loyalty-terms-link {
+    margin: spacing(x3) 0;
+    text-align: center;
+    font-weight: $font-bodyStrong-s-weight;
+    text-decoration: none;
+
+    &:hover {
+         text-decoration: underline;
+    }
+}
+</style>
---
+++ packages/components/organisms/f-loyalty/src/components/_tests/Terms.test.js
@@ -0,0 +1,71 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import VueI18n from 'vue-i18n';
+import Terms from '../Terms.vue';
+import i18nMocker from './helper';
+import tenantConfigs from '../../tenants';
+
+const localVue = createLocalVue();
+localVue.use(VueI18n);
+
+let messages,
+    wrapper;
+
+beforeEach(() => {
+    jest.resetAllMocks();
+
+    messages = {
+        'en-GB': tenantConfigs['en-GB'].messages,
+        'en-AU': tenantConfigs['en-AU'].messages
+    };
+});
+
+describe('Terms.vue', () => {
+    // Arrange
+    const component = {
+        name: 'mockWrapper',
+        components: {
+            Terms
+        },
+        template: '<div><terms /></div>'
+    };
+
+    const i18n = {
+        locale: 'en-AU',
+        fallbackLocale: 'en-AU',
+        messages
+    };
+
+    beforeEach(() => {
+        // Arrange
+        wrapper = mount(component, {
+            localVue,
+            i18n,
+            mocks: {
+                $t: key => i18nMocker(key, i18n.locale)
+            }
+        });
+    });
+
+    it.each([
+        'en-GB',
+        'en-AU'
+    ])('should render the terms url in the href attribute', key => {
+        // Act
+        const link = wrapper.find('[data-test-id="terms-and-conditions"]');
+
+        // Assert
+        expect(link.text()).toEqual(messages[key].termsText);
+    });
+
+    it.each([
+        'en-GB',
+        'en-AU'
+    ])('should render the correct text for the link', key => {
+        // Act
+        const link = wrapper.find('[data-test-id="terms-and-conditions"]');
+        const href = link.attributes('href');
+
+        // Assert
+        expect(href).toEqual(messages[key].termsUrl);
+    });
+});
