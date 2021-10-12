88c0aa6d4d3cd0facb46947459633dc2b9ea683b
Joshua Ng <joshua.ng@just-eat.com>
2021-10-12 16:11:45 +0100

    Updated a11y tests to reflect new page.object changes.

--- packages/components/atoms/f-compatibility/test/accessibility/axe-accessibility.spec.js
+++ packages/components/atoms/f-compatibility/test/accessibility/axe-accessibility.spec.js
@@ -1,12 +1,12 @@
 import { getAccessibilityTestResults } from '../../../../../../test/utils/axe-helper';
 
 const Compatibility = require('../../test-utils/component-objects/f-compatibility.component');
+
 const compatibility = new Compatibility();
 
 describe('Accessibility tests', () => {
     beforeEach(() => {
-        compatibility.open();
-        compatibility.waitForComponent();
+        compatibility.load();
     });
     it('a11y - should test f-compatibility component WCAG compliance', () => {
         // Act
