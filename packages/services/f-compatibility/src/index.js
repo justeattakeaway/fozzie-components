d8e7c17ae01e5c51cf640fcbe94bdeb0fa230039
Simon Sanders <simon.sanders@just-eat.com>
2021-10-14 09:07:09 +0100

    f-compatibility@v0.1.0 - create service scaffold.

---
+++ packages/services/f-compatibility/src/index.js
@@ -0,0 +1,21 @@
+
+
+/**
+* @overview Add description of service here
+*
+* @module f-compatibility
+*/
+
+/**
+ *  @class compatibility
+ *  @type {Object}
+ */
+const compatibility = {
+};
+
+/**
+ * @returns {compatibility} Module for checking browser compatibility
+ */
+export default compatibility;
+
+
