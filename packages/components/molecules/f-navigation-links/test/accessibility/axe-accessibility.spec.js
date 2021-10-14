7c5c7c3d980a472f41cf7b91fd031fbd8caf9c4a
billy.oliver <billy.oliver@just-eat.com>
2021-10-14 09:43:01 +0100

    f-navigation-links - Fixed up test

--- packages/components/molecules/f-navigation-links/test/accessibility/axe-accessibility.spec.js
+++ packages/components/molecules/f-navigation-links/test/accessibility/axe-accessibility.spec.js
@@ -2,13 +2,15 @@ import { getAccessibilityTestResults } from '../../../../../../test/utils/axe-he
 
 const NavigationLinks = require('../../test-utils/component-objects/f-navigationLinks.component');
 
-const navigationLinks = new NavigationLinks();
+let navigationLinks;
 
 describe('Accessibility tests', () => {
     beforeEach(() => {
-        navigationLinks.open();
-        navigationLinks.waitForComponent();
+        navigationLinks = new NavigationLinks();
+
+        navigationLinks.load();
     });
+
     it('a11y - should test f-navigationLinks component WCAG compliance', () => {
         // Act
         const axeResults = getAccessibilityTestResults('f-navigationLinks');
