6d207a35f8a18168673025da9b201ec8ff08c323
Simon Sanders <simon.sanders@just-eat.com>
2021-10-20 08:30:04 +0100

    f-compatibility@v0.1.0 - add data and outline logic.

--- packages/services/f-compatibility/src/index.js
+++ packages/services/f-compatibility/src/index.js
@@ -1,21 +1,27 @@
-
-
 /**
 * @overview Service to check browser compatibilities and warn users with incompatible browsers
 *
 * @module f-compatibility
 */
 
+import Bowser from 'bowser';
+import { UNSUPPORTED_BROWSERS, BROWSER_DATA } from './constants';
+
 /**
  *  @class compatibility
  *  @type {Object}
  */
 const compatibility = {
+    method: () => {
+        const browser = Bowser.getParser(window.navigator.userAgent);
+        const isUnsupported = browser.satisfies(UNSUPPORTED_BROWSERS);
+        if (isUnsupported) {
+            console.log(BROWSER_DATA);
+        }
+    }
 };
 
 /**
  * @returns {compatibility} Module for checking browser compatibility
  */
 export default compatibility;
-
-
