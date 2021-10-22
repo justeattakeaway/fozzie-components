47c1c753090176ce9653adf33221a3a6ef7c41ba
jamieomaguire <jamieolivermaguire@googlemail.com>
2021-10-22 17:47:09 +0100

    add light prop validation for error boundary

--- packages/components/molecules/f-restaurant-card/src/assets/vue/mixins/errorBoundary.mixin.js
+++ packages/components/molecules/f-restaurant-card/src/assets/vue/mixins/errorBoundary.mixin.js
@@ -1,7 +1,12 @@
+const objectIsVueComponent = obj => 'render' in obj;
+
 export default {
     props: {
         errorBoundary: {
             type: [Object, String],
+            validator: value => (typeof value === 'object'
+                ? objectIsVueComponent(value)
+                : true),
             default: 'div'
         }
     }
