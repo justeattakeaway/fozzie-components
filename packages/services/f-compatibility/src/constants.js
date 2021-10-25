294ae152c915ea5d5c07b84387a94e50c3bea6b7
Simon Sanders <simon.sanders@just-eat.com>
2021-10-25 14:37:00 +0100

    f-compatibility@v0.1.0 - detect IE and create banner.

--- packages/services/f-compatibility/src/constants.js
+++ packages/services/f-compatibility/src/constants.js
@@ -14,27 +14,27 @@ export const BROWSER_DATA = {
         tenants: {
             'en-GB': {
                 title: 'Just Eat can no longer be used with Internet Explorer',
-                content: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
+                message: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
             },
             'en-IE': {
                 title: 'Just Eat can no longer be used with Internet Explorer',
-                content: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
+                message: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
             },
             'it-IT': {
                 title: 'Just Eat non può essere più utilizzato con Internet Explorer',
-                content: 'Al fine di garantire un\'esperienza sicura, Internet Explorer non è più supportato. Usa un altro browser, come Chrome, Firefox o Safari per i tuoi ordini su Just Eat'
+                message: 'Al fine di garantire un\'esperienza sicura, Internet Explorer non è più supportato. Usa un altro browser, come Chrome, Firefox o Safari per i tuoi ordini su Just Eat'
             },
             'es-ES': {
                 title: 'Just Eat ya no se puede utilizar con Internet Explorer',
-                content: 'Hemos terminado el soporte de Internet Explorer para que tengas una experiencia más segura. Para realizar tu orden, por favor utiliza otro navegador como Chrome, Firefox o Safari.'
+                message: 'Hemos terminado el soporte de Internet Explorer para que tengas una experiencia más segura. Para realizar tu orden, por favor utiliza otro navegador como Chrome, Firefox o Safari.'
             },
             'en-AU': {
                 title: 'Just Eat can no longer be used with Internet Explorer',
-                content: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
+                message: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
             },
             'en-NZ': {
                 title: 'Just Eat can no longer be used with Internet Explorer',
-                content: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
+                message: 'We\'ve stopped supporting Internet Explorer so we can give you the most secure experience. Get your order in on another browser, like Chrome, Firefox or Safari.'
             }
         }
     }
--- packages/services/f-compatibility/src/index.js
+++ packages/services/f-compatibility/src/index.js
@@ -4,24 +4,73 @@
 * @module f-compatibility
 */
 
-import Bowser from 'bowser';
 import { UNSUPPORTED_BROWSERS, BROWSER_DATA } from './constants';
 
+const Bowser = require('bowser/bundled');
+
+/* eslint-disable func-names */
+
 /**
- *  @class compatibility
- *  @type {Object}
+ *  @function attachEvents
  */
-const compatibility = {
-    method: () => {
-        const browser = Bowser.getParser(window.navigator.userAgent);
-        const isUnsupported = browser.satisfies(UNSUPPORTED_BROWSERS);
-        if (isUnsupported) {
-            console.log(BROWSER_DATA);
+const attachEvents = function () {
+    const closeButton = document.getElementById('closeButton');
+    closeButton.addEventListener('click', function () {
+        const parent = this.parentNode;
+        parent.innerHTML = '';
+        parent.removeNode();
+    });
+};
+
+/**
+ *  @function buildBanner
+ */
+const buildBanner = function (title, message) {
+    const heading = document.createElement('h2');
+    heading.innerText = title;
+    const content = document.createElement('p');
+    content.innerText = message;
+    const contentContainer = document.createElement('div');
+    contentContainer.setAttribute('style', 'font-family:JetSansDigital,Arial,sans-serif;left:35%;top:15%;width:30%;text-align:center;position:absolute;z-index:9000;background-color:white;border-radius:16px;box-shadow: 0px 3px 4px 0px rgba(0,0,0,0.12);padding:24px;');
+    contentContainer.appendChild(heading);
+    contentContainer.appendChild(content);
+    const overlay = document.createElement('div');
+    overlay.setAttribute('style', 'background-color:rgba(0,0,0,.5);bottom:0px;left:0px;position:fixed;right:0px;top:0px;z-index:9000;');
+    overlay.innerHTML = '<svg id="closeButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" style="position:absolute;right:16px;top:16px;z-index:9000;height:32px;width:32px;padding:0;border:1px solid transparent;border-radius:50%;background-color:#fff;cursor:pointer;"><path fill-rule="evenodd" d="M 13.3 0.7 a 1 1 0 0 0 -1.4 0 L 7 5.6 L 2.1 0.7 A 1 1 0 1 0 0.7 2.1 L 5.6 7 L 0.7 11.9 a 1 1 0 1 0 1.4 1.4 L 7 8.4 l 4.9 4.9 a 1 1 0 1 0 1.4 -1.4 L 8.4 7 l 4.9 -4.9 c 0.4 -0.4 0.4 -1 0 -1.4 Z" /></svg>';
+    overlay.appendChild(contentContainer);
+    const bodyTag = document.getElementsByTagName('body');
+    bodyTag[0].appendChild(overlay);
+    attachEvents();
+};
+
+/**
+ *  @function unsupportedBrowserAction
+ */
+const unsupportedBrowserAction = function (unsupportedBrowser) {
+    const browserData = BROWSER_DATA[unsupportedBrowser];
+    if (browserData.displayBanner) {
+        const tenant = document.getElementsByTagName('html')[0].lang;
+        buildBanner(browserData.tenants[tenant].title, browserData.tenants[tenant].message);
+    }
+};
+
+/**
+ *  @function compatibility
+ */
+const compatibility = function () {
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    const unsupportedBrowsers = Object.keys(UNSUPPORTED_BROWSERS);
+    for (let i = 0; i < unsupportedBrowsers.length; i++) {
+        const browserToTest = {};
+        browserToTest[unsupportedBrowsers[i]] = UNSUPPORTED_BROWSERS[unsupportedBrowsers[i]];
+        if (browser.satisfies(browserToTest)) {
+            unsupportedBrowserAction(unsupportedBrowsers[i]);
+            break;
         }
     }
 };
 
 /**
- * @returns {compatibility} Module for checking browser compatibility
+ * @returns {compatibility} function for checking browser compatibility
  */
 export default compatibility;
