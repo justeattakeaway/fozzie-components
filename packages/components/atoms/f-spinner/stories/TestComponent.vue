729a7d288d97c909dcdafd658f60a7868e6d615a
Faye <faye.carter@justeattakeaway.com>
2021-10-27 08:31:16 +0100

    Add test Component

---
+++ packages/components/atoms/f-spinner/stories/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>
+        Component has loaded
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+
+    mounted () {
+        setTimeout(() => {
+            this.$parent.$emit('stopSpinner');
+        }, 2000);
+    }
+};
+</script>
