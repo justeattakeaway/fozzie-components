0c03295747f6b4ddd71cfc2737effa3a570a0de4
Faye <faye.carter@justeattakeaway.com>
2021-10-27 09:20:15 +0100

    Add tests

--- packages/components/atoms/f-spinner/src/components/_tests/Spinner.test.js
+++ packages/components/atoms/f-spinner/src/components/_tests/Spinner.test.js
@@ -1,10 +1,41 @@
 import { shallowMount } from '@vue/test-utils';
 import VSpinner from '../Spinner.vue';
 
+const $on = jest.fn();
+
 describe('Spinner', () => {
+    const propsData = {};
+
     it('should be defined', () => {
-        const propsData = {};
+        // Arrange & Act
         const wrapper = shallowMount(VSpinner, { propsData });
+
+        // Assert
         expect(wrapper.exists()).toBe(true);
+    });
+
+    describe('mounted :: ', () => {
+        const mockFunction = expect.any(Function);
+        let wrapper;
+
+        beforeEach(() => {
+            // Arrange & Act
+            wrapper = shallowMount(VSpinner, {
+                propsData,
+                mocks: {
+                    $on
+                }
+            });
+        });
+
+        it('should create an event listener for `stopSpinner` events', () => {
+            // Assert
+            expect(wrapper.vm.$on).toHaveBeenCalledWith('stopSpinner', mockFunction);
+        });
+
+        it('should create an event listener for `startSpinner` events', () => {
+            // Assert
+            expect(wrapper.vm.$on).toHaveBeenCalledWith('startSpinner', mockFunction);
+        });
     });
 });
