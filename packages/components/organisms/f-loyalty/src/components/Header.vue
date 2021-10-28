948b96186f8e69f1e5db23ad90407ed0ad1b0856
matthew hardern <matthewhardern@gmail.com>
2021-10-28 16:02:40 +0100

    make home not router link

--- packages/components/organisms/f-loyalty/src/components/Header.vue
+++ packages/components/organisms/f-loyalty/src/components/Header.vue
@@ -42,7 +42,7 @@ export default {
             {
                 name: 'Home',
                 url: '/',
-                routerLink: true
+                routerLink: false
             },
             {
                 name: 'For You',
