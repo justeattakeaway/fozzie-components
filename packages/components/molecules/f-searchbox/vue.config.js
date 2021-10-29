7cc434b73104260694d464fb71327cd3e5f5ca36
matthew hardern <matthewhardern@gmail.com>
2021-10-29 10:11:46 +0100

    adding customized local indentname to solve our css bleed issues

--- packages/components/molecules/f-searchbox/vue.config.js
+++ packages/components/molecules/f-searchbox/vue.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const packageJson = require('./package.json');
 
 const rootDir = path.join(__dirname, '..', '..');
 const sassOptions = require('../../../../config/sassOptions')(rootDir);
 
 // vue.config.js
 module.exports = {
+    css: {
+        requireModuleExtension: false,
+        loaderOptions: {
+            css: {
+                modules: {
+                    localIdentName: `[name]__${packageJson.version}--[hash:base64:7]`
+                },
+                localsConvention: 'camelCaseOnly'
+            }
+        }
+    },
     chainWebpack: config => {
         config.module
             .rule('scss-importer')
--- packages/components/molecules/f-user-message/vue.config.js
+++ packages/components/molecules/f-user-message/vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const packageJson = require('./package.json');
 
 const rootDir = path.join(__dirname, '..', '..');
 const sassOptions = require('../../../../config/sassOptions')(rootDir);
@@ -6,6 +7,17 @@ const devServer = require('./vue.config.devServer');
 
 // vue.config.js
 module.exports = {
+    css: {
+        requireModuleExtension: false,
+        loaderOptions: {
+            css: {
+                modules: {
+                    localIdentName: `[name]__${packageJson.version}--[hash:base64:7]`
+                },
+                localsConvention: 'camelCaseOnly'
+            }
+        }
+    },
     chainWebpack: config => {
         config.module
             .rule('scss-importer')
