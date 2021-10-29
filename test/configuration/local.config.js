f786f284b5ddc93fca7cda0a4f2941a8fb4cd75a
Lizzie Turney <lizzie.turney@just-eat.com>
2021-10-29 16:13:50 +0100

    changed local logs

--- test/configuration/local.config.js
+++ test/configuration/local.config.js
@@ -1,7 +1,7 @@
 const { testType } = require('./shared.config');
 
 const configuration = {
-    logLevel: 'silent',
+    logLevel: 'error',
     baseUrl: 'http://localhost:8080',
     bail: 0,
     headless: false,
