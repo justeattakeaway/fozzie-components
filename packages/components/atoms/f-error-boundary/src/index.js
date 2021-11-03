20f6676e309cba1ac5d6c426c306e577442fbabd
Damian Mullins <damian.mullins@just-eat.com>
2021-11-03 15:30:39 +0000

    f-error-boundary@v0.1.0 - Initial generated component.

---
+++ packages/components/atoms/f-error-boundary/src/index.js
@@ -0,0 +1,14 @@
+
+/**
+* @overview Add description of service here
+*
+* @module f-error-boundary
+*/
+
+export default class ServiceConstructor {
+    constructor () {
+        throw new Error('Not implemented yet!');
+    }
+}
+
+
