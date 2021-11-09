8c0d7168897cc6b06584eef85ee374de986ca6a7
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2021-11-09 09:35:50 +0000

    f-card-with-content@0.2.0 - Add card functionality

--- packages/components/molecules/f-card-with-content/CHANGELOG.md
+++ packages/components/molecules/f-card-with-content/CHANGELOG.md
@@ -4,6 +4,20 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.2.0
+------------------------------
+*November 9, 2021*
+
+### Added
+- Heading
+- Description
+- Primary and secondary buttons
+  - `href`s are passed in via prop like `primary-button-href`.
+  - Emit events like `primary-button-click`.
+- Named slot for icon.
+- Storybook story.
+
+
 v0.1.0
 ------------------------------
 *November 3, 2021*
--- packages/components/molecules/f-card-with-content/stories/CardWithContent.stories.js
+++ packages/components/molecules/f-card-with-content/stories/CardWithContent.stories.js
@@ -1,8 +1,10 @@
 // Uncomment the import below to add prop controls to your Story (and add `withKnobs` to the decorators array)
-// import {
-//     withKnobs, select, boolean
-// } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
+import {
+    BagRunBgIcon,
+    BagSadBgIcon,
+    BagSorryBgIcon
+} from '@justeat/f-vue-icons';
 import CardWithContent from '../src/components/CardWithContent.vue';
 
 export default {
@@ -10,11 +12,50 @@ export default {
     decorators: [withA11y]
 };
 
-export const CardWithContentComponent = () => ({
-    components: { CardWithContent },
-    props: {
+export const CardWithContentComponent = (args, { argTypes }) => ({
+    components: {
+        BagRunBgIcon,
+        BagSadBgIcon,
+        BagSorryBgIcon,
+        CardWithContent
     },
-    template: '<card-with-content />'
+
+    props: Object.keys(argTypes),
+
+    template: `<card-with-content
+                    :card-heading="cardHeading"
+                    :card-description="cardDescription"
+                    :primaryButtonText="primaryButtonText"
+                    :primaryButtonHref="primaryButtonHref"
+                    :secondaryButtonText="secondaryButtonText"
+                    :secondaryButtonHref="secondaryButtonHref">
+                    <template #icon>
+                        <component :is="icon" />
+                    </template>
+                </card-with-content>`
 });
 
 CardWithContentComponent.storyName = 'f-card-with-content';
+CardWithContentComponent.args = {
+    cardHeading: 'Something went wrong',
+    cardDescription: 'Looks like a problem on our end, sorry. Try again soon.',
+    icon: '',
+    primaryButtonText: 'Go back',
+    primaryButtonHref: '/',
+    secondaryButtonText: '',
+    secondaryButtonHref: ''
+};
+CardWithContentComponent.argTypes = {
+    icon: {
+        control: { type: 'select' },
+        options: [
+            '', 'bag-sad-bg-icon', 'bag-run-bg-icon', 'bag-sorry-bg-icon'
+        ]
+    },
+    cardHeading: { control: { type: 'text' } },
+    cardDescription: { control: { type: 'text' } },
+    primaryButtonText: { control: { type: 'text' } },
+    primaryButtonHref: { control: { type: 'text' } },
+    secondaryButtonText: { control: { type: 'text' } },
+    secondaryButtonHref: { control: { type: 'text' } }
+};
