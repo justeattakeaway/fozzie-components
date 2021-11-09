bec4ff090c0948b78a7d7b6bd51b9e035c7469be
Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>
2021-11-09 10:08:27 +0000

    f-form-field - Formatting fixes.

--- packages/components/atoms/f-form-field/src/components/FormSelectionControl.vue
+++ packages/components/atoms/f-form-field/src/components/FormSelectionControl.vue
@@ -88,95 +88,95 @@ export default {
 </script>
 
 <style lang="scss" module>
-	/**
-		Changing the color of an inline SVG. The color needs to be encoded, so '#' turns into '%23'.
-		Had a lot of problems trying to save the SVG in the `assets` folder, since it wouldn't load, and including it
-		in `f-vue-icons` wouldn't help, since we need to use this in the `<style>` section, and not as a Vue component.
-	*/
-	@mixin tick-svg($color) {
-		$encodedColor: '%23' + str-slice(#{$color}, 2);
-		background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg fill='none' viewBox='0 0 15 12' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m4.5 9.4722-2.9861-2.9861c-0.15636-0.1569-0.36876-0.2451-0.59027-0.2451-0.22152 0-0.43392 0.0882-0.59028 0.2451-0.15691 0.15637-0.2451 0.36877-0.2451 0.59028 0 0.22152 0.088195 0.43392 0.2451 0.59028l3.5771 3.5771c0.15636 0.1569 0.36876 0.2451 0.59028 0.2451 0.22151 0 0.43391-0.0882 0.59028-0.2451l8.9229-8.9243c0.1569-0.15637 0.2451-0.36877 0.2451-0.59028 0-0.22152-0.0882-0.43392-0.2451-0.59028-0.1564-0.15691-0.3688-0.2451-0.5903-0.2451s-0.4339 0.088195-0.5903 0.2451l-8.3333 8.3333z' clip-rule='evenodd' fill='#{$encodedColor}' fill-rule='evenodd'/%3E%3C/svg%3E");
-		background-repeat: no-repeat;
-		background-position: center;
-		background-size: 14px;
-	}
+    /**
+        Changing the color of an inline SVG. The color needs to be encoded, so '#' turns into '%23'.
+        Had a lot of problems trying to save the SVG in the `assets` folder, since it wouldn't load, and including it
+        in `f-vue-icons` wouldn't help, since we need to use this in the `<style>` section, and not as a Vue component.
+    */
+    @mixin tick-svg($color) {
+        $encodedColor: '%23' + str-slice(#{$color}, 2);
+        background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg fill='none' viewBox='0 0 15 12' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m4.5 9.4722-2.9861-2.9861c-0.15636-0.1569-0.36876-0.2451-0.59027-0.2451-0.22152 0-0.43392 0.0882-0.59028 0.2451-0.15691 0.15637-0.2451 0.36877-0.2451 0.59028 0 0.22152 0.088195 0.43392 0.2451 0.59028l3.5771 3.5771c0.15636 0.1569 0.36876 0.2451 0.59028 0.2451 0.22151 0 0.43391-0.0882 0.59028-0.2451l8.9229-8.9243c0.1569-0.15637 0.2451-0.36877 0.2451-0.59028 0-0.22152-0.0882-0.43392-0.2451-0.59028-0.1564-0.15691-0.3688-0.2451-0.5903-0.2451s-0.4339 0.088195-0.5903 0.2451l-8.3333 8.3333z' clip-rule='evenodd' fill='#{$encodedColor}' fill-rule='evenodd'/%3E%3C/svg%3E");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 14px;
+    }
 
-	.c-formField-field--noFocus {
-		&:focus,
-		&:active,
-		&:focus-within {
-			box-shadow: none;
-		}
-	}
+    .c-formField-field--noFocus {
+        &:focus,
+        &:active,
+        &:focus-within {
+            box-shadow: none;
+        }
+    }
 
-	.c-formField-field--checkbox {
-		position: absolute;
-		opacity: 0;
-		z-index: -1;
-	}
+    .c-formField-field--checkbox {
+        position: absolute;
+        opacity: 0;
+        z-index: -1;
+    }
 
-	.c-formField-field--checkbox + label {
-		cursor: pointer;
-		font-weight: $font-weight-regular;
-		@include font-size('body-l');
-	}
+    .c-formField-field--checkbox + label {
+        cursor: pointer;
+        font-weight: $font-weight-regular;
+        @include font-size('body-l');
+    }
 
-	.c-formField-field--checkbox:disabled + label {
-		cursor: default;
-	}
+    .c-formField-field--checkbox:disabled + label {
+        cursor: default;
+    }
 
-	.c-formField-field--checkbox + label:before {
-		content: '';
-		border: 1px solid $color-grey-45;
-		border-radius: 2px;
-		display: inline-block;
-		width: 22px;
-		height: 22px;
-		margin-right: spacing();
-		margin-top: spacing();
-		vertical-align: -5px;
-	}
+    .c-formField-field--checkbox + label:before {
+        content: '';
+        border: 1px solid $color-grey-45;
+        border-radius: 2px;
+        display: inline-block;
+        width: 22px;
+        height: 22px;
+        margin-right: spacing();
+        margin-top: spacing();
+        vertical-align: -5px;
+    }
 
-	.c-formField-field--checkbox:checked + label:before {
-		@include tick-svg($color-white);
+    .c-formField-field--checkbox:checked + label:before {
+        @include tick-svg($color-white);
 
-		border: 1px solid $color-interactive-brand;
-		background-color: $color-interactive-brand;
-	}
+        border: 1px solid $color-interactive-brand;
+        background-color: $color-interactive-brand;
+    }
 
-	.c-formField-field--checkbox:focus + label:before {
-		box-shadow: 0 0 0 1px $color-white, 0 0 0 3px $color-focus;
+    .c-formField-field--checkbox:focus + label:before {
+        box-shadow: 0 0 0 1px $color-white, 0 0 0 3px $color-focus;
 
-		border: 1px solid $color-interactive-brand;
+        border: 1px solid $color-interactive-brand;
 
-		/*
-			https://stackoverflow.com/a/58570835
+        /*
+            https://stackoverflow.com/a/58570835
 
-			Visible in Windows high-contrast themes
-			box-shadow will be hidden in these modes and
-			transparency will not be hidden in high-contrast
-			thus box-shadow will not show but the outline will
-			providing accessibility */
-		outline: transparent dotted 2px;
-	}
+            Visible in Windows high-contrast themes
+            box-shadow will be hidden in these modes and
+            transparency will not be hidden in high-contrast
+            thus box-shadow will not show but the outline will
+            providing accessibility */
+        outline: transparent dotted 2px;
+    }
 
-	.c-formField-field--checkbox:focus:not(:checked) + label:before {
-		box-shadow: 0 0 0 1px $color-white, 0 0 0 3px $color-focus;
+    .c-formField-field--checkbox:focus:not(:checked) + label:before {
+        box-shadow: 0 0 0 1px $color-white, 0 0 0 3px $color-focus;
 
-		border: 1px solid $color-grey-45;
-	}
+        border: 1px solid $color-grey-45;
+    }
 
-	.c-formField-field--checkbox:not(:disabled):not(:checked) + label:hover:before {
-		background-color: darken($color-white, $color-hover-01);
-	}
+    .c-formField-field--checkbox:not(:disabled):not(:checked) + label:hover:before {
+        background-color: darken($color-white, $color-hover-01);
+    }
 
-	.c-formField-field--checkbox:disabled + label:before {
-		border: 1px solid $color-grey-30;
-		background-color: $color-grey-30;
-		cursor: default;
-	}
+    .c-formField-field--checkbox:disabled + label:before {
+        border: 1px solid $color-grey-30;
+        background-color: $color-grey-30;
+        cursor: default;
+    }
 
-	.c-formField-field--checkbox:disabled:checked + label:before {
-		@include tick-svg($color-grey-45);
-	}
+    .c-formField-field--checkbox:disabled:checked + label:before {
+        @include tick-svg($color-grey-45);
+    }
 </style>
