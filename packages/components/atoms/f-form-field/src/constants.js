efe53122bb527decbae8c971de8b68fa9bc7c812
Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>
2021-11-11 11:18:43 +0000

    f-form-field-checkbox - Small code review suggestions.

--- packages/components/atoms/f-form-field/src/constants.js
+++ packages/components/atoms/f-form-field/src/constants.js
@@ -1,10 +1,18 @@
 export const VALID_SELECTION_CONTROL_TYPES = ['radio', 'checkbox'];
 export const DEFAULT_SELECTION_CONTROL_TYPE = 'checkbox';
-export const VALID_INPUT_TYPES = ['text', 'email', 'password', 'number', 'tel'].concat(VALID_SELECTION_CONTROL_TYPES);
+export const VALID_TEXT_INPUT_TYPES = ['text', 'email', 'password', 'number', 'tel'];
+
+export const VALID_INPUT_TYPES = [
+    ...VALID_TEXT_INPUT_TYPES,
+    ...VALID_SELECTION_CONTROL_TYPES
+];
+
 export const CUSTOM_INPUT_TYPES = ['dropdown', 'textarea'];
 
-export const VALID_TEXT_INPUT_TYPES = ['text', 'email', 'password', 'number', 'tel'];
-export const VALID_ICON_INPUT_TYPES = VALID_TEXT_INPUT_TYPES.concat('dropdown');
+export const VALID_ICON_INPUT_TYPES = [
+    ...VALID_TEXT_INPUT_TYPES,
+    'dropdown'
+];
 
 export const DEFAULT_INPUT_TYPE = 'text';
 
