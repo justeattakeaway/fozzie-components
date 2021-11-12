a6007c4aa8692026ce61e73bdf11be1ea0d28d0f
Billy Oliver <billy.oliver@gmail.com>
2021-11-12 10:10:24 +0000

    f-contact-preferences@v0.5.1 - Added an api provider layer for GET/POST (#1464)
    * f-contact-preferences - Added an api provider layer for the getting and saving of the contact preferences data
    
    * Update packages/components/pages/f-contact-preferences/src/services/providers/_tests/contactPreferences.api.test.js
    
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>
    
    * Update packages/components/pages/f-contact-preferences/src/services/providers/_tests/contactPreferences.api.test.js
    
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>
    
    * Update packages/components/pages/f-contact-preferences/src/services/providers/_tests/contactPreferences.api.test.js
    
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>

--- packages/components/pages/f-contact-preferences/CHANGELOG.md
+++ packages/components/pages/f-contact-preferences/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.5.1
+------------------------------
+*November 11, 2021*
+
+### Added
+- Api provider for getting and saving consumer preferences data.
+
+
 v0.5.0
 ------------------------------
 *November 4, 2021*
---
+++ packages/components/pages/f-contact-preferences/src/constants.js
@@ -0,0 +1,11 @@
+export const UPDATE_PREFERENCES = 'updatePreferences';
+export const UPDATE_PREFERENCE = 'updatePreference';
+
+export const STOP_LOADING_SPINNER_EVENT = 'stop-spinner';
+
+export const CONSUMER_PREFERENCES_URL = 'consumer/preferences';
+
+export const CONVERSATION_ID_NAME = 'x-je-conversation';
+
+export const NON_EDITABLE_PREFERENCE_KEYS = ['orderstatus'];
+export const NON_VISIBLE_PREFERENCE_KEYS = ['reviewmeal'];
