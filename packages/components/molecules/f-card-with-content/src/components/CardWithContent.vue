108abfba8a300119115d4a86d0fa20f6f0668afa
Xander Marjoram <xander.marjoram@just-eat.com>
2021-11-12 15:03:00 +0000

    f-card-with-content@0.2.0 - Add component functionality (#1460)
    * f-card-with-content@0.2.0 - Add card functionality
    
    * f-card-with-content@0.2.0 - Updated readme
    
    * f-card-with-content@0.2.0 - Initial visual regression tests
    
    * fozzie-components@5.2.1 - Updated documentation for visual regression test setup
    
    * Minor: Whitespace fix
    
    * Minor: Changelog updates
    
    * Minor: Make heading size vary with viewport width
    And use large heading to better fit with designs.
    
    * f-card-with-content@0.2.0 - Use objects for button props
    
    * Minor: Revert changes unrelated to f-card-with-content
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

--- packages/components/molecules/f-card-with-content/src/components/CardWithContent.vue
+++ packages/components/molecules/f-card-with-content/src/components/CardWithContent.vue
@@ -1,32 +1,115 @@
 <template>
-    <div
-        :class="$style['c-cardWithContent']"
-        data-test-id="cardWithContent">
-        I am a CardWithContent Component (GB)
-    </div>
+    <card-component
+        has-outline
+        is-page-content-wrapper
+        data-test-id="cardWithContent"
+        :class="$style['c-cardWithContent']">
+        <div
+            v-if="$slots.icon"
+            :class="$style['c-cardWithContent-icon']">
+            <slot name="icon" />
+        </div>
+
+        <h1
+            v-if="cardHeading"
+            :class="$style['c-cardWithContent-heading']">
+            {{ cardHeading }}
+        </h1>
+
+        <p
+            v-if="cardDescription"
+            :class="$style['c-cardWithContent-description']">
+            {{ cardDescription }}
+        </p>
+
+        <f-button
+            v-if="primaryButton && primaryButton.text"
+            button-size="large"
+            button-type="primary"
+            is-full-width
+            :href="primaryButton.href"
+            :to="primaryButton.to"
+            :class="$style['c-cardWithContent-button']"
+            @click.native="$emit('primary-button-click')">
+            {{ primaryButton.text }}
+        </f-button>
+
+        <f-button
+            v-if="secondaryButton && secondaryButton.text"
+            button-size="large"
+            button-type="secondary"
+            is-full-width
+            :href="secondaryButton.href"
+            :to="secondaryButton.to"
+            :class="$style['c-cardWithContent-button']"
+            @click.native="$emit('secondary-button-click')">
+            {{ secondaryButton.text }}
+        </f-button>
+    </card-component>
 </template>
 
 <script>
+import CardComponent from '@justeat/f-card';
+import '@justeat/f-card/dist/f-card.css';
+import FButton from '@justeat/f-button';
+import '@justeat/f-button/dist/f-button.css';
 
 export default {
     name: 'CardWithContent',
-    components: {},
+    components: {
+        CardComponent,
+        FButton
+    },
     props: {
+        cardHeading: {
+            type: String,
+            default: ''
+        },
+        cardDescription: {
+            type: String,
+            default: ''
+        },
+        primaryButton: {
+            type: Object,
+            default: () => null
+        },
+        secondaryButton: {
+            type: Object,
+            default: () => null
+        }
     }
 };
 </script>
 
 <style lang="scss" module>
-
 .c-cardWithContent {
     display: flex;
-    justify-content: center;
-    min-height: 80vh;
-    width: 80vw;
-    margin: auto;
-    border: 1px solid red;
-    font-family: $font-family-base;
-    @include font-size(heading-m);
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
 }
 
+.c-cardWithContent-heading {
+    margin-bottom: 0;
+    @include font-size('heading-l', true, 'narrow');
+
+    @include media('>=wide') {
+        @include font-size('heading-l', true, 'default');
+    }
+}
+
+.c-cardWithContent-description {
+    @include font-size('body-l');
+    margin-top: spacing();
+}
+
+.c-cardWithContent-button {
+    margin: spacing(x4) 0 spacing(x0.5);
+}
+
+.c-cardWithContent-icon {
+    margin: auto;
+    margin-bottom: spacing(x4);
+    width: 200px;
+}
 </style>
--- packages/components/molecules/f-card-with-content/stories/CardWithContent.stories.js
+++ packages/components/molecules/f-card-with-content/stories/CardWithContent.stories.js
@@ -1,8 +1,11 @@
 // Uncomment the import below to add prop controls to your Story (and add `withKnobs` to the decorators array)
-// import {
-//     withKnobs, select, boolean
-// } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
+import { action } from '@storybook/addon-actions';
+import {
+    BagRunBgIcon,
+    BagSadBgIcon,
+    BagSorryBgIcon
+} from '@justeat/f-vue-icons';
 import CardWithContent from '../src/components/CardWithContent.vue';
 
 export default {
@@ -10,11 +13,55 @@ export default {
     decorators: [withA11y]
 };
 
-export const CardWithContentComponent = () => ({
-    components: { CardWithContent },
-    props: {
+export const CardWithContentComponent = (args, { argTypes }) => ({
+    components: {
+        BagRunBgIcon,
+        BagSadBgIcon,
+        BagSorryBgIcon,
+        CardWithContent
     },
-    template: '<card-with-content />'
+
+    props: Object.keys(argTypes),
+
+    template: `<card-with-content
+                    :card-heading="cardHeading"
+                    :card-description="cardDescription"
+                    :primary-button="primaryButton"
+                    :secondary-button="secondaryButton"
+                    @primary-button-click="primaryButtonClick"
+                    @secondary-button-click="secondaryButtonClick">
+                    <template #icon>
+                        <component :is="icon" />
+                    </template>
+                </card-with-content>`,
+
+    methods: {
+        primaryButtonClick: action('primary-button-click'),
+
+        secondaryButtonClick: action('secondary-button-click')
+    }
 });
 
 CardWithContentComponent.storyName = 'f-card-with-content';
+CardWithContentComponent.args = {
+    cardHeading: 'Something went wrong',
+    cardDescription: 'Looks like a problem on our end, sorry. Try again soon.',
+    icon: '',
+    primaryButton: {
+        href: '/',
+        text: 'Go back'
+    },
+    secondaryButton: null
+};
+CardWithContentComponent.argTypes = {
+    icon: {
+        control: { type: 'select' },
+        options: [
+            '', 'bag-sad-bg-icon', 'bag-run-bg-icon', 'bag-sorry-bg-icon'
+        ]
+    },
+    cardHeading: { control: { type: 'text' } },
+    cardDescription: { control: { type: 'text' } },
+    primaryButton: { control: { type: 'object' } },
+    secondaryButton: { control: { type: 'object' } }
+};
