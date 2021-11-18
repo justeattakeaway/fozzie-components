dfc66462f1603710cc43ed806278eb790f1291f9
Thomas William McClean <thomas.mcclean@justeattakeaway.com>
2021-11-18 11:32:08 +0000

    f-account-info@v0.1.0 - Initial Commit

---
+++ packages/components/pages/f-account-info/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+v0.1.0
+------------------------------
+*November 18, 2021*
+
+### Added
+- Add your change under the following headings: `Added`/`Changed`/`Deprecated`/`Removed`/`Fixed`/`Security`
+!!! Please, add your component to `.circleci/config.yml` under `save_cache_dist_directories` manually after the component is generated.
---
+++ packages/components/pages/f-account-info/src/components/AccountInfo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+        :class="$style['c-accountInfo']"
+        data-test-id="accountInfo">
+        {{ copy.text }}
+    </div>
+</template>
+
+<script>
+import { globalisationServices } from '@justeat/f-services';
+import tenantConfigs from '../tenants';
+
+// eslint-disable-next-line no-unused-vars
+import AccountInfoServiceApi from '../services/AccountInfoServiceApi';
+
+export default {
+    name: 'AccountInfo',
+    components: {},
+    props: {
+        locale: {
+            type: String,
+            default: ''
+        }
+    },
+    data () {
+        const locale = globalisationServices.getLocale(tenantConfigs, this.locale, this.$i18n);
+        const localeConfig = tenantConfigs[locale];
+
+        return {
+            copy: { ...localeConfig }
+        };
+    }
+};
+</script>
+
+<style lang="scss" module>
+
+.c-accountInfo {
+    display: flex;
+    justify-content: center;
+    min-height: 80vh;
+    width: 80vw;
+    margin: auto;
+    border: 1px solid red;
+    font-family: $font-family-base;
+    @include font-size(heading-m);
+}
+
+</style>
