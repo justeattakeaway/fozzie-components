a1a0ce226e68173b215f28966de2c68ec5e2406e
Thomas William McClean <thomas.mcclean@justeattakeaway.com>
2021-11-18 15:56:06 +0000

    f-account-info@v0.2.0 - Initial UI

--- packages/components/pages/f-account-info/CHANGELOG.md
+++ packages/components/pages/f-account-info/CHANGELOG.md
@@ -4,6 +4,15 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.2.0
+------------------------------
+*November 19, 2021*
+
+### Added
+- Initial UI elements - similar to design
+- Initial en-GB translation messages
+
+
 v0.1.0
 ------------------------------
 *November 18, 2021*
--- packages/components/pages/f-account-info/src/components/_tests/AccountInfo.test.js
+++ packages/components/pages/f-account-info/src/components/_tests/AccountInfo.test.js
@@ -1,10 +1,30 @@
-import { shallowMount } from '@vue/test-utils';
-import AccountInfo from '../AccountInfo.vue';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { VueI18n } from '@justeat/f-globalisation';
+
+import ContactPreferences from '../AccountInfo.vue';
+import tenantConfigs from '../../tenants';
+
+const localVue = createLocalVue();
+
+localVue.use(VueI18n);
+
+const i18n = {
+    locale: 'en-GB',
+    messages: {
+        'en-GB': tenantConfigs['en-GB'].messages
+    }
+};
 
 describe('AccountInfo', () => {
     it('should be defined', () => {
-        const propsData = {};
-        const wrapper = shallowMount(AccountInfo, { propsData });
+        // Arrange
+        // Act
+        const wrapper = shallowMount(ContactPreferences, {
+            i18n,
+            localVue
+        });
+
+        // Assert
         expect(wrapper.exists()).toBe(true);
     });
 });
