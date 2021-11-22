fc8353cd873ad1423c17cca1e506acb79c397563
Billy Oliver <billy.oliver@gmail.com>
2021-11-22 10:21:55 +0000

    f-contact-preferences@v0.5.3 - Added Vuex Store and wired up Template (#1478)
    * f-contact-preferences - Added a Provider for the API GET request
    
    * f-contact-preferences - Added repository layers + testing
    
    * f-contact-preferences - Added a Store to maintain the state and a mapper to convert the api requirements to the view requirements.
    
    * f-contact-preferences - Removed redundant code plus updated store and tests
    
    * f-contact-preferences - Added some changes to the Store by merging the update to the prefs and ver together plus fixed up the Component & tests
    
    * f-contact-preferences - Amended Version/ChangeLog
    
    * f-contact-preferences - Removed redundant event/event-handler
    
    * f-contact-preferences - Review changes
    
    * f-contact-preferences - Review changes
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/pages/f-contact-preferences/src/components/_tests/ContactPreferences.test.js
+++ packages/components/pages/f-contact-preferences/src/components/_tests/ContactPreferences.test.js
@@ -1,12 +1,24 @@
+import Vuex from 'vuex';
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import { VueI18n } from '@justeat/f-globalisation';
-
 import ContactPreferences from '../ContactPreferences.vue';
 import tenantConfigs from '../../tenants';
+import {
+    contactPreferencesViewModel,
+    baseUrl,
+    token
+} from '../../../test-utils/setup';
 
 const localVue = createLocalVue();
-
 localVue.use(VueI18n);
+localVue.use(Vuex);
+
+let wrapper;
+let cookiesSpy;
+let httpSpy;
+let sutMocks;
+let sutProps;
+let dataDefaults;
 
 const i18n = {
     locale: 'en-GB',
@@ -15,25 +27,209 @@ const i18n = {
     }
 };
 
-const baseProps = {
-    smartGatewayBaseUrl: '/'
+const storeActions = {
+    loadPreferences: jest.fn(),
+    savePreferences: jest.fn(),
+    editPreference: jest.fn()
 };
 
-describe('ContactPreferences', () => {
-    it('should be defined', () => {
-        // Arrange
-        const propsData = {
-            ...baseProps
-        };
+const storeState = contactPreferencesViewModel;
 
-        // Act
-        const wrapper = shallowMount(ContactPreferences, {
-            i18n,
-            localVue,
-            propsData
+const mountContactPreferences = ({
+    actions = storeActions,
+    state = storeState,
+    mocks = sutMocks,
+    propsData = sutProps,
+    data = dataDefaults
+} = {}) => shallowMount(ContactPreferences, {
+    i18n,
+    localVue,
+    propsData,
+    data,
+    store: new Vuex.Store({
+        modules: {
+            fContactPreferencesModule: {
+                state,
+                actions,
+                namespaced: true
+            }
+        }
+    }),
+    mocks
+});
+
+describe('ContactPreferences Component', () => {
+    beforeEach(() => {
+        // Arrange & Act
+        dataDefaults = () => ({
+            isFormDirty: false,
+            showErrorPage: false
+        });
+        cookiesSpy = jest.fn();
+        httpSpy = jest.fn();
+        sutMocks = {
+            $parent: {
+                $emit: jest.fn()
+            },
+            $http: httpSpy,
+            $cookies: cookiesSpy
+        };
+        sutProps = {
+            authToken: token,
+            smartGatewayBaseUrl: baseUrl
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('when mounting the component and calling `initialise`', () => {
+        it('should call the load action with the correct parameters', () => {
+            // Arrange & Act
+            wrapper = mountContactPreferences();
+
+            // Assert
+            expect(storeActions.loadPreferences).toHaveBeenCalledWith(expect.any(Object), {
+                api: {
+                    baseUrl,
+                    cookies: cookiesSpy,
+                    httpClient: httpSpy
+                },
+                authToken: token
+            });
         });
 
-        // Assert
-        expect(wrapper.exists()).toBe(true);
+        it('should set showErrorPage flag to true if an error occurs', () => {
+            // Arrange & Act
+            const errorActions = {
+                loadPreferences: jest.fn().mockImplementationOnce(() => {
+                    throw new Error('some-error');
+                })
+            };
+            wrapper = mountContactPreferences({ actions: errorActions });
+
+            // Assert
+            expect(wrapper.vm.showErrorPage).toEqual(true);
+        });
+
+        it('should not show the error card if no errors', () => {
+            // Arrange & Act
+            wrapper = mountContactPreferences();
+            const element = wrapper.find('[data-test-id="contactPreferences-error-card"]');
+
+            // Assert
+            expect(element.exists()).toEqual(false);
+        });
+
+        it('should show the error card if showErrorPage is true', async () => {
+            // Arrange & Act
+            wrapper = mountContactPreferences();
+            await wrapper.setData({ showErrorPage: true });
+            const element = wrapper.find('[data-test-id="contactPreferences-error-card"]');
+
+            // Assert
+            expect(element.exists()).toEqual(true);
+        });
+
+        it('should set the `isFormDirty` flag to false', async () => {
+            // Arrange
+            wrapper = mountContactPreferences();
+            await wrapper.setData({ isFormDirty: true });
+
+            // Act - re-mount the page to prove assertion
+            wrapper = mountContactPreferences();
+
+            // Assert
+            expect(wrapper.vm.isFormDirty).toEqual(false);
+        });
+    });
+
+    describe('when clicking a preference checkbox', () => {
+        it.each([
+            [true, false],
+            [false, true]
+        ])('should call the Mutation correctly with %s if previously %s', async (setValue, previousValue) => {
+            // Arrange
+            storeState.preferences.find(e => e.key === 'news').emailValue = previousValue;
+            wrapper = mountContactPreferences();
+            const input = wrapper.find('[data-test-id="contactPreferences-news-checkbox"]');
+
+            // Act
+            await input.setChecked(setValue);
+
+            // Assert
+            expect(storeActions.editPreference).toHaveBeenCalledWith(
+                expect.any(Object),
+                {
+                    key: 'news',
+                    field: 'emailValue',
+                    value: setValue
+                }
+            );
+        });
+
+        it('should set the `isFormDirty` flag to true', async () => {
+            // Act
+            wrapper = mountContactPreferences();
+            const input = wrapper.find('[data-test-id="contactPreferences-news-checkbox"]');
+
+            // Act
+            await input.setChecked(!input.element.checked);
+
+            // Assert
+            expect(wrapper.vm.isFormDirty).toEqual(true);
+        });
+    });
+
+    describe('when clicking the save preferences button', () => {
+        it('should call the save action with the correct parameters', async () => {
+            // Arrange
+            wrapper = mountContactPreferences();
+            await wrapper.setData({ isFormDirty: true });
+
+            // Act
+            wrapper.vm.onFormSubmit();
+
+            // Assert
+            expect(storeActions.savePreferences).toHaveBeenCalledWith(expect.any(Object), {
+                api: {
+                    baseUrl,
+                    cookies: cookiesSpy,
+                    httpClient: httpSpy
+                },
+                authToken: token
+            });
+        });
+
+        it('should not call the save action if no changes', async () => {
+            // Arrange
+            wrapper = mountContactPreferences();
+            await wrapper.setData({ isFormDirty: false });
+
+            // Act
+            wrapper.vm.onFormSubmit();
+
+            // Assert
+            expect(storeActions.savePreferences).not.toHaveBeenCalled();
+        });
+
+        it('should set showErrorPage flag to true if an error occurs', async () => {
+            // Arrange
+            const errorActions = {
+                loadPreferences: jest.fn(),
+                savePreferences: jest.fn().mockImplementationOnce(() => {
+                    throw new Error('some-error');
+                })
+            };
+            wrapper = mountContactPreferences({ actions: errorActions });
+            await wrapper.setData({ isFormDirty: true }); // Allow the data to be submitted
+
+            // Act
+            wrapper.vm.onFormSubmit();
+
+            // Assert
+            expect(wrapper.vm.showErrorPage).toEqual(true);
+        });
     });
 });
