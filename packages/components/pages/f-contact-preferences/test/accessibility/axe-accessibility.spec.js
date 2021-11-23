d0d736b4bd7bb020242d635b2ade5de65f431707
Lizzie Turney <lizzie.turney@just-eat.com>
2021-11-23 15:45:21 +0000

    started accessibility tests

--- packages/components/pages/f-contact-preferences/test/accessibility/axe-accessibility.spec.js
+++ packages/components/pages/f-contact-preferences/test/accessibility/axe-accessibility.spec.js
@@ -1,4 +1,5 @@
 import { getAccessibilityTestResults } from '../../../../../../test/utils/axe-helper';
+import forEach from 'mocha-each';
 
 const ContactPreferences = require('../../test-utils/component-objects/f-contactPreferences.component');
 
@@ -7,13 +8,33 @@ let contactPreferences;
 describe('Accessibility tests', () => {
     beforeEach(() => {
         contactPreferences = new ContactPreferences();
-
-        contactPreferences.load();
-        contactPreferences.waitForComponent();
     });
 
-    it('a11y - should test f-contactPreferences component WCAG compliance', () => {
+    forEach([
+        ['en-GB']
+    ]).it('a11y - should test f-contact-preferences component WCAG compliance', locale => {
+        // Arrange
+        contactPreferences.withQuery('&knob-Locale', locale);
+
         // Act
+        contactPreferences.load();
+        contactPreferences.waitForComponent();
+        const axeResults = getAccessibilityTestResults('f-contactPreferences');
+
+        // Assert
+        expect(axeResults.violations.length).toBe(0);
+    });
+
+    forEach([
+        ['en-GB']
+    ]).it('should test f-contact-preferences (error page) WCAG compliance', locale => {
+        // Arrange
+        contactPreferences
+        .withQuery('&knob-Locale', locale)
+        .withQuery('&knob-Set Api State', 'api-post-failed');
+
+        // Act
+        contactPreferences.load();
         const axeResults = getAccessibilityTestResults('f-contactPreferences');
 
         // Assert
