b445113e1fd4aa6e44fdd947393e253716b79266
Andy May <andy.may@just-eat.com>
2021-11-24 15:57:41 +0000

    Create AccountInfoApi class

---
+++ packages/components/pages/f-account-info/src/constants.js
@@ -0,0 +1,4 @@
+
+export const CONSUMER_DETAILS_URL = 'consumer';
+
+export const CONVERSATION_ID_NAME = 'x-je-conversation';
---
+++ packages/components/pages/f-account-info/test-utils/setup.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://smartGatewayBaseUrl.com';
+
+export const token = 'some-auth-token';
+
+export const conversationId = 'b7386108-95e6-4e73-9421-5b066c089153';
