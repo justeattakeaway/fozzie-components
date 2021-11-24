c093553d0adf5153cd9cbf354346606f19f49138
AndyMayJE <andy.may@just-eat.com>
2021-11-24 09:04:26 +0000

    f-contact-preferences@0.7.0 - Rename boolean fields (#1486)
    ### Changed
    - Rename boolean fields to match naming conventions

--- packages/components/pages/f-contact-preferences/src/components/ContactPreferences.vue
+++ packages/components/pages/f-contact-preferences/src/components/ContactPreferences.vue
@@ -2,13 +2,13 @@
     <div
         data-test-id="contact-preferences">
         <card-component
-            v-if="!showErrorPage"
+            v-if="!shouldShowErrorPage"
             :card-heading="$t('heading')"
             is-page-content-wrapper
             has-outline>
             <form @submit.prevent="onFormSubmit">
                 <div
-                    v-for="{ key, emailEnabled, emailValue, smsEnabled, smsValue } in preferences"
+                    v-for="{ key, isEmailEnabled, emailValue, isSmsEnabled, smsValue } in preferences"
                     :key="key">
                     <h2
                         :class="$style['c-contactPreferences-subtitle']">
@@ -20,12 +20,12 @@
                             <input
                                 type="checkbox"
                                 :data-test-id="`contact-preferences-${key}-email-checkbox`"
-                                :disabled="!emailEnabled"
+                                :disabled="!isEmailEnabled"
                                 :checked="emailValue"
                                 @change="editPreferenceValue(key, Object.keys({ emailValue })[0], $event.target.checked)">
                             <span
                                 :class="{
-                                    [$style['c-contactPreferences-labelText--disabled']]: !emailEnabled
+                                    [$style['c-contactPreferences-labelText--disabled']]: !isEmailEnabled
                                 }">
                                 {{ $t(`${key}.email`) }}
                                 <template v-if="$te(`${key}.emailDescription`)">
@@ -37,12 +37,12 @@
                             <input
                                 type="checkbox"
                                 :data-test-id="`contact-preferences-${key}-sms-checkbox`"
-                                :disabled="!smsEnabled"
+                                :disabled="!isSmsEnabled"
                                 :checked="smsValue"
                                 @change="editPreferenceValue(key, Object.keys({ smsValue })[0], $event.target.checked)">
                             <span
                                 :class="{
-                                    [$style['c-contactPreferences-labelText--disabled']]: !smsEnabled
+                                    [$style['c-contactPreferences-labelText--disabled']]: !isSmsEnabled
                                 }">
                                 {{ $t(`${key}.sms`) }}
                                 <template v-if="$te(`${key}.smsDescription`)">
@@ -124,7 +124,7 @@ export default {
         return {
             isFormDirty: false,
             error: {},
-            showErrorPage: false,
+            shouldShowErrorPage: false,
             tenantConfigs,
             contactPreferencesApi: new ContactPreferencesApi({
                 httpClient: this.$http,
@@ -157,7 +157,7 @@ export default {
         * @param {object} Error - The error that has recently occurred
         */
         handleErrorState (error) {
-            this.showErrorPage = true;
+            this.shouldShowErrorPage = true;
             this.error = error;
         },
 
