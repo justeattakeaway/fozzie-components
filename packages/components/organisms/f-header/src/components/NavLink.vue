592f69d80200c8c10e4d23f6b7be08bbf1bf4b28
Xander Marjoram <xander.marjoram@just-eat.com>
2021-12-01 11:29:16 +0000

    f-header@7.2.0 - Extract NavLink and CountrySelector components (#1494)
    * Minor: yarn.lock update
    
    * Minor: Tidy up login state storybook knobs
    
    * f-header@7.2.0 - Extract NavLink component and move styles into SCSS file
    
    * f-header@7.2.0 - Extract CountrySelector component and update tests
    
    * Minor: Neaten up tests
    
    * f-header@7.2.0 - Version updates
    
    * f-header@7.2.0 - Fix build
    
    * Minor: Remove unused class
    
    * f-header@7.2.0 - Decouple `li` from NavLink and CountrySelector
    And move some unshared styles back into Navigation.vue
    
    * Minor: Fix styling and knobs for visual tests
    
    * Minor: Fix logged in state for tests
    
    * Minor: Fix mobile tests too
    
    Co-authored-by: Xander Marjoram <xander.marjoram@justeattakeaway.com>

---
+++ packages/components/organisms/f-header/src/components/NavLink.vue
@@ -0,0 +1,58 @@
+<template>
+    <a
+        v-bind="$attrs"
+        :data-trak="JSON.stringify(dataTrak)"
+        :href="href"
+        :class="[
+            $style['c-nav-list-link'],
+            { [$style['c-nav-list-link--leftPaddingBelowMid']]: leftPaddingBelowMid },
+            { [$style['c-nav-list-link--alt']]: isAltColour },
+            { [$style['c-nav-list-link--transparent']]: backgroundTheme === 'transparent' }
+        ]">
+        <slot name="icon" />
+
+        {{ text }}
+    </a>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+
+    props: {
+        dataTrak: {
+            type: Object,
+            default: () => undefined
+        },
+
+        href: {
+            type: String,
+            required: true
+        },
+
+        text: {
+            type: String,
+            required: true
+        },
+
+        leftPaddingBelowMid: {
+            type: Boolean,
+            default: false
+        },
+
+        isAltColour: {
+            type: Boolean,
+            default: false
+        },
+
+        backgroundTheme: {
+            type: String,
+            default: 'white'
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+@import '../assets/scss/navigation.scss';
+</style>
