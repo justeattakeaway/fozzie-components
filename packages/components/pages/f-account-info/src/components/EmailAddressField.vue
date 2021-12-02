08891124850182b92d20fc65050b160eba27ffd1
Thomas William McClean <spam@tommcclean.me>
2021-12-02 13:38:43 +0000

    f-account-info@v0.6.0 - Added form validation wiring, rules and visual feedback (#1498)
    * f-account-info - Versioning
    
    * f-account-info - Use correct link endpoints
    
    * f-account-info - Initial Validation Structure
    
    * f-account-info - Get validation scheme working
    
    * f-account-info - Refactoring validation methods
    
    * f-account-info - Add additional validation rules
    
    * f-account-info - Pull Email out of Form Fields
    
    * f-account-info - Refactor character rules
    
    * f-account-info - Refactor isValidName validation
    
    * f-account-info - Update log tags
    
    * f-account-info - Add visual validation feedback
    
    * f-account-info - Refactoring and Cleanup
    
    * f-account-info - Extract delete account markup
    
    * f-account-info - Finalise versioning
    
    * f-account-info - PR feedback
    
    * f-account-info - Added JSDoc Info
    
    * f-account-info - Correct typo
    
    * f-account-info - Test isValidName
    
    * f-account-info - Add Phone Number Field and Validation
    
    * f-account-info - Refine when validation occurs for phone number
    
    * f-account-info - Add Phone Number to Fields
    
    * f-account-info - Move f-link CSS loading
    
    * f-account-info - Add new props to Story
    
    * f-account-info - Tests for isFormValid
    
    * f-account-info - Standardise validation names
    
    * f-account-info - Bump versioning
    
    * f-account-info - Update Watch hooks
    
    * f-account-info - Update story props
    
    Co-authored-by: Thomas William McClean <thomas.mcclean@justeattakeaway.com>

---
+++ packages/components/pages/f-account-info/src/components/EmailAddressField.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <form-field
+            :label-text="$t('fields.emailAddressLabel')"
+            disabled
+            :placeholder="$t('fields.emailAddressPlaceholder')"
+            class="u-spacingBottom--large"
+            :value="emailAddress" />
+
+        <f-link
+            is-distinct
+            :class="$style['c-accountInfo-customerCareLink']"
+            href="/help/article/203097431/how-do-i-manage-my-account"
+            target="_blank">
+            {{ $t('contactCustomerCareTeam') }}
+        </f-link>
+    </div>
+</template>
+
+<script>
+import FormField from '@justeat/f-form-field';
+import '@justeat/f-form-field/dist/f-form-field.css';
+
+import FLink from '@justeat/f-link';
+import '@justeat/f-link/dist/f-link.css';
+
+export default {
+    components: {
+        FormField,
+        FLink
+    },
+
+    props: {
+        emailAddress: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+.c-accountInfo-customerCareLink {
+    display: block;
+    margin-bottom: spacing(x4);
+}
+</style>
