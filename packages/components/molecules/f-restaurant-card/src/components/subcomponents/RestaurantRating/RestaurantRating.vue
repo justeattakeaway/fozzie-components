1295937e887d111f14df5af7cce2aa2bcfb28afa
Jamie Maguire <jamieolivermaguire@googlemail.com>
2021-12-06 18:11:17 +0000

    Update packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantRating/RestaurantRating.vue
    Co-authored-by: Ashley Watson-Nolan <ashley.watson-nolan@justeattakeaway.com>

--- packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantRating/RestaurantRating.vue
+++ packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantRating/RestaurantRating.vue
@@ -150,7 +150,7 @@ export default {
 
 <style lang="scss" module>
 .c-restaurantCard-rating {
-    @include font-size($font-body-s-paragraph);
+    @include font-size(body-s);
     display: flex;
     align-items: center;
     padding: 0;
