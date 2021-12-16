cad89cc0dced16003521d272ea67c6ffa88040d9
Anastasiya Tyshkavets <a.tyshkavets@just-eat.com>
2021-12-16 12:20:27 +0000

    generate component

---
+++ packages/components/atoms/f-filter-pill/src/components/FilterPill.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        :class="$style['c-filterPill']"
+        data-test-id="filter-pill-component">
+        I am a FilterPill Component (GB)
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'FilterPill',
+    components: {},
+    props: {
+    }
+};
+</script>
+
+<style lang="scss" module>
+
+.c-filterPill {
+    display: flex;
+    justify-content: center;
+    min-height: 80vh;
+    width: 80vw;
+    margin: auto;
+    border: 1px solid red;
+    font-family: $font-family-base;
+    @include font-size(heading-m);
+}
+
+</style>
