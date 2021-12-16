bcca91a8a30e3811751c54108fe9ddae76a5296b
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2021-12-16 11:22:21 +0000

    fozzie-components@v5.7.0 - Modify Storybook config to prevent out of memory exception when running global builds. (#1561)
    * Serve storybook instead of build
    
    * Update Storybook dep to fix memory leak
    
    * Fix storybook memory leak and revert config
    
    * Test optimisation changes
    
    * Add changelogs
    
    * Remove approval process for jobs

--- packages/storybook/CHANGELOG.md
+++ packages/storybook/CHANGELOG.md
@@ -3,6 +3,14 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+v0.48.0
+------------------------------
+*December 16, 2021*
+
+### Changed
+- Storybook webpack config to fix CircleCI master builds.
+
+
 v0.47.0
 ------------------------------
 *November 03, 2021*
--- packages/storybook/config/storybook/main.js
+++ packages/storybook/config/storybook/main.js
@@ -31,6 +31,11 @@ module.exports = {
                     }
                 ]
             },
+            // Added to prevent CircleCI from running out of memory - https://github.com/storybookjs/storybook/issues/6408#issuecomment-648197797
+            optimization: {
+                minimize: false,
+                minimizer: [],
+            },
             resolve: {
                 ...config.resolve,
                 extensions: [...(config.resolve.extensions || [])]
