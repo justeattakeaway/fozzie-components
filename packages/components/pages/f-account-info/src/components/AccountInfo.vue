e33d66011c225bd244a46ddced163cff7f3a1826
Sacha Zvetelman <sacha.zvetelman@justeattakeaway.com>
2021-12-17 15:34:31 +0000

    f-account-info-analytics - Reverting changes to fields since validations have now been fixed.

--- packages/components/pages/f-account-info/src/components/AccountInfo.vue
+++ packages/components/pages/f-account-info/src/components/AccountInfo.vue
@@ -17,7 +17,7 @@
                 method="post"
                 @submit.prevent="onFormSubmit">
                 <form-field
-                    v-model="consumer.firstName"
+                    :value="consumer.firstName"
                     data-test-id="account-info-consumer-firstName"
                     maxlength="50"
                     :label-text="$t('consumer.firstNameLabel')"
@@ -37,7 +37,7 @@
                 </form-field>
 
                 <form-field
-                    v-model="consumer.lastName"
+                    :value="consumer.lastName"
                     data-test-id="account-info-consumer-lastName"
                     maxlength="50"
                     :label-text="$t('consumer.lastNameLabel')"
@@ -57,7 +57,7 @@
                 </form-field>
 
                 <form-field
-                    v-model="consumer.phoneNumber"
+                    :value="consumer.phoneNumber"
                     maxlength="16"
                     :label-text="$t('consumer.phoneNumberLabel')"
                     :placeholder="$t('consumer.phoneNumberPlaceholder')"
@@ -81,7 +81,7 @@
                 </h2>
 
                 <form-field
-                    v-model="consumer.line1"
+                    :value="consumer.line1"
                     data-test-id="account-info-consumer-line1"
                     maxlength="50"
                     :label-text="$t('consumer.addressLabel')"
@@ -98,19 +98,19 @@
                 </form-field>
 
                 <form-field
-                    v-model="consumer.line2"
+                    :value="consumer.line2"
                     maxlength="50"
                     :placeholder="$t('consumer.line2Placeholder')"
                     @input="onEditConsumer('line2', $event, true)" />
 
                 <form-field
-                    v-model="consumer.line3"
+                    :value="consumer.line3"
                     maxlength="50"
                     :placeholder="$t('consumer.line3Placeholder')"
                     @input="onEditConsumer('line3', $event, true)" />
 
                 <form-field
-                    v-model="consumer.locality"
+                    :value="consumer.locality"
                     maxlength="50"
                     :label-text="$t('consumer.localityLabel')"
                     :placeholder="$t('consumer.localityPlaceholder')"
@@ -126,7 +126,7 @@
                 </form-field>
 
                 <form-field
-                    v-model="consumer.postcode"
+                    :value="consumer.postcode"
                     maxlength="50"
                     :label-text="$t('consumer.postcodeLabel')"
                     :placeholder="$t('consumer.postcodePlaceholder')"
