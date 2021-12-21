66e7092c1ed8f8f07f7089f91c0eb6c2aa1a7bb8
Alix Richards <alexandra.j.e.richards@hotmail.com>
2021-12-21 11:56:42 +0000

    f-image-tile@0.2.0  - Add toggle functionality (#1566)
    * Add initial component markup
    Add props to storybook story
    
    * Added displayText prop to story
    Updated template to use displayText prop
    
    Added $emit when filter is toggled
    Added function to create emit object
    
    WIP updating isSelected prop
    
    * Added tests for methods, props and wip watcher test
    
    * Added npm package version change
    Added unique id for input
    Added image as prop
    WIP watch isSelected()
    Commented out watch test
    
    * Added test ids
    Added props tests
    Added watch test
    
    * Updated stories to use controls instead of knobs
    Added a demo story to show imageTile within a container which tests the $emit event
    
    * removed unneeded prop from test container
    Added changelog
    
    * removed unneeded default value
    Updated story name
    
    * Updated storybook title
    Updated component test file with data-test-id
    
    * renamed data-test-id to image-tile-component
    Removed unused prop in storybook
    
    * Moving end of tag on to same line as last attribute
    
    * Corrected spacing within css
    Updated aria-hidden label conditions
    
    * Update packages/components/atoms/f-image-tile/src/components/ImageTile.vue
    
    Co-authored-by: Anastasiya <a.tyshkavets@justeattakeaway.com>
    
    * Update packages/components/atoms/f-image-tile/src/components/ImageTile.vue
    
    Co-authored-by: Anastasiya <a.tyshkavets@justeattakeaway.com>
    
    * Update packages/components/atoms/f-image-tile/src/components/ImageTile.vue
    
    Co-authored-by: Anastasiya <a.tyshkavets@justeattakeaway.com>
    
    * Update packages/components/atoms/f-image-tile/src/components/ImageTile.vue
    
    Co-authored-by: Anastasiya <a.tyshkavets@justeattakeaway.com>
    
    * Updated tests to use it.each()
    
    * renamed filterEmitObject to createEmitObject
    
    * Renamed isFilterSelected to isToggleSelected
    
    Co-authored-by: Alix Richards <alix.richards@justeattakeaway.com>
    Co-authored-by: Anastasiya <a.tyshkavets@justeattakeaway.com>

---
+++ packages/components/atoms/f-image-tile/stories/TestContainer.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="$style['c-testContainer']">
+        <image-tile
+            :key="cuisineId"
+            :tile-id="cuisineId"
+            :href="cuisineId"
+            :img-src="cuisineImage"
+            :display-text="cuisineId"
+            :is-selected="selected"
+            @toggle="toggleCuisine" />
+    </div>
+</template>
+
+<script>
+import ImageTile from '../src/components/ImageTile.vue';
+
+export default {
+    name: 'Container',
+    components: {
+        ImageTile
+    },
+    props: {
+        cuisineId: {
+            type: String,
+            default: 'Chicken'
+        },
+        cuisineImage: {
+            type: String,
+            default: 'https://via.placeholder.com/150'
+        }
+    },
+    data () {
+        return {
+            selected: false
+        };
+    },
+    methods: {
+        toggleCuisine () {
+            this.selected = !this.selected;
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+    .c-testContainer {
+        width: 200px;
+        margin: auto;
+        background-color: pink;
+    }
+</style>
