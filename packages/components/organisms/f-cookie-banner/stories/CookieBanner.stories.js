1e20fd5433f3eefebb6c0d1e278df25095c76428
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2021-12-22 16:57:16 +0000

    f-cookie-banner@v4.0.0 - Fix percy styles.

--- packages/components/organisms/f-cookie-banner/stories/CookieBanner.stories.js
+++ packages/components/organisms/f-cookie-banner/stories/CookieBanner.stories.js
@@ -4,6 +4,7 @@ import {
 import { withA11y } from '@storybook/addon-a11y';
 import CookieBanner from '../src/components/CookieBanner.vue';
 import '@justeat/f-button/dist/f-button.css'; // these styles are imported to fix visual regression tests
+import '@justeat/f-link/dist/f-link.css'; // these styles are imported to fix visual regression tests
 
 export default {
     title: 'Components/Organisms',
