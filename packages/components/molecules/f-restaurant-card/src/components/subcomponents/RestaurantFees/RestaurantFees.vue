cdcf233045c72324145177ad9c90fed3e87e279b
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-01-17 15:42:42 +0000

    remove extra semicolon

--- packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantFees/RestaurantFees.vue
+++ packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantFees/RestaurantFees.vue
@@ -55,7 +55,7 @@ export default {
         content: '\2022'; // round bullet character
         color: $color-content-subdued;
         line-height: 1;
-        margin: 0 spacing(x0.5);;
+        margin: 0 spacing(x0.5);
     }
 
     &:last-of-type {
