07b515a2562ade00174885ab9e77c3bc1a2d6c38
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-01-17 13:49:18 +0000

    give restaurant image a default background colour

--- packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantImage/RestaurantImage.vue
+++ packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantImage/RestaurantImage.vue
@@ -23,6 +23,7 @@ export default {
 $img-borderRadius : $radius-rounded-c;
 
 .c-restaurantCard-img {
+  background-color: $color-grey-30;
   background-size: cover;
   background-position: center;
   border-radius: $img-borderRadius;
