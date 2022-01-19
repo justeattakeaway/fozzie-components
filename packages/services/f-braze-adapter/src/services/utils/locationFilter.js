610476440641b7767bb473633b814154d52afa6a
matthew hardern <matthewhardern@gmail.com>
2022-01-19 09:45:20 +0000

    changing to rely on url being present

--- packages/services/f-braze-adapter/src/services/utils/locationFilter.js
+++ packages/services/f-braze-adapter/src/services/utils/locationFilter.js
@@ -4,20 +4,28 @@
  * @returns Array
  */
 const locationFilter = (cards, { location }) => cards.filter(card => {
-    // will be undefined if no locations KVP exists
-    const locationsList = card?.location?.split(',');
-    // return true if NO KVP
-    if (locationsList === undefined) {
-        return true;
-    }
-    // if we have a location attribute but NO current location return false
-    if (!location) {
-        return false;
-    }
-    const locationToMatch = location.toLowerCase().replace(/\s/g, '');
-    const matchedLocations = locationsList.map(l => l.toLowerCase().replace(/\s/g, ''))
+    const needsLocation = card?.url?.includes('$LOCATION');
+    // const needsLatitude = card?.url?.includes('$LAT');
+    // const needsLongitude = card?.url?.includes('$LON');
+
+    if (needsLocation) {
+        // will be undefined if no locations KVP exists
+        const locationsList = card?.location?.split(',');
+        // return true if NO KVP
+        if (locationsList === undefined) {
+            return true;
+        }
+        // if we have a location attribute but NO current location return false
+        if (!location) {
+            return false;
+        }
+        const locationToMatch = location.toLowerCase().replace(/\s/g, '');
+        const matchedLocations = locationsList.map(l => l.toLowerCase().replace(/\s/g, ''))
             .filter(l => locationToMatch === l).length;
-    return matchedLocations > 0;
+        return matchedLocations > 0;
+    }
+
+    return true;
 });
 
 export default locationFilter;
