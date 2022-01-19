b963e980923badaa7d926c37a949beddee562524
Faye Carter <faye.carter@justeattakeaway.com>
2022-01-19 15:50:14 +0000

    form field check

--- packages/components/organisms/f-form/src/components/Form.vue
+++ packages/components/organisms/f-form/src/components/Form.vue
@@ -105,7 +105,7 @@ export default {
         },
 
         formFieldData () {
-            if (this.formData.formFields?.length) {
+            if (Array.isArray(this.formData.formFields)) {
                 return this.formData.formFields.map(field => this.createField(field));
             }
 
