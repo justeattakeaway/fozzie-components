3771a7a203c2fb3d22d7067c8f516d829b5de5b1
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-01-27 14:00:15 +0000

    render nothing if no slot available in slot wrapper

--- packages/components/molecules/f-restaurant-card/src/components/RenderlessSlotWrapper.js
+++ packages/components/molecules/f-restaurant-card/src/components/RenderlessSlotWrapper.js
@@ -2,6 +2,8 @@
 //  This is so that it can be used as part of 'conditional' dynamic components
 export default {
     render () {
-        return this.$slots.default;
+        return this.$slots.default
+            ? this.$slots.default
+            : null;
     }
 };
