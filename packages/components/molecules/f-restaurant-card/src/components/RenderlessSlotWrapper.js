e59a94f721a05bf66f7e1445937d4076b80a8082
dawid-charzewski <dawid.charzewski@justeattakeaway.com>
2022-01-27 14:23:08 +0000

    Update Renderless component

--- packages/components/molecules/f-restaurant-card/src/components/RenderlessSlotWrapper.js
+++ packages/components/molecules/f-restaurant-card/src/components/RenderlessSlotWrapper.js
@@ -1,7 +1,10 @@
 //  This renderless component is designed to simply render it's default slot.
 //  This is so that it can be used as part of 'conditional' dynamic components
 export default {
-    render () {
-        return this.$slots.default;
+    name: 'Renderless',
+    functional: true,
+    render (h, { slots }) {
+        const { default: defaultSlot } = slots();
+        return defaultSlot || h(false);
     }
 };
