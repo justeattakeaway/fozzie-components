cc97ed978620511dd68706dcb60ca75564ca183c
matthew hardern <matthewhardern@gmail.com>
2022-01-31 14:07:24 +0000

    changes to the filter function

--- packages/services/f-braze-adapter/src/services/utils/locationFilter.js
+++ packages/services/f-braze-adapter/src/services/utils/locationFilter.js
@@ -10,37 +10,53 @@ const hasValidLocation = (lat, lon) => {
 };
 
 /**
+ * match location against ones in list
+ * @param location
+ * @param locationsList
+ * @returns {boolean}
+ */
+const matchLocation = (location, locationsList) => {
+    const locationToMatch = location.toLowerCase().replace(/\s/g, '');
+    const matchedLocations = locationsList.map(l => l.toLowerCase().replace(/\s/g, ''))
+        .filter(l => locationToMatch === l).length;
+    return matchedLocations > 0;
+};
+
+/**
  * @param cards
  * @param currentLocation
  * @returns Array
  */
 const locationFilter = (cards, { location, latitude, longitude }) => cards.filter(card => {
-    const needsLocation = card?.url?.includes('$LOCATION');
-    const needsLatitude = card?.url?.includes('$LAT');
-    const needsLongitude = card?.url?.includes('$LON');
+    const needsLocation = card?.url?.includes('$LOCATION$');
+    const needsLatitude = card?.url?.includes('$LAT$');
+    const needsLongitude = card?.url?.includes('$LON$');
 
-    if (needsLocation) {
-        // will be undefined if no locations KVP exists
-        const locationsList = card?.location?.split(',');
-        // return true if NO KVP
-        if (locationsList === undefined) {
-            return true;
-        }
-        // if we have a location attribute but NO current location return false
+    // will be undefined if no locations KVP exists
+    const locationsList = card?.location?.split(',');
+    // return true if NO KVP
+    if (locationsList === undefined) {
+        return !(needsLongitude || needsLatitude || needsLocation);
+    }
+
+    if (needsLongitude || needsLatitude || needsLocation) {
+        // if we have a location attribute and or one of the placeholders but
+        // NO current location return false
         if (!location) {
             return false;
         }
         // if we don't have a valid lat long throw it out
-        if ((needsLongitude && needsLatitude) && !hasValidLocation(latitude, longitude)) {
+        if (!hasValidLocation(latitude, longitude)) {
             return false;
         }
-        const locationToMatch = location.toLowerCase().replace(/\s/g, '');
-        const matchedLocations = locationsList.map(l => l.toLowerCase().replace(/\s/g, ''))
-            .filter(l => locationToMatch === l).length;
-        return matchedLocations > 0;
     }
 
-    return true;
+    if (!location) {
+        return true;
+    }
+
+    // match the location and return based on this
+    return matchLocation(location, locationsList);
 });
 
 export default locationFilter;
