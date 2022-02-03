a2939ed62a47793249ac90e659a71d03add6a5fd
Jamie Maguire <jamieolivermaguire@googlemail.com>
2022-02-03 12:31:13 +0000

    f-restaurant-card@0.16.1 - use new spacing values #1645

--- packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantAvailability/RestaurantAvailability.vue
+++ packages/components/molecules/f-restaurant-card/src/components/subcomponents/RestaurantAvailability/RestaurantAvailability.vue
@@ -96,7 +96,7 @@ export default {
         content: '\2022'; // round bullet character
         color: $color-content-subdued;
         line-height: 1;
-        margin: 0 spacing(x0.5);
+        margin: 0 spacing(a);
     }
 }
 </style>
