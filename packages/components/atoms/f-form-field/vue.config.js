0ef2840b089c7ad85a5c3169dc3bdf7b76176cfa
Mariya Radchuk <maryia.radchuk@justeattakeaway.com>
2022-02-07 09:24:50 +0000

    f-form-field@4.7.0 - postcss inline svg (#1634)
    * f-form-field - postcss inline svg
    
    * f-form-field - removed duplicated styles

--- packages/components/atoms/f-form-field/vue.config.js
+++ packages/components/atoms/f-form-field/vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const rootDir = path.join(__dirname, '..', '..');
+const assetsPlugin = require('postcss-assets');
 const sassOptions = require('../../../../config/sassOptions')(rootDir);
 
 // vue.config.js
@@ -17,6 +18,17 @@ module.exports = {
                 additionalData: `@import "../assets/scss/common.scss";`
             });
     },
+
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    assetsPlugin
+                ]
+            }
+        }
+    },
+
     pluginOptions: {
         lintStyleOnBuild: true
     }
