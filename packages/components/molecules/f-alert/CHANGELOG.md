ffe9969c28d92b174add457630859227c52a4c9f
Lizzie Turney <lizzie.turney@just-eat.com>
2022-02-08 08:57:05 +0000

    added ignorepath to jest config

--- packages/components/molecules/f-alert/CHANGELOG.md
+++ packages/components/molecules/f-alert/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+Latest – to be added to the next release
+------------------------------
+*February 8, 2022*
+
+### Added
+- Visual regression tests to component
+
+
 v4.2.0
 ------------------------------
 *February 2, 2022*
---
+++ packages/components/molecules/f-alert/test/visual/f-alert.visual.spec.js
@@ -0,0 +1,32 @@
+import forEach from 'mocha-each';
+
+const Alert = require('../../test-utils/component-objects/f-alert.component');
+
+const alert = new Alert();
+
+forEach(['success', 'warning', 'info', 'danger'])
+.describe('f-alert - %s - visual tests', type => {
+    beforeEach(() => {
+        // Arrange
+        alert.path = `&args=type:${type};`;
+    });
+
+    it('should display the f-alert (%s) component as dismissible', () => {
+        // Act
+        alert.load();
+
+        // Assert
+        browser.percyScreenshot(`f-alert - ${type} - dismissible`, 'desktop');
+    });
+
+    it('should display the f-alert (%s) component as undismissible', () => {
+        // Arrange
+        alert.path += 'isDismissible:false';
+
+        // Act
+        alert.load();
+
+        // Assert
+        browser.percyScreenshot(`f-alert - ${type} - dismissible`, 'desktop');
+    });
+});
