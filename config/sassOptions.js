b004ec248ecb1e450a6b75bcfba22b504df94989
ashleynolan <nolly00@gmail.com>
2022-02-10 15:27:15 +0000

    Testing fozzie without eyeglass dependency

--- config/sassOptions.js
+++ config/sassOptions.js
@@ -1,27 +1,34 @@
-const magicImporter = require('node-sass-magic-importer');
-const eyeglass = require('eyeglass');
+// const magicImporter = require('node-sass-magic-importer');
+// const eyeglass = require('eyeglass');
 
-module.exports = function init (rootDir) {
-    const sassOptions = eyeglass({
-        eyeglass: {
-            root: rootDir
-        },
-        includePaths: ['node_modules/']
-    });
+module.exports = function init () {
+// module.exports = function init (rootDir) {
+//     const sassOptions = eyeglass({
+//         eyeglass: {
+//             root: rootDir
+//         },
+//         includePaths: ['node_modules/']
+//     });
 
-    sassOptions.importer = [
-        magicImporter({
-            cwd: rootDir
-        }),
-        sassOptions.importer
-    ];
+    //     sassOptions.importer = [
+    //         magicImporter({
+    //             cwd: rootDir
+    //         }),
+    //         sassOptions.importer
+    //     ];
 
+    //     return {
+    //         sassOptions: {
+    //             eyeglass: sassOptions.eyeglass,
+    //             functions: sassOptions.functions,
+    //             includePaths: sassOptions.includePaths,
+    //             importer: sassOptions.importer
+    //         },
+    //         sourceMap: true
+    //     };
     return {
         sassOptions: {
-            eyeglass: sassOptions.eyeglass,
-            functions: sassOptions.functions,
-            includePaths: sassOptions.includePaths,
-            importer: sassOptions.importer
+            includePaths: ['node_modules/']
         },
         sourceMap: true
     };
