3a3b58d386e592355459d478e2cd250bcc4e7152
Matthew Hardern <matthewhardern@gmail.com>
2022-02-15 10:09:08 +0000

    f-braze-adapter@v4.2.0  - url location substitution (#1657)
    * first cut of url substitution
    
    * adding in the filter to the filters list
    
    * changelog update and version number increase
    
    * adding missing dollar sign
    
    * change to test names
    
    * removing no longer require url in tests

---
+++ packages/services/f-braze-adapter/src/services/utils/urlLocationSubstitution.js
@@ -0,0 +1,14 @@
+/**
+ * Replaces any found location placeholders with users current location data
+ * @param cards
+ * @param location
+ * @param longitude
+ * @param latitude
+ * @returns {*}
+ */
+const urlLocationSubstitution = (cards, { location, longitude, latitude }) => cards.map(card => ({
+    ...card,
+    url: card?.url?.replace('$LOCATION$', location).replace('$LAT$', latitude).replace('$LON$', longitude)
+}));
+
+export default urlLocationSubstitution;
