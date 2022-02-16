88e5654da3f6180c94858dd8dac4f28d3aca7642
Billy Oliver <billy.oliver@gmail.com>
2022-02-16 14:33:55 +0000

    f-contact-preferences@v0.17.0 - Included Accept-Language header for consumerApi calls (#1676)
    * ### Added - Included Accept-Language header for consumerApi calls
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/pages/f-contact-preferences/CHANGELOG.md
+++ packages/components/pages/f-contact-preferences/CHANGELOG.md
@@ -4,9 +4,12 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
-Latest – to be added to the next release
+v0.17.0
 ------------------------------
-*February 14, 2022*
+*February 16, 2022*
+
+### Added
+- Included 'Accept-Language' header for all consumerApi calls
 
 ### Updated
 - Converted Storybook Knobs to Controls
--- packages/components/pages/f-contact-preferences/src/constants.js
+++ packages/components/pages/f-contact-preferences/src/constants.js
@@ -5,7 +5,11 @@ export const EVENT_SPINNER_STOP_LOADING = 'stop-spinner';
 
 export const CONSUMER_PREFERENCES_URL = 'consumer/preferences';
 
-export const CONVERSATION_ID_NAME = 'x-je-conversation';
+export const CONVERSATION_ID_COOKIE_NAME = 'x-je-conversation';
+
+export const AUTHORISATION_HEADER_NAME = 'Authorization';
+export const ACCEPT_LANGUAGE_HEADER_NAME = 'Accept-Language';
+export const CONVERSATION_ID_HEADER_NAME = 'x-je-conversation';
 
 export const NON_EDITABLE_PREFERENCE_KEYS = ['orderstatus'];
 export const NON_VISIBLE_PREFERENCE_KEYS = ['reviewmeal'];
