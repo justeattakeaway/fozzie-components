73701b94fedad7c80e1a729ed2acabae1d2f2d56
Lizzie Turney <lizzie.turney@just-eat.com>
2022-02-17 14:59:51 +0000

    updated icons

--- packages/tools/f-vue-icons/CHANGELOG.md
+++ packages/tools/f-vue-icons/CHANGELOG.md
@@ -3,6 +3,15 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html)
 
+v3.7.0
+------------------------------
+*February 17, 2022*
+
+### Added
+- info icon (old name, new icon).
+- success icon with fill removed
+
+
 v3.6.0
 ------------------------------
 *February 01, 2022*
--- packages/tools/f-vue-icons/src/components/SuccessIcon.js
+++ packages/tools/f-vue-icons/src/components/SuccessIcon.js
@@ -10,6 +10,6 @@ export default {
         const attrs = ctx.data.attrs || {};
         ctx.data.attrs = attrs;
 
-        return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="c-ficon c-ficon--success" {...ctx.data}><g fill="none" fill-rule="evenodd" transform="translate(-2 -2)"><path fill="#017A39" d="M24.446 8.781l-11.27 11.27a1.24 1.24 0 01-1.755 0l-5.057-5.056a1.24 1.24 0 011.756-1.75l4.178 4.178L23.079 6.641a11.814 11.814 0 101.366 2.148l.001-.008z"></path></g></svg>;
+        return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="c-ficon c-ficon--success" {...ctx.data}><g fill-rule="evenodd" transform="translate(-2 -2)"><path d="M24.446 8.781l-11.27 11.27a1.24 1.24 0 01-1.755 0l-5.057-5.056a1.24 1.24 0 011.756-1.75l4.178 4.178L23.079 6.641a11.814 11.814 0 101.366 2.148l.001-.008z"></path></g></svg>;
     }
 };
