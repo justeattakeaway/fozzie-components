92a2e64e840eb4c0880c157f082bd67ca1b93cdf
Lizzie Turney <lizzie.turney@just-eat.com>
2022-02-21 14:39:40 +0000

    updated footer component

--- packages/components/organisms/f-footer/test/visual/f-footer.visual.desktop.spec.js
+++ packages/components/organisms/f-footer/test/visual/f-footer.visual.desktop.spec.js
@@ -12,44 +12,48 @@ describe('f-footer - Desktop Visual Tests', () => {
     forEach(['en-GB', 'en-AU', 'en-IE', 'en-NZ', 'es-ES', 'it-IT'])
         .it('should display the base footer for tenant: "%s"', tenant => {
             // Arrange
-            footer.withQuery('&knob-Locale', tenant);
-            footer.withQuery('&knob-Show country selector', 'false');
-            footer.withQuery('&knob-Show courier links', 'false');
+            footer.withQuery('args', `locale:${tenant}`);
 
+            // Act
             footer.load();
+
+            // Assert
             browser.percyScreenshot(`f-footer - Base - ${tenant}`, 'desktop');
         });
 
     forEach(['en-GB', 'en-AU', 'en-IE', 'en-NZ', 'es-ES', 'it-IT'])
         .it('should display the footer with country selector for tenant: "%s"', tenant => {
             // Arrange
-            footer.withQuery('&knob-Locale', tenant);
-            footer.withQuery('&knob-Show country selector', 'true');
-            footer.withQuery('&knob-Show courier links', 'false');
+            footer.withQuery('args', `locale:${tenant};showCountrySelector:true`);
 
+            // Act
             footer.load();
+
+            // Assert
             browser.percyScreenshot(`f-footer - Country Selector - ${tenant}`, 'desktop');
         });
 
     forEach(['en-AU', 'en-NZ'])
         .it('should display the footer with courier links for tenant: "%s"', tenant => {
             // Arrange
-            footer.withQuery('&knob-Locale', tenant);
-            footer.withQuery('&knob-Show country selector', 'false');
-            footer.withQuery('&knob-Show courier links', 'true');
+            footer.withQuery('args', `locale:${tenant};showCourierLinks:true`);
 
+            // Act
             footer.load();
+
+            // Assert
             browser.percyScreenshot(`f-footer - Courier Links - ${tenant}`, 'desktop');
         });
 
     forEach(['en-AU', 'en-NZ'])
         .it('should display the footer with courier links and country selector for tenant: "%s"', tenant => {
             // Arrange
-            footer.withQuery('&knob-Locale', tenant);
-            footer.withQuery('&knob-Show country selector', 'true');
-            footer.withQuery('&knob-Show courier links', 'true');
+            footer.withQuery('args', `locale:${tenant};showCountrySelector:true;showCourierLinks:true`);
 
+            // Act
             footer.load();
+
+            // Assert
             browser.percyScreenshot(`f-footer - Courier Links and Country Selector - ${tenant}`, 'desktop');
         });
 });
