a6e8dceb99ad11759523a3631ab8341ccfe2ba96
Mariya Radchuk <maryia.radchuk@justeattakeaway.com>
2022-02-23 14:41:01 +0000

    f-takeawaypay-activation@2.10.0 - changed logger to new log (#1699)

--- packages/components/pages/f-takeawaypay-activation/src/components/ActivationLoggedIn.vue
+++ packages/components/pages/f-takeawaypay-activation/src/components/ActivationLoggedIn.vue
@@ -116,7 +116,7 @@ export default {
     methods: {
         async activate () {
             this.activationInProgress = true;
-            const activationSuccessful = await TakeawaypayActivationServiceApi.activate(this.activateUrl, this.employeeId, this.authToken, this.consumerId, this.$store, this.$logger);
+            const activationSuccessful = await TakeawaypayActivationServiceApi.activate(this.activateUrl, this.employeeId, this.authToken, this.consumerId, this.$log);
             this.$emit('activation-result', activationSuccessful);
             this.activationInProgress = false;
         }
