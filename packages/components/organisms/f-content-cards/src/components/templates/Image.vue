c1861a225a852d8bae214d17c55365821f52e974
matthew hardern <matthewhardern@gmail.com>
2022-03-15 10:32:10 +0000

    Adding Image component to start the new Content cards sub components that will begin to make up the body of a card component

---
+++ packages/components/organisms/f-content-cards/src/components/templates/Image.vue
@@ -0,0 +1,57 @@
+<template>
+    <div :class="$style['c-content-cardImage--wrapper']">
+        <img
+            data-test-id="content-card-background-image"
+            :class="$style['c-content-cardImage']"
+            :src="backgroundImageUrl"
+            alt="hey">
+        <img
+            data-test-id="content-card-restaurant-logo"
+            :class="$style['c-content-cardLogo']"
+            :src="restaurantLogoUrl"
+            alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContentCardImage',
+    props: {
+        backgroundImageUrl: {
+            type: String,
+            required: true
+        },
+        restaurantLogoUrl: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+$image-borderRadius: $radius-rounded-c;
+
+.c-content-cardImage--wrapper {
+    height: 160px;
+    overflow: hidden;
+    position: relative;
+}
+
+.c-content-cardLogo {
+    position: absolute;
+    left: spacing(d);
+    top: spacing(d);
+    border-radius: $image-borderRadius;
+    width: 48px;
+}
+
+.c-content-cardImage {
+    width:100%;
+    border-radius: $image-borderRadius;
+
+    @include media('<=narrow') {
+        border-radius: 0;
+    }
+}
+</style>
