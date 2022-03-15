3f7db148dacdede6f188842d0367ce1f94da3184
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2022-03-15 13:14:13 +0000

    f-navigation-links@v1.1.0 - Added missing AAA.

--- packages/components/molecules/f-navigation-links/src/components/_tests/NavigationLinks.test.js
+++ packages/components/molecules/f-navigation-links/src/components/_tests/NavigationLinks.test.js
@@ -50,25 +50,34 @@ const propsData = {
 
 describe('NavigationLinks', () => {
     it('should be defined', () => {
+        // Arrange & Act
         const wrapper = shallowMount(NavigationLinks, { propsData });
 
+        // Assert
         expect(wrapper.exists()).toBe(true);
     });
 
     it('should not fail if no `links` prop supplied', () => {
+        // Arrange & Act
         const wrapper = shallowMount(NavigationLinks);
 
+        // Assert
         expect(wrapper.html).toMatchSnapshot();
     });
 
     it('should display the list elements correctly for each `link` supplied', () => {
+        // Arrange
         const wrapper = shallowMount(NavigationLinks, { propsData });
 
+        // Act
         const links = wrapper.find('[data-test-id="navigationLinks"]');
+
+        // Assert
         expect(links).toMatchSnapshot();
     });
 
     it('should pass to attribute to `<v-link>` when present', () => {
+        // Arrange
         const expected = '/account/info';
         const wrapper = shallowMount(NavigationLinks, {
             propsData: {
@@ -81,12 +90,15 @@ describe('NavigationLinks', () => {
             }
         });
 
+        // Act
         const link = wrapper.find('[data-test-id="link1"]');
 
+        // Assert
         expect(link.attributes('to')).toEqual(expected);
     });
 
     it('should pass href attribute to `<v-link>` when present', () => {
+        // Arrange
         const expected = '/account/info';
         const wrapper = shallowMount(NavigationLinks, {
             propsData: {
@@ -99,12 +111,15 @@ describe('NavigationLinks', () => {
             }
         });
 
+        // Act
         const link = wrapper.find('[data-test-id="link1"]');
 
+        // Assert
         expect(link.attributes('href')).toEqual(expected);
     });
 
     it('should pass href attribute to `<v-link>` when both `to` and `href` present in links data', () => {
+        // Arrange
         const expected = '/account/info';
         const wrapper = shallowMount(NavigationLinks, {
             propsData: {
@@ -118,13 +133,16 @@ describe('NavigationLinks', () => {
             }
         });
 
+        // Act
         const link = wrapper.find('[data-test-id="link1"]');
 
+        // Assert
         expect(link.attributes('href')).toEqual(expected);
         expect(link.attributes('to')).toBeUndefined();
     });
 
     it('should skip link item if both `to` and `href` missing in props.links object', () => {
+        // Arrange
         const wrapper = shallowMount(NavigationLinks, {
             propsData: {
                 links: [
@@ -140,24 +158,30 @@ describe('NavigationLinks', () => {
             }
         });
 
+        // Act
         const links = wrapper.find('[data-test-id="navigationLinks"]');
         const link = wrapper.find('[data-test-id="link1"]');
 
+        // Assert
         expect(links.findAll('li').length).toEqual(1);
         expect(link.exists()).toBeFalsy();
     });
 
     it('should contain `aria-label` with content `navigation`', () => {
+        // Arrange
         const wrapper = shallowMount(NavigationLinks);
 
+        // Act
         const links = wrapper.find('[data-test-id="navigationLinks"]');
 
+        // Assert
         expect(links.attributes('aria-label')).toBe('navigation');
     });
 
     describe('computed ::', () => {
         describe('filterLinks ::', () => {
             it('should filter props.links if link does not have `to` or `href` property', () => {
+                // Arrange
                 const wrapper = shallowMount(NavigationLinks, {
                     propsData: {
                         links: [
@@ -178,7 +202,10 @@ describe('NavigationLinks', () => {
                     }
                 });
 
+                // Act
                 const result = wrapper.vm.filterLinks;
+
+                // Assert
                 expect(result.length).toEqual(2);
                 expect(result.find(x => x.id === 'link1')).toBeUndefined();
             });
