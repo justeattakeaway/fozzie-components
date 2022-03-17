9484c1a7d456ed1ce7379fea952e201cc04b36a3
Billy Oliver <billy.oliver@gmail.com>
2022-03-17 11:22:20 +0000

    f-account-info@v0.20.0 - Added success & failed alerts to the Save button (#1753)
    * f-account-info - Added success & failed alerts attached to the Save button
    
    * f-account-info - Styling changes
    
    * Review tweak
    
    * Changed the load error title to meet figma design
    
    * Review tweak
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Kevin Rodrigues <kevin.rodrigues@just-eat.com>

--- packages/components/pages/f-account-info/src/components/AccountInfo.vue
+++ packages/components/pages/f-account-info/src/components/AccountInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <f-card
-            v-if="!shouldShowErrorPage"
+            v-if="!shouldShowLoadErrorCard"
             :card-heading="$t('accountDetails')"
             data-test-id="account-info"
             has-inner-spacing-large
@@ -170,6 +170,11 @@
                     </template>
                 </form-field>
 
+                <notifications
+                    :error-message-key="error.messageKey"
+                    :show-save-error-alert="shouldShowSaveErrorAlert"
+                    :show-successful-alert="shouldShowSuccessfulAlert" />
+
                 <f-button
                     :class="[$style['c-accountInfo-submitButton']]"
                     data-test-id="account-info-save-changes-button"
@@ -199,12 +204,8 @@
         <f-card-with-content
             v-else
             data-test-id="account-info-error-card"
-            :card-heading="$t('errorMessages.errorHeading')"
-            :card-description="$t(error.messageKey)"
-            has-inner-spacing-large
-            card-size-custom="medium"
-            has-outline
-            :class="[$style['c-accountInfo-errorCard']]">
+            :card-heading="$t('errorMessages.loading.heading')"
+            :card-description="$t(error.messageKey)">
             <template #icon>
                 <bag-sad-bg-icon />
             </template>
@@ -231,14 +232,16 @@ import {
 import EmailAddressField from './EmailAddressField.vue';
 import DeleteAccount from './DeleteAccount.vue';
 import AccountInfoValidationMixin from './AccountInfoValidationMixin.vue';
+import Notifications from './Notifications.vue';
 import tenantConfigs from '../tenants';
 import ConsumerApi from '../services/providers/Consumer.api';
 import fAccountInfoModule from '../store/accountInfo.module';
-
 import {
     EVENT_SPINNER_STOP_LOADING
 } from '../constants';
-import { AccountInfoError } from '../exceptions';
+import InfoPageError from '../exceptions/InfoPageError';
+
+const standardLogTags = ['account-pages', 'account-info'];
 
 export default {
     components: {
@@ -249,6 +252,7 @@ export default {
         FErrorMessage,
         EmailAddressField,
         DeleteAccount,
+        Notifications,
         BagSadBgIcon
     },
 
@@ -283,7 +287,9 @@ export default {
             isFormSubmitting: false,
             hasFormUpdate: false,
             hasAddressBeenUpdated: false,
-            shouldShowErrorPage: false,
+            shouldShowLoadErrorCard: false,
+            shouldShowSaveErrorAlert: false,
+            shouldShowSuccessfulAlert: false,
             error: {}
         };
     },
@@ -318,13 +324,32 @@ export default {
             'saveConsumerDetails'
         ]),
 
+        setFormUpdateState (isDirty, isAddressField) {
+            this.hasFormUpdate = isDirty;
+            this.shouldShowSaveErrorAlert = false;
+            this.shouldShowSuccessfulAlert = !isDirty;
+
+            if (isAddressField) {
+                this.hasAddressBeenUpdated = true;
+            }
+        },
+
         /**
         * Informs the template that we are in an Error State.
-        * @param {object} Error - The error that has recently occurred
+        * @param {object} error - The error that has recently occurred
         */
-        handleErrorState (error) {
-            this.shouldShowErrorPage = true;
-            this.error = error;
+        handleLoadErrorState (error) {
+            this.shouldShowLoadErrorCard = true;
+            this.error = new InfoPageError(error?.message, error?.response?.status, 'errorMessages.loading.description');
+        },
+
+        /**
+        * Informs the template that we failed to save.
+        * @param {object} error - The error that has recently occurred
+        */
+        handleSaveErrorState (error) {
+            this.shouldShowSaveErrorAlert = true;
+            this.error = new InfoPageError(error?.message, error?.response?.status, 'errorMessages.saving.description');
         },
 
         /**
@@ -339,12 +364,10 @@ export default {
         async initialise () {
             try {
                 await this.loadConsumerDetails({ api: this.consumerApi, authToken: this.authToken });
-                this.$log.info('Consumer details fetched successfully', ['account-pages', 'account-info']);
-                this.hasFormUpdate = false;
-                this.hasAddressBeenUpdated = false;
+                this.$log.info('Consumer details fetched successfully', standardLogTags);
             } catch (error) {
-                this.$log.error('Error fetching consumer details', error, ['account-pages', 'account-info']);
-                this.handleErrorState(new AccountInfoError(error.message, error?.response?.status));
+                this.$log.error('Error fetching consumer details', error, standardLogTags);
+                this.handleLoadErrorState(error);
             } finally {
                 this.$nextTick(() => {
                     this.$parent.$emit(EVENT_SPINNER_STOP_LOADING);
@@ -352,6 +375,16 @@ export default {
             }
         },
 
+        /**
+        * If there are any form changes
+        * then informs the Template that we are submitting the form
+        * then Saves the State (via the api)
+        * then lowers the hasFormUpdate flag as the form data is now currently clean again
+        * then informs the Template that we are now not submitting the form
+        *
+        * If an error occurs then this is logged and the Template is
+        * informed that it is in a state of error.
+        */
         async onFormSubmit () {
             if (this.isFormInvalid()) {
                 this.logValidationFailure();
@@ -366,13 +399,12 @@ export default {
 
             try {
                 await this.saveConsumerDetails({ api: this.consumerApi, authToken: this.authToken });
-                this.$log.info('Consumer details saved successfully', ['account-pages', 'account-info']);
-                this.hasFormUpdate = false;
+                this.$log.info('Consumer details saved successfully', standardLogTags);
                 this.analyticsTrackFormSubmission(this.hasAddressBeenUpdated);
-                this.hasAddressBeenUpdated = false;
+                this.setFormUpdateState(false, false);
             } catch (error) {
-                this.$log.error('Error saving consumer details', error, ['account-pages', 'account-info']);
-                this.handleErrorState(new AccountInfoError(error.message, error?.response?.status));
+                this.$log.error('Error saving consumer details', error, standardLogTags);
+                this.handleSaveErrorState(error);
             } finally {
                 this.setSubmittingState(false);
             }
@@ -394,11 +426,7 @@ export default {
         */
         onEditConsumer (field, value, isAddressField = false) {
             this.editConsumerDetails({ field, value });
-            this.hasFormUpdate = true;
-
-            if (isAddressField) {
-                this.hasAddressBeenUpdated = true;
-            }
+            this.setFormUpdateState(true, isAddressField);
         },
 
         /**
@@ -432,9 +460,5 @@ export default {
 
 .c-accountInfo-changePasswordButton {
     margin-top: spacing(d);
-}
-
-.c-accountInfo-errorCard {
-    margin-left: 0;
 }
 </style>
---
+++ packages/components/pages/f-account-info/src/components/Notifications.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <f-alert
+            v-if="showSaveErrorAlert"
+            data-test-id="account-info-error-alert"
+            :class="[
+                $style['c-accountInfo-alert'],
+                $style['c-accountInfo-inheritWidthAboveNarrow']
+            ]"
+            type="danger"
+            :heading="$t('errorMessages.saving.heading')">
+            {{ $t(errorMessageKey) }}
+        </f-alert>
+
+        <f-alert
+            v-if="showSuccessfulAlert"
+            data-test-id="account-info-success-alert"
+            :class="[
+                $style['c-accountInfo-alert'],
+                $style['c-accountInfo-inheritWidthAboveNarrow']
+            ]"
+            type="success"
+            :heading="$t('successMessages.saving.heading')"
+        />
+    </div>
+</template>
+
+<script>
+import FAlert from '@justeat/f-alert';
+import '@justeat/f-alert/dist/f-alert.css';
+
+export default {
+    name: 'AccountInfoNotifications',
+
+    components: {
+        FAlert
+    },
+
+    props: {
+        errorMessageKey: {
+            type: String,
+            default: ''
+        },
+        showSaveErrorAlert: {
+            type: Boolean
+        },
+        showSuccessfulAlert: {
+            type: Boolean
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+.c-accountInfo-inheritWidthAboveNarrow {
+    width: 100%;
+
+    @include media('>narrow') {
+        width: inherit;
+    }
+}
+
+.c-accountInfo-alert {
+    margin-top: spacing(f);
+}
+</style>
