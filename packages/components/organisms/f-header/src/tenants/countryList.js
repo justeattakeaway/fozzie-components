61999316872d5f7e23cf1e5ce16963fb94d29ce2
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2022-03-22 15:45:11 +0000

    f-header@9.8.0 - Accessibility updates

--- packages/components/organisms/f-header/src/tenants/countryList.js
+++ packages/components/organisms/f-header/src/tenants/countryList.js
@@ -3,6 +3,7 @@ export default [
         key: 'gb',
         flagKey: 'gb',
         dataTestKey: 'gb',
+        lang: 'en',
         localisedName: 'United Kingdom',
         siteUrl: 'https://www.just-eat.co.uk',
         gtm: 'click_country_gb'
@@ -11,6 +12,7 @@ export default [
         key: 'au',
         flagKey: 'au',
         dataTestKey: 'au',
+        lang: 'en',
         localisedName: 'Australia',
         siteUrl: 'https://www.menulog.com.au',
         gtm: 'click_country_au'
@@ -19,6 +21,7 @@ export default [
         key: 'at',
         flagKey: 'at',
         dataTestKey: 'at',
+        lang: 'de-AT',
         localisedName: 'Österreich',
         siteUrl: 'https://www.lieferando.at',
         gtm: 'click_country_at'
@@ -27,6 +30,7 @@ export default [
         key: 'be',
         flagKey: 'be',
         dataTestKey: 'be',
+        lang: 'nl-BE',
         localisedName: 'België',
         siteUrl: 'https://www.takeaway.com/be',
         gtm: 'click_country_be'
@@ -35,6 +39,7 @@ export default [
         key: 'bg',
         flagKey: 'bg',
         dataTestKey: 'bg',
+        lang: 'en',
         localisedName: 'Bulgaria',
         siteUrl: 'https://www.takeaway.com/bg',
         gtm: 'click_country_bg'
@@ -43,6 +48,7 @@ export default [
         key: 'ca_en',
         flagKey: 'ca',
         dataTestKey: 'ca_en',
+        lang: 'en',
         localisedName: 'Canada',
         siteUrl: 'https://www.skipthedishes.com',
         gtm: 'click_country_ca_en'
@@ -51,6 +57,7 @@ export default [
         key: 'ca_fr',
         flagKey: 'ca',
         dataTestKey: 'ca_fr',
+        lang: 'fr-CA',
         localisedName: 'Canada (FR)',
         siteUrl: 'https://www.skipthedishes.com/fr',
         gtm: 'click_country_ca_fr'
@@ -59,6 +66,7 @@ export default [
         key: 'dk',
         flagKey: 'dk',
         dataTestKey: 'dk',
+        lang: 'da',
         localisedName: 'Danmark',
         siteUrl: 'https://www.just-eat.dk',
         gtm: 'click_country_dk'
@@ -67,6 +75,7 @@ export default [
         key: 'fr',
         flagKey: 'fr',
         dataTestKey: 'jet_fr',
+        lang: 'fr',
         localisedName: 'France',
         siteUrl: 'https://www.just-eat.fr',
         gtm: 'click_country_fr'
@@ -75,6 +84,7 @@ export default [
         key: 'de',
         flagKey: 'de',
         dataTestKey: 'de',
+        lang: 'de',
         localisedName: 'Deutschland',
         siteUrl: 'https://www.lieferando.de',
         gtm: 'click_country_de'
@@ -83,6 +93,7 @@ export default [
         key: 'ie',
         flagKey: 'ie',
         dataTestKey: 'ie',
+        lang: 'en',
         localisedName: 'Ireland',
         siteUrl: 'https://www.just-eat.ie',
         gtm: 'click_country_ie'
@@ -91,6 +102,7 @@ export default [
         key: 'il',
         flagKey: 'il',
         dataTestKey: 'il',
+        lang: 'en',
         localisedName: 'Israel',
         siteUrl: 'https://www.10bis.co.il/next',
         gtm: 'click_country_il'
@@ -99,6 +111,7 @@ export default [
         key: 'it',
         flagKey: 'it',
         dataTestKey: 'it',
+        lang: 'it',
         localisedName: 'Italia',
         siteUrl: 'https://www.justeat.it',
         gtm: 'click_country_it'
@@ -107,6 +120,7 @@ export default [
         key: 'lu',
         flagKey: 'lu',
         dataTestKey: 'lu',
+        lang: 'en',
         localisedName: 'Luxembourg',
         siteUrl: 'https://www.takeaway.com/lu',
         gtm: 'click_country_lu'
@@ -115,6 +129,7 @@ export default [
         key: 'nl',
         flagKey: 'nl',
         dataTestKey: 'nl',
+        lang: 'nl',
         localisedName: 'Nederland',
         siteUrl: 'https://www.thuisbezorgd.nl',
         gtm: 'click_country_nl'
@@ -123,6 +138,7 @@ export default [
         key: 'nz',
         flagKey: 'nz',
         dataTestKey: 'nz',
+        lang: 'en',
         localisedName: 'New Zealand',
         siteUrl: 'https://www.menulog.co.nz',
         gtm: 'click_country_nz'
@@ -131,6 +147,7 @@ export default [
         key: 'no',
         flagKey: 'no',
         dataTestKey: 'no',
+        lang: 'no',
         localisedName: 'Norge',
         siteUrl: 'https://www.just-eat.no',
         gtm: 'click_country_no'
@@ -139,6 +156,7 @@ export default [
         key: 'pl',
         flagKey: 'pl',
         dataTestKey: 'pl',
+        lang: 'pl',
         localisedName: 'Polska',
         siteUrl: 'https://www.pyszne.pl',
         gtm: 'click_country_pl'
@@ -147,6 +165,7 @@ export default [
         key: 'pt',
         flagKey: 'pt',
         dataTestKey: 'pt',
+        lang: 'pt',
         localisedName: 'Portugal',
         siteUrl: 'https://www.takeaway.com/pt',
         gtm: 'click_country_pt'
@@ -155,7 +174,8 @@ export default [
         key: 'ro',
         flagKey: 'ro',
         dataTestKey: 'ro',
-        localisedName: 'Romania',
+        lang: 'ro',
+        localisedName: 'România',
         siteUrl: 'https://www.takeaway.com/ro',
         gtm: 'click_country_ro'
     },
@@ -163,6 +183,7 @@ export default [
         key: 'es',
         flagKey: 'es',
         dataTestKey: 'es',
+        lang: 'es',
         localisedName: 'España',
         siteUrl: 'https://www.just-eat.es',
         gtm: 'click_country_es'
@@ -171,6 +192,7 @@ export default [
         key: 'ch_ch',
         flagKey: 'ch',
         dataTestKey: 'ch_ch',
+        lang: 'de-CH',
         localisedName: 'Schweiz',
         siteUrl: 'https://www.eat.ch',
         gtm: 'click_country_ch'
@@ -179,6 +201,7 @@ export default [
         key: 'ch_en',
         flagKey: 'ch',
         dataTestKey: 'ch_en',
+        lang: 'en',
         localisedName: 'Switzerland',
         siteUrl: 'https://www.eat.ch/en',
         gtm: 'click_country_ch_en'
@@ -187,6 +210,7 @@ export default [
         key: 'ch_fr',
         flagKey: 'ch',
         dataTestKey: 'ch_fr',
+        lang: 'fr-CH',
         localisedName: 'Suisse',
         siteUrl: 'https://www.eat.ch/fr',
         gtm: 'click_country_ch_fr'
