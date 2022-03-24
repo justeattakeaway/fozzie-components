45826be25646f9ca589f21c50ad21dfa66b177f5
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2022-03-24 09:14:10 +0000

    Minor: AAA comments

--- packages/components/organisms/f-footer/src/components/_tests/CountrySelector.test.js
+++ packages/components/organisms/f-footer/src/components/_tests/CountrySelector.test.js
@@ -32,41 +32,58 @@ describe('CountrySelector', () => {
     });
 
     it('should be defined', () => {
+        // Assert
         expect(wrapper.exists()).toBe(true);
     });
 
     it('button should exist', () => {
+        // Assert
         expect(button.exists()).toBe(true);
     });
 
     it('list should not be displayed by default', () => {
+        // Assert
         expect(list.isVisible()).toBe(false);
     });
 
     it('list should be displayed when button is clicked', async () => {
+        // Act
         await button.trigger('click'); // wait for DOM to update as a result of click being triggered
 
+        // Assert
         expect(list.isVisible()).toBe(true);
     });
 
     it('list should not be displayed when button is clicked again', () => {
+        // Act
         button.trigger('click');
         button.trigger('click');
+
+        // Assert
         expect(list.isVisible()).toBe(false);
     });
 
     it('list should contain country from props', () => {
+        // Arrange & Act
         const country = wrapper.find(`[data-test-id="countrySelector-country-${wrapper.key}"]`);
+
+        // Assert
         expect(country.text()).toBe(localisedName);
     });
 
     it('should contain correct link', () => {
+        // Arrange & Act
         const link = wrapper.find('[data-test-id="countrySelector-countryLink"]');
+
+        // Assert
         expect(link.attributes('href')).toBe(siteUrl);
     });
 
     it('should contain `lang` attribute', () => {
+        // Arrange & Act
         const text = wrapper.find('[data-test-id="countrySelector-countryLink"]').find('p');
+
+        // Assert
         expect(text.attributes('lang')).toBe(lang);
     });
 });
