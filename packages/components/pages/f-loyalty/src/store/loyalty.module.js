3f02934915a2763b6e2773f7991734ac1596117a
Matthew Hardern <matthewhardern@gmail.com>
2022-04-07 14:19:20 +0100

    f-loyalty@v1.7.2 - jwt error logging and change to Tabs import (#1793)
    * changes to add in some logging in for decoding jwt
    
    * changelog and version number
    
    * removing not required spread
    
    * adding missing package
    
    * adding error emit event to avoid being put on generic error stack
    
    * adding error emit event to avoid being put on generic error stack
    
    * changes to fix tabs, turns out tabs was renamed in v2
    
    * re run
    
    * minor package size bump
    
    * remove useless comment

--- packages/components/pages/f-loyalty/src/store/loyalty.module.js
+++ packages/components/pages/f-loyalty/src/store/loyalty.module.js
@@ -10,10 +10,32 @@ import {
 /**
  * @function resolveGlobalUserID
  * @param  {string} authToken - auth token.
+ * @param logger
+ * @param errorHandler
  */
-const resolveGlobalUserID = authToken => {
-    const tokenData = jwtDecode(authToken);
-    return tokenData?.global_user_id;
+const resolveGlobalUserID = (authToken, logger, errorHandler) => {
+    try {
+        const tokenData = jwtDecode(authToken);
+        return tokenData?.global_user_id;
+    } catch (error) {
+        logger.error(
+            'Error Decoding JWT token',
+            error,
+            'loyalty',
+            {
+                authToken,
+                exception: error.name,
+                exceptionMessage: error.message,
+                exceptionStackTrace: error.stack,
+                traceId: error.traceId || (error.response && error.response.data.traceId),
+                errorCode: error.errorCode
+            }
+        );
+        // return null and emit error to allow component to emit on error event to
+        // prevent error being handled by general catch all error handler
+        errorHandler(error);
+        return null;
+    }
 };
 
 export default {
@@ -47,16 +69,16 @@ export default {
      * @returns {void}
      */
     actions: {
-        [ACTION_SET_GLOBAL_USER_ID]: ({ commit }, authToken) => {
-            commit(ACTION_SET_GLOBAL_USER_ID, resolveGlobalUserID(authToken));
+        [ACTION_SET_GLOBAL_USER_ID]: ({ commit }, { authToken, logger, errorHandler }) => {
+            commit(ACTION_SET_GLOBAL_USER_ID, resolveGlobalUserID(authToken, logger, errorHandler));
         },
 
         [ACTION_INITIALISE_LOYALTY]: ({ commit, dispatch }, {
-            brazeApiKey, authToken
+            brazeApiKey, authToken, logger, errorHandler
         }) => {
             commit(MUTATION_SET_AUTH_TOKEN, authToken);
             commit(MUTATION_SET_BRAZE_KEY, brazeApiKey);
-            dispatch(ACTION_SET_GLOBAL_USER_ID, authToken);
+            dispatch(ACTION_SET_GLOBAL_USER_ID, { authToken, logger, errorHandler });
         }
     },
 
