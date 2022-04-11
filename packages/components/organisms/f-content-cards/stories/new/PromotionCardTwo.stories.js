daa68d2c820e91f399326ba1b0a65f0344e6b660
matthew hardern <matthewhardern@gmail.com>
2022-04-11 13:46:07 +0100

    changes to make controls work

--- packages/components/organisms/f-content-cards/stories/new/PromotionCardTwo.stories.js
+++ packages/components/organisms/f-content-cards/stories/new/PromotionCardTwo.stories.js
@@ -10,32 +10,30 @@ export default {
 
 function cardGenerator (type, title, image, icon, description, cta) {
     return {
-        card: {
-            type,
-            id: 'NWU1NTJjMWU2YThkNjM0ODllYzE3OGI5XyRfY2M9ZDg1MzM1ODktM2IyMC0xZmJkLWYwMzEtMTE5MjNjYjhiMjcyJm12PTVlNTUyYzFlNmE4ZDYzNDg5ZWMxNzhiZCZwaT1jbXA=',
-            viewed: false,
-            title,
-            imageUrl: null,
-            subtitle: description,
-            description: ['Optional small T&C copy so it doesnt take up too much space making the card massive.'],
-            created: null,
-            updated: '2020-02-25T14:21:15.000Z',
-            categories: [],
-            expiresAt: '2020-03-25T23:55:00.000Z',
-            url: 'https://www.just-eat.co.uk/area/s637jj',
-            linkText: 'www.just-eat.co.uk',
-            aspectRatio: 1,
-            order: '1',
-            image,
-            ctaText: cta,
-            icon,
-            pinned: false,
-            dismissible: true,
-            dismissed: false,
-            clicked: false,
-            Ra: null,
-            Rf: null
-        }
+        type,
+        id: 'NWU1NTJjMWU2YThkNjM0ODllYzE3OGI5XyRfY2M9ZDg1MzM1ODktM2IyMC0xZmJkLWYwMzEtMTE5MjNjYjhiMjcyJm12PTVlNTUyYzFlNmE4ZDYzNDg5ZWMxNzhiZCZwaT1jbXA=',
+        viewed: false,
+        title,
+        imageUrl: null,
+        subtitle: description,
+        description: ['Optional small T&C copy so it doesnt take up too much space making the card massive.'],
+        created: null,
+        updated: '2020-02-25T14:21:15.000Z',
+        categories: [],
+        expiresAt: '2020-03-25T23:55:00.000Z',
+        url: 'https://www.just-eat.co.uk/area/s637jj',
+        linkText: 'www.just-eat.co.uk',
+        aspectRatio: 1,
+        order: '1',
+        image,
+        ctaText: cta,
+        icon,
+        pinned: false,
+        dismissible: true,
+        dismissed: false,
+        clicked: false,
+        Ra: null,
+        Rf: null
     };
 }
 
@@ -55,15 +53,17 @@ export const PromotionCardTwoComponent = (args, { argTypes }) => ({
 
     provide,
 
-    data () {
-        return cardGenerator(
-            'Promotion_Card_1',
-            this.title,
-            this.image,
-            this.icon,
-            this.description,
-            this.cta
-        );
+    computed: {
+        card () {
+            return cardGenerator(
+                'Promotion_Card_1',
+                this.title,
+                this.image,
+                this.icon,
+                this.description,
+                this.cta
+            );
+        }
     },
 
     template: '<promotion-card-two :card="card" />'
