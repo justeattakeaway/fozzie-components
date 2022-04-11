923d6bf56f0080c6dc9a5e773f9cff9ec071eaf5
Lizzie Turney <lizzie.turney@just-eat.com>
2022-04-11 09:08:44 +0100

    small css change

--- packages/components/organisms/f-header/src/components/Navigation.vue
+++ packages/components/organisms/f-header/src/components/Navigation.vue
@@ -158,7 +158,7 @@
                         :class="[
                             $style['c-nav-list-text'],
                             $style['c-nav-list-btn'],
-                            { [$style['c-nav-list-btn--hover']]: headerBackgroundTheme === 'white' }
+                            { [$style['c-nav-list-btn--hoverAboveMid']]: headerBackgroundTheme === 'white' }
                         ]"
                         @click.prevent="toggleUserMenu"
                         @keydown.space.prevent="toggleUserMenu">
@@ -702,6 +702,11 @@ export default {
         &:focus {
             border-radius: 0;
         }
+
+        &:hover {
+            background: $color-container-subtle;
+            border-radius: 0;
+        }
     }
 }
 
@@ -711,7 +716,7 @@ export default {
     }
 }
 
-.c-nav-list-btn--hover {
+.c-nav-list-btn--hoverAboveMid {
     &:hover {
         background: $color-container-subtle;
         border-radius: $nav-focus-borderRadius;
