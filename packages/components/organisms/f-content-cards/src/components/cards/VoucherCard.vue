d239475e4625334b2b4476d582500c6bde08fab5
Matthew Hardern <matthewhardern@gmail.com>
2022-04-19 09:13:02 +0100

    f-content-cards@v8.0.0-alpha.6 - Adding in the new Voucher Card (#1801)
    * adding voucher card bits
    
    * adding new voucher card
    
    * storybook update

---
+++ packages/components/organisms/f-content-cards/src/components/cards/VoucherCard.vue
@@ -0,0 +1,53 @@
+<template>
+    <content-card-container
+        #default="{
+            card: {
+                title, subtitle, image, icon, description, code, url
+            }
+        }"
+        :is-clickable="false"
+        :card="card">
+        <content-card-image
+            :restaurant-logo-url="icon"
+            :background-image-url="image" />
+        <content-card-body
+            :title="title"
+            :subtitle="subtitle"
+            :description="description"
+            has-border-footer
+        >
+            <template #footer>
+                <content-card-voucher-code
+                    card-type="voucher-card"
+                    :code="code"
+                    :url="url" />
+            </template>
+        </content-card-body>
+    </content-card-container>
+</template>
+
+<script>
+import ContentCardContainer from '../templates/ContentCardContainer.vue';
+import ContentCardImage from '../templates/ContentCardImage.vue';
+import ContentCardBody from '../templates/ContentCardBody.vue';
+import ContentCardVoucherCode from '../templates/ContentCardVoucherCode.vue';
+
+export default {
+    components: {
+        ContentCardContainer,
+        ContentCardImage,
+        ContentCardBody,
+        ContentCardVoucherCode
+    },
+    props: {
+        card: {
+            type: Object,
+            default: () => ({})
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ packages/components/organisms/f-content-cards/src/components/cards/_tests/VoucherCard.test.js
@@ -0,0 +1,93 @@
+import { mount } from '@vue/test-utils';
+import VoucherCard from '../VoucherCard.vue';
+
+const MOCK_CARD = {
+    image: 'https://example.com/image.jpg',
+    icon: 'https://example.com/logo.jpg',
+    title: '__TEST_TITLE__',
+    subtitle: '__TEST_SUBTITLE__',
+    description: ['__TEST_DESCRIPTION__'],
+    ctaText: '__TEST_CTA_TEXT__',
+    url: 'https://example.com',
+    code: 'SPECIALVOUCHER'
+};
+const MOCK_COPY_LABEL = 'Copy Code';
+const MOCK_COPY_LABEL_COPIED = 'Offer code copied';
+
+describe('VoucherCard.vue', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        // Arrange
+        wrapper = mount(VoucherCard, {
+            provide: {
+                emitCardView: jest.fn(),
+                emitCardClick: jest.fn(),
+                emitVoucherCodeClick: jest.fn(),
+                copy: {
+                    copyCodeLabel: MOCK_COPY_LABEL,
+                    codeCopiedLabel: MOCK_COPY_LABEL_COPIED
+                }
+            },
+            propsData: {
+                card: MOCK_CARD
+            }
+        });
+    });
+
+    it('should display the cards image when card is passed to prop', () => {
+        // Act
+        const image = wrapper.find('[data-test-id="content-card-background-image"]');
+
+        // Assert
+        expect(image.attributes('src')).toEqual(MOCK_CARD.image);
+    });
+
+    it('should display the restaurant logo when card is passed to prop', () => {
+        // Act
+        const logo = wrapper.find('[data-test-id="content-card-restaurant-logo"]');
+
+        // Assert
+        expect(logo.attributes('src')).toEqual(MOCK_CARD.icon);
+    });
+
+    it('should display the title when card is passed to prop', () => {
+        // Act
+        const title = wrapper.find('[data-test-id="content-card-title"]');
+
+        // Assert
+        expect(title.text()).toEqual(MOCK_CARD.title);
+    });
+
+    it('should display the subtitle when card is passed to prop', () => {
+        // Act
+        const subtitle = wrapper.find('[data-test-id="content-card-subtitle"]');
+
+        // Assert
+        expect(subtitle.text()).toEqual(MOCK_CARD.subtitle);
+    });
+
+    it('should display the description when card is passed to prop', () => {
+        // Act
+        const description = wrapper.find('[data-test-id="content-card-description-item"]');
+
+        // Assert
+        expect(description.text()).toEqual(MOCK_CARD.description[0]);
+    });
+
+    it('should display the voucher code when card is passed to prop', () => {
+        // Act
+        const code = wrapper.find('[data-test-id="contentCard-voucher-card-code"]');
+
+        // Assert
+        expect(code.text()).toEqual(MOCK_CARD.code);
+    });
+
+    it('should copy code label when card is passed to the prop', () => {
+        // Act
+        const label = wrapper.find('[data-test-id="contentCard-not-copied-label"]');
+
+        // Assert
+        expect(label.text()).toEqual(MOCK_COPY_LABEL);
+    });
+});
