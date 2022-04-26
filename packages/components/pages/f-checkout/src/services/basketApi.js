34b4e11c79d861f96dfd9b96855d184730ee9d44
Charles Waite <charles.waite@justeattakeaway.com>
2022-04-26 15:53:10 +0100

    Add Axios retry wrapper to recall APIs when failed

--- packages/components/pages/f-checkout/src/services/basketApi.js
+++ packages/components/pages/f-checkout/src/services/basketApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import retryWrapper from '../axios-retry-wrapper';
 
 export default {
     async getBasket (url, tenant, language, timeout) {
+        retryWrapper(axios, { retryAmount: 3 });
         const config = {
             headers: {
                 'Content-Type': 'application/json',
