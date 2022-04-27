4d2cbc832cd509a6bcb08f67c557fb3832a59bb0
Charles Waite <charles.waite@justeattakeaway.com>
2022-04-27 10:44:46 +0100

    Move retry to address geocoding endpoint 2

--- packages/components/pages/f-checkout/src/services/addressGeocodingApi.js
+++ packages/components/pages/f-checkout/src/services/addressGeocodingApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import retryWrapper from '../axios-retry-wrapper';
 
 export default {
-    retryWrapper(axios, { retryAmount: 3 });
     async getGeoLocation (url, postData, timeout, state) {
+        retryWrapper(axios, { retryAmount: 3 });
         const authHeader = state.authToken && `Bearer ${state.authToken}`;
 
         const config = {
