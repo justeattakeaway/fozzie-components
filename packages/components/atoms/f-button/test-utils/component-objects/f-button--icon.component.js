368ffd98d0e240cee981c4e9a691ff721ab044e8
Lizzie Turney <lizzie.turney@just-eat.com>
2022-05-26 15:17:25 +0100

    started removal of functions

--- packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
+++ packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
@@ -7,13 +7,13 @@ module.exports = class IconButton extends Page {
 
     get component () { return $('[data-test-id="action-button-component"]'); }
 
-    async load () {
-        await super.load(this.component);
-    }
+    // async load () {
+    //     await super.load(this.component);
+    // }
 
-    async waitForComponent () {
-        await super.waitForComponent(this.component);
-    }
+    // async waitForComponent () {
+    //     await super.waitForComponent(this.component);
+    // }
 
     async isComponentDisplayed () {
         return this.component.isDisplayed();
--- packages/components/atoms/f-button/test-utils/component-objects/f-button--link.component.js
+++ packages/components/atoms/f-button/test-utils/component-objects/f-button--link.component.js
@@ -7,13 +7,13 @@ module.exports = class LinkButton extends Page {
 
     get component () { return $('[data-test-id="link-button-component"]'); }
 
-    async load () {
-        await super.load(this.component);
-    }
+    // async load () {
+    //     await super.load(this.component);
+    // }
 
-    async waitForComponent () {
-        await super.waitForComponent(this.component);
-    }
+    // async waitForComponent () {
+    //     await super.waitForComponent(this.component);
+    // }
 
     async isComponentDisplayed () {
         return this.component.isDisplayed();
