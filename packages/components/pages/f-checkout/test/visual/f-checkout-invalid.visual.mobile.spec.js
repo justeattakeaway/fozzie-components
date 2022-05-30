c855785e3821ac1fbc7ad8abc510c5529812e911
Charles Waite <charles.waite@justeattakeaway.com>
2022-05-30 09:47:49 +0100

    Add same visual tests for mobile

--- packages/components/pages/f-checkout/test/visual/f-checkout-invalid.visual.mobile.spec.js
+++ packages/components/pages/f-checkout/test/visual/f-checkout-invalid.visual.mobile.spec.js
@@ -32,4 +32,55 @@ describe('f-checkout - Invalid - Mobile Visual Tests', () => {
         // Assert
         browser.percyScreenshot('f-checkout - Delivery - Authenticated - "Get Checkout 403" Error Page', 'mobile');
     });
+
+    it('should display the error page component if the url is invalid', () => {
+        checkout = new Checkout();
+
+        const args = argumentStringBuilder({ serviceType: 'invalid-url' });
+        checkout.withQuery('args', args);
+
+        // Act
+        checkout.loadError();
+        // Assert
+        browser.percyScreenshot('f-checkout - Invalid - Base State', 'mobile');
+    });
+
+    it('should display an error dialog if the basket has invalid products', () => {
+        checkout = new Checkout();
+
+        const args = argumentStringBuilder({ getBasketError: 'invalid-products' });
+        checkout.withQuery('args', args);
+
+        // Act
+        checkout.load();
+
+        // Assert
+        browser.percyScreenshot('f-checkout - Basket error: Invalid products - Base State', 'mobile');
+    });
+
+    it('should display an error dialog if the basket has offline products', () => {
+        checkout = new Checkout();
+
+        const args = argumentStringBuilder({ getBasketError: 'offline-products' });
+        checkout.withQuery('args', args);
+
+        // Act
+        checkout.load();
+
+        // Assert
+        browser.percyScreenshot('f-checkout - Basket error: Offline products - Base State', 'mobile');
+    });
+
+    it('should display an error dialog if age verification is required', () => {
+        checkout = new Checkout();
+
+        const args = argumentStringBuilder({ getBasketError: 'age-restriction' });
+        checkout.withQuery('args', args);
+
+        // Act
+        checkout.load();
+
+        // Assert
+        browser.percyScreenshot('f-checkout - Basket error: Age restriction required - Base State', 'mobile');
+    });
 });
