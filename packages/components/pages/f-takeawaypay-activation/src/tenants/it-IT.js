a725f1e192d1118b59c4f597b3be8f44198b1c04
Oleksii <oleksii.shevchuk@justeattakeaway.com>
2022-06-06 19:09:58 +0300

    f-takeawaypay-activation@v2.13.0 - Added IE/ES/IT localization

---
+++ packages/components/pages/f-takeawaypay-activation/src/tenants/it-IT.js
@@ -0,0 +1,27 @@
+const messages = {
+    locale: 'it-IT',
+
+    messages: {
+        titleActivationAvailable: 'Bentornato, {consumerGivenName}. Attiviamo Just Eat Pay sul tuo account.',
+        titleActivationNotLoggedIn: 'Cosa devi sapere su Just Eat Pay.',
+        titleActivationSuccessful: 'L’operazione è andata a buon fine.',
+        titleActivationFailed: ' Spiacenti, al momento non possiamo attivare Just Eat Pay sul tuo account',
+        descriptionActivationAvailable1: 'Just Eat Pay sarà attivato per',
+        descriptionActivationAvailable2: 'Non sei tu? Scegli un altro account o registrati.',
+        descriptionActivationNotLoggedIn: 'Hai già un account Just Eat?',
+        descriptionActivationFailed: 'Riprova o contatta il tuo amministratore Just Eat Pay per ricevere assistenza.',
+        descriptionActivationSuccessful: 'Just Eat Pay è ora attivo sul tuo account.',
+        activationInProgress: 'Attivazione del tuo account, attendi'
+    },
+    actions: {
+        activateTakeawayPay: 'Attiva Just Eat Pay ',
+        startOrdering: 'Inizia a ordinare',
+        createAccount: 'Crea un nuovo account',
+        loginToAccount1: 'Usa un altro account',
+        loginToAccount2: 'Ho già un account'
+    }
+};
+
+export default {
+    messages
+};
