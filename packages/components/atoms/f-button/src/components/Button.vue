87c1e5e8a2de5f2fb5340e3e3fa158db597a0267
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-06-08 15:30:34 +0100

    fake test change

--- packages/components/atoms/f-button/src/components/Button.vue
+++ packages/components/atoms/f-button/src/components/Button.vue
@@ -16,6 +16,7 @@
         v-bind="$attrs"
         :aria-live="getAriaLive"
         :aria-busy="isLoading"
+        data-test-attr="jamie"
         v-on="!isLoading && $listeners">
         <template v-if="hasNestedContent">
             <span
