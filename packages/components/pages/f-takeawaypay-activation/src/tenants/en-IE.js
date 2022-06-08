0daf0eb8400bf0acff832fbc7d24dbc1b05df662
oleksiishevchukje <oleksii.shevchuk@just-eat.com>
2022-06-08 18:13:12 +0300

    f-takeawaypay-activation@v2.13.0 - Added IE/ES/IT localization (#1910)
    Co-authored-by: Oleksii <oleksii.shevchuk@justeattakeaway.com>

---
+++ packages/components/pages/f-takeawaypay-activation/src/tenants/en-IE.js
@@ -0,0 +1,27 @@
+const messages = {
+    locale: 'en-IE',
+
+    messages: {
+        titleActivationAvailable: 'Welcome back, {consumerGivenName}. Let’s activate Just Eat Pay on your account.',
+        titleActivationNotLoggedIn: 'Let’s get you started with Just Eat Pay.',
+        titleActivationSuccessful: 'All systems go.',
+        titleActivationFailed: ' We can’t activate Just Eat Pay on your account right now, sorry.',
+        descriptionActivationAvailable1: 'Just Eat Pay will be activated for',
+        descriptionActivationAvailable2: 'Not you? Choose a different account or sign up.',
+        descriptionActivationNotLoggedIn: 'Do you already have a Just Eat account?',
+        descriptionActivationFailed: 'Try again or get in touch with your Just Eat Pay administrator for help.',
+        descriptionActivationSuccessful: 'Just Eat Pay is now activated on your account.',
+        activationInProgress: 'Activating your account, please wait'
+    },
+    actions: {
+        activateTakeawayPay: 'Activate Just Eat Pay',
+        startOrdering: 'Start ordering',
+        createAccount: 'Create a new account',
+        loginToAccount1: 'Use a different account',
+        loginToAccount2: 'I have an account'
+    }
+};
+
+export default {
+    messages
+};
---
+++ packages/components/pages/f-takeawaypay-activation/src/tenants/es-ES.js
@@ -0,0 +1,27 @@
+const messages = {
+    locale: 'es-ES',
+
+    messages: {
+        titleActivationAvailable: 'Bienvenido de nuevo, {consumerGivenName}. Vamos a activar Just Eat Pay en tu cuenta.',
+        titleActivationNotLoggedIn: 'Vamos a ayudarte a empezar con Just Eat Pay.',
+        titleActivationSuccessful: 'Todos los sistemas funcionan.',
+        titleActivationFailed: ' Ahora mismo no podemos activar Just Eat Pay en tu cuenta, lo sentimos.',
+        descriptionActivationAvailable1: 'Just Eat Pay se activará para',
+        descriptionActivationAvailable2: '¿No eres tú? Elige otra cuenta o suscríbete.',
+        descriptionActivationNotLoggedIn: '¿Ya tienes una cuenta de Just Eat?',
+        descriptionActivationFailed: 'Vuelve a intentarlo o ponte en contacto con tu administrador de Just Eat Pay para recibir ayuda.',
+        descriptionActivationSuccessful: 'Just Eat Pay está activado ahora en tu cuenta.',
+        activationInProgress: 'Activando su cuenta, por favor espere'
+    },
+    actions: {
+        activateTakeawayPay: 'Activar Just Eat Pay',
+        startOrdering: 'Empezar a realizar pedidos',
+        createAccount: 'Crear cuenta nueva',
+        loginToAccount1: 'Utilizar otra cuenta',
+        loginToAccount2: 'Tengo una cuenta '
+    }
+};
+
+export default {
+    messages
+};
