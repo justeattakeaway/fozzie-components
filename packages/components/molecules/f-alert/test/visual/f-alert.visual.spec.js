1e046f236d911e52e9bafcd15fa8445f1c4bb8de
Billy Oliver <billy.oliver@gmail.com>
2022-06-10 14:28:30 +0100

    f-alert@v4.4.1 - Bumped f-wdio-utils version & fixed up breaking changes (#1914)
    * f-alert - Bumped f-wdio-utils version & fixed up breaking changes
    
    * Added visual tests for both desktop and mobile
    
    * Refactored test loop
    
    * Fixed up incorrect args passing
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/molecules/f-alert/test/visual/f-alert.visual.spec.js
+++ packages/components/molecules/f-alert/test/visual/f-alert.visual.spec.js
@@ -2,32 +2,40 @@ import forEach from 'mocha-each';
 
 const Alert = require('../../test-utils/component-objects/f-alert.component');
 
-const alert = new Alert();
+let alert;
 
-forEach(['success', 'warning', 'info', 'danger'])
-.describe('f-alert - %s - visual tests', type => {
+forEach([
+    ['desktop', 'success'],
+    ['desktop', 'warning'],
+    ['desktop', 'info'],
+    ['desktop', 'danger'],
+    ['mobile', 'success'],
+    ['mobile', 'warning'],
+    ['mobile', 'info'],
+    ['mobile', 'danger']
+])
+.describe('f-alert - %s - %s - visual tests', (device, type) => {
     beforeEach(() => {
         // Arrange
-        alert.path = `&args=type:${type};`;
+        if (device === 'mobile') {
+            browser.setWindowSize(414, 731);
+        }
+        alert = new Alert();
     });
 
     it('should display the f-alert (%s) component as dismissible', async () => {
         // Act
-        await alert.load();
+        await alert.load({ type: `${type}` });
 
         // Assert
-        await browser.percyScreenshot(`f-alert - ${type} - dismissible`, 'desktop');
+        await browser.percyScreenshot(`f-alert - ${type} - dismissible`, device);
     });
 
     it('should display the f-alert (%s) component as undismissible', async () => {
-        // Arrange
-        alert.path += 'isDismissible:false';
-
         // Act
-        await alert.load();
-        await alert.waitForComponent();
+        await alert.load({ isDismissible: false, type: `${type}` });
 
         // Assert
-        await browser.percyScreenshot(`f-alert - ${type} - undismissible`, 'desktop');
+        await browser.percyScreenshot(`f-alert - ${type} - undismissible`, device);
     });
 });
