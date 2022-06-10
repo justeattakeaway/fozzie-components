0a6f7bf924aabc049c9e95d3e79ee7b719229410
Billy Oliver <billy.oliver@gmail.com>
2022-06-10 14:28:30 +0100

    f-alert@v4.4.1 - Bumped f-wdio-utils version & fixed up breaking changes (#1914)
    * f-alert - Bumped f-wdio-utils version & fixed up breaking changes
    
    * Added visual tests for both desktop and mobile
    
    * Refactored test loop
    
    * Fixed up incorrect args passing
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/molecules/f-alert/test-utils/component-objects/f-alert.component.js
+++ packages/components/molecules/f-alert/test-utils/component-objects/f-alert.component.js
@@ -1,25 +1,11 @@
-const Page = require('@justeat/f-wdio-utils/src/page.object');
+const Page = require('@justeat/f-wdio-utils/src/base.page');
 
 module.exports = class Alert extends Page {
     constructor () {
         super('molecule', 'alert-component');
     }
 
-    get component () { return $('[data-test-id="alert-component"]'); }
-
     get exitButton () { return $('[data-test-id="alert-dismiss"]'); }
-
-    async load () {
-        await super.load(this.component);
-    }
-
-    async isComponentDisplayed () {
-        return this.component.isDisplayed();
-    }
-
-    async waitForComponent () {
-        await super.waitForComponent(this.component, 1000);
-    }
 
     async clickExitButton () {
         await this.exitButton.click();
