1cb0be6cf5e54131417a61302228633d0d23f32f
billy.oliver <billy.oliver@just-eat.com>
2022-06-10 15:14:44 +0100

    f-button - Bumped f-wdio-utils version & fixed up breaking changes

--- packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
+++ packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
@@ -1,15 +1,12 @@
-const Page = require('@justeat/f-wdio-utils/src/page.object');
+const Page = require('@justeat/f-wdio-utils/src/base.page');
 
 module.exports = class IconButton extends Page {
     constructor () {
-        super('atom-folder', 'f-button--icon-button-component');
+        super('atom-folder', 'f-button--icon-button-component', '[data-test-id="action-button-component"]');
     }
 
+    // Overridding base implementation
     get component () { return $('[data-test-id="action-button-component"]'); }
-
-    async isComponentDisplayed () {
-        return this.component.isDisplayed();
-    }
 
     async isComponentClickable () {
         return this.component.isClickable();
