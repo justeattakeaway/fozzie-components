4859f21325a3cdb7421e64c04cff806dee6e9ffc
billy.oliver <billy.oliver@just-eat.com>
2022-06-13 19:35:15 +0100

    Refactored the Generator, inc. new refs., remove redundancy and fixed up lint errors

--- packages/tools/generator-component/generators/app/templates/test-utils/component-objects/f-skeleton.component.js
+++ packages/tools/generator-component/generators/app/templates/test-utils/component-objects/f-skeleton.component.js
@@ -2,6 +2,6 @@ const Page = require('@justeat/f-wdio-utils/src/base.page');
 
 module.exports = class <%= name.filename %> extends Page {
     constructor () {
-        super('<%= storybook.componentCategory.toLowerCase().slice(0, -1) %>', 'v-<%= name.default %>-component');
+        super('<%= storybook.componentCategory.toLowerCase().slice(0, -1) %>', '<%= name.default %>-component');
     }
 };
--- packages/tools/generator-component/generators/app/templates/test/accessibility/axe-accessibility.spec.js
+++ packages/tools/generator-component/generators/app/templates/test/accessibility/axe-accessibility.spec.js
@@ -2,19 +2,19 @@ import { getAxeResults } from '../../../../../../test/utils/axe-helper'; // esli
 
 const <%= name.filename %> = require('../../test-utils/component-objects/f-<%= name.default %>.component');
 
-let <%= name.class %>;
+describe('f-<%= name.default %> - Accessibility tests', () => {
+    let <%= name.class %>;
 
-describe('Accessibility tests', () => {
     beforeEach(() => {
         <%= name.class %> = new <%= name.filename %>();
-        <%= name.class %>.load();
     });
 
     it('a11y - should test f-<%= name.default %> component WCAG compliance', () => {
         // Act
-        const axeResults = getAxeResults('f-<%= name.default %>');
+        <%= name.class %>.load();
 
         // Assert
+        const axeResults = getAxeResults('f-<%= name.default %>');
         expect(axeResults.violations.length).toBe(0);
     });
 });
