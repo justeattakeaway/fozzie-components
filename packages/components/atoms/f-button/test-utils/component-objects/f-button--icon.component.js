edfa86d2eab09f41f72eeb4952841f54e6b26668
Billy Oliver <billy.oliver@gmail.com>
2022-06-13 16:46:51 +0100

    Update f-button--icon.component.js

--- packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
+++ packages/components/atoms/f-button/test-utils/component-objects/f-button--icon.component.js
@@ -5,7 +5,7 @@ module.exports = class IconButton extends Page {
         super('atom-folder', 'f-button--icon-button-component');
     }
 
-    // Overridding base implementation
+    // Overriding base implementation
     get component () { return $('[data-test-id="action-button-component"]'); }
 
     async isComponentClickable () {
