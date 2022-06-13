5faf7ef5d63b181a753c25003726c5629f38bb9f
Billy Oliver <billy.oliver@gmail.com>
2022-06-13 18:46:20 +0100

    f-filter-pill - Bumped f-wdio-utils version & fixed up breaking changes (#1934)
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/atoms/f-filter-pill/test-utils/component-objects/f-filter-pill.component.js
+++ packages/components/atoms/f-filter-pill/test-utils/component-objects/f-filter-pill.component.js
@@ -1,14 +1,7 @@
-const Page = require('@justeat/f-wdio-utils/src/page.object');
-const { COMPONENT } = require('./f-filter-pill-selectors');
+const Page = require('@justeat/f-wdio-utils/src/base.page');
 
 module.exports = class FilterPill extends Page {
     constructor () {
         super('atom', 'filter-pill-component');
-    }
-
-    get component () { return $(COMPONENT); }
-
-    async isComponentDisplayed () {
-        return this.component.isDisplayed();
     }
 };
