b33482321c53800f50961e1b89b74c91f38a6d36
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2022-06-13 15:25:23 +0100

    f-account-info@0.24.0 - Add autocomplete attribute to form fields

--- packages/components/pages/f-account-info/CHANGELOG.md
+++ packages/components/pages/f-account-info/CHANGELOG.md
@@ -4,9 +4,17 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v0.24.0
+------------------------------
+*June 13, 2022*
+
+### Added
+- `autocomplete="on"` to form fields.
+
+
 v0.23.0
 ------------------------------
-*May 31 2022*
+*May 31, 2022*
 
 ### Removed
 - Debug code
@@ -17,7 +25,7 @@ v0.23.0
 
 v0.22.0
 ------------------------------
-*May 24 2022*
+*May 24, 2022*
 
 ### Removed
 - Temp logging to record if auth expired or not
--- packages/components/pages/f-account-info/src/components/AccountInfo.vue
+++ packages/components/pages/f-account-info/src/components/AccountInfo.vue
@@ -20,6 +20,7 @@
                 <form-field
                     :value="consumer.firstName"
                     name="account-info-consumer-firstName"
+                    autocomplete="on"
                     maxlength="50"
                     :label-text="$t('consumer.firstNameLabel')"
                     :placeholder="$t('consumer.firstNamePlaceholder')"
@@ -44,6 +45,7 @@
                 <form-field
                     :value="consumer.lastName"
                     name="account-info-consumer-lastName"
+                    autocomplete="on"
                     maxlength="50"
                     :label-text="$t('consumer.lastNameLabel')"
                     :placeholder="$t('consumer.lastNamePlaceholder')"
@@ -68,6 +70,7 @@
                 <form-field
                     :value="consumer.phoneNumber"
                     name="account-info-consumer-phoneNumber"
+                    autocomplete="on"
                     maxlength="16"
                     :label-text="$t('consumer.phoneNumberLabel')"
                     :placeholder="$t('consumer.phoneNumberPlaceholder')"
@@ -97,6 +100,7 @@
                 <form-field
                     :value="consumer.line1"
                     name="account-info-consumer-line1"
+                    autocomplete="on"
                     maxlength="50"
                     :label-text="$t('consumer.addressLabel')"
                     :placeholder="$t('consumer.line1Placeholder')"
@@ -116,6 +120,7 @@
                 <form-field
                     :value="consumer.line2"
                     name="account-info-consumer-line2"
+                    autocomplete="on"
                     maxlength="50"
                     :placeholder="$t('consumer.line2Placeholder')"
                     @input="onEditConsumer('line2', $event, true)" />
@@ -123,6 +128,7 @@
                 <form-field
                     :value="consumer.line3"
                     name="account-info-consumer-line3"
+                    autocomplete="on"
                     maxlength="50"
                     :placeholder="$t('consumer.line3Placeholder')"
                     @input="onEditConsumer('line3', $event, true)" />
@@ -130,6 +136,7 @@
                 <form-field
                     :value="consumer.locality"
                     name="account-info-consumer-locality"
+                    autocomplete="on"
                     maxlength="50"
                     :label-text="$t('consumer.localityLabel')"
                     :placeholder="$t('consumer.localityPlaceholder')"
@@ -149,6 +156,7 @@
                 <form-field
                     :value="consumer.postcode"
                     name="account-info-consumer-postcode"
+                    autocomplete="postal-code"
                     maxlength="50"
                     :label-text="$t('consumer.postcodeLabel')"
                     :placeholder="$t('consumer.postcodePlaceholder')"
