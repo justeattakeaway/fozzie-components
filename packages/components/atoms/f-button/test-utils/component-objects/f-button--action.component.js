771f6cd09954d42ff742df88ba05f7374bd48ab0
Billy Oliver <billy.oliver@gmail.com>
2022-06-14 09:48:48 +0100

    f-button@v3.4.1 - Bumped f-wdio-utils version & fixed up breaking changes (#1930)
    * f-button - Bumped f-wdio-utils version & fixed up breaking changes
    
    * Removed redundant code
    
    * Update packages/components/atoms/f-button/test-utils/component-objects/f-button--action.component.js
    
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>
    
    * Update f-button--icon.component.js
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>

--- packages/components/atoms/f-button/test-utils/component-objects/f-button--action.component.js
+++ packages/components/atoms/f-button/test-utils/component-objects/f-button--action.component.js
@@ -1,15 +1,12 @@
-const Page = require('@justeat/f-wdio-utils/src/page.object');
+const Page = require('@justeat/f-wdio-utils/src/base.page');
 
 module.exports = class ActionButton extends Page {
     constructor () {
         super('atom-folder', 'f-button--button-component');
     }
 
+    // Overriding base implementation
     get component () { return $('[data-test-id="action-button-component"]'); }
-
-    async isComponentDisplayed () {
-        return this.component.isDisplayed();
-    }
 
     async isComponentClickable () {
         return this.component.isClickable();
