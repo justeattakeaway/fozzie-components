b5ef6e33296ce0a3b58dee74633a969456daba44
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2022-06-15 14:39:16 +0100

    f-link@2.4.0 - Update descriptions for screen readers

--- packages/components/atoms/f-link/src/tenants/it-IT.js
+++ packages/components/atoms/f-link/src/tenants/it-IT.js
@@ -1,7 +1,7 @@
 export default {
     ariaDescription: {
-        newLocation: 'Stai aprendo una nuova finestra/schermata/scheda',
-        external: 'Stai aprendo un sito esterno',
-        externalNewLocation: 'Stai aprendo un sito esterno in una nuova finestra/schermata/scheda'
+        newLocation: 'Stai per aprire una nuova pagina',
+        external: 'Stai per accedere ad un sito esterno',
+        externalNewLocation: 'Stai per accedere ad un sito esterno, in una nuova pagina'
     }
 };
