ce585a8fa7d0da48131f442382560abce34e8b10
FayeCarter <faye.carter@just-eat.com>
2022-06-23 14:55:01 +0100

    f-tabs@3.0.0 - Update sass and fozzie (#1988)
    * Update sass and fozzie
    
    * bump storybook
    
    * Update sass and fozzie
    
    * Bump version
    
    * fix version
    
    Co-authored-by: Faye Carter <faye.carter@justeattakeaway.com>

--- packages/storybook/vue.config.js
+++ packages/storybook/vue.config.js
@@ -52,6 +52,7 @@ module.exports = {
                         'f-user-message',
                         'f-mega-modal',
                         'f-restaurant-card',
+                        'f-tabs',
                         'f-navigation-links'
                     ];
                     const pathContainsUpdatedComponentOrType = updateComponentsAndTypes.some(a => absPath.includes(a));
