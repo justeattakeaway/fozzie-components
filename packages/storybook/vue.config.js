b5aa750b4f5a8b2cb7148a519760dd97ca6dfeb0
Faye Carter <faye.carter@justeattakeaway.com>
2022-06-23 16:27:04 +0100

    Update sass and fozzie

--- packages/storybook/vue.config.js
+++ packages/storybook/vue.config.js
@@ -53,7 +53,8 @@ module.exports = {
                         'f-mega-modal',
                         'f-restaurant-card',
                         'f-tabs',
-                        'f-navigation-links'
+                        'f-navigation-links',
+                        'f-status-banner'
                     ];
                     const pathContainsUpdatedComponentOrType = updateComponentsAndTypes.some(a => absPath.includes(a));
 
