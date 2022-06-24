73bf973e292653db3f795caff72389e93608540a
Billy Oliver <billy.oliver@gmail.com>
2022-06-24 12:43:44 +0100

    f-footer - Bumped f-wdio-utils version & fixed up breaking changes (#1979)
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

--- packages/components/organisms/f-footer/CHANGELOG.md
+++ packages/components/organisms/f-footer/CHANGELOG.md
@@ -4,6 +4,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+v7.7.3
+-----------------------------
+*Jun 22, 2022*
+
+### Changed
+- Bumped wdio version and fixed breaking changes.
+
+
 v7.7.2
 ------------------------------
 *Jun 6, 2022*
--- packages/components/organisms/f-footer/test/visual/f-footer.visual.mobile.spec.js
+++ packages/components/organisms/f-footer/test/visual/f-footer.visual.mobile.spec.js
@@ -5,17 +5,15 @@ const Footer = require('../../test-utils/component-objects/f-footer.component');
 let footer;
 
 describe('f-footer - Mobile Visual Tests', () => {
-    beforeEach(async () => {
+    beforeEach(() => {
+        // Arrange
         footer = new Footer();
     });
 
     forEach(['en-GB', 'en-AU', 'en-IE', 'en-NZ', 'es-ES', 'it-IT'])
         .it('should display the base footer for tenant: "%s"', async tenant => {
-            // Arrange
-            await footer.withQuery('args', `locale:${tenant}`);
-
             // Act
-            await footer.load();
+            await footer.load({ locale: tenant });
 
             // Assert
             await browser.percyScreenshot(`f-footer - Base - ${tenant}`, 'mobile');
@@ -23,11 +21,8 @@ describe('f-footer - Mobile Visual Tests', () => {
 
     forEach(['en-GB', 'en-AU', 'en-IE', 'en-NZ', 'es-ES', 'it-IT'])
         .it('should display the footer with country selector for tenant: "%s"', async tenant => {
-            // Arrange
-            await footer.withQuery('args', `locale:${tenant};showCountrySelector:true`);
-
             // Act
-            await footer.load();
+            await footer.load({ locale: tenant, showCountrySelector: true });
 
             // Assert
             await browser.percyScreenshot(`f-footer - Country Selector - ${tenant}`, 'mobile');
@@ -35,11 +30,8 @@ describe('f-footer - Mobile Visual Tests', () => {
 
     forEach(['en-AU', 'en-NZ'])
         .it('should display the footer with courier links for tenant: "%s"', async tenant => {
-            // Arrange
-            await footer.withQuery('args', `locale:${tenant};showCourierLinks:true`);
-
             // Act
-            await footer.load();
+            await footer.load({ locale: tenant, showCourierLinks: true });
 
             // Assert
             await browser.percyScreenshot(`f-footer - Courier Links - ${tenant}`, 'mobile');
@@ -47,11 +39,8 @@ describe('f-footer - Mobile Visual Tests', () => {
 
     forEach(['en-AU', 'en-NZ'])
         .it('should display the footer with courier links and country selector for tenant: "%s"', async tenant => {
-            // Arrange
-            await footer.withQuery('args', `locale:${tenant};showCountrySelector:true;showCourierLinks:true`);
-
             // Act
-            await footer.load();
+            await footer.load({ locale: tenant, showCountrySelector: true, showCourierLinks: true });
 
             // Assert
             await browser.percyScreenshot(`f-footer - Courier Links and Country Selector - ${tenant}`, 'mobile');
