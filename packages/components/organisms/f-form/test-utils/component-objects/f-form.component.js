4b63b8c0b2d186249bc0e34164689d1366dc2244
Ben Siggery <bensiggery28@gmail.com>
2022-06-30 11:18:05 +0100

    Move f-wdio-utils to root package.jsom

--- packages/components/organisms/f-form/test-utils/component-objects/f-form.component.js
+++
@@ -1,7 +0,0 @@
-const Page = require('@justeat/f-wdio-utils/src/base.page');
-
-module.exports = class Form extends Page {
-    constructor () {
-        super('organism', 'v-form-component');
-    }
-};
