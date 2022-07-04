2040f011aeec3a3a998fbc8ee575f7eff62c5c00
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2022-07-04 16:41:21 +0100

    f-wdio-utils@v1.1.0 - Update Axe implementation to support async (#2014)
    * Update Axe implementation to support async
    
    * Fix linting

--- packages/components/atoms/f-button/CHANGELOG.md
+++ packages/components/atoms/f-button/CHANGELOG.md
@@ -3,6 +3,14 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+v4.2.0
+------------------------------
+*July 4, 2022*
+
+### Changed
+- Update f-wdio-utils to v1.1.0 to use new Axe Core implementation.
+- Accessibility tests to be async.
+
 
 v4.1.0
 ------------------------------
--- packages/components/atoms/f-button/test/accessibility/axe-accessibility.spec.js
+++ packages/components/atoms/f-button/test/accessibility/axe-accessibility.spec.js
@@ -2,31 +2,29 @@ import ActionButton from '../../test-utils/component-objects/f-button--action.co
 import LinkButton from '../../test-utils/component-objects/f-button--link.component';
 import IconButton from '../../test-utils/component-objects/f-button--icon.component';
 
-const { getAxeResults } = require('../../../../../../test/utils/axe-helper');
-
 describe('Accessibility tests', () => {
-    it('a11y - should test f-button action component WCAG compliance', () => {
+    it('a11y - should test f-button action component WCAG compliance', async () => {
         // Act
-        ActionButton.load();
-        const axeResults = getAxeResults('f-button - action');
+        await ActionButton.load();
+        const axeResults = await ActionButton.getAxeResults('f-button - action');
 
         // Assert
         expect(axeResults.violations.length).toBe(0);
     });
 
-    it('a11y - should test f-button link component WCAG compliance', () => {
+    it('a11y - should test f-button link component WCAG compliance', async () => {
         // Act
-        LinkButton.load();
-        const axeResults = getAxeResults('f-button - link');
+        await LinkButton.load();
+        const axeResults = await LinkButton.getAxeResults('f-button - link');
 
         // Assert
         expect(axeResults.violations.length).toBe(0);
     });
 
-    it('a11y - should test f-button icon component WCAG compliance', () => {
+    it('a11y - should test f-button icon component WCAG compliance', async () => {
         // Act
-        IconButton.load();
-        const axeResults = getAxeResults('f-button - icon');
+        await IconButton.load();
+        const axeResults = await IconButton.getAxeResults('f-button - icon');
 
         // Assert
         expect(axeResults.violations.length).toBe(0);
