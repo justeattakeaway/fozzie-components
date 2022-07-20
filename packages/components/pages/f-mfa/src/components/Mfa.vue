edbe16158e6d5d8a03ff556901707f056d31f72b
Billy Oliver <billy.oliver@gmail.com>
2022-07-20 16:58:19 +0100

    f-mfa@v0.1.0 - Initial yeoman generated files (#2043)
    * The initial files for the new f-mfa component as created by the yeoman generator
    
    * Fixed up lint issue in test
    
    * Used the term `sut` for the tests
    
    * Updated the documentation
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>

---
+++ packages/components/pages/f-mfa/src/components/Mfa.vue
@@ -0,0 +1,53 @@
+<template>
+    <div
+        :class="$style['c-mfa']"
+        data-test-id="v-mfa-component">
+        {{ copy.text }}
+    </div>
+</template>
+
+<script>
+import { globalisationServices } from '@justeat/f-services';
+import tenantConfigs from '../tenants';
+
+export default {
+    name: 'VMfa',
+
+    components: {},
+
+    props: {
+        locale: {
+            type: String,
+            default: ''
+        },
+        smartGatewayBaseUrl: {
+            type: String,
+            required: true
+        }
+    },
+
+    data () {
+        const locale = globalisationServices.getLocale(tenantConfigs, this.locale, this.$i18n);
+        const localeConfig = tenantConfigs[locale];
+
+        return {
+            copy: { ...localeConfig }
+        };
+    }
+};
+</script>
+
+<style lang="scss" module>
+@use '@justeat/fozzie/src/scss/fozzie' as f;
+
+.c-mfa {
+    display: flex;
+    justify-content: center;
+    min-height: 80vh;
+    width: 80vw;
+    margin: auto;
+    border: 1px solid red;
+    font-family: f.$font-family-base;
+    @include f.font-size(heading-m);
+}
+</style>
