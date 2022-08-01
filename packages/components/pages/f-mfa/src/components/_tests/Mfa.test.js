8686bc1d19e25b72cd8d56aa88704c59992a7d75
Billy Oliver <billy.oliver@gmail.com>
2022-08-01 15:12:36 +0100

    f-mfa@v0.3.0 - Added/Styled MFA page template (#2048)
    * Added the new mfa template and applied the required css as per the figma design + Add in GB translations + fixed-up/prepared unit tests
    
    * Extended wait time from 10 to 30 + wrapped content
    
    * Made some ccs tweak and inc. the correct icon
    
    * Updated deps
    
    * Update ver
    
    * Update CHANGELOG.md
    
    * f-mfa@0.3.0 - PR comments and match designs more closely
    
    * f-mfa@0.3.0 - Replace v-html with component interpolation
    
    * Minor: PR comment
    
    Co-authored-by: billy.oliver <billy.oliver@just-eat.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>

--- packages/components/pages/f-mfa/src/components/_tests/Mfa.test.js
+++ packages/components/pages/f-mfa/src/components/_tests/Mfa.test.js
@@ -1,21 +1,77 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { VueI18n } from '@justeat/f-globalisation';
 import sut from '../Mfa.vue';
+import tenantConfigs from '../../tenants';
 
 describe('Mfa', () => {
+    const localVue = createLocalVue();
+    localVue.use(VueI18n);
     const baseUrl = 'https://api.test.co.uk/';
     let wrapper;
+    let sutMocks;
+    let sutProps;
+    let sutData;
+    // let initialiseSpy;
+    // let onFormSubmitSpy;
+    // let onShowHelpInfoSpy;
+
+    const i18n = {
+        locale: 'en-GB',
+        messages: {
+            'en-GB': tenantConfigs['en-GB'].messages
+        }
+    };
+
+    const logMocks = {
+        info: jest.fn(),
+        error: jest.fn()
+    };
+
+    const mountSut = async ({
+        mocks = sutMocks,
+        propsData = sutProps,
+        data = sutData
+    } = {}) => {
+        // initialiseSpy = jest.spyOn(sut.methods, 'initialise');
+        // onFormSubmitSpy = jest.spyOn(sut.methods, 'onFormSubmit');
+        // onShowHelpInfoSpy = jest.spyOn(sut.methods, 'onShowHelpInfo');
+
+        const mock = shallowMount(sut, {
+            i18n,
+            localVue,
+            propsData,
+            data,
+            mocks
+        });
+        await mock.vm.$nextTick();
+
+        return mock;
+    };
 
     beforeEach(() => {
         // Arrange
-        const propsData = {
+        sutData = () => ({
+            verificationCode: '',
+            email: '',
+            isSubmitting: false,
+            showErrorPage: false
+        });
+        sutProps = {
             smartGatewayBaseUrl: baseUrl
         };
-
-        // Act
-        wrapper = shallowMount(sut, { propsData });
+        sutMocks = {
+            $log: logMocks
+        };
     });
 
-    it('should be defined', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should be defined', async () => {
+        // Act
+        wrapper = await mountSut();
+
         // Assert
         expect(wrapper.exists()).toBe(true);
     });
