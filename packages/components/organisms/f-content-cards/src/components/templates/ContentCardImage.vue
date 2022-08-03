71674b0aa32c3d701d83eea2f45a6dda7f26c9c6
Matthew Hardern <matthewhardern@gmail.com>
2022-08-03 15:16:34 +0100

    f-content-cards@v9.0.0 - Merging beta branch back into master to fix muddle up over versions (#2074)
    * f-content-cards@v8.0.0-alpha.1 (#1747)
    
    * Adding Image component to start the new Content cards sub components that will begin to make up the body of a card component
    
    * change to version number and changelog
    
    * fixing a style lint error on the image component
    
    * f-content-cards@v8.0.0-alpha.2 - Adding Content card Body component (#1771)
    
    * adding content card body and change to some file anmes
    
    * adding box shadow variable in
    
    * fixing style lint issues
    
    * fixing style lint issues
    
    * adding version number
    
    * changing index name
    
    * f-content-cards@v8.0.0 alpha.3 - Adding container element (#1783)
    
    * adding the content card container component
    
    * changes to version number
    
    * change to unit tests
    
    * f-content-cards@v8.0.0-alpha.4 - Adding Voucher code component (#1788)
    
    * adding in voucher code component and tests
    
    * voucher code component
    
    * change to class names
    
    * change to how classes were evaluated
    
    * changes to style names
    
    * f-content-cards@v8.0.0-alpha.5 - Promotion Card Two (#1796)
    
    * adding in the new promotion card and unit tests for it
    
    * changes to promotion card name
    
    * package json version number change
    
    * changes to make controls work
    
    * change card type in story file
    
    * f-content-cards@v8.0.0-alpha.6 - Adding in the new Voucher Card (#1801)
    
    * adding voucher card bits
    
    * adding new voucher card
    
    * storybook update
    
    * adding promotion card 1 (#1812)
    
    * f-content-cards@v8.0.0-alpha.8 - Exporting new cards & updating card body (#1826)
    
    * adding chnages to export new cards plus some minor layout changes and an update to the voucher card
    
    * increasing size for bundlewatch until we remove the old cards
    
    * updating to beta to pull into offers page (#1844)
    
    * f-content-cards@v8.0.0-beta.2 - removing unnecessary height value (#1871)
    
    * removing unnecessary height value
    
    * adding changelog
    
    * f-content-cards@v8.0.0-beta.4 - adding in min height on image with object cover (#1878)
    
    * f-content-cards@v8.0.0-alpha.5 - Promotion Card Two (#1796)
    
    * adding in the new promotion card and unit tests for it
    
    * changes to promotion card name
    
    * package json version number change
    
    * changes to make controls work
    
    * change card type in story file
    
    * style changes to account for different image sizes
    
    * yarn lock fix
    
    * changes to fix errors
    
    * Update new content cards to use correct fozzie import
    
    * Fix sass
    
    * Add f-button as a new dependency
    
    * fixing missing padding
    
    Co-authored-by: Ben Siggery <bensiggery28@gmail.com>

---
+++ packages/components/organisms/f-content-cards/src/components/templates/ContentCardImage.vue
@@ -0,0 +1,61 @@
+<template>
+    <div :class="$style['c-content-cardImage--wrapper']">
+        <img
+            data-test-id="content-card-background-image"
+            :class="$style['c-content-cardImage']"
+            :src="backgroundImageUrl"
+            alt="hey">
+        <img
+            data-test-id="content-card-restaurant-logo"
+            :class="$style['c-content-cardLogo']"
+            :src="restaurantLogoUrl"
+            alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        backgroundImageUrl: {
+            type: String,
+            required: true
+        },
+        restaurantLogoUrl: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" module>
+@use '@justeat/fozzie/src/scss/fozzie' as f;
+
+$image-borderRadius: f.$radius-rounded-c;
+$image-minHeight: 160px;
+$image-logoWidth: 48px;
+
+.c-content-cardImage--wrapper {
+    overflow: hidden;
+    position: relative;
+}
+
+.c-content-cardLogo {
+    position: absolute;
+    left: f.spacing(d);
+    top: f.spacing(d);
+    border-radius: $image-borderRadius;
+    width: $image-logoWidth;
+}
+
+.c-content-cardImage {
+    width: 100%;
+    border-radius: $image-borderRadius;
+    object-fit: cover;
+    min-height: $image-minHeight;
+
+    @include f.media('<=narrow') {
+        border-radius: 0;
+    }
+}
+</style>
