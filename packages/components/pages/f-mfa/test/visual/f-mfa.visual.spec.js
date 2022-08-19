baa3d3a91d0061cb859ac8269952a6762fa86ff3
Josh Ng <joshua.ng2332@gmail.com>
2022-08-19 15:20:42 +0100

    Added visual regression tests and supporting structure. (#2105)
    Co-authored-by: Joshua Ng <joshua.ng@just-eat.com>

--- packages/components/pages/f-mfa/test/visual/f-mfa.visual.spec.js
+++ packages/components/pages/f-mfa/test/visual/f-mfa.visual.spec.js
@@ -5,8 +5,10 @@ const devices = [
     'mobile'
 ];
 
+const illegalCode = '123';
+
 devices.forEach(device => {
-    describe('f-mfa - %s - Visual tests', () => {
+    describe('f-mfa - Visual tests', () => {
         beforeEach(async () => {
             // Arrange
             if (device === 'mobile') {
@@ -14,12 +16,43 @@ devices.forEach(device => {
             }
         });
 
-        it('should display the f-mfa component', async () => {
+        it('should display the f-mfa base component', async () => {
             // Act
             await Mfa.load();
 
             // Assert
-            await browser.percyScreenshot('f-mfa - Visual Test', device);
+            await browser.percyScreenshot('f-mfa - Base Component', device);
+        });
+
+        it('should display the f-mfa help page', async () => {
+            // Act
+            await Mfa.load();
+
+            await Mfa.goToHelp();
+
+            // Assert
+            await browser.percyScreenshot('f-mfa - Help Page', device);
+        });
+
+        it('should display the f-mfa illegal code error on the page', async () => {
+            // Act
+            await Mfa.load();
+
+            await Mfa.setFieldValue('mfaCodeInput', illegalCode);
+            await Mfa.goToSubmit();
+
+            // Assert
+            await browser.percyScreenshot('f-mfa - Code Error', device);
+        });
+
+        it('should display the f-mfa load error page', async () => {
+            // Act
+            await Mfa.load({
+                code: ''
+            });
+
+            // Assert
+            await browser.percyScreenshot('f-mfa - Load Error Page', device);
         });
     });
 });
