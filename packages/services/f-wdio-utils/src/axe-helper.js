a2b827a99fe8291c1e9d7e9b4fc84c34b9b73e5c
Ben Siggery <14013357+siggerzz@users.noreply.github.com>
2022-09-01 14:33:29 +0100

    fozzie-components@v7.27.0 - GitHub Actions Browser Tests (#2089)
    * New job to create environment
    
    * New job to create environment
    
    * Change to bearer token
    
    * Reference new secret
    
    * Change header name
    
    * Attempt to deploy storybook for PR
    
    * Change branch
    
    * change directory
    
    * change directory
    
    * Change deploy script
    
    * deploy to correct branch
    
    * Add new matrix for browser tests
    
    * Only deploy Storybook on master
    
    * Specify concurrency
    
    * Update config files
    
    * Add build to browser-tests
    
    * Add a11y / visual tests
    
    * Comment out browser tests in CircleCI
    
    * Fix script name
    
    * Test failures / artifact generation
    
    * Upload allure on failure
    
    * Continue tests on failure
    
    * Fail fast set to false
    
    * Fix artifact name
    
    * Fix tests
    
    * Disable Visual tests in GH Actions
    
    * Add changelog
    
    * Revert file change
    
    * Fix merge conflicts
    
    * Update yarn.lock
    
    * Lock Chrome version in CircleCI
    
    * Check tests work in GH actions
    
    * Lock chromedriver

--- packages/services/f-wdio-utils/src/axe-helper.js
+++ packages/services/f-wdio-utils/src/axe-helper.js
@@ -15,7 +15,7 @@ class AxeHelper {
         const filePath = `../../../../test/results/axe-violations/${componentName}-a11y-violations`;
 
         // axe-reports can't create the CSV in CI due to permissions so we have to create the file ourselves.
-        if (process.env.CIRCLECI) {
+        if (process.env.CI) {
             try {
                 if (!fs.existsSync(filePath)) {
                     exec(`touch ${filePath}`, (error, stdout, stderr) => {
