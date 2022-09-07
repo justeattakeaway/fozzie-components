0565f826da5e24a078a7747c7d0c7362752dabd2
Ben Siggery <bensiggery28@gmail.com>
2022-09-07 16:07:17 +0100

    Revert danger config

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,6 @@
 /* global danger, fail, message */
-const { github } = danger;
-console.log('pr is', github);
+const { pr } = danger.github;
+console.log('pr is', pr);
 const bodyAndTitle = (pr.body + pr.title).toLowerCase();
 const isTrivial = bodyAndTitle.includes('#trivial'); // turns off all danger checks
 const isGlobalConfigUpdate = bodyAndTitle.includes('#globalconfig'); // turns off danger checks for packages outside the root
