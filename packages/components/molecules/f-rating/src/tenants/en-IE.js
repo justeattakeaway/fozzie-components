a0db262e40d33495c72d9ce36056f3add4af6c6d
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2022-10-25 15:01:27 +0100

    f-rating@0.5.0 - Add description to rating component & tests. (#2174)
    * f-rating@0.5.0 - Add description to rating component & tests.
    
    * f-rating@0.5.0 - Remove custom class.
    
    * f-rating@0.5.0 - Change default prop and add more visual tests.
    
    * f-rating@0.5.0 - Update readme.
    
    * f-rating@0.5.0 - PR comments, first set.
    
    * f-rating@0.5.0 - PR comments, second set.
    
    * f-rating@0.5.0 - Fix build.
    
    * f-rating@0.5.0 - PR comments.
    
    * f-rating@0.5.0 - PR comments.

--- packages/components/molecules/f-rating/src/tenants/en-IE.js
+++ packages/components/molecules/f-rating/src/tenants/en-IE.js
@@ -1,6 +1,12 @@
 export default {
     locale: 'en-IE',
     ratings: {
-        starsDescription: '{rating} star out of {total} | {rating} stars out of {total}'
+        starsDescription: '{rating} star out of {maxStarRating} | {rating} stars out of {maxStarRating}',
+        ratingDisplayType: {
+            noRating: 'No ratings yet',
+            short: '{reviewCount}',
+            medium: '{rating} of {maxStarRating}',
+            long: 'View {reviewCount} reviews'
+        }
     }
 };
