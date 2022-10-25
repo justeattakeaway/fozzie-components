00d2eb0c15d4dbc68e2a01d0d45fb7fc10479a56
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2022-10-25 15:01:27 +0100

    f-rating@0.5.0 - Add description to rating component & tests. (#2174)
    * f-rating@0.5.0 - Add description to rating component & tests.
    
    * f-rating@0.5.0 - Remove custom class.
    
    * f-rating@0.5.0 - Change default prop and add more visual tests.
    
    * f-rating@0.5.0 - Update readme.
    
    * f-rating@0.5.0 - PR comments, first set.
    
    * f-rating@0.5.0 - PR comments, second set.
    
    * f-rating@0.5.0 - Fix build.
    
    * f-rating@0.5.0 - PR comments.
    
    * f-rating@0.5.0 - PR comments.

--- packages/components/molecules/f-rating/src/components/_tests/Rating.test.js
+++ packages/components/molecules/f-rating/src/components/_tests/Rating.test.js
@@ -2,6 +2,7 @@ import { createLocalVue, shallowMount } from '@vue/test-utils';
 import { VueI18n } from '@justeat/f-globalisation';
 import VRating from '../Rating.vue';
 import i18n from './helpers/setup';
+import { VALID_STAR_RATING_DISPLAY_TYPE } from '../../constants';
 
 const localVue = createLocalVue();
 localVue.use(VueI18n);
@@ -46,7 +47,7 @@ describe('Rating', () => {
             });
 
             describe('when invoked', () => {
-                it('should return a percentage from a combination of `starRating` and `maxStarRating`', () => {
+                it('should return a correct calculated number from a combination of `starRating` and `maxStarRating`', () => {
                     // Arrange
                     propsData = {
                         starRating: 2
@@ -58,7 +59,7 @@ describe('Rating', () => {
                     });
 
                     // Act & Assert
-                    expect(wrapper.vm.getRatingStarPercentage).toBe('40%');
+                    expect(wrapper.vm.getRatingStarPercentage).toBe(40);
                 });
             });
         });
@@ -155,6 +156,125 @@ describe('Rating', () => {
 
                 // Act & Assert
                 expect(validator(6)).toBe(false);
+            });
+        });
+
+        describe('`ratingDisplayType`', () => {
+            it.each(VALID_STAR_RATING_DISPLAY_TYPE)('should allow correct value `%s`', displayType => {
+                // Act
+                const { validator } = VRating.props.ratingDisplayType;
+
+                // Assert
+                expect(validator(displayType)).toBe(true);
+            });
+
+            describe('when `ratingDisplayType` is truthy', () => {
+                it('should display descriptive content', () => {
+                    // Arrange & Act
+                    const { validator } = VRating.props.ratingDisplayType;
+                    validator('short');
+
+                    propsData = {
+                        starRating: 2,
+                        maxStarRating: 5,
+                        ratingDisplayType: 'short'
+                    };
+                    wrapper = shallowMount(VRating, {
+                        propsData,
+                        localVue,
+                        i18n,
+                        mocks: {
+                            $tc
+                        }
+                    });
+
+                    const result = wrapper.find('[data-test-id="c-rating-displayType"]');
+
+                    // Assert
+                    expect(result.exists()).toBe(true);
+                });
+            });
+
+            describe('when `ratingDisplayType` is falsey', () => {
+                it('should not display descriptive content', () => {
+                    // Arrange & Act
+                    propsData = {
+                        starRating: 2,
+                        maxStarRating: 5,
+                        ratingDisplayType: null
+                    };
+                    wrapper = shallowMount(VRating, {
+                        propsData,
+                        localVue,
+                        i18n,
+                        mocks: {
+                            $tc
+                        }
+                    });
+
+                    const result = wrapper.find('[data-test-id="c-rating-displayType"]');
+
+                    // Assert
+                    expect(result.exists()).toBe(false);
+                });
+            });
+        });
+    });
+
+    describe('methods', () => {
+        describe('`getRatingDisplayFormat`', () => {
+            it('should exist', () => {
+                // Act & Assert
+                expect(wrapper.vm.getRatingDisplayFormat).toBeDefined();
+            });
+
+            describe('when invoked', () => {
+                describe('and `ratingDisplayType` is passed through', () => {
+                    it.each([
+                        ['noRating', 'No ratings yet'],
+                        ['short', '499'],
+                        ['medium', '2 of 5'],
+                        ['long', 'View 499 reviews']
+                    ])('should return the correct display type for each rating component', (type, expected) => {
+                        // Arrange
+                        propsData = {
+                            starRating: 2,
+                            reviewCount: 499,
+                            ratingDisplayType: type
+                        };
+
+                        wrapper = shallowMount(VRating, {
+                            propsData,
+                            localVue,
+                            i18n
+                        });
+
+                        // Act
+                        const result = wrapper.vm.getRatingDisplayFormat();
+
+                        // Assert
+                        expect(result).toBe(expected);
+                    });
+                });
+
+                describe('and `ratingDisplayType` is not passed through', () => {
+                    it('should return the default rating display type', () => {
+                        wrapper = shallowMount(VRating, {
+                            propsData,
+                            localVue,
+                            i18n,
+                            mocks: {
+                                $t: () => null
+                            }
+                        });
+
+                        // Act
+                        const result = wrapper.vm.getRatingDisplayFormat();
+
+                        // Assert
+                        expect(result).toBe(null);
+                    });
+                });
             });
         });
     });
