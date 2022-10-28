750cc1e2e859d769d55c945e8d372a973eab177a
Ben Siggery <bensiggery28@gmail.com>
2022-10-28 15:32:07 +0100

    Update visual tests to include multiple tenants

--- packages/components/pages/f-registration/test/visual/f-registration.visual.desktop.spec.js
+++ packages/components/pages/f-registration/test/visual/f-registration.visual.desktop.spec.js
@@ -5,79 +5,83 @@ const devices = [
     'mobile'
 ];
 
+const locales = ['en-GB', 'en-AU', 'en-NZ', 'es-ES', 'it-IT'];
+
 devices.forEach(device => {
-    describe('f-registration - visual tests', () => {
-        beforeEach(async () => {
-            if (device === 'mobile') {
-                await browser.setWindowSize(414, 731);
-            }
+    locales.forEach(locale => {
+        describe('f-registration - visual tests', () => {
+            beforeEach(async () => {
+                if (device === 'mobile') {
+                    await browser.setWindowSize(414, 731);
+                }
+    
+                await Registration.load({locale});
+            });
 
-            await Registration.load();
-        });
-
-        it('should display component', async () => {
-            // Assert
-            await browser.percyScreenshot('f-registration - Base', device);
-        });
-
-        it('should display the "Email address is already registered" error', async () => {
-            // Arrange
-            const userInfo = {
-                firstName: 'Test',
-                lastName: 'User',
-                email: 'test@user.com',
-                password: 'testuser123'
-            };
-
-            // Act
-            Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
-            await browser.pause(500);
-            await Registration.submit();
-
-            // Assert
-            await browser.percyScreenshot('f-registration - "Email is already registered error"', device);
-        });
-
-        it('should display error when form field is empty', async () => {
-            // Act
-            await Registration.submit();
-
-            // Assert
-            await browser.percyScreenshot('f-registration - "Mandatory field errors"', device);
-        });
-
-        it('should display error when form input is invalid', async () => {
-            // Arrange
-            const userInfo = {
-                firstName: '123*',
-                lastName: '456*',
-                email: '***@**',
-                password: 'llanfairpwllgwyngyllgogerychwyr'
-            };
-
-            // Act
-            Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
-            await Registration.submit();
-
-            // Assert
-            await browser.percyScreenshot('f-registration - "Invalid input error"', device);
-        });
-
-        it('should display error when form input is too long', async () => {
-            // Arrange
-            const userInfo = {
-                firstName: 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij',
-                lastName: 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij',
-                email: 'abcdefghijabcdefghijabc@defghijabcdefghijabcdefghijabcdefghij.com',
-                password: 'abcdefghijabcdefghijabcdefghi'
-            };
-
-            // Act
-            Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
-            await Registration.submit();
-
-            // Assert
-            await browser.percyScreenshot('f-registration - "Input exceed max length error"', device);
+            it(`should display the ${locale} registration component`, async () => {
+                // Assert
+                await browser.percyScreenshot(`f-registration - ${locale} - Base`, device);
+            });
+    
+            it(`should display the "Email address is already registered" error - ${locale}`, async () => {
+                // Arrange
+                const userInfo = {
+                    firstName: 'Test',
+                    lastName: 'User',
+                    email: 'test@user.com',
+                    password: 'testuser123'
+                };
+    
+                // Act
+                Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
+                await browser.pause(500);
+                await Registration.submit();
+    
+                // Assert
+                await browser.percyScreenshot(`f-registration - ${locale} - "Email is already registered error"`, device);
+            });
+    
+            it(`should display error when form field is empty - ${locale}`, async () => {
+                // Act
+                await Registration.submit();
+    
+                // Assert
+                await browser.percyScreenshot(`f-registration - ${locale} - "Mandatory field errors"`, device);
+            });
+    
+            it(`should display error when form input is invalid - ${locale}`, async () => {
+                // Arrange
+                const userInfo = {
+                    firstName: '123*',
+                    lastName: '456*',
+                    email: '***@**',
+                    password: 'llanfairpwllgwyngyllgogerychwyr'
+                };
+    
+                // Act
+                Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
+                await Registration.submit();
+    
+                // Assert
+                await browser.percyScreenshot(`f-registration - ${locale} - "Invalid input error"`, device);
+            });
+    
+            it(`should display error when form input is too long - ${locale}`, async () => {
+                // Arrange
+                const userInfo = {
+                    firstName: 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij',
+                    lastName: 'abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij',
+                    email: 'abcdefghijabcdefghijabc@defghijabcdefghijabcdefghijabcdefghij.com',
+                    password: 'abcdefghijabcdefghijabcdefghi'
+                };
+    
+                // Act
+                Object.keys(userInfo).forEach(field => Registration.setFieldValue(field, userInfo[field]));
+                await Registration.submit();
+    
+                // Assert
+                await browser.percyScreenshot(`f-registration - ${locale} - "Input exceed max length error"`, device);
+            });
         });
     });
 });
