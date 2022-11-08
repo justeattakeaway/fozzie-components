630ad3ce01b92c4d28943aa6129318971885a1ac
Thomas William McClean <spam@tommcclean.me>
2022-11-08 10:46:36 +0000

    f-account-info@1.7.0 - Remove conversation ID logic (#2144)
    * f-account-info@1.6.0 - Remove conversation ID logic
    
    * f-account-info@1.7.0 - Re-add test
    
    * Minor: Update readme
    
    (To trigger new Percy build)
    
    Co-authored-by: Thomas William McClean <thomas.mcclean@justeattakeaway.com>
    Co-authored-by: Xander Marjoram <xander.marjoram@outlook.com>

--- packages/components/pages/f-account-info/src/services/providers/_tests/Consumer.api.test.js
+++ packages/components/pages/f-account-info/src/services/providers/_tests/Consumer.api.test.js
@@ -3,14 +3,11 @@ import ConsumerApi from '../Consumer.api';
 import {
     CONSUMER_DETAILS_URL,
     CONSUMER_ADDRESSES_URL,
-    CONVERSATION_ID_HEADER_NAME,
-    AUTHORISATION_HEADER_NAME,
-    CONVERSATION_ID_COOKIE_NAME
+    AUTHORISATION_HEADER_NAME
 } from '../../../constants';
 import {
     baseUrl,
     token,
-    conversationId,
     consumerUpdateBody
 } from '../../../../test-utils/setup';
 
@@ -53,32 +50,15 @@ describe('ConsumerApi Provider', () => {
     });
 
     describe('When calling `getConsumerDetails`', () => {
-        it('should set the cookie with a new conversation id if not provided with one', async () => {
-            // Act
-            await apiProvider.getConsumerDetails(token);
-
-            // Assert
-            expect(cookiesSetSpy).toHaveBeenCalledWith(CONVERSATION_ID_COOKIE_NAME, expect.anything());
-        });
-
-        it('should not set the cookie if already provided with a conversation id', async () => {
-            // Act
-            await apiProvider.getConsumerDetails(token, conversationId);
-
-            // Assert
-            expect(cookiesSetSpy).not.toHaveBeenCalled();
-        });
-
         it('should send the correct parameters', async () => {
             // Arrange
             const expectedUri = `${baseUrl}/${CONSUMER_DETAILS_URL}`;
             const expectedHeaders = {
-                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`,
-                [CONVERSATION_ID_HEADER_NAME]: conversationId
+                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`
             };
 
             // Act
-            await apiProvider.getConsumerDetails(token, conversationId);
+            await apiProvider.getConsumerDetails(token);
 
             // Assert
             expect(httpGetSpy).toHaveBeenCalledWith(expectedUri, expectedHeaders);
@@ -86,32 +66,15 @@ describe('ConsumerApi Provider', () => {
     });
 
     describe('When calling `getConsumerAddresses`', () => {
-        it('should set the cookie with a new conversation id if not provided with one', async () => {
-            // Act
-            await apiProvider.getConsumerAddresses(token);
-
-            // Assert
-            expect(cookiesSetSpy).toHaveBeenCalledWith(CONVERSATION_ID_COOKIE_NAME, expect.anything());
-        });
-
-        it('should not set the cookie if already provided with a conversation id', async () => {
-            // Act
-            await apiProvider.getConsumerAddresses(token, conversationId);
-
-            // Assert
-            expect(cookiesSetSpy).not.toHaveBeenCalled();
-        });
-
         it('should send the correct parameters', async () => {
             // Arrange
             const expectedUri = `${baseUrl}/${CONSUMER_ADDRESSES_URL}`;
             const expectedHeaders = {
-                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`,
-                [CONVERSATION_ID_HEADER_NAME]: conversationId
+                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`
             };
 
             // Act
-            await apiProvider.getConsumerAddresses(token, conversationId);
+            await apiProvider.getConsumerAddresses(token);
 
             // Assert
             expect(httpGetSpy).toHaveBeenCalledWith(expectedUri, expectedHeaders);
@@ -119,33 +82,16 @@ describe('ConsumerApi Provider', () => {
     });
 
     describe('When calling `patchConsumer`', () => {
-        it('should set the cookie with a new conversation id if not provided with one', async () => {
-            // Act
-            await apiProvider.patchConsumer(token);
-
-            // Assert
-            expect(cookiesSetSpy).toHaveBeenCalledWith(CONVERSATION_ID_COOKIE_NAME, expect.anything());
-        });
-
-        it('should not set the cookie if already provided with a conversation id', async () => {
-            // Act
-            await apiProvider.patchConsumer(token, consumerUpdateBody, conversationId);
-
-            // Assert
-            expect(cookiesSetSpy).not.toHaveBeenCalled();
-        });
-
         it('should send the correct parameters', async () => {
             // Arrange
             const expectedUri = `${baseUrl}/${CONSUMER_DETAILS_URL}`;
             const expectedBody = consumerUpdateBody;
             const expectedHeaders = {
-                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`,
-                [CONVERSATION_ID_HEADER_NAME]: conversationId
+                [AUTHORISATION_HEADER_NAME]: `Bearer ${token}`
             };
 
             // Act
-            await apiProvider.patchConsumer(token, consumerUpdateBody, conversationId);
+            await apiProvider.patchConsumer(token, consumerUpdateBody);
 
             // Assert
             expect(httpPatchSpy).toHaveBeenCalledWith(expectedUri, expectedBody, expectedHeaders);
