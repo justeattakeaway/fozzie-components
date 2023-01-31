7184c40b7a1028083d6a6b56510c3706d1cc084d
Matthew Hardern <matthewhardern@gmail.com>
2023-01-31 09:31:46 +0000

    f-content-cards@v10.3.0 - changes to stampcard card for accessibility (#2233)
    * changes to stampcard card for accessibility and adding new up to date stories
    
    * changes to mock data for custom adapter
    
    * Update packages/components/organisms/f-content-cards/README.md
    
    Co-authored-by: Jiten Asmucrai <36330866+jasmucrai@users.noreply.github.com>
    
    * change to readme
    
    ---------
    
    Co-authored-by: Jiten Asmucrai <36330866+jasmucrai@users.noreply.github.com>

---
+++ packages/components/organisms/f-content-cards/stories/base/ContentCardsCustomAdapter.stories.js
@@ -0,0 +1,56 @@
+import { withA11y } from '@storybook/addon-a11y';
+import { action } from '@storybook/addon-actions';
+import ContentCards, { STATE_DEFAULT } from '../../src/components/ContentCards';
+import generateStampCards from '../mockData/stampCards';
+import StampCard from '../../src/components/cardTemplates/StampCard1.vue';
+
+export default {
+    title: 'Components/Organisms/f-content-cards',
+    decorators: [withA11y]
+};
+
+export const CustomContentCardsAdapter = (args, { argTypes }) => ({
+    components: {
+        ContentCards,
+        StampCard
+    },
+
+    props: Object.keys(argTypes),
+
+    data: () => ({
+        adapters: []
+    }),
+
+    created () {
+        const adapter = () => ({
+            source: 'CustomAdapter',
+            initialise: (_, callback) => {
+                callback(generateStampCards());
+            },
+            handleCardView: card => {
+                action('view')(card);
+            },
+            handleCardClick: card => {
+                action('click')(card);
+            }
+        });
+
+        this.adapters.push(adapter());
+    },
+
+    template: `
+        <content-cards
+            :adapters="adapters"
+            :locale="locale"
+        >
+            <template #${STATE_DEFAULT}="{ cards }">
+                <stamp-card v-for="card in cards" :key="card.id" :card="card"></stamp-card>
+            </template>
+        </content-cards>`
+});
+
+CustomContentCardsAdapter.args = {
+    locale: 'en-GB'
+};
+
+CustomContentCardsAdapter.storyName = 'Custom Adapter';
