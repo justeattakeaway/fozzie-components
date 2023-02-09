f3fc1306af6cd483ee7d86e048c7e4a168494262
gareth-balfour <gareth.balfour@justeattakeaway.com>
2023-02-09 15:50:05 +0000

    Linting fixes

--- packages/components/pages/f-mfa/test/visual/f-mfa.visual.spec.js
+++ packages/components/pages/f-mfa/test/visual/f-mfa.visual.spec.js
@@ -7,52 +7,63 @@ const devices = [
 
 const illegalCode = '123';
 
+const locales = [
+    'en-GB',
+    'en-AU',
+    'en-NZ',
+    'en-IE',
+    'es-ES',
+    'it-IT'
+];
+
 devices.forEach(device => {
-    describe('f-mfa - Visual tests', () => {
-        beforeEach(async () => {
-            // Arrange
-            if (device === 'mobile') {
-                await browser.setWindowSize(414, 731);
-            }
-        });
+    locales.forEach(locale => {
+        describe('f-mfa - Visual tests', () => {
+            beforeEach(async () => {
+                // Arrange
+                if (device === 'mobile') {
+                    await browser.setWindowSize(414, 731);
+                }
 
-        it('should display the f-mfa base component', async () => {
-            // Act
-            await Mfa.load();
-
-            // Assert
-            await browser.percyScreenshot('f-mfa - Base Component', device);
-        });
-
-        it('should display the f-mfa help page', async () => {
-            // Act
-            await Mfa.load();
-
-            await Mfa.goToHelp();
-
-            // Assert
-            await browser.percyScreenshot('f-mfa - Help Page', device);
-        });
-
-        it('should display the f-mfa illegal code error on the page', async () => {
-            // Act
-            await Mfa.load();
-
-            await Mfa.setFieldValue('mfaCodeInput', illegalCode);
-            await Mfa.goToSubmit();
-
-            // Assert
-            await browser.percyScreenshot('f-mfa - Code Error', device);
-        });
-
-        it('should display the f-mfa load error page', async () => {
-            // Act
-            await Mfa.load({
-                code: ''
+                // Act
+                await Mfa.load({ locale });
             });
 
-            // Assert
-            await browser.percyScreenshot('f-mfa - Load Error Page', device);
+            it(`should display the ${locale} f-mfa base component`, async () => {
+                // Assert
+                await browser.percyScreenshot(`f-mfa - ${locale} Base Component`, device);
+            });
+
+            it('should display the f-mfa help page', async () => {
+                // Act
+                await Mfa.goToHelp();
+
+                // Assert
+                await browser.percyScreenshot(`f-mfa - ${locale} Help Page`, device);
+            });
+
+            it('should display the f-mfa illegal code error on the page', async () => {
+                // Act
+                await Mfa.setFieldValue('mfaCodeInput', illegalCode);
+                await Mfa.goToSubmit();
+
+                // Assert
+                await browser.percyScreenshot(`f-mfa - ${locale} Code Error`, device);
+            });
+
+            it('should display the f-mfa load error page', async () => {
+                // Act
+                await Mfa.load({
+                    code: '',
+                    locale
+                });
+
+                // Assert
+                await browser.percyScreenshot(
+                    `f-mfa - ${locale} Load Error Page`,
+                    device
+                );
+            });
         });
     });
 });
