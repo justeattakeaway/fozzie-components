bef67016d40c3d68270aa2d5a54d26162142b6a3
Gareth Balfour <garethbalfour@hotmail.com>
2023-02-13 10:55:04 +0000

    f-mfa@v0.24.0 - add translations for es, ie and it (#2238)
    * f-mfa@v0.24.0 - add translations for es, ie and it
    
    * Linting fixes
    
    ---------
    
    Co-authored-by: gareth-balfour <gareth.balfour@justeattakeaway.com>

--- packages/components/pages/f-mfa/src/tenants/it-IT.js
+++ packages/components/pages/f-mfa/src/tenants/it-IT.js
@@ -1,4 +1,45 @@
-export default {
+const messages = {
     locale: 'it-IT',
-    text: 'I am a VMfa Component (IT)'
+    verificationPage: {
+        subTitle: 'Ti abbiamo inviato un’email con il codice di accesso',
+        instructionsPrimaryText: 'Abbiamo inviato un’email con il codice di verifica a {0} per garantire la sicurezza del tuo account. Inserisci il codice qui sotto per accedere.',
+        instructionsSecondaryText: 'Il tuo codice sarà valido per 30 minuti.',
+        formField: {
+            labelText: 'Il tuo codice di verifica',
+            placeholderText: 'Inserisci il codice',
+            validationMessage: 'Si prega di inserire un codice valido'
+        },
+        submitErrorText: 'Controlla e riprova',
+        primaryButtonText: 'Continua',
+        secondaryButtonText: 'Hai bisogno di aiuto?'
+    },
+    helpInfo: {
+        title: 'Aiuto',
+        subTitle: 'Hai bisogno di aiuto?',
+        instructionsPrimaryText: 'Abbiamo inviato un codice a {0}. Il codice sarà valido per 30 minuti. In caso di problemi:',
+        instructionsPoint1Text: 'Attendi qualche minuto l’arrivo dell’email',
+        instructionsPoint2Text: 'Controlla la cartella dello spam',
+        instructionsPoint3Text: 'Prova ad accedere in un altro modo',
+        instructionsPoint4Text: 'Consulta le nostre {0} se hai ancora bisogno di aiuto',
+        instructionsHelpLink: 'https://www.justeat.it/help/article/6621416520349/perche-a-volte-mi-viene-richiesto-un-codice-di-verifica-quando-uso-just-eat',
+        instructionsHelpText: 'nuestras preguntas frecuentes',
+        primaryButtonText: 'OK',
+        secondaryButtonText: 'Accedi in un altro modo'
+    },
+    errorMessages: {
+        validation: 'Controlla e riprova',
+        submitMfa: {
+            heading: 'Qualcosa è andato storto',
+            description: 'Lo sentimos. Por favor inténtalo de nuevo.'
+        },
+        loading: {
+            heading: 'Qualcosa è andato storto',
+            message: 'Lo sentimos. Por favor inténtalo de nuevo.',
+            primaryButtonText: 'Torna indietro'
+        }
+    }
+};
+
+export default {
+    messages
 };
