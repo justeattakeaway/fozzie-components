3d3932d8d9403289b1804272d54f011e648cb5fc
Dawid <68010631+dawidchar@users.noreply.github.com>
2023-02-20 14:07:18 +0000

    f-restaurant-card@v1.4.0 - Add max rating Prop (#2235)
    * Minor - Add maxRating
    
    * Add units tests
    
    * Empty commit
    
    ---------
    
    Co-authored-by: Dawid Charzewski <dawid.charzewski@justeattakeaway.com>

--- packages/components/molecules/f-restaurant-card/stories/RestaurantRating.stories.js
+++ packages/components/molecules/f-restaurant-card/stories/RestaurantRating.stories.js
@@ -16,6 +16,7 @@ RestaurantRatingComponent.args = {
     isOwnRating: false,
     mean: 5.00,
     count: 1400,
+    maxRating: 6,
     accessibleMessage: 'rated 5 stars out of 6',
     notRatedMessage: 'No ratings yet',
     isOwnRatingMessage: 'You'
