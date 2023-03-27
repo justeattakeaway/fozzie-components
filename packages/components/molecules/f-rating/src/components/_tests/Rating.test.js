b93fac67c420995083446ce8df546073cdfdd826
Kevin Rodrigues <kevin.rodrigues@just-eat.com>
2023-03-27 14:58:58 +0100

    f-rating@0.15.0 - Add check to locale before returning description. (#2261)
    * f-rating@0.15.0 - Add check to locale before returning description.
    
    * f-rating@0.15.0 - Add check locale check.
    
    * f-rating@0.15.0 - Add check locale check.
    
    * f-rating@0.15.0 - Add readme comment for locale usage.
    
    * f-rating@0.15.0 - Add readme comment for locale usage.
    
    * f-rating@0.15.0 - Correct changelog entry.

--- packages/components/molecules/f-rating/src/components/_tests/Rating.test.js
+++ packages/components/molecules/f-rating/src/components/_tests/Rating.test.js
@@ -54,7 +54,8 @@ describe('Rating', () => {
                     // Arrange
                     propsData = {
                         starRating: 1,
-                        ratingDisplayType: 'short'
+                        ratingDisplayType: 'short',
+                        locale: 'en-GB'
                     };
                     wrapper = shallowMount(VRating, {
                         propsData,
@@ -74,7 +75,8 @@ describe('Rating', () => {
                     // Arrange
                     propsData = {
                         starRating: type,
-                        ratingDisplayType: 'short'
+                        ratingDisplayType: 'short',
+                        locale: 'en-GB'
                     };
                     wrapper = shallowMount(VRating, {
                         propsData,
@@ -163,6 +165,7 @@ describe('Rating', () => {
                         propsData.starRating = 2;
                         propsData.reviewCount = 1;
                         propsData.ratingDisplayType = 'short';
+                        propsData.locale = 'en-GB';
                         wrapper = shallowMount(VRating, {
                             propsData,
                             localVue,
@@ -337,7 +340,7 @@ describe('Rating', () => {
 
             it('should be set to `null` by default', () => {
                 // Act & Assert
-                expect(VRating.props.ratingDisplayType.default).toBe(null);
+                expect(VRating.props.ratingDisplayType.default).toBe('');
             });
 
             describe('when `ratingDisplayType` is truthy', () => {
@@ -430,7 +433,8 @@ describe('Rating', () => {
                         propsData = {
                             starRating: 2,
                             reviewCount: 0,
-                            ratingDisplayType: 'short'
+                            ratingDisplayType: 'short',
+                            locale: 'en-GB'
                         };
 
                         wrapper = shallowMount(VRating, {
@@ -453,7 +457,8 @@ describe('Rating', () => {
                         propsData = {
                             starRating: 2,
                             reviewCount: 499,
-                            ratingDisplayType: 'medium'
+                            ratingDisplayType: 'medium',
+                            locale: 'en-GB'
                         };
 
                         wrapper = shallowMount(VRating, {
@@ -481,7 +486,8 @@ describe('Rating', () => {
                         propsData = {
                             starRating: 2,
                             reviewCount: 499,
-                            ratingDisplayType: type
+                            ratingDisplayType: type,
+                            locale: 'en-GB'
                         };
 
                         wrapper = shallowMount(VRating, {
@@ -505,7 +511,7 @@ describe('Rating', () => {
                             localVue,
                             i18n,
                             mocks: {
-                                $t: () => null
+                                $t: () => ''
                             }
                         });
 
@@ -513,7 +519,7 @@ describe('Rating', () => {
                         const result = wrapper.vm.getRatingDisplayFormat();
 
                         // Assert
-                        expect(result).toBe(null);
+                        expect(result).toBe('');
                     });
                 });
             });
