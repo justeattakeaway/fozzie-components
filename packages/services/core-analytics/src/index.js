9e197f5eff8d60412ded85acf6ca1000580409a0
Billy Oliver <billy.oliver@gmail.com>
2023-07-11 14:56:14 +0100

    core-analytics@1.21.0 - Clone of f-analytics without dependency on Vue, Vuex & SSR (#2302)
    * WCP-356 - Cloned the f-analytics into f-jet-analytics plus added a test harness to use for testing later
    
    * Cloned f-analytics into core-analytics but without a dependency on Vue, Vuex and without SSR functionality
    
    * Review tweaks
    
    * Added the alibilty to supply the AnonymousUser cookie name as an option in case not the same as L-JE plus removed some redundant code
    
    ---------
    
    Co-authored-by: Billy Oliver <billy.oliver@just-eat.com>

---
+++ packages/services/core-analytics/src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @overview Fozzie GTM / GA Analytics JS Wrapper
+ *
+ * @module f-analytics
+ */
+
+import AnalyticsService from './services/analytics.service';
+
+export default AnalyticsService;
