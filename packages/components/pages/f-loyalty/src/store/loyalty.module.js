29e638b606277c2ee2998d636d5f38407543f454
matthew hardern <matthewhardern@gmail.com>
2023-08-01 14:46:32 +0100

    updating loyalty to remove braze adapter

--- packages/components/pages/f-loyalty/src/store/loyalty.module.js
+++ packages/components/pages/f-loyalty/src/store/loyalty.module.js
@@ -1,23 +1,9 @@
-import jwtDecode from 'jwt-decode';
 import {
     MUTATION_SET_AUTH_TOKEN,
-    MUTATION_SET_BRAZE_KEY,
     ACTION_INITIALISE_LOYALTY,
-    ACTION_SET_GLOBAL_USER_ID,
-    MUTATION_SET_GLOBAL_USER_ID,
     MUTATION_SET_TENANT,
-    MUTATION_SET_API_URL,
-    MUTATION_SET_ADAPTER_EXPERIMENT
+    MUTATION_SET_API_URL
 } from './types';
-
-/**
- * @function resolveGlobalUserID
- * @param  {string} authToken - auth token.
- */
-const resolveGlobalUserID = authToken => {
-    const tokenData = jwtDecode(authToken);
-    return tokenData?.global_user_id;
-};
 
 export default {
     namespaced: true,
@@ -35,24 +21,12 @@ export default {
             state.authToken = value;
         },
 
-        [MUTATION_SET_BRAZE_KEY]: (state, value) => {
-            state.brazeApiKey = value;
-        },
-
-        [MUTATION_SET_GLOBAL_USER_ID]: (state, value) => {
-            state.globalUserId = value;
-        },
-
         [MUTATION_SET_TENANT]: (state, value) => {
             state.tenant = value;
         },
 
         [MUTATION_SET_API_URL]: (state, value) => {
             state.stampCardsAPIUrl = value;
-        },
-
-        [MUTATION_SET_ADAPTER_EXPERIMENT]: (state, value) => {
-            state.inStampCardsAdapterExperiment = value;
         }
     },
 
@@ -64,25 +38,14 @@ export default {
      * @returns {void}
      */
     actions: {
-        [ACTION_SET_GLOBAL_USER_ID]: ({ commit }, { authToken, logger, errorHandler }) => {
-            commit(ACTION_SET_GLOBAL_USER_ID, resolveGlobalUserID(authToken, logger, errorHandler));
-        },
-
-        [ACTION_INITIALISE_LOYALTY]: ({ commit, dispatch, getters }, {
-            brazeApiKey,
+        [ACTION_INITIALISE_LOYALTY]: ({ commit }, {
             authToken,
             tenant,
-            stampCardsAPIUrl,
-            inStampCardsAdapterExperiment
+            stampCardsAPIUrl
         }) => {
             commit(MUTATION_SET_AUTH_TOKEN, authToken);
-            commit(MUTATION_SET_BRAZE_KEY, brazeApiKey);
             commit(MUTATION_SET_TENANT, tenant);
             commit(MUTATION_SET_API_URL, stampCardsAPIUrl);
-            commit(MUTATION_SET_ADAPTER_EXPERIMENT, inStampCardsAdapterExperiment);
-            if (getters.isAuthenticated) {
-                dispatch(ACTION_SET_GLOBAL_USER_ID, { authToken });
-            }
         }
     },
 
