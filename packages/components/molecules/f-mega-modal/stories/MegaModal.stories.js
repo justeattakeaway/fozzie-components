e7d24a284a462496e81fb2a47e8db16a96507689
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2023-08-08 16:19:07 +0100

    f-mega-modal@7.6.0 - Add `position: fixed;` to fix z-index

--- packages/components/molecules/f-mega-modal/stories/MegaModal.stories.js
+++ packages/components/molecules/f-mega-modal/stories/MegaModal.stories.js
@@ -10,31 +10,41 @@ export const MegaModalComponent = (args, { argTypes }) => ({
     components: { MegaModal },
     props: Object.keys(argTypes),
     template: `
-        <mega-modal
-            :is-open="isOpen"
-            :is-narrow="isNarrow"
-            :is-wide="isWide"
-            :is-flush="isFlush"
-            :is-full-height="isFullHeight"
-            :is-scrollable="isScrollable"
-            :is-close-fixed="isCloseFixed"
-            :is-positioned-bottom="isPositionedBottom"
-            :is-close-rounded="isCloseRounded"
-            :is-text-aligned-center="isTextAlignedCenter"
-            :has-close-button="hasCloseButton"
-            :has-overlay="hasOverlay"
-            :close-button-style="closeButtonStyle"
-            :is-mode-right-to-left="isModeRightToLeft"
-            :close-on-blur="closeOnBlur"
-            :close-button-copy="closeButtonCopy"
-            :title="titleCopy"
-            :titleHtmlTag="titleHtmlTag">
+        <div>
+            <mega-modal
+                :is-open="isOpen"
+                :is-narrow="isNarrow"
+                :is-wide="isWide"
+                :is-flush="isFlush"
+                :is-full-height="isFullHeight"
+                :is-scrollable="isScrollable"
+                :is-close-fixed="isCloseFixed"
+                :is-positioned-bottom="isPositionedBottom"
+                :is-close-rounded="isCloseRounded"
+                :is-text-aligned-center="isTextAlignedCenter"
+                :has-close-button="hasCloseButton"
+                :has-overlay="hasOverlay"
+                :close-button-style="closeButtonStyle"
+                :is-mode-right-to-left="isModeRightToLeft"
+                :close-on-blur="closeOnBlur"
+                :close-button-copy="closeButtonCopy"
+                :title="titleCopy"
+                :titleHtmlTag="titleHtmlTag">
 
-            <p data-test-id="mega-modal-content">
-                Let's find another restaurant to order from.
-            </p>
+                <p data-test-id="mega-modal-content">
+                    Let's find another restaurant to order from.
+                </p>
+            </mega-modal>
 
-        </mega-modal>
+            <div style="position: relative; z-index: 8999;">
+                <h1>Some content to test stacking!</h1>
+                <p>This content should all be behind the modal.</p>
+                <ol>
+                    ${'<li>List item with a kinda long name</li>'.repeat(25)}
+                </ol>
+            </div>
+
+        </div>
     `
 });
 
