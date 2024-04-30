a17cfcaab863b59ed4ed17ff7e84c4683ed82d12
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-04-30 14:51:18 +0100

    fozzie-components@7.57.0 - Update babel to latest versions

--- packages/components/organisms/f-content-cards/babel.config.js
+++ packages/components/organisms/f-content-cards/babel.config.js
@@ -2,10 +2,6 @@ module.exports = api => {
     // Use `isTest` to determine what presets and plugins to use with jest
     const isTest = api.env('test');
     const presets = [];
-    const plugins = [
-        '@babel/plugin-proposal-optional-chaining', // https://babeljs.io/docs/en/babel-plugin-proposal-optional-chaining
-        '@babel/plugin-proposal-class-properties' // https://babeljs.io/docs/en/babel-plugin-syntax-class-properties
-    ];
     const builtIns = (api.env('development') ? 'entry' : false);
 
     if (!isTest) {
@@ -18,7 +14,6 @@ module.exports = api => {
     }
 
     return {
-        presets,
-        plugins
+        presets
     };
 };
