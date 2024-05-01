118527cbcfd6e3b8653950ada8dd8fa1f2419afd
Xander Marjoram <xander.marjoram@justeattakeaway.com>
2024-05-01 10:13:24 +0100

    f-vue-icons@3.14.1 - More package updates (#2521)
    * fozzie-components@7.56.2 - More package updates
    
    * Package updates

--- packages/tools/fozzie/src/utilities/getSubdirectories.js
+++ packages/tools/fozzie/src/utilities/getSubdirectories.js
@@ -1,11 +1,11 @@
-const glob = require('glob');
+const { globSync } = require('glob');
 
 /**
  * Get all subdirectory paths for a given source
  * @param {string} src - The source to search from for subdirectories
  * @returns {string[]} - An array of all subdirectory paths
  */
-const getAllSubdirectoryPaths = src => glob.sync(`${src}/**/`);
+const getAllSubdirectoryPaths = src => globSync(`${src}/**/`);
 
 module.exports = {
     getAllSubdirectoryPaths
